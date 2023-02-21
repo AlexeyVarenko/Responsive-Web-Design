ced9f2494d0c95e2b4976808335b9b9b4564c774
AlexeyVarenko <alexey_varenko@mail.ru>
2023-02-21 10:02:57 +0300

    Create step12.js

---
+++ step12.js
@@ -0,0 +1,7 @@
+/*Step 12
+In the previous step you turned the words link to cat pictures into a link by placing them between opening and closing anchor (a) tags. You can do the same to words inside of an element, such as a p element.
+
+In the text of your p element, turn the words cat photos into a link to https://freecatphotoapp.com by adding opening and closing anchor (a) tags around these words.*/
+
+<p>See more  <a href="https://freecatphotoapp.com">cat photos</a> in our gallery.</p>
+      <a href="https://freecatphotoapp.com">link to cat pictures</a>
