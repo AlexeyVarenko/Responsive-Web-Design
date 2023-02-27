cecc291e5bd8eb882f7519a57b157dfa130391f1
AlexeyVarenko <alexey_varenko@mail.ru>
2023-02-27 20:08:27 +0300

    Create step25.js

---
+++ step25.js
@@ -0,0 +1,12 @@
+/*Step 24
+A figure caption (figcaption) element is used to add a caption to describe the image contained within the figure element. For example, <figcaption>A cute cat</figcaption> adds the caption A cute cat.
+
+After the image nested in the figure element, add a figcaption element with text set to:
+
+Cats love lasagna.*/
+
+
+<figure>
+          <img src="https://cdn.freecodecamp.org/curriculum/cat-photo-app/lasagna.jpg" alt="A slice of lasagna on a plate.">
+          <figcaption>Cats love lasagna.</figcaption>
+        </figure>
