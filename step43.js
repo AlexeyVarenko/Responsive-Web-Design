f5afe002f914e1e51ef343ad0ad0573582002f93
AlexeyVarenko <alexey_varenko@mail.ru>
2023-03-06 19:47:51 +0300

    Create step43.js

---
+++ step43.js
@@ -0,0 +1,8 @@
+/*Step 42
+Use the button element to create a clickable button. For example, <button>Click Here</button> creates a button with the text Click Here.
+
+Add a button element with the text Submit below the input element. The default behavior of clicking a form button without any attributes submits the form to the location specified in the form's action attribute.*/
+
+
+<input type="text" name="catphotourl" placeholder="cat photo URL" required>
+          <button>Submit</button>
