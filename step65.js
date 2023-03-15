6a746c4853684771b8091f6226d6942908e06333
AlexeyVarenko <alexey_varenko@mail.ru>
2023-03-15 19:46:38 +0300

    Create step65.js

---
+++ step65.js
@@ -0,0 +1,4 @@
+/*Step 64
+Make the text freeCodeCamp.org into a link by enclosing it in an anchor (a) element. The href attribute should be set to https://www.freecodecamp.org.*/
+
+ <p>No Copyright - <a href="https://www.freecodecamp.org">freeCodeCamp.org</a></p>
