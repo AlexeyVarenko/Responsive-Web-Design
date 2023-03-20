5391b5fc9e6c4e99089166c45058f4c85059be11
AlexeyVarenko <alexey_varenko@mail.ru>
2023-03-20 09:29:10 +0300

    Create step1.8.js

---
+++ step1.8.js
@@ -0,0 +1,8 @@
+/*Step 8
+There will be two sections on the menu, one for coffees and one for desserts. Add a section element within the main element so you have a place to put all the coffees available.*/
+
+<main>
+      <h1>CAMPER CAFE</h1>
+      <p>Est. 2020</p>
+      <section></section>
+    </main>
