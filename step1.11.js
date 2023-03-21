90e99ac4d00a01366274bdfbe335f68f536af164
AlexeyVarenko <alexey_varenko@mail.ru>
2023-03-21 10:49:58 +0300

    Create step1.11.js

---
+++ step1.11.js
@@ -0,0 +1,13 @@
+/*Step 11
+You can add style to an element by specifying it in the style element and setting a property for it like this:
+
+element {
+ property: value;
+}
+Center your h1 element by setting its text-align property to the value center.*/
+
+
+<style>
+      h1{text-align:center;}
+      
+    </style>
