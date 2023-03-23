3adb42d9f8149483b707c339e4ef3927b34d1a32
AlexeyVarenko <alexey_varenko@mail.ru>
2023-03-23 09:38:57 +0300

    Update step1.12.js

--- step1.12.js
+++ step1.12.js
@@ -1,6 +1,13 @@
 /*Step 12
+<<<<<<< HEAD
 In the previous step, you used a type selector to style the h1 element. Center the h2 and p elements by adding a new type selector for each one to the existing style element.*/
 
+=======
+
+In the previous step, you used a type selector to style the h1 element. Center the h2 and p elements by adding a new type selector for each one to the existing style element.*/
+
+
+>>>>>>> e2763e2adf55f5515094f151b0d530fd36b6884a
 <style>
       h1 {
         text-align: center;
@@ -11,4 +18,10 @@ In the previous step, you used a type selector to style the h1 element. Center t
       p {
         text-align: center;
       }
+<<<<<<< HEAD
+=======
+
+
+
+>>>>>>> e2763e2adf55f5515094f151b0d530fd36b6884a
     </style>
