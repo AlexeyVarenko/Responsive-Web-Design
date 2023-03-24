bbe6a909c6dba0442acee50e1d735cc5882b42c9
AlexeyVarenko <alexey_varenko@mail.ru>
2023-03-24 11:12:18 +0300

    Create step1.16.js

---
+++ step1.16.js
@@ -0,0 +1,9 @@
+/*Step 16
+Now you need to link the styles.css file so the styles will be applied again. Nest a self-closing link element in the head element. Give it a rel attribute value stylesheet and an href attribute value of styles.css.*/
+
+
+<head>
+<link rel="stylesheet" href="styles.css">
+<meta charset="utf-8" />
+<title>Cafe Menu</title>
+</head>
