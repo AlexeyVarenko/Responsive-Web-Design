b70ceca9df6cb242fd970cd38953d74e76c66f43
AlexeyVarenko <alexey_varenko@mail.ru>
2023-03-25 17:02:59 +0300

    Create step1.20.js

---
+++ step1.20.js
@@ -0,0 +1,15 @@
+/*Step 20
+The div element is used mainly for design layout purposes unlike the other content elements you have used so far. Add a div element inside the body element and then move all the other elements inside the new div.*/
+
+
+<body>
+    <div>
+      <main>
+      <h1>CAMPER CAFE</h1>
+      <p>Est. 2020</p>
+      <section>
+        <h2>Coffee</h2>
+      </section>
+    </main>
+    </div>
+    </body>
