b6e115331ac648d816bf884ae0c643ce66fea3c7
AlexeyVarenko <alexey_varenko@mail.ru>
2023-03-27 12:50:18 +0300

    Create step1.21.js

---
+++ step1.21.js
@@ -0,0 +1,14 @@
+/*Step 21
+The goal now is to make the div not take up the entire width of the page. The CSS width property is perfect for this. Create a new type selector in the style sheet that gives your div element a width of 300px.*/
+
+
+body {
+    background-color: burlywood;
+  }
+  div {
+    width: 300px;
+  }
+  
+  h1, h2, p {
+    text-align: center;
+  }
