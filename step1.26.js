4201ba7429fa3f845022c92aa64e6c570375a026
AlexeyVarenko <alexey_varenko@mail.ru>
2023-03-28 18:03:48 +0300

    Create step1.26.js

---
+++ step1.26.js
@@ -0,0 +1,15 @@
+/*Step 26
+So far you have been using type selectors to style elements. A class selector is defined by a name with a dot directly in front of it, like this:
+
+.class-name {
+  styles
+}
+Change the existing div selector into a class selector by replacing div with a class named menu.*/
+
+
+.menu {
+    width: 80%;
+    background-color: burlywood;
+    margin-left: auto;
+    margin-right: auto;
+  }
