24904bf8a86e2cb36ac9f82a1fe8f8b1c9820ecb
AlexeyVarenko <alexey_varenko@mail.ru>
2023-04-03 18:02:44 +0300

    Create 1.41.js

---
+++ 1.41.js
@@ -0,0 +1,13 @@
+/*Step 41
+Now go ahead and change both the flavor and price class' widths to be 50% again.*/
+
+
+.flavor {
+    text-align: left;
+    width: 50%;
+  }
+  
+  .price {
+    text-align: right;
+    width: 50%;
+  }
