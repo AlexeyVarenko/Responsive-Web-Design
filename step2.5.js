c2ba3b15f0bb9085982c4a6154fabe216965e867
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-04-28 09:45:24 +0300

    Create step2.5.js

---
+++ step2.5.js
@@ -0,0 +1,11 @@
+/*Step 5
+You can have multiple self-closing meta elements on a web page. What distinguishes one meta element from the other is the attribute. You should add a new meta element for each attribute you want to specify.
+
+Add another self-closing meta element within the head. Give it a name attribute set to viewport and a content attribute set to width=device-width, initial-scale=1.0 so your page looks the same on all devices.*/
+
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Colored Markers</title>
+  </head>
