61ef30a05f65b2f205bba57593f9d930bfda7081
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-05-04 15:33:37 +0300

    Create step2.19.js

---
+++ step2.19.js
@@ -0,0 +1,9 @@
+/*Step 19
+Add the class two to the second marker div, and add the class three to the third marker div.*/
+
+<div class="marker one">
+      </div>
+      <div class="marker two">
+      </div>
+      <div class="marker three">
+      </div>
