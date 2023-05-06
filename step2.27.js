87e0aa0e1b45d50591736bd015bc32b54efe0d82
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-05-06 10:44:51 +0300

    Create step2.27.js

---
+++ step2.27.js
@@ -0,0 +1,9 @@
+/*Step 27
+Now that you have the primary RGB colors, it's time to combine them.
+
+For the rgb function in the .container rule, set the red, green, and blue values to the max of 255.*/
+
+.container {
+    background-color: rgb(255, 255, 255);
+    padding: 10px 0;
+  }
