6c6f2ae4021aa5de8591085bf768455445942c95
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-05-09 09:36:07 +0300

    Create step2.29.js

---
+++ step2.29.js
@@ -0,0 +1,7 @@
+/*Step 29
+To create the next secondary color, cyan, update the rgb function in the .two CSS rule to combine pure green and pure blue.*/
+
+
+.two {
+    background-color: rgb(0, 255, 255);
+  }
