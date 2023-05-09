b91b4e89afac136dce0a1c4825c04988757b5f63
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-05-09 09:36:13 +0300

    Create step2.30.js

---
+++ step2.30.js
@@ -0,0 +1,7 @@
+/*Step 30
+To create the final secondary color, magenta, update the rgb function in the .three CSS rule to combine pure blue and pure red.*/
+
+
+.three {
+    background-color: rgb(255, 0, 255);
+  }
