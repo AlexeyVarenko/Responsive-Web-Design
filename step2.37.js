8c0178d0971c56f6094b0168e08eda9ebe10588c
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-05-12 18:47:40 +0300

    Create step2.37.js

---
+++ step2.37.js
@@ -0,0 +1,13 @@
+
+/*Step 37
+Notice that the red and cyan colors are very bright right next to each other. This contrast can be distracting if it's overused on a website, and can make text hard to read if it's placed on a complementary-colored background.
+
+It's better practice to choose one color as the dominant color, and use its complementary color as an accent to bring attention to certain content on the page.
+
+First, in the h1 rule, use the rgb function to set its background-color to cyan.*/
+
+
+h1 {
+    text-align: center;
+    background-color: rgb(0, 255, 255);
+  }
