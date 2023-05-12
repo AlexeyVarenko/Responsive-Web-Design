b9884d2624b599122eca62efdedc4a7ce7afc0f4
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-05-12 18:47:46 +0300

    Create step2.38.js

---
+++ step2.38.js
@@ -0,0 +1,11 @@
+/*Step 38
+Next, in the .one CSS rule, use the rgb function to set the background-color to black. And in the .two CSS rule, use the rgb function to set the background-color to red.*/
+
+
+.one {
+    background-color: rgb(0, 0, 0);
+  }
+  
+  .two {
+    background-color: rgb(255, 0, 0);
+  }
