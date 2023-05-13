8a07adb8a4d18ab37de86c7ce78d50eba7eb57ca
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-05-13 13:50:16 +0300

    Create step2.42.js

---
+++ step2.42.js
@@ -0,0 +1,15 @@
+/*Step 42
+Update the .one CSS rule to target the new red class.*/
+
+
+.red {
+    background-color: rgb(0, 0, 0);
+  }
+  
+  .two {
+    background-color: rgb(0, 0, 0);
+  }
+  
+  .three {
+    background-color: rgb(0, 0, 0);
+  }
