640d2b76243b71fa3c06f27a678b0d95dc601449
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-05-15 19:15:31 +0300

    Create step2.45.js

---
+++ step2.45.js
@@ -0,0 +1,15 @@
+/*Step 45
+Update the CSS class selector .two so it targets the new green class. And update the .three class selector so it targets the new blue class.*/
+
+
+.red {
+    background-color: rgb(255, 0, 0);
+  }
+  
+  .green {
+    background-color: rgb(0, 0, 0);
+  }
+  
+  .blue {
+    background-color: rgb(0, 0, 0);
+  }
