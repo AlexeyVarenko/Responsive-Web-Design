43ffb982068920c4175ab6fce5ab7be4bd3ddf13
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-05-27 13:19:53 +0300

    Create step2.72.js

---
+++ step2.72.js
@@ -0,0 +1,10 @@
+/*Step 72
+To make the marker look more realistic, give the sleeve a transparent white color.
+
+First, set the sleeve element's background-color to white.*/
+
+.sleeve {
+    background-color:white;
+    width: 110px;
+    height: 25px;
+  }
