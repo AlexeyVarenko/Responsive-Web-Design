2cee68279b550c67ab3e2215b59fd29357bf309e
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-06-08 16:53:04 +0300

    Create step3.5.js

---
+++ step3.5.js
@@ -0,0 +1,9 @@
+/*Step 5
+Nest a self-closing link element within the head element. Give it a rel attribute with value of stylesheet and an href attribute with a value of styles.css.*/
+
+
+<head>
+    <link rel="stylesheet" href="styles.css">
+    <meta charset="UTF-8">
+    <title>Registration Form</title>
+  </head>
