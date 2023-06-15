8a52c44ccd6491dd8540e944b729b647995bfee1
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-06-15 09:32:32 +0300

    Create step3.24.js

---
+++ step3.24.js
@@ -0,0 +1,11 @@
+/*Step 24
+Let us go to the next part of the registration form. This section will ask for the type of account the user is opening, and will confirm the user has read the terms and conditions.
+
+Start by adding three label elements to the second fieldset.*/
+
+
+<fieldset>
+        <label></label>
+        <label></label>
+        <label></label>
+      </fieldset>
