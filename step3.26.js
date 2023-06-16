331627c91da5c7746254c32dd4ad7de3b21c41da
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-06-16 09:31:52 +0300

    Create step3.26.js

---
+++ step3.26.js
@@ -0,0 +1,9 @@
+/*Step 26
+For the terms and conditions, add an input with a type of checkbox to the third label element. Make this input element required because users should not sign up without reading the terms and conditions.*/
+
+
+<fieldset>
+        <label><input type="radio" /></label>
+        <label><input type="radio" /></label>
+        <label> <input type="checkbox" required /> </label>
+      </fieldset>
