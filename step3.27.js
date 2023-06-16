5be1bf0ea41eacb5620722c29d252acc560a813d
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-06-16 09:31:58 +0300

    Create step3.27.js

---
+++ step3.27.js
@@ -0,0 +1,13 @@
+/*Step 27
+Within each corresponding label element, and immediately after the input element, add a space and add the following text:
+
+Personal Account
+Business Account
+I accept the terms and conditions*/
+
+
+<fieldset>
+        <label><input type="radio" /> Personal Account </label>
+        <label><input type="radio" /> Business Account </label>
+        <label><input type="checkbox" required /> I accept the terms and conditions </label>
+      </fieldset>
