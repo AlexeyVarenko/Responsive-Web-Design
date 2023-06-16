4f98b3727bf95c331a7ec23ca2a32a52cfae8d03
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-06-16 09:31:45 +0300

    Create step3.25.js

---
+++ step3.25.js
@@ -0,0 +1,11 @@
+/*Step 25
+Users will be allowed to choose either a Personal Account or Business Account.
+
+To do this, within each of the first two label elements, add one input element with type="radio".*/
+
+
+<fieldset>
+<label> <input  type="radio" /> </label>
+<label> <input  type="radio" /> </label>
+<label></label>
+</fieldset>
