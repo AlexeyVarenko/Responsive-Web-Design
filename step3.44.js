deffae3571e3c904cf10ff5369a4951e661c8fbc
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-06-24 12:23:09 +0300

    Create step3.44.js

---
+++ step3.44.js
@@ -0,0 +1,9 @@
+/*Step 44
+Center the form element, by giving it a margin of 0 auto. Then, fix its size to a maximum width of 500px, and a minimum width of 300px. In between that range, allow it to have a width of 60vw.*/
+
+form{
+    margin:0 auto;
+    max-width:500px;
+     min-width:300px;
+     width:60vw;
+  }
