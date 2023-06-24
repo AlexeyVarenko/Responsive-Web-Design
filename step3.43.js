6f3a95dd2507ce2011272533b02a32f56d3eac55
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-06-24 12:23:04 +0300

    Create step3.43.js

---
+++ step3.43.js
@@ -0,0 +1,8 @@
+/*Step 43
+Center the h1 and p elements by giving them a margin of 1em auto. Then, align their text in the center as well.*/
+
+
+h1, p {
+    margin:1em auto;
+    text-align:center;
+  }
