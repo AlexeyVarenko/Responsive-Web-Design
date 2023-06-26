dd305212004d0a9629fa0592f44dfe65dbb93407
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-06-26 11:50:57 +0300

    Create step3.45.js

---
+++ step3.45.js
@@ -0,0 +1,10 @@
+/*Step 45
+During development, it is useful to see the fieldset default borders. However, they make the content appear too separated.
+
+Remove the border, and add 2rem of padding only to the top and bottom of each fieldset. Be sure to remove the padding from the left and right.*/
+
+
+fieldset{
+    border:0;
+    padding: 2rem 0 2rem 0;
+  }
