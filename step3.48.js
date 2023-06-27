e65063e224187f899a1cf8c7504d2a152629fb94
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-06-27 10:16:30 +0300

    Create step3.48.js

---
+++ step3.48.js
@@ -0,0 +1,13 @@
+/*Step 48
+It would be nicer to have the label text appear above the form elements.
+
+Select all input, textarea, and select elements, and make them take up the full width of their parent elements.
+
+Also, add 10px of margin to the top of the selected elements. Set the other margins to 0.*/
+
+
+input, textarea, select{
+    width:100%;
+    margin:10px 0 0 0;
+  
+  }
