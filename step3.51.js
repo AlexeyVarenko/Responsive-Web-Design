c7a3fb8990ada470715beb25ce0d62677787b6a7
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-06-28 10:07:38 +0300

    Create step3.51.js

---
+++ step3.51.js
@@ -0,0 +1,7 @@
+/*Step 51
+Add some space between the .inline elements and the label text, by giving a right margin of 0.5em. Also, set all the other margin to 0.*/
+
+.inline {
+    width: unset;
+    margin: 0 0.5em 0 0 ;
+  }
