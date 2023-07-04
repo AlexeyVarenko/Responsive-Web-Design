004af251fc230ed1b97807a60973b6e41ab2fe5b
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-07-04 09:44:30 +0300

    Create step3.63.js

---
+++ step3.63.js
@@ -0,0 +1,8 @@
+/*Step 63
+Last, but not least, change the text color of the terms and conditions link to #dfdfe2.
+
+Well done! You have completed the final part of the Registration Form practice project.*/
+
+a {
+    color:#dfdfe2;
+  }
