c3333b0d58f62ddca38fc60edca9902e03cfa15e
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-07-06 10:10:57 +0300

    Create step4.6.js

---
+++ step4.6.js
@@ -0,0 +1,9 @@
+/*Step 6
+Add a div element in the body.
+
+Set the class attribute equal to canvas. For example, <div class="my-div">.
+
+This will act as the canvas for your painting.*/
+
+
+<div class="canvas"></div>
