fa91e2a32477ae80b87543b2cb04f1bf94d96dae
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-07-17 12:43:55 +0300

    Create step4.34.js

---
+++ step4.34.js
@@ -0,0 +1,13 @@
+/*Step 34
+The colors and shapes of your painting are too sharp to pass as a Rothko.
+
+Use the filter property to blur the painting by 2px in the .canvas element.
+
+Here's an example of a rule that add a 3px blur:
+
+p {
+  filter: blur(3px);
+}*/
+
+
+filter: blur(2px);
