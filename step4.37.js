393af8cc9e4f9606542cf818185534a0a3c5b07c
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-07-18 19:14:38 +0300

    Create step4.37.js

---
+++ step4.37.js
@@ -0,0 +1,6 @@
+/*Step 37
+The rectangles are too small and their edges don't have the soft quality of a painting.
+
+Increase the area and soften the edges of .one by setting its box-shadow to 0 0 3px 3px #efb762.*/
+
+box-shadow: 0 0 3px 3px #efb762;
