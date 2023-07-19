6ea339b42c6335776b4cbc86923a8eac1e1f3f35
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-07-19 19:07:14 +0300

    Create step4.41.js

---
+++ step4.41.js
@@ -0,0 +1,4 @@
+/*Step 41
+Use the border-radius property on the .two selector, to set its top-left radius and bottom-right radius to 8px, and top-right radius and bottom-left radius to 10px.*/
+
+border-radius: 8px 10px;
