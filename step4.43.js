9b195c1f6771df0378e3752c7a06fef84d5090c8
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-07-20 12:04:23 +0300

    Create step4.43.js

---
+++ step4.43.js
@@ -0,0 +1,7 @@
+/*Step 43
+Rotate each rectangle to give them more of an imperfect, hand-painted look.
+
+Use the transform property on the .one selector to rotate it counter clockwise by 0.6 degrees.*/
+
+
+transform: rotate(-0.6deg)
