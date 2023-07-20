46fabfbc8a0c95e1083b6bd3d51d10566b92347f
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-07-20 12:04:21 +0300

    Create step4.42.js

---
+++ step4.42.js
@@ -0,0 +1,7 @@
+/*Step 42
+The border-radius property accepts up to four values to round the top-left, top-right, bottom-right, and bottom-left corners.
+
+Round the top-left corner of .three by 30 pixels, the top-right by 25 pixels, the bottom-right by 60 pixels, and bottom-left by 12 pixels.*/
+
+
+border-radius: 30px 25px 60px 12px;
