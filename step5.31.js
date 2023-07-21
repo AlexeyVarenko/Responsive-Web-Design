b90de717a3da60051dbba14adf252902bf9e8c5f
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-07-21 13:52:14 +0300

    Create step5.31.js

---
+++ step5.31.js
@@ -0,0 +1,14 @@
+/*Step 3
+Within your head element, add a title element with the text set to Photo Gallery, and a link element to add your styles.css file to the page.*/
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Photo Gallery</title>
+    <link rel="stylesheet" href="styles.css"></link>
+  </head>
+  <body>
+  </body>
+</html>
