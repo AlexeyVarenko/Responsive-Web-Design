9294469cac8f6fa150f8a72511495f70182a7d76
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-07-24 16:47:34 +0300

    Create step5.9.js

---
+++ step5.9.js
@@ -0,0 +1,9 @@
+/*Step 9
+Remove the margin from your body element, set the font-family to sans-serif, and give it a background-color of #f5f6f7 as the value.*/
+
+
+body{
+    font-family:sans-serif;
+    background-color: #f5f6f7;
+    margin: 0
+  }
