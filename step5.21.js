83e8fc36eb5935f56aa0147f6bb0b0cc2f02aa21
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-07-28 16:53:08 +0300

    Create step5.21.js

---
+++ step5.21.js
@@ -0,0 +1,14 @@
+/*Step 21
+The alt image attribute should describe the image content. Screen readers announce the alternative text in place of images. If the image can't be loaded, this text is presented in place of the image.
+
+To complete the project, add an alt attribute to all nine of your cat images to describe them. Use a value at least five characters long for each.*/
+
+<img alt="dormouse" src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg">
+      <img alt="playful" src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/2.jpg">
+      <img alt="strict" src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/3.jpg">
+      <img alt="restwind" src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/4.jpg">
+      <img alt="scared" src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/5.jpg">
+      <img alt="brothers" src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/6.jpg">
+      <img alt="hunter" src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/7.jpg">
+      <img alt="ginger" src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/8.jpg">
+      <img alt="holiday" src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/9.jpg"></img>
