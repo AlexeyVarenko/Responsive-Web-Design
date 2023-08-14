4b07f386350bbdcd5e6836db921105d46991439d
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-08-14 17:26:50 +0300

    Create step6.42.js

---
+++ step6.42.js
@@ -0,0 +1,8 @@
+/*Step 42
+Use your existing .divider element as an example to add a new divider after the p element.*/
+
+
+<div class="daily-value small-text">
+<p class="bold right">% Daily Value *</p>
+<div class="divider"> </div>
+</div>
