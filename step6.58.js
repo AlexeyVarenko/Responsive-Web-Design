26af05e8b9e330667c4a508ab66be1b854bf2f8a
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-08-21 17:26:09 +0200

    Create step6.58.js

---
+++ step6.58.js
@@ -0,0 +1,6 @@
+/*Step 58
+
+The advantage to creating these dividers is that you can apply specific classes to style them individually. Add double-indent to the class for your last .divider.*/
+
+
+<div class="divider double-indent"></div>
