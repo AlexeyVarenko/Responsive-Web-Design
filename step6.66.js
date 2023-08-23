ab91333507a3d2bc80e0468c1ab524667f8ae3ae
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-08-23 10:03:30 +0200

    Create step6.66.js

---
+++ step6.66.js
@@ -0,0 +1,9 @@
+/* Step 66
+
+Create a .note selector, and set the size of the font to 0.6rem. Also set the top and bottom margins to 5px, removing the left and right margins.*/
+
+
+.note{
+    font-size: 0.6rem;
+    margin: 5px 0 5px 0;
+  }
