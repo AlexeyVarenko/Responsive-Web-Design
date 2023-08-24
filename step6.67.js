54853639d798b2316734e563f1e9f157a1f11d37
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-08-24 11:55:42 +0300

    Create step6.67.js

---
+++ step6.67.js
@@ -0,0 +1,12 @@
+/*Step 67
+
+Give the .note selector a left and right padding of 8px, removing the top and bottom padding. Also set the text-indent property to -8px.
+
+With these last changes, your nutrition label is complete!*/
+
+.note {
+  font-size: 0.6rem;
+  margin: 5px 0;
+  padding: 0px 8px 0px 8px;
+  text-indent: -8px;
+}
