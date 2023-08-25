122e27252a20eac5ae356a475ac7697d58f8e132
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-08-25 09:38:35 +0300

    Create step7.3.js

---
+++ step7.3.js
@@ -0,0 +1,10 @@
+/*Step 3
+Continuing with the meta elements, a viewport definition tells the browser how to render the page. Including one betters visual accessibility on mobile, and improves SEO (search engine optimization).
+
+Add a viewport definition with a content attribute detailing the width and initial-scale of the page.*/
+
+<head>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="styles.css" />
+</head>
