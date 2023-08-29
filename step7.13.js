392a964fef4dff0006f24bcb2ada3f555a028364
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-08-29 19:14:53 +0300

    Create step7.13.js

---
+++ step7.13.js
@@ -0,0 +1,8 @@
+/*Step 13
+Use the > selector to target the unordered list elements within the nav elements, and use Flexbox to evenly space the children.*/
+
+
+nav > ul{
+    justify-content: space-evenly;
+    display: flex;
+  }
