be4ff6291837cd1dff6ee411d757cc9d03344991
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-08-29 19:14:51 +0300

    Create step7.12.js

---
+++ step7.12.js
@@ -0,0 +1,17 @@
+/*Step 12
+To enable navigation on the page, add an unordered list with the following three list items:
+
+INFO
+HTML
+CSS
+The list items text should be wrapped in anchor tags.*/
+
+
+<nav>
+<ul>
+  <li> <a href="INFO">INFO</a></li>
+  <li> <a href="HTML">HTML</a></li>
+  <li> <a href="CSS">CSS</a></li>
+</ul>
+
+</nav>
