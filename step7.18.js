b11ed35703677af49a5df81d472b1738921549f4
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-08-31 18:37:11 +0300

    Create step7.18.js

---
+++ step7.18.js
@@ -0,0 +1,10 @@
+/*Step 18
+To be able to navigate within the page, give each anchor element an href corresponding to the id of the h2 elements.*/
+
+
+
+<ul>
+<li><a href="#student-info" id="INFO" h2="INFO">INFO</a></li>
+<li><a href="#html-questions" id="HTML" h2="HTML">HTML</a></li>
+<li><a href="#css-questions" id="CSS" h2="CSS">CSS</a></li>
+</ul>
