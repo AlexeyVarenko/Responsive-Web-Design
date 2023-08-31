a62c636761ab0b03ffb1c6dffee86dec716c0e4b
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-08-31 18:37:13 +0300

    Create step7.19.js

---
+++ step7.19.js
@@ -0,0 +1,12 @@
+/*Step 19
+Filling out the content of the quiz, below #student-info, add three div elements with a class of info.
+
+Then, within each div nest one label element, and one input element.*/
+
+
+<section role="region" aria-labelledby="student-info">
+<h2 id="student-info">Student Info</h2>
+<div class="info" > <label></label> <input></input></div>
+<div class="info" ><label></label> <input></input></div>
+<div class="info" ><label></label> <input></input></div>
+</section>
