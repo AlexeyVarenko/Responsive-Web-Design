6f5906e27813c7af51b6b0220e2db36ac782dd8c
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-09-02 09:50:01 +0300

    Create step7.24.js

---
+++ step7.24.js
@@ -0,0 +1,8 @@
+/*Step 24
+Within the span element, add the text (Date of Birth).*/
+
+
+<div class="info">
+            <label for="birth-date">D.O.B.<span class="sr-only">(Date of Birth)</span></label>
+            <input type="date" name="birth-date" id="birth-date" />
+          </div>
