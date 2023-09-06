45ca23fa7d9e68e9f69b145d57f9fa67059ebd35
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-09-06 12:49:56 +0300

    Create step7.34.js

---
+++ step7.34.js
@@ -0,0 +1,7 @@
+/*Step 34
+To prevent unnecessary repetition, target the before pseudo-element of the p element, and give it a content property of "Question #".*/
+
+
+p::before{
+    content: "Question #";
+  }
