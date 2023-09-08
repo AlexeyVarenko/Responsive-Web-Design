2c183a0ef2c1b14ba8aa7763078a850bb9e00548
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-09-08 11:53:06 +0300

    Create step7.39.js

---
+++ step7.39.js
@@ -0,0 +1,8 @@
+/*Step 39
+Nest one textarea element within the second div.answer element, and set the number of rows and columns it has.
+
+Then, give the textarea placeholder text describing an example answer.*/
+
+<div class="answer">
+<textarea rows="3" cols="20" placeholder="Yes/No"></textarea>
+</div>
