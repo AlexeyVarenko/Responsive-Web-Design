e6337eb954573e65f125697c666d72602eede1c1
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-09-08 11:53:04 +0300

    Create step7.38.js

---
+++ step7.38.js
@@ -0,0 +1,14 @@
+/*Step 38
+Link the first label element to the select element, and give the select element a name attribute.*/
+
+
+<div class="question-block">
+              <label for="option">Are you a frontend developer?</label>
+            </div>
+            <div class="answer">
+              <select required id="option" name="option">
+                <option value="">Select an option</option>
+                <option value="yes">Yes</option>
+                <option value="no">No</option>
+              </select>
+            </div>
