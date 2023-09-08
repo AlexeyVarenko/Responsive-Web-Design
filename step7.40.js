51f87c5271e9cd8eafca34a52210a45d0582ed6b
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-09-08 11:53:07 +0300

    Create step7.40.js

---
+++ step7.40.js
@@ -0,0 +1,10 @@
+/*Step 40
+As with the other input and label elements, link the textarea to its corresponding label element, and give it a name attribute.*/
+
+
+<div class="question-block">
+              <label for="question">Do you have any questions:</label>
+            </div>
+            <div class="answer">
+              <textarea id="question" name="question" rows="5" cols="24" placeholder="Who is flexbox..."></textarea>
+            </div>
