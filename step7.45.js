0b50c32731a8c86591add4808fea24073d80f243
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-09-11 18:10:55 +0300

    Create step7.45.js

---
+++ step7.45.js
@@ -0,0 +1,15 @@
+/*Step 45
+Back to styling the page. Select the list elements within the navigation bar, and give them the following styles:
+
+color: #dfdfe2;
+margin: 0 0.2rem;
+padding: 0.2rem;
+display: block;*/
+
+
+nav > ul > li{
+    color: #dfdfe2;
+  margin: 0 0.2rem;
+  padding: 0.2rem;
+  display: block;
+  }
