e8f7e7c8042d994402f548085fe873d01598ecf2
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-09-13 18:18:49 +0300

    Create step7.51.js

---
+++ step7.51.js
@@ -0,0 +1,14 @@
+/*Step 51
+Set the width of the section elements to 80% of their parent container. Then, use margins to center the section elements, adding 10px to the bottom margin.
+
+Also, ensure the section elements cannot be larger than 600px in width.*/
+
+
+section{
+    width: 80%;
+    margin-top: 0;
+    margin-right: auto;
+    margin-bottom:10px;
+    margin-left: auto;
+    max-width: 600px;
+  }
