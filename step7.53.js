1b7ef5f4c3b9a4a24dd4fd6afa82e0d2644177de
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-09-14 18:53:19 +0300

    Create step7.53.js

---
+++ step7.53.js
@@ -0,0 +1,9 @@
+/*Step 53
+Add padding to the top and left of the .info elements, and set the other values to 0.*/
+
+.info{
+    padding-top: 1px;
+    padding-right: 0;
+    padding-bottom: 0;
+    padding-left: 1px;
+  }
