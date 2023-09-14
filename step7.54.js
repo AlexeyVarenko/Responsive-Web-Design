fc089074de9e24d757a7b4c8fd6c0b6bea7129e2
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-09-14 18:53:20 +0300

    Create step7.54.js

---
+++ step7.54.js
@@ -0,0 +1,19 @@
+/*Step 54
+Give the .formrow elements top margin, and left and right padding. The other padding values should be 0.
+
+Then, increase the font size for all input elements.*/
+
+
+.formrow{
+    margin-top: 1px;
+    margin-left: 0;
+    margin-bottom: 0;
+    margin-right: 0;
+    padding-top: 0;
+    padding-right: 1px;
+      padding-bottom: 0;
+      padding-left: 1px;
+    }
+    input{
+      font-size: 14px;
+    }
