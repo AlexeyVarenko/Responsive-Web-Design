a99d89ed5e3b407e45cbfc1d0b2be5c81e822084
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-09-15 14:50:46 +0300

    Create step7.58.js

---
+++ step7.58.js
@@ -0,0 +1,18 @@
+/*Step 58
+To neaten the .question-block elements, set the following CSS properties:
+
+text-align: left;
+display: block;
+width: 100%;
+margin-top: 20px;
+padding-top: 5px;*/
+
+
+
+.question-block{
+    text-align: left;
+  display: block;
+  width: 100%;
+  margin-top: 20px;
+  padding-top: 5px;
+  }
