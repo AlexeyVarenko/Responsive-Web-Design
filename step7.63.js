59a2f98b9c741ad0a7a0426b936d2122f61f741f
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-09-18 19:34:50 +0300

    Create step7.63.js

---
+++ step7.63.js
@@ -0,0 +1,9 @@
+/*Step 63
+Set the footer background color to #2a2a40, and use Flexbox to horizontally center the text.*/
+
+
+footer{
+    background-color:#2a2a40;
+    display: flex;
+    justify-content: center
+  }
