12615f35048e368908658afde3ab8d9026b1c8ce
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-09-19 19:19:31 +0300

    Create step7.65.js

---
+++ step7.65.js
@@ -0,0 +1,11 @@
+/*Step 65
+Horizontally center all the text within the address element, and add some padding.*/
+
+
+address{
+    text-align: center;
+    padding-top: 1px;
+    padding-right: 1px;
+    padding-bottom: 1px;
+    padding-left: 1px;
+  }
