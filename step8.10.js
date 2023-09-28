d5e378b5e382cc68e60aa3f41f4493c6a68b2993
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-09-28 09:20:14 +0300

    Create step8.10.js

---
+++ step8.10.js
@@ -0,0 +1,15 @@
+/*Step 10
+The tr element is used to indicate a table row. Add a tr element within your thead element. In your new tr element, add a td element, followed by three th elements.
+
+The td element indicates a data cell, while the th element indicates a header cell.*/
+
+
+<thead>
+              <tr>
+                <td>
+                  <th><th>
+                    <th><th>
+                      <th><th>
+                  <td>
+              </tr>
+            </thead>
