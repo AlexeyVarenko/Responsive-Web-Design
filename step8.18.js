33c57bcabdd33ba61313772cab7350f15673c088
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-09-30 12:20:38 +0300

    Create step8.18.js

---
+++ step8.18.js
@@ -0,0 +1,18 @@
+/*Step 18
+Within your thead, add a tr. Inside that, add a td and three th elements.*/
+
+
+
+<table>
+            <caption>Liabilities</caption>
+            <thead>
+              <tr>
+                <td></td>
+                <th></th>
+                <th></th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+            </tbody>
+          </table>
