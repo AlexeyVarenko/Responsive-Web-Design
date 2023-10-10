7b1bd6abbf7ffcbe67675156c91a23842cfdf5bb
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-10-10 18:49:54 +0300

    Create step8.41.js

---
+++ step8.41.js
@@ -0,0 +1,12 @@
+/*Step 41
+Now apply some color to your #years. Make the text #fff and the background #0a0a23.*/
+
+
+#years {
+    display: flex;
+    justify-content: flex-end;
+    position: sticky;
+    top: 0;
+    color: #fff;
+    background: #0a0a23;
+  }
