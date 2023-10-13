ffdfd571d35ff30fd376d4c89b5437b883fadb78
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-10-13 19:53:48 +0300

    Create step8.45.js

---
+++ step8.45.js
@@ -0,0 +1,7 @@
+/*Step 45
+You wrapped your tables in a container with the table-wrap class. Create a selector for that class, and give it a padding set to 0 0.75rem 1.5rem 0.75rem.*/
+
+
+.table-wrap{
+    padding: 0 0.75rem 1.5rem 0.75rem
+  }
