67c172b367f48b23d6e381f9bc87822107fc231c
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-10-16 18:31:50 +0300

    Create step8.50.js

---
+++ step8.50.js
@@ -0,0 +1,11 @@
+/*Step 50
+Ensure your table fills its container with a width property set to 100%, then position it relatively and give it a top margin of 3rem.*/
+
+
+table {
+    border-collapse: collapse;
+    border: 0;
+    width: 100%;
+    margin-top: 3rem;
+    position: relative;
+  }
