da06bee3a84d4ebd2a6dbd4a9e438c28638959a1
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-10-18 18:56:28 +0300

    Create step8.59.js

---
+++ step8.59.js
@@ -0,0 +1,7 @@
+/*Step 58
+The :nth-of-type() pseudo-selector is used to target specific elements based on their order among siblings of the same type. Use this pseudo-selector to target the third td element within your total table rows. Give it a right padding of 0.5rem.*/
+
+
+tr.total td:nth-of-type(3){
+    padding-right: 0.5rem;
+    }
