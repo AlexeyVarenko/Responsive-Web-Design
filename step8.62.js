86b601617f8c704b48c28dd4f4fc61a621f0a19a
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-10-19 19:14:34 +0200

    Create step8.62.js

---
+++ step8.62.js
@@ -0,0 +1,11 @@
+/*Step 62
+
+Select the th elements within your tr.data elements. Align the text to the left, and give them a top padding of 0.3rem and a left padding of 0.5rem.*/
+
+
+
+tr.data th{
+    text-align: left;
+    padding-top: 0.3rem;
+    padding-left: 0.5rem;
+  }
