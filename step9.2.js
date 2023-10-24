ea981f577df3a9e0288e7ffc3781035e1ce17656
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-10-24 22:00:25 +0100

    Create step9.2.js

---
+++ step9.2.js
@@ -0,0 +1,8 @@
+/*Step 2
+
+Within your head element, add a meta tag with the charset attribute of utf-8. Also add a title element with the text fCC Cat Painting.*/
+
+
+<meta charset="UTF-8">
+  
+  <title>fCC Cat Painting</title>
