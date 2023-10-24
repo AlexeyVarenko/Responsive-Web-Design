3c66d047df49ad842b64049c12ebc60ff26a7e71
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-10-24 22:00:20 +0100

    Create step8.66.js

---
+++ step8.66.js
@@ -0,0 +1,10 @@
+/*Step 66
+
+Create another selector for the td elements within your tr.data element, but specifically select the last one. Give this a right padding of 0.5rem.
+
+With this, your balance sheet is complete!*/
+
+
+tr.data td:last-of-type{
+    padding-right: 0.5rem
+    }
