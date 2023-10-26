417a68272f0020b956dcfcc2e1260b7a8840de12
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-10-26 10:12:17 +0100

    Create step9.3.js

---
+++ step9.3.js
@@ -0,0 +1,6 @@
+/* Step 3
+
+Add a link element within your head element. For that link element, set the rel attribute to stylesheet and the href to ./styles.css.*/
+
+
+<link rel="stylesheet" href="./styles.css"></link>
