40bbc26f239f27fc41f84bfd2c6dccc9b9a826ed
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-10-27 10:08:21 +0100

    Create step9.7.js

---
+++ step9.7.js
@@ -0,0 +1,11 @@
+/* Step 7
+
+Using a class selector, give the .cat-head element a width of 205px and a height of 180px. Also, give it a border of 1px solid #000 and a border-radius of 46%.*/
+
+
+.cat-head{
+    width: 205px;
+    height: 180px;
+    border: 1px solid #000;
+    border-radius: 46%;
+  }
