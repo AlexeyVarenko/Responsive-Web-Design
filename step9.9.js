5fac830c9bc8cff65465aec38a3cf6e40ce56e29
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-10-28 10:27:59 +0200

    Create step9.9.js

---
+++ step9.9.js
@@ -0,0 +1,14 @@
+/*Step 9
+
+CSS positioning lets you set how you want an element to be positioned in the browser. It has a position property you can set to static, absolute, relative, sticky or fixed.
+
+Once you set the position property of the element, you can move the element around by setting a pixel or a percentage value for one or more of the top, right, left, or bottom properties.
+
+static is the default positioning for all elements. If you assign it to an element, you won't be able to move it around with top, right, left, or bottom.
+
+Give .cat-head a position property of static, then set the top and left properties to 100px each.*/
+
+
+position: static;
+  top: 100px;
+  left: 100px;
