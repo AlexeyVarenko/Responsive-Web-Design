598665f71947022248d968509e699c7a5b0b0ec7
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-10-28 10:27:57 +0200

    Create step9.11.js

---
+++ step9.11.js
@@ -0,0 +1,10 @@
+/* Step 11
+
+The next position property is absolute. When you use the absolute value for your position property, the element is taken out of the normal flow of the document, and then its position is determined by the top, right, bottom, and left properties.
+
+Set the position property of your .cat-head element to absolute, then set top and left properties to any pixel value.*/
+
+
+position: absolute;
+  top: 100px;
+  left: 100px;
