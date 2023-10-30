b8c3630998adc13fde68786b2275318a9d6bf32b
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-10-30 14:25:43 +0300

    Create step9.14.js

---
+++ step9.14.js
@@ -0,0 +1,9 @@
+/*Step 14
+Now replace the position property value of your .cat-head with fixed. Leave both top and left as they are.
+
+After that, scroll up and down to see how the fixed value works.*/
+
+
+position: fixed;
+  top: 100px;
+  left: 100px;
