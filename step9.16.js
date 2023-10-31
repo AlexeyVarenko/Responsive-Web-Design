e8638800aeab5ff6a64b629f19a34d66a9cc4739
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-10-31 07:05:17 +0300

    Create step9.16.js

---
+++ step9.16.js
@@ -0,0 +1,12 @@
+/*Step 16
+You should now center the cat head.
+
+Give the .cat-head element a position property set to absolute. Set a value of 0 for the right, left, top, bottom properties, then set its margin property on all sides to auto. That's one way to center an element vertically and horizontally using CSS positioning.*/
+
+
+position: absolute;
+top: 0;
+left: 0;
+right: 0;
+bottom: 0;
+margin:auto; 
