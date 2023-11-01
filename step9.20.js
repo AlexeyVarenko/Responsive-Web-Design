a739dac4837d2e3d3410073c7588f7c13af6f180
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-11-01 17:23:26 +0300

    Create step9.20.js

---
+++ step9.20.js
@@ -0,0 +1,9 @@
+/*Step 20
+Inside your .cat-ears element, create two div elements with the classes cat-left-ear and cat-right-ear respectively.*/
+
+
+
+<div class="cat-ears">
+          <div class="cat-left-ear"> </div>
+            <div class="cat-right-ear"> </div>
+        </div>
