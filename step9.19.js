5205b3f3546a1c912e1fd69d0e6a2b20f625df4e
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-11-01 17:23:24 +0300

    Create step9.19.js

---
+++ step9.19.js
@@ -0,0 +1,9 @@
+/*Step 19
+Now you should work on the cat's ears. There will be a right and a left ear, and inside each, there will be an inner ear.
+
+Inside your .cat-head element, create a div element with the class cat-ears.*/
+
+
+<div class="cat-head">
+        <div class="cat-ears"> </div>
+      </div>
