f79b884227bb81710f07e3acbc38191af46fa2f3
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-11-02 19:03:18 +0300

    Create step9.23.js

---
+++ step9.23.js
@@ -0,0 +1,11 @@
+/*Step 23
+You are going to make each ear look like a triangle.
+
+Using a class selector, give the .cat-left-ear element a left and right border of 35px solid transparent each. Also, set the bottom border to 70px solid #5e5e5e.*/
+
+
+.cat-left-ear{
+    border-left: 35px solid transparent;
+    border-right: 35px solid transparent;
+    border-bottom: 70px solid #5e5e5e;
+  }
