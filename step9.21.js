a09941108f9f19cf357a3fdc539de0d43b97e732
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-11-02 19:03:12 +0300

    Create step9.21.js

---
+++ step9.21.js
@@ -0,0 +1,9 @@
+/*Step 21
+Inside your .cat-left-ear element, create another div element with the class cat-left-inner-ear.*/
+
+
+
+
+<div class="cat-left-ear">
+            <div class="cat-left-inner-ear"></div>
+          </div>
