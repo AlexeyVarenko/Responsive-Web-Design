750ad0a38a1f8e34d3a72fdd6620a8e2667cbcb7
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-11-04 08:31:43 +0300

    Create step9.25.js

---
+++ step9.25.js
@@ -0,0 +1,7 @@
+/*Step 25
+Those edges are too sharp for an ear. So, set the border-top-left-radius to 90px and the border-top-right-radius to 10px.*/
+
+
+
+border-top-left-radius: 90px;
+  border-top-right-radius:10px; 
