f592499ee58d38c25d4de9f5f354b329b2ff9332
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-11-06 12:29:22 +0300

    Create step9.27.js

---
+++ step9.27.js
@@ -0,0 +1,11 @@
+/*Step 27
+Now you can work on the right ear of the cat. You have the HTML for it already.
+
+Using a class selector, give the .cat-right-ear element a left and right border of 35px solid transparent each. Also, set the bottom border to 70px solid #5e5e5e.*/
+
+
+.cat-right-ear{
+    border-right: 35px solid transparent;
+    border-left: 35px solid transparent;
+    border-bottom:70px solid #5e5e5e; 
+  }
