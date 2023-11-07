96e3c37d1187ebc895b8b190cb7da2750b4d35e8
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-11-07 16:40:43 +0300

    Create step9.31.js

---
+++ step9.31.js
@@ -0,0 +1,9 @@
+/*Step 31
+The ears should always be placed above the part of the head it overlaps. You can do this with the z-index property.
+
+z-index is a property you can use to define the order of overlapping HTML elements. Any element with a higher z-index will always be positioned over an element with a lower z-index.
+
+To see z-index in action, set the z-indexproperty of the left ear to -1.*/
+
+
+z-index: -1;
