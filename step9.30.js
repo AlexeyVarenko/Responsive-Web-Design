600e836517535a012f273c2dc6688331240f16fd
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-11-07 16:40:41 +0300

    Create step9.30.js

---
+++ step9.30.js
@@ -0,0 +1,6 @@
+/*Step 30
+Remove the sharp border of the right ear by setting the border-top-left-radius to 90px and the border-top-right-radius to 10px.*/
+
+
+border-top-left-radius: 90px;
+border-top-right-radius: 10px;
