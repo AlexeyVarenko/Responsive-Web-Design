ebe319d7c0b676f3a29f55dc49fe5502a0f55318
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-11-07 16:40:45 +0300

    Create step9.32.js

---
+++ step9.32.js
@@ -0,0 +1,7 @@
+/*Step 32
+That's not the behavior you want. You should make the ears display over the head so the borders that overlap with them don't show.
+
+Instead of -1, set the z-index property of the left ear to 1.*/
+
+
+z-index: 1;
