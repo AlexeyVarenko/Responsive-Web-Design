b5ab8aa21facb8d79a00253cc7c81515c89b430a
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-11-09 20:46:40 +0300

    Create step9.38.js

---
+++ step9.38.js
@@ -0,0 +1,7 @@
+/*Step 38
+Move the right inner ear into position with a position property set to absolute, a top of 22px and a left of -20px.*/
+
+
+position: absolute;
+  top: 22px;
+  left: -20px;
