f2608653b27a7c2b5924ae6b835942ddb3ce45c0
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-11-09 20:46:36 +0300

    Create step9.36.js

---
+++ step9.36.js
@@ -0,0 +1,9 @@
+/*Step 36
+To remove all the pointed edges of the ear, set a bottom-right and bottom-left border radius of 40% each, a top-left border radius of 90px, and a top-right border radius of 10px.*/
+
+
+
+border-bottom-right-radius: 40%;
+  border-top-left-radius: 90px;
+  border-bottom-left-radius: 40%;
+  border-top-right-radius: 10px;
