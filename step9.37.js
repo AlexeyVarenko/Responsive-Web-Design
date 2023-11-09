cf92bdd9fba2dea17f6e76c496620b808e934e9f
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-11-09 20:46:38 +0300

    Create step9.37.js

---
+++ step9.37.js
@@ -0,0 +1,9 @@
+/*Step 37
+It's time to work on the right inner ear. Using a class selector, give your .cat-right-inner-ear element a left and right border of 20px solid transparent. Also, give it a bottom border of 40px solid #3b3b4f.*/
+
+
+.cat-right-inner-ear{
+    border-left: 20px solid transparent;
+    border-right: 20px solid transparent;
+    border-bottom: 40px solid #3b3b4f;
+  }
