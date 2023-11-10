eed848d8f0d7f8ef1e4ab022be3ead9e04b9a67b
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-11-10 17:17:32 +0300

    Create step9.40.js

---
+++ step9.40.js
@@ -0,0 +1,10 @@
+/*Step 40
+You will now start working on the cat's eyes. Like the ears, the eyes will have inner eyes.
+
+Create a div element with the class cat-eyes. Inside the cat-eyes element, create two div elements with the class cat-left-eye and cat-right-eye respectively.*/
+
+
+<div class="cat-eyes">
+          <div class="cat-left-eye"></div>
+          <div class="cat-right-eye"></div>
+        </div>
