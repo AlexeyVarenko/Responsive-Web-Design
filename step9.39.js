680c6cadaaf1abee9a73e4d4290d7f32ab8732e6
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-11-10 17:17:30 +0300

    Create step9.39.js

---
+++ step9.39.js
@@ -0,0 +1,8 @@
+/*Step 39
+As you did for the left inner ear, remove the sharp edges of the right inner ear by setting a bottom-right and bottom-left border radius of 40%, a top-left border radius of 90px, and a top-right border radius of 10px.*/
+
+
+border-bottom-right-radius: 40%;
+border-bottom-left-radius: 40%;
+border-top-left-radius: 90px;
+border-top-right-radius: 10px;
