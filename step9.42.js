54a0b5faf04ca76d90d6bd3f376adb4b10b1a87c
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-11-11 08:28:22 +0300

    Create step9.42.js

---
+++ step9.42.js
@@ -0,0 +1,7 @@
+/*Step 42
+Inside the .cat-right-eye element, create another div element with the class cat-right-inner-eye.*/
+
+
+<div class="cat-right-eye">
+            <div class="cat-right-inner-eye"></div>
+          </div>
