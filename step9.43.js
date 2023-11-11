7fad1a7aa3ac973a44533bdd0b8dded99b929211
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-11-11 08:28:23 +0300

    Create step9.43.js

---
+++ step9.43.js
@@ -0,0 +1,9 @@
+/*Step 43
+Using a class selector, give your .cat-left-eye element a width of 30px and a height of 40px. Also, give it a background-color of #000.*/
+
+
+.cat-left-eye{
+    width: 30px;
+    height: 40px;
+    background-color: #000;
+  }
