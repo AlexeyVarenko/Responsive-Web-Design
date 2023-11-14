bcd8810ea915b5616ccac9bdca53cb95d54770d5
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-11-14 17:48:45 +0300

    Create step9.48.js

---
+++ step9.48.js
@@ -0,0 +1,6 @@
+/*Step 48
+To make the right eye look like an eye, give it a border radius of 60%. Also, using the transform property, rotate it at -25 degrees.*/
+
+
+BORDER-RADIUS: 60%;
+  transform: rotate(-25deg);
