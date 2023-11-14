bf0906f21a79a34f67db86f288965b8e343c5550
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-11-14 17:48:48 +0300

    Create step9.50.js

---
+++ step9.50.js
@@ -0,0 +1,9 @@
+/*Step 50
+Move the left inner eye into position with a position property of absolute, a top of 8px, and a left of 2px. Also, give it a border radius of 60% and rotate it at 10 degrees.*/
+
+
+position:absolute;
+  top:8px;
+  left:2px;
+  border-radius:60%;
+  transform:rotate(10deg);
