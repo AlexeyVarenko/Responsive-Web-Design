ca6faf008923cdd8ed5a55db084789dc378d8dc3
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-11-15 17:38:09 +0300

    Create step9.51.js

---
+++ step9.51.js
@@ -0,0 +1,10 @@
+/*Step 51
+Using a class selector, give your .cat-right-inner-eye element a width of 10px and a height of 20px. Also, give it a background color of #fff.*/
+
+
+
+.cat-right-inner-eye{
+    width: 10px;
+    height: 20px;
+    background-color: #fff;
+  }
