1c44d735b2cd72a7ed9c87e176faf8b7a8fe85c7
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-11-16 17:05:44 +0300

    Create step9.54.js

---
+++ step9.54.js
@@ -0,0 +1,10 @@
+/*Step 54
+Using a class selector, give your .cat-nose element a left and right border of 15px solid transparent each. Also give it a bottom border of 20px solid #442c2c.*/
+
+
+.cat-nose{
+    border-left: 15px solid transparent;
+    border-right: 15px solid transparent;
+    border-bottom: 20px solid #442c2c;
+  
+  }
