e340ffb3580191994e1bc905bcd84b23e303ba88
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-11-16 17:05:48 +0300

    Create step9.56.js

---
+++ step9.56.js
@@ -0,0 +1,6 @@
+/*Step 56
+Remove the sharp edges of the nose with border radius of 50% each on the top-left, bottom-right, and bottom-left corners. Also, rotate it at 180 degrees.*/
+
+
+border-radius: 50%;
+  transform: rotate(180deg);
