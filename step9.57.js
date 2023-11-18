64195e6faa05e655e54e0aec329db1af8d57c78c
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-11-18 14:42:09 +0300

    Create step9.57.js

---
+++ step9.57.js
@@ -0,0 +1,7 @@
+/*Step 57
+Now you will start working on the mouth. There will be a right line and left line for the mouth.
+
+Create a div element with the class cat-mouth.*/
+
+
+<div class="cat-mouth"></div>
