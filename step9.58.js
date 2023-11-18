8806bfff6bdb37768d3465b4cb496bf2f8c250b7
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-11-18 14:42:10 +0300

    Create step9.58.js

---
+++ step9.58.js
@@ -0,0 +1,9 @@
+/*Step 58
+Inside your .cat-mouth element, create a div element with the class cat-mouth-line-left and another div with the class cat-mouth-line-right.*/
+
+
+
+<div class="cat-mouth">
+          <div class="cat-mouth-line-left"></div>
+          <div class="cat-mouth-line-right"></div>
+        </div>
