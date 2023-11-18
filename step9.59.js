e0dce9d06d7127bba495f567f10fb11a5057657c
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-11-18 14:42:12 +0300

    Create step9.59.js

---
+++ step9.59.js
@@ -0,0 +1,9 @@
+/*Step 59
+Using a descendant selector, select the two div elements inside the div with class cat-mouth. Give it a width of 30px, a height of 50px, and a border of 2px solid #000.*/
+
+
+.cat-mouth div{
+    width: 30px;
+    height: 50px;
+    border: 2px solid #000;
+  }
