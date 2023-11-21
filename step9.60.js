7b1f93711df3c3f664c94e0412a7faa432ac3608
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-11-21 12:07:33 +0300

    Create step9.60.js

---
+++ step9.60.js
@@ -0,0 +1,7 @@
+/*Step 60
+You are going to make the two mouth lines into an elliptical shape. So, give the .cat-mouth div selector a border color of black transparent transparent transparent and a border radius of 190%/190px 150px 0 0.*/
+
+
+
+border-color: black transparent transparent transparent;
+  border-radius: 190%/190px 150px 0 0;
