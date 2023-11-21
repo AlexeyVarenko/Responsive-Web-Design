af776d7f5936703d78f0d5cb6e5c58efd7956898
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-11-21 12:07:34 +0300

    Create step9.61.js

---
+++ step9.61.js
@@ -0,0 +1,9 @@
+/*Step 61
+Using a class selector, give your .cat-mouth-line-left element a position of absolute, a top of 88px and a left of 74px. This would move it into the right position.*/
+
+
+.cat-mouth-line-left{
+    position: absolute;
+    top: 88px;
+    left: 74px;
+  }
