5862924f40bc39c2aeefc84798f00707665ce33a
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-11-22 11:37:03 +0300

    Create step9.65.js

---
+++ step9.65.js
@@ -0,0 +1,7 @@
+/*Step 65
+The last thing you will work on is the whiskers. There are going to be 6 of them, meaning there will be three on each side.
+
+Create a div element with the class cat-whiskers.*/
+
+
+<div class="cat-whiskers"></div>
