306ba544f7211ac093d4b87033bef42a081b3e9c
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-11-23 15:20:47 +0300

    Create step9.66.js

---
+++ step9.66.js
@@ -0,0 +1,8 @@
+/*Step 66
+Inside the .cat-whiskers element, create two div elements with the class cat-whiskers-left and cat-whiskers-right.*/
+
+
+<div class="cat-whiskers">
+          <div class="cat-whiskers-left"></div>
+          <div class="cat-whiskers-right"></div>
+        </div>
