12bd5c7af74b3a332f68d11d7357fad8a85e3755
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-11-23 15:20:50 +0300

    Create step9.68.js

---
+++ step9.68.js
@@ -0,0 +1,10 @@
+/*Step 68
+Inside the .cat-whiskers-right element, create 3 div elements with the class cat-whisker-right-top, cat-whisker-right-middle, and cat-whisker-right-bottom.*/
+
+
+
+<div class="cat-whiskers-right">
+            <div class="cat-whisker-right-top"></div>
+            <div class="cat-whisker-right-middle"></div>
+            <div class="cat-whisker-right-bottom"></div> 
+          </div>
