2db9d528fcea1517c30a2bf2f3a931cdd01ca050
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-11-24 15:34:55 +0300

    Create step9.71.js

---
+++ step9.71.js
@@ -0,0 +1,11 @@
+/*Step 71
+Using a class selector, move the .cat-whisker-left-top element into place with a position of absolute, a top of 120px, and a left of 52px.*/
+
+
+
+.cat-whisker-left-top{
+    position: absolute;
+    top: 120px;
+    left: 52px;
+
+  }
