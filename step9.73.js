fe185d15cff671883a181f0a13cc241d4b1a1bd3
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-11-25 12:35:04 +0300

    Create step9.73.js

---
+++ step9.73.js
@@ -0,0 +1,9 @@
+/*Step 73
+Use a class selector to target the .cat-whisker-left-middle element. Then move it into place with a position property set to absolute, a top of 127px, and a left of 52px.*/
+
+
+.cat-whisker-left-middle{
+    position: absolute;
+    top: 127px;
+    left: 52px;
+  }
