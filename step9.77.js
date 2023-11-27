4599e827dc722ac0d87525d4de92cbcd584351d7
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-11-27 10:16:31 +0300

    Create step9.77.js

---
+++ step9.77.js
@@ -0,0 +1,10 @@
+/*Step 77
+Now you will work on moving the right whiskers into place. Use class selector to target the .cat-whisker-right-top element and give it a position of absolute, a top of 120px, and a left of 109px.*/
+
+
+
+.cat-whisker-right-top{
+    position: absolute;
+    top: 120px;
+    left: 109px;
+  }
