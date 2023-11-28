56a7c2b0c7b19cc3ef50cdcf4cde3f091ecc3335
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-11-28 08:32:03 +0300

    Create step9.79.js

---
+++ step9.79.js
@@ -0,0 +1,9 @@
+/*Step 79
+Use a class selector to target the cat-whisker-right-middle element, then move the right middle whisker into position with a position of absolute, a top of 127px, and a left of 109px.*/
+
+
+.cat-whisker-right-middle{
+    position: absolute;
+    top: 127px;
+    left: 109px;
+  }
