3895be3243a661c6de9202c06e7fa94cbbd7eda5
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-11-29 10:55:58 +0300

    Create step9.81.js

---
+++ step9.81.js
@@ -0,0 +1,10 @@
+/*Step 81
+Use class selector to target the .cat-whisker-right-bottom element, then move it into place with a position of absolute, a top of 134px, and a left of 109px.*/
+
+
+
+.cat-whisker-right-bottom{
+    position: absolute;
+    top: 134px;
+    left: 109px;
+      }
