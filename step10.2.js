68a71812d3421903c3e4e9c63efc735640656389
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-11-30 09:37:03 +0300

    Create step10.2.js

---
+++ step10.2.js
@@ -0,0 +1,7 @@
+/*Step 2
+Add two meta tags, one to optimize your page for mobile devices, and one to specify an accepted charset for the page.*/
+
+
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="UTF-8"></meta>
