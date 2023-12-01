b7d341df3c7250598e252a1b03ffcd2f01d7bf4d
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-12-01 09:53:17 +0300

    Create step10.6.js

---
+++ step10.6.js
@@ -0,0 +1,15 @@
+/*Step 6Passed
+Remember that a class attribute can have multiple values. To separate your white keys from your black keys, you'll add a second class value of black--key. Add this to your second, third, fifth, sixth, and seventh .key elements.*/
+
+
+<div id="piano">
+      <div class="keys">
+        <div class="key"></div>
+        <div class="key black--key"></div>
+        <div class="key black--key"></div>
+        <div class="key"></div>
+        <div class="key black--key"></div>
+        <div class="key black--key"></div>
+        <div class="key black--key"></div>
+      </div>
+    </div>
