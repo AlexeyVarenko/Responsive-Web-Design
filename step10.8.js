1938653efce3553e4342341fcb10c4009da50d4b
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-12-02 12:02:46 +0300

    Create step10.8.js

---
+++ step10.8.js
@@ -0,0 +1,8 @@
+/*Step 8
+Add a link element within your head element. For that link element, set the rel attribute to stylesheet and the href to ./styles.css.*/
+
+
+<meta charset="UTF-8" />
+    <title>Piano</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./styles.css"></link>
