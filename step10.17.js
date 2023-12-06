286471a9153aef279709ae779501f7ec5d614f6c
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-12-06 16:22:56 +0300

    Create step10.17.js

---
+++ step10.17.js
@@ -0,0 +1,12 @@
+/*Step 17
+Give the .key a margin of 2px and a float property set to left.*/
+
+
+.key {
+    background-color: #ffffff;
+    position: relative;
+    width: 41px;
+    height: 175px;
+    margin: 2px;
+    float: left;
+  }
