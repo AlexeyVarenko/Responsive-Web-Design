391332cd7ebd7d452f82f1e74103b7144fb2e761
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-12-07 08:23:30 +0300

    Create step10.22.js

---
+++ step10.22.js
@@ -0,0 +1,10 @@
+/*Step 22
+Start styling the logo by creating a .logo selector. Set the width to 200px, a position of absolute and a top set to 23px.*/
+
+
+
+.logo{
+    width:200px;
+    position: absolute;
+    top: 23px;
+  }
