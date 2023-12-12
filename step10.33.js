6e354ee52e5b0fb99e257a8811c60702d1570b73
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-12-12 08:13:07 +0300

    Create step10.33.js

---
+++ step10.33.js
@@ -0,0 +1,15 @@
+/*Step 33
+For the new @media rule, set the width of the #piano to 675px and the width of the .keys to 633px.
+
+With that, your piano is complete!*/
+
+
+
+@media (max-width: 1199px) and (min-width: 769px) {
+    #piano{
+      width: 675px;
+    }
+    .keys{
+      width: 633px;
+    }
+   }
