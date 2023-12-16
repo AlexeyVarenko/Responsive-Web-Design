e6da3120755ad63408ad644883f4f5af2f44383b
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-12-16 08:27:37 +0300

    Create step11.13.js

---
+++ step11.13.js
@@ -0,0 +1,11 @@
+/*Step 13
+Center the parts of your building by turning the .bb1 element into a flexbox parent. Use the flex-direction and align-items properties to center the children.*/
+
+
+.bb1 {
+    width: 10%;
+    height: 70%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
