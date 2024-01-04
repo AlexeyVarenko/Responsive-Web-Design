20abedb6022518e8574b8e7e107c851f34f629ce
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-01-04 10:27:07 +0300

    Create step11.57.js

---
+++ step11.57.js
@@ -0,0 +1,9 @@
+/*Step 57
+Remove the margin and border-top properties and values from .bb2a to turn it into a triangle for the top of the building.*/
+
+
+.bb2a {
+    border-bottom: 1vw solid #000;
+    border-left: 5vw solid transparent;
+    border-right: 5vw solid transparent;
+  }
