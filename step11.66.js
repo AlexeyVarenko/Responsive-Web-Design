cbe8f8b09bdd4cf51d462d0cd85b70f190e819b5
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-01-08 07:52:52 +0300

    Create step11.66.js

---
+++ step11.66.js
@@ -0,0 +1,17 @@
+/*Step 66
+Move the display, flex-direction, and align-items properties and values from .bb1 to the new building-wrap class.*/
+
+
+.building-wrap {
+    width: 10%;
+   height: 70%;
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+ }
+ 
+ /* BACKGROUND BUILDINGS - "bb" stands for "background building" */
+ .bb1 {
+   width: 10%;
+   height: 70%;
+ }
