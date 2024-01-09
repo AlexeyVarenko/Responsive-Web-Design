32bf8d8a8270848b619273ccf09383f8543fbd39
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-01-09 08:12:59 +0300

    Create step11.69.js

---
+++ step11.69.js
@@ -0,0 +1,11 @@
+/*Step 69
+Nest four new div elements within .bb4c, give them all the class of bb4-window. These will be windows for this building.*/
+
+
+<div class="bb4c">
+          <div class="bb4-window"></div>
+          <div class="bb4-window"></div>
+          <div class="bb4-window"></div>
+          <div class="bb4-window"></div>
+          
+        </div>
