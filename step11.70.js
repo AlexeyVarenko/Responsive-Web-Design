99980d8a5fba392bd3278b0689eebadeb4e4f01a
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-01-10 09:27:09 +0300

    Create step11.70.js

---
+++ step11.70.js
@@ -0,0 +1,11 @@
+/*Step 70
+Give the bb4-window class a width of 18%, a height of 90%, and add your --window-color4 variable as the background-color.*/
+
+
+
+
+.bb4-window{
+    width: 18%;
+    height: 90%;
+    background-color: var(--window-color4);
+  }
