8b2f6abb639eac6f451d2329332ff2b106e5e46f
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-01-10 09:27:13 +0300

    Create step11.72.js

---
+++ step11.72.js
@@ -0,0 +1,10 @@
+/*Step 72
+Add the new window-wrap class to the .bb4c element.*/
+
+
+<div class="bb4c window-wrap">
+          <div class="bb4-window"></div>
+          <div class="bb4-window"></div>
+          <div class="bb4-window"></div>
+          <div class="bb4-window "></div>
+        </div>
