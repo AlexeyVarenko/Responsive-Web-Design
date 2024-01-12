63cbb244e07696b698a077e452c9315470487996
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-01-12 12:21:55 +0300

    Create step11.76.js

---
+++ step11.76.js
@@ -0,0 +1,15 @@
+/*Step 76
+Move the background-color property and value from .fb1 to .fb1b.*/
+
+
+
+.fb1 {
+    width: 10%;
+    height: 60%;
+  }
+  
+  .fb1b {
+    width: 60%;
+    height: 10%;
+    background-color: var(--building-color4);
+  }
