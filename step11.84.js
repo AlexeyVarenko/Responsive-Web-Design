282e24e6307af773057a45971c96b29cddc65723
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-01-15 13:19:38 +0300

    Create step11.84.js

---
+++ step11.84.js
@@ -0,0 +1,9 @@
+/*Step 84
+Add your window-wrap class to .fb2b to position the new window elements.*/
+
+
+<div class="fb2b window-wrap">
+          <div class="fb2-window"></div>
+          <div class="fb2-window"></div>
+          <div class="fb2-window"></div>
+        </div>
