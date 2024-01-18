6d452f1d6d792d052740365e0b41bb22b8ba1038
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-01-18 11:31:17 +0300

    Create step11.94.js

---
+++ step11.94.js
@@ -0,0 +1,9 @@
+/*Step 94
+Add your window-wrap class to the .fb3a element to center and space the windows.*/
+
+
+<div class="fb3a window-wrap">
+          <div class="fb3-window"></div>
+          <div class="fb3-window"></div>
+          <div class="fb3-window"></div>
+        </div>
