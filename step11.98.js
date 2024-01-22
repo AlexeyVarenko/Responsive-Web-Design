4d074927ea79c96e7acf17569ae46ff68f882c52
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-01-22 09:52:00 +0300

    Create step11.98.js

---
+++ step11.98.js
@@ -0,0 +1,17 @@
+/*Step 98
+Add your --building-color1 variable as value of the background-color property of .fb4b. Then, remove the background-color from .fb4.*/
+
+
+
+.fb4 {
+    width: 8%;
+    height: 45%;
+    position: relative;
+    left: 10%;
+  }
+  
+  .fb4b {
+    width: 100%;
+    height: 89%;
+    background-color: var(--building-color1);
+  }
