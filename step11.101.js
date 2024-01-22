32e6812e7e9c80e3cbd367fc22f5585c6f29c720
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-01-22 09:51:54 +0300

    Create step11.101.js

---
+++ step11.101.js
@@ -0,0 +1,11 @@
+/*Step 101
+Fill in the windows with your secondary color for this building. Also add a margin of 10% to give the windows some space.*/
+
+
+.fb4-window {
+    width: 30%;
+    height: 10%;
+    border-radius: 50%;
+    margin: 10%;
+    background-color: var(--window-color1);
+  }
