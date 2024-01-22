e3dd65beced710b875d66c36b363df26d832d7ea
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-01-22 09:52:02 +0300

    Create step11.99.js

---
+++ step11.99.js
@@ -0,0 +1,13 @@
+/*Step 99
+Nest six div elements within .fb4b and give them all a class of fb4-window.*/
+
+
+
+<div class="fb4b">
+          <div class="fb4-window"></div>
+          <div class="fb4-window"></div>
+          <div class="fb4-window"></div>
+          <div class="fb4-window"></div>
+          <div class="fb4-window"></div>
+          <div class="fb4-window"></div>
+        </div>
