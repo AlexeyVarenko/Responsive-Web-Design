c3fedada5b7a64dbfee5a51b433462ab1eec7b1e
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-01-24 12:54:48 +0300

    Create step11.109.js

---
+++ step11.109.js
@@ -0,0 +1,22 @@
+/*Step 109
+You can remove the background-color for this building now, since it isn't needed.*/
+
+
+
+.fb6 {
+    width: 9%;
+    height: 38%;
+    background: repeating-linear-gradient(
+        90deg,
+        var(--building-color3),
+        var(--building-color3) 10%,
+        transparent 10%,
+        transparent 30%
+      ),
+      repeating-linear-gradient(
+        var(--building-color3),
+        var(--building-color3) 10%,
+        var(--window-color3) 10%,
+        var(--window-color3) 30%
+      );
+  }
