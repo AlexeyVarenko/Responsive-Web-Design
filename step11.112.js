41b12230b0e6bd849ae27ecce375803ce0406ecb
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-01-25 11:18:10 +0300

    Create step11.112.js

---
+++ step11.112.js
@@ -0,0 +1,13 @@
+/*Step 112
+Give the sky class a radial-gradient. Use #ffcf33 from 0% to 20%, #ffff66 at 21%, and #bbeeff at 100%. This will add circular gradient to the background that will be your sun.*/
+
+
+
+.sky{
+    background: radial-gradient(
+          #ffcf33,
+          #ffcf33 20%,
+          #ffff66 21%,
+          #bbeeff 100%
+        )
+  }
