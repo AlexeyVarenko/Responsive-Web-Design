5a74c9b822fc886cac21ad8fa69f690e9f0e6419
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-01-29 10:35:44 +0300

    Create step12.2.js

---
+++ step12.2.js
@@ -0,0 +1,13 @@
+/*Add a title element with the text Magazine, a link element for the https://fonts.googleapis.com/css?family=Anton%7CBaskervville%7CRaleway&display=swap font stylesheet, a link for the https://use.fontawesome.com/releases/v5.8.2/css/all.css FontAwesome stylesheet, and a link for your ./styles.css stylesheet.
+
+Your font stylesheet will load three separate fonts: Anton, Baskervville, and Raleway.*/
+
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Magazine</title>
+    <link href="https://fonts.googleapis.com/css?family=Anton%7CBaskervville%7CRaleway&display=swap" rel="stylesheet"></link>
+     <link href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" rel="stylesheet"></link>
+     <link href="./styles.css" rel="stylesheet"></link>
+  </head>
