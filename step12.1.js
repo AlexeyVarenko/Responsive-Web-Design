62cd0871eb8f909b1706de582843d1ca040e0b79
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-01-29 10:35:42 +0300

    Create step12.1.js

---
+++ step12.1.js
@@ -0,0 +1,16 @@
+/*Step 1
+Begin with your standard HTML boilerplate. Add a DOCTYPE declaration, an html element specifying this page is in English, a head element, and a body element.
+
+Add a <meta> tag with the appropriate charset and a <meta> tag for mobile responsiveness within the head element.*/
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+ <meta charset="UTF-8"></meta>
+</head>
+<body>
+</body>
+</html>
