2386d52de8d0654dd4b4690098a8e544253dd2dd
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-01-30 10:39:24 +0300

    Create step12.6.js

---
+++ step12.6.js
@@ -0,0 +1,24 @@
+/*Step 6
+Your image currently takes up a lot of space. To better see what you are working on, add a width attribute to the img element, with a value of 400.
+
+You will remove this later on when you have worked on the CSS.*/
+
+
+
+<main>
+      <section class="heading">
+        <header class="hero">
+          <img width="400"
+            src="https://cdn.freecodecamp.org/platform/universal/fcc_meta_1920X1080-indigo.png"
+            alt="freecodecamp logo"
+            loading="lazy"
+            class="hero-img"
+          />
+          <h1 class="hero-title">OUR NEW CURRICULUM</h1>
+          <p class="hero-subtitle">
+            Our efforts to restructure our curriculum with a more project-based
+            focus
+          </p>
+        </header>
+      </section>
+    </main>
