68e83b90bfff1c442c68c606023205ba218b5b75
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-01-31 12:50:02 +0300

    Create step12.8.js

---
+++ step12.8.js
@@ -0,0 +1,16 @@
+/*Step 8
+The Referer HTTP header contains information about the address or URL of a page that a user might be visiting from. This information can be used in analytics to track how many users from your page visit freecodecamp.org, for example. Setting the rel attribute to noreferrer omits this information from the HTTP request. Give your a element a rel attribute set to noreferrer.*/
+
+
+
+<div class="author">
+          <p class="author-name">
+            By
+            <a href="https://freecodecamp.org" target="_blank" rel="noreferrer"
+              >freeCodeCamp</a
+            >
+          </p>
+          <p class="publish-date">March 7, 2019</p>
+        </div>
+      </section>
+    </main>
