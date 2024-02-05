132e7b2c467a4e0167c90f56dbfbae5efe99e04b
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-02-05 11:38:28 +0300

    Create step12.21.js

---
+++ step12.21.js
@@ -0,0 +1,11 @@
+/*Step 21
+Within your aside element, create two img elements, a blockquote element, and a third img element. Give the blockquote element a class set to image-quote./
+
+
+
+<aside class="image-wrapper">
+          <img></img>
+          <img></img>
+          <blockquote class="image-quote"></blockquote>
+           <img></img>
+        </aside>
