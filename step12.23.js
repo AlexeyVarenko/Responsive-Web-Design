4befcbd3fbe8649fbb00d39fc9b7fe30d2083fe4
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-02-06 12:13:00 +0300

    Create step12.23.js

---
+++ step12.23.js
@@ -0,0 +1,24 @@
+/*Step 23
+Within your .image-wrapper element, give the second img element a src of https://cdn.freecodecamp.org/testable-projects-fcc/images/calc.png, an alt of image of a calculator project, a loading attribute set to lazy, a class set to image-2, a width attribute set to 400, and a height attribute set to 400.*/
+
+
+
+<aside class="image-wrapper">
+          <img
+            src="https://cdn.freecodecamp.org/testable-projects-fcc/images/random-quote-machine.png"
+            alt="image of the quote machine project"
+            loading="lazy"
+            class="image-1"
+            width="600"
+            height="400"
+          />
+          <img
+          src="https://cdn.freecodecamp.org/testable-projects-fcc/images/calc.png"
+            alt="image of a calculator project"
+            loading="lazy"
+            class="image-2"
+            width="400"
+            height="400" />
+          <blockquote class="image-quote"></blockquote>
+          <img />
+        </aside>
