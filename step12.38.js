02e29c4efccd0ead1c9a63d6b87da0dc786e7c2d
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-02-12 07:47:56 +0300

    Create step12.38.js

---
+++ step12.38.js
@@ -0,0 +1,11 @@
+/*Step 38
+For additional control over the layout of your content, you can have a CSS Grid within a CSS Grid.
+
+Set the display property of your .heading selector to grid.*/
+
+
+
+.heading {
+    grid-column: 2 / 3;
+    display: grid;
+  }
