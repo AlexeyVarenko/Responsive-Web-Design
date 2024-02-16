5fd690099b45a9dcc6573d6d7712b430f9e9e977
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-02-16 09:33:48 +0300

    Create step12.50.js

---
+++ step12.50.js
@@ -0,0 +1,9 @@
+/*Step 50
+Create a .social-icons selector. Give it a display property set to grid, and a font-size property set to 3rem.*/
+
+
+
+.social-icons{
+    display: grid;
+    font-size: 3rem;
+  }
