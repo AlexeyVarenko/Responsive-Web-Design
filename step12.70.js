5052d53e6f9e72cf1017985f4b3f0fd63cd850ac
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-02-24 10:11:31 +0300

    Create step12.70.js

---
+++ step12.70.js
@@ -0,0 +1,6 @@
+/*Step 70
+Create a .lists li rule to target the list items within your .lists element. Give it a margin-bottom property set to 1.5rem.*/1.5rem
+
+.lists li{
+    margin-bottom: 1.5rem;
+  }
