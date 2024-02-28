20df433f535cb9e17f93d26a219fe3a5dafccb8b
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-02-28 07:51:07 +0300

    Create step13.1.js

---
+++ step13.1.js
@@ -0,0 +1,20 @@
+/*Step 1
+Begin with the standard boilerplate. Add your DOCTYPE declaration, your html element with the language set to English, your head and body elements.
+
+Add your meta element for the correct charset, your title element, and a link element for the ./styles.css file.
+
+Set the title to Ferris Wheel.*/
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+ <meta charset="UTF-8"></meta>
+ <title>Ferris Wheel</title>
+ <link href="styles.css" rel="stylesheet"></link>
+</head>
+<body>
+  
+</body>
+</html>
