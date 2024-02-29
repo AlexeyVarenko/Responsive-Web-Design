d053a0328e07125ad0183d00f23aa13242821e95
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-02-29 13:56:26 +0300

    Create step13.4.js

---
+++ step13.4.js
@@ -0,0 +1,12 @@
+/*Step 4
+Set the position property of the .wheel selector to absolute. Set the height and width both to 55vw.*/
+
+
+.wheel {
+    border: 2px solid black;
+    border-radius: 50%;
+    margin-left: 50px;
+    height: 55vw;
+    width: 55vw;  
+    position: absolute;
+  }
