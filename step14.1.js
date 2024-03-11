54ff5583d6af62a462f57d42e8b1f6e07b40852a
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-03-11 07:42:09 +0300

    Create step14.1.js

---
+++ step14.1.js
@@ -0,0 +1,18 @@
+/*Step 1
+You will be building a happy Flappy Penguin, and further exploring CSS transforms and animations in the process.
+
+Begin with your basic HTML boilerplate. Include the DOCTYPE declaration, html element with a language set to English, the appropriate meta tags, a head, body, and title element. Also, link your stylesheet to the page.*/
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+ <meta charset="UTF-8"></meta>
+ <title>Ferris Wheel</title>
+ <link href="styles.css" rel="stylesheet"></link>
+</head>
+<body>
+  
+</body>
+</html>
