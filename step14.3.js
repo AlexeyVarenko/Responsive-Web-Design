c2abcdc6a53a36108cfa7938a806bff36b2be69b
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-03-12 08:48:31 +0300

    Create step14.3.js

---
+++ step14.3.js
@@ -0,0 +1,9 @@
+/*Step 3
+Normalise your page's sizing, by removing the body element's margin and padding.*/
+
+
+body {
+    background: linear-gradient(45deg, rgb(118, 201, 255), rgb(247, 255, 222));
+    margin: 0;
+    padding: 0;
+  }
