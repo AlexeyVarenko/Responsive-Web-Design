d48f44f0b639d958ff57225e4d06ec5195d151ac
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-03-13 08:54:40 +0300

    Create step14.8.js

---
+++ step14.8.js
@@ -0,0 +1,9 @@
+/*Step 8
+Give the .ground element a background with a linear gradient angled 90 degrees clockwise, starting at rgb(88, 175, 236) and ending at rgb(182, 255, 255).*/
+
+
+.ground {
+    width: 100vw;
+    height: 400px;
+    background: linear-gradient(90deg, rgb(88, 175, 236), rgb(182, 255, 255));
+  }
