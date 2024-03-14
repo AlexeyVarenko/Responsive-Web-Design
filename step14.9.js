3ce23cc7bcd23b6f630ba1cdc86d2632e6ebbb30
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-03-14 08:54:55 +0300

    Create step14.9.js

---
+++ step14.9.js
@@ -0,0 +1,12 @@
+/*Step 9
+As the .ground element will be third in the stacking context of the page layout, set its z-index to 3, and position to absolute.*/
+
+
+
+.ground {
+    width: 100vw;
+    height: 400px;
+    background: linear-gradient(90deg, rgb(88, 175, 236), rgb(182, 255, 255));
+    z-index: 3;
+    position: absolute;
+  }
