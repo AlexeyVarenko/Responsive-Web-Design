f4cc1250636e27110b2c3162d3ea62c12d923924
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-03-14 08:54:51 +0300

    Create step14.10.js

---
+++ step14.10.js
@@ -0,0 +1,9 @@
+/*Step 10
+Above the .ground element, add a div with a class of penguin. This div will contain Flappy Penguin.*/
+
+
+
+<body>
+    <div class="penguin"></div>
+    <div class="ground"></div>
+  </body>
