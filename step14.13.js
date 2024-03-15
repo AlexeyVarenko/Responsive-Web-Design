84207d0ae3334ef62dc18b2848715320e55a7ee1
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-03-15 08:40:44 +0300

    Create step14.13.js

---
+++ step14.13.js
@@ -0,0 +1,12 @@
+/*Step 13
+To create some scenery in the background, you will add two mountains.
+
+Above the .penguin element, add a div with a class of left-mountain.*/
+
+
+
+<body>
+    <div class="left-mountain"></div>
+    <div class="penguin"></div>
+    <div class="ground"></div>
+  </body>
