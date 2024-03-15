64725d1f546dbf62b4afba923a6f28d767063655
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-03-15 08:40:45 +0300

    Create step14.14.js

---
+++ step14.14.js
@@ -0,0 +1,9 @@
+/*Step 14
+Target the .left-mountain element, and set its width and height to 300px. Then, set the background to a linear gradient starting at rgb(203, 241, 228) and ending at rgb(80, 183, 255).*/
+
+
+.left-mountain{
+    width: 300px;
+    height: 300px;
+    background: linear-gradient(rgb(203, 241, 228), rgb(80, 183, 255)); 
+  }
