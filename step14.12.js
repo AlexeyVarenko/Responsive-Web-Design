449ac82cea451ab9071599a0f8f1809508ae97ce
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-03-15 08:40:41 +0300

    Create step14.12.js

---
+++ step14.12.js
@@ -0,0 +1,11 @@
+/*Step 12
+Use the margin property to horizontally center the .penguin element, and set the margin-top to 75px.*/
+
+
+
+.penguin {
+    width: 300px;
+    height: 300px;
+    margin: auto;
+    margin-top: 75px;
+  }
