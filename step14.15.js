f32775187cd5f20113a3e7d38d423bb96d9ffb07
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-03-18 08:55:35 +0300

    Create step14.15.js

---
+++ step14.15.js
@@ -0,0 +1,11 @@
+/*Step 15
+To prevent the mountain from pushing the .ground element, adjust its position to prevent it from taking up space in the page layout.*/
+
+
+
+.left-mountain {
+    width: 300px;
+    height: 300px;
+    background: linear-gradient(rgb(203, 241, 228), rgb(80, 183, 255));
+    position: absolute;
+  }
