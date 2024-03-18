13b9720a09d669436136c5ccf46d1c9fdafa7896
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-03-18 08:55:26 +0300

    Create step14.20.js

---
+++ step14.20.js
@@ -0,0 +1,10 @@
+/*Step 20
+Target the .back-mountain element, and set its width and height to 300px. Then, set the background to a linear gradient starting at rgb(203, 241, 228) and ending at rgb(47, 170, 255).*/
+
+
+
+.back-mountain{
+    width: 300px;
+    height: 300px;
+    background: linear-gradient(rgb(203, 241, 228), rgb(47, 170, 255));
+  }
