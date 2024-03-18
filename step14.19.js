798b6015f7f75be6ef51abcae91603f80644f4a8
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-03-18 08:55:28 +0300

    Create step14.19.js

---
+++ step14.19.js
@@ -0,0 +1,10 @@
+/*Step 19
+To give the effect of a mountain range, add another mountain, by creating a new div immediately after .left-mountain, and give the new div the class of back-mountain.*/
+
+
+<body>
+    <div class="left-mountain"></div>
+    <div class="back-mountain"></div>
+    <div class="penguin"></div>
+    <div class="ground"></div>
+  </body>
