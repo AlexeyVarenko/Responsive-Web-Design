24adb0ba53f737c99f398b63017ad371b31ac735
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-03-18 08:55:31 +0300

    Create step14.17.js

---
+++ step14.17.js
@@ -0,0 +1,13 @@
+/*Step 17
+Set the stack level of the mountain element such that it remains directly behind the .ground element.*/
+
+
+
+.left-mountain {
+    width: 300px;
+    height: 300px;
+    background: linear-gradient(rgb(203, 241, 228), rgb(80, 183, 255));
+    position: absolute;
+    transform: skew(0deg, 44deg);
+    z-index: 2;
+  }
