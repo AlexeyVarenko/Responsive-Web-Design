67a5fa037383848fca1e8e30b76396537e67c4a0
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-03-19 11:02:40 +0300

    Create step14.23.js

---
+++ step14.23.js
@@ -0,0 +1,14 @@
+/*Step 23
+Rotate the .back-mountain element by 45deg clockwise. Then, give it a left property of 110px, and a top property of 225px.*/
+
+
+.back-mountain {
+    width: 300px;
+    height: 300px;
+    background: linear-gradient(rgb(203, 241, 228), rgb(47, 170, 255));
+    position: absolute;
+    z-index: 1;
+    transform: rotate(45deg);
+    left: 110px;
+    top: 225px;
+  }
