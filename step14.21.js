2594f49f63015a52c9e05bf6350ba015f409cf7c
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-03-19 11:02:36 +0300

    Create step14.21.js

---
+++ step14.21.js
@@ -0,0 +1,10 @@
+/*Step 21
+Set the position property of the .back-mountain to prevent it from taking up space in the page layout.*/
+
+
+.back-mountain {
+    width: 300px;
+    height: 300px;
+    background: linear-gradient(rgb(203, 241, 228), rgb(47, 170, 255));
+    position: absolute;
+  }
