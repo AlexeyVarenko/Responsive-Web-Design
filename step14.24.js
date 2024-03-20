2b43f5cae213279929f2ebab57b7f2c06ccaca15
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-03-20 10:42:29 +0300

    Create step14.24.js

---
+++ step14.24.js
@@ -0,0 +1,12 @@
+/*Step 24
+To finish the background, add a sun, by creating a new div element immediately after the .back-mountain element, and give it the class of sun.*/
+
+
+
+<body>
+<div class="left-mountain"></div>
+<div class="back-mountain"></div>
+<div class="sun"></div>
+<div class="penguin"></div>
+<div class="ground"></div>
+</body>
