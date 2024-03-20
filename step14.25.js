87369d09159f44e8cea07c671e6ea4b10e8b4a29
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-03-20 10:42:32 +0300

    Create step14.25.js

---
+++ step14.25.js
@@ -0,0 +1,9 @@
+/*Step 25
+Give the .sun element a width and height of 200px, and a background-color of yellow.*/
+
+
+.sun{
+    width: 200px;
+    height: 200px;
+    background-color: yellow;
+  } 
