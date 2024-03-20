68290895d922876219468f0dfa85acf0c3694ca2
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-03-20 10:42:33 +0300

    Create step14.26.js

---
+++ step14.26.js
@@ -0,0 +1,12 @@
+/*Step 26
+Set the position property of the sun to prevent it from taking up space in the page layout, and set the border-radius such that the sun's shape is a circle.*/
+
+
+
+.sun {
+    width: 200px;
+    height: 200px;
+    background-color: yellow;
+    position: absolute;
+    border-radius: 50%;
+  }
