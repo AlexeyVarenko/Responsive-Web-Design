4a3dca3186b18df25e1068ba1e004228c2218511
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-03-21 07:40:58 +0300

    Create step14.29.js

---
+++ step14.29.js
@@ -0,0 +1,12 @@
+/*Step 29
+Change the stack level of the .penguin element such that it appears in front of the .ground element, and give it a position of relative.*/
+
+
+.penguin {
+    width: 300px;
+    height: 300px;
+    margin: auto;
+    margin-top: 75px;
+    z-index: 4;
+    position: relative;
+  }
