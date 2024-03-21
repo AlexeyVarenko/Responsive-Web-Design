62b0a18c57278bb4b4b96d5df1564af768960a0c
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-03-21 07:40:57 +0300

    Create step14.28.js

---
+++ step14.28.js
@@ -0,0 +1,11 @@
+/*Step 28
+Your penguin will consist of two main sections: the head, and the body.
+
+Within .penguin, add two new div elements. The first with a class of penguin-head, and the second with a class of penguin-body.*/
+
+
+
+<div class="penguin">
+      <div class="penguin-head"></div>
+      <div class="penguin-body"></div>
+    </div>
