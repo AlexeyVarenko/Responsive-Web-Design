c8edd9ed7afe4bb4a587fe78eaa64990d14ca9ba
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-03-21 07:40:55 +0300

    Create step14.27.js

---
+++ step14.27.js
@@ -0,0 +1,15 @@
+/*Step 27
+Position the sun in the top right corner of the screen such that 75px of its top and right edges are off screen.*/
+
+
+
+.sun {
+    width: 200px;
+    height: 200px;
+    background-color: yellow;
+    position: absolute;
+    border-radius: 50%;
+    top: -75px;
+    right: -75px;
+    
+  }
