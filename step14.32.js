a2cfb64c9b04dd166c362aca4d1d725a26bb455b
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-03-22 07:49:26 +0300

    Create step14.32.js

---
+++ step14.32.js
@@ -0,0 +1,10 @@
+/*Step 32
+Target the .penguin-body element, and give it a width of 53%, and a height of 45%. Then, set the background to a linear gradient at 45deg, rgb(134, 133, 133) from 0%, rgb(234, 231, 231) from 25%, and white from 67%.*/
+
+
+
+.penguin-body{
+    width: 53%;
+    height: 45%;
+    background: linear-gradient(45deg, rgb(134, 133, 133) 0%, rgb(234, 231, 231) 25%, white 67%);
+  }
