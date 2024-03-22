73f35411907644a102a6f0f3772f38c46f833e18
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-03-22 07:49:23 +0300

    Create step14.30.js

---
+++ step14.30.js
@@ -0,0 +1,10 @@
+/*Step 30
+Target the .penguin-head element, and give it a width half of its parent's, and a height of 45%. Then, set the background to a linear gradient at 45deg starting at gray, and ending at rgb(239, 240, 228).*/
+
+
+
+.penguin-head{
+    width: 50%;
+    height: 45%;
+    background: linear-gradient(45deg, gray, rgb(239, 240, 228));
+  }
