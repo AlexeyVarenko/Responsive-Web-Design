68b8173348cfefa0cc93dfe6cac7c32580f58852
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-03-25 07:46:55 +0300

    Create step14.37.js

---
+++ step14.37.js
@@ -0,0 +1,18 @@
+/*Step 37
+Change the stack level of the .penguin-head element such that it appears in front of the .penguin-body element.*/
+
+
+
+.penguin-head {
+    width: 50%;
+    height: 45%;
+    background: linear-gradient(
+      45deg,
+      gray,
+      rgb(239, 240, 228)
+    );
+    border-radius: 70% 70% 65% 65%;
+    top: 10%;
+    left: 25%;
+    z-index: 1;
+  }
