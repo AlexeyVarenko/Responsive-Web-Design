6aa72028e699447392f55a1fa9bfe461e55d2de4
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-03-25 07:46:53 +0300

    Create step14.36.js

---
+++ step14.36.js
@@ -0,0 +1,18 @@
+/*Step 36
+Position the .penguin-body element 40% from the top, and 23.5% from the left of its parent.*/
+
+
+
+.penguin-body {
+    width: 53%;
+    height: 45%;
+    background: linear-gradient(
+      45deg,
+      rgb(134, 133, 133) 0%,
+      rgb(234, 231, 231) 25%,
+      white 67%
+    );
+    border-radius: 80% 80% 100% 100%;
+    top: 40%;
+    left: 23.5%;
+  }
