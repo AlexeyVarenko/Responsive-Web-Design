44545c8644fd08973f3d82eae3e534951029718e
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-03-27 09:20:09 +0300

    Create step14.44.js

---
+++ step14.44.js
@@ -0,0 +1,8 @@
+/*Step 44
+Start the penguin's face, by adding two div elements within .penguin-head, and giving them both a class of face.*/
+
+
+<div class="penguin-head">
+        <div class="face"></div>
+        <div class="face"></div>
+      </div>
