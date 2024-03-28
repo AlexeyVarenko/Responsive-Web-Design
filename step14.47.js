a8954abb4b1b9bb4ffbfb0984f0848f87f5ebc0b
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-03-28 07:47:49 +0300

    Create step14.47.js

---
+++ step14.47.js
@@ -0,0 +1,12 @@
+/*Step 47
+Position the .face elements so that they are 15% from the top.*/
+
+
+
+.face {
+  width: 60%;
+  height: 70%;
+  background-color: white;
+  border-radius: 70% 70% 60% 60%;
+  top: 15%;
+}
