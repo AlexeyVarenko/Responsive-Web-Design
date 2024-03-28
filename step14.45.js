5c9ad5b51e1a7b61c2a8183bd337c85c6f8d5828
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-03-28 07:47:45 +0300

    Create step14.45.js

---
+++ step14.45.js
@@ -0,0 +1,10 @@
+/*Step 45
+Give the .face elements a width of 60%, a height of 70%, and a background-color of white.*/
+
+
+
+.face{
+    width: 60%;
+    height: 70%;
+    background-color: white;
+  }
