8066b838c60da67d0d94c9e50e931d107c5dd017
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-03-28 07:47:47 +0300

    Create step14.46.js

---
+++ step14.46.js
@@ -0,0 +1,11 @@
+/*Step 46
+Make the top corners of the .face elements have a radius of 70%, and the bottom corners have a radius of 60%.*/
+
+
+
+.face {
+    width: 60%;
+    height: 70%;
+    background-color: white;
+    border-radius: 70% 70% 60% 60%;
+  }
