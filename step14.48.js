9e44faf4c185b6bd0d70b195503c1bd219d2469b
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-03-29 07:43:11 +0300

    Create step14.48.js

---
+++ step14.48.js
@@ -0,0 +1,11 @@
+/*Step 48
+Currently, the two .face elements are on top of each other.
+
+Fix this, by adding a class of left to the first .face element, and a class of right to the second .face element.*/
+
+
+
+<div class="penguin-head">
+        <div class="face left" ></div>
+        <div class="face right"></div>
+      </div>
