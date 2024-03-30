0a29a7f1fd23677eba4f4761c54cd4e38d600467
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-03-30 09:02:56 +0300

    Create step14.53.js

---
+++ step14.53.js
@@ -0,0 +1,13 @@
+/*Step 53
+Position the .chin element such that it is 25% from the top, and 5% from the left of its parent. Then, give the top corners a radius of 70%, and the bottom corners a radius of 100%.*/
+
+
+
+.chin {
+    width: 90%;
+    height: 70%;
+    background-color: white;
+    top: 25%;
+    left: 5%;
+    border-radius: 70% 70% 100% 100%;
+  }
