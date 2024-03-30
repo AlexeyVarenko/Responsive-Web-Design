cf6379e8de1d4c5acba159a3342079ddf37651e1
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-03-30 09:02:53 +0300

    Create step14.51.js

---
+++ step14.51.js
@@ -0,0 +1,10 @@
+/*Step 51
+Below the .face.right element, add a div element with a class of chin.*/
+
+
+
+<div class="penguin-head">
+        <div class="face left"></div>
+        <div class="face right"></div>
+        <div class="chin"></div>
+      </div>
