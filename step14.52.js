58da69a2c0b58922c51777ea3aaf50a02e83c194
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-03-30 09:02:54 +0300

    Create step14.52.js

---
+++ step14.52.js
@@ -0,0 +1,9 @@
+/*Step 52
+Target the .chin element, and give it a width of 90%, height of 70%, and background-color of white.*/
+
+
+.chin{
+    width: 90%;
+    height: 70%;
+    background-color: white;
+  }
