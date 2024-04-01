57e4314f1366e1a661ef7e7ce0c8c5c2520420e1
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-04-01 07:48:59 +0300

    Create step14.56.js

---
+++ step14.56.js
@@ -0,0 +1,13 @@
+/*Step 56
+Below the .chin element, add two div elements each with a class of eye. Also, give the first .eye element a class of left, and the second .eye element a class of right.*/
+
+
+
+<div class="penguin-head">
+<div class="face left"></div>
+<div class="face right"></div>
+<div class="chin"></div>
+<div class="eye left"></div>
+<div class="eye right"></div>
+
+</div>
