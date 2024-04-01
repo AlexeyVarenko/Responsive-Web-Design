a9b20516d8588b08205424e80bf4fc86834b5ad7
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-04-01 07:48:55 +0300

    Create step14.54.js

---
+++ step14.54.js
@@ -0,0 +1,10 @@
+/*Step 54
+So far, the .face and .chin elements have the same background-color.
+
+Create a custom CSS property called --penguin-face, and set it to white.*/
+
+
+
+:root {
+    --penguin-face:  white;
+  }
