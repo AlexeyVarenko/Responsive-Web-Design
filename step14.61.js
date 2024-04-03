0e922cf6958c2537e3e8f75d67581d13a15a7f45
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-04-03 07:52:16 +0300

    Create step14.61.js

---
+++ step14.61.js
@@ -0,0 +1,10 @@
+/*Step 61
+Target the .eye-lid elements, and give them a width of 150%, height of 100%, and background-color of --penguin-face.*/
+
+
+
+.eye-lid{
+    width: 150%;
+    height: 100%;
+    background-color: var(--penguin-face);
+  }
