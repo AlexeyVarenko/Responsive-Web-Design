1a5ac9e84e12a4bf4b45a9b4953afe477a8d75d5
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-04-03 07:52:24 +0300

    Create step14.57.js

---
+++ step14.57.js
@@ -0,0 +1,10 @@
+/*Step 57
+Target the .eye elements, and give them a width of 15%, height of 17%, and background-color of black.*/
+
+
+
+.eye{
+    width: 15%;
+    height: 17%;
+    background-color: black;
+  }
