b8bd34d7f4cc89d2a593355f7650bd1216d33732
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-04-03 07:52:17 +0300

    Create step14.60.js

---
+++ step14.60.js
@@ -0,0 +1,15 @@
+/*Step 60
+Within each .eye element, add a div with a class of eye-lid.*/
+
+
+<div class="penguin-head">
+        <div class="face left"></div>
+        <div class="face right"></div>
+        <div class="chin"></div>
+        <div class="eye left">
+          <div class="eye-lid"></div>
+        </div>
+        <div class="eye right">
+          <div class="eye-lid"></div>
+        </div>
+      </div>
