67d72653ad53a362231a48d77c9bfd99cf0d4fe4
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-04-03 07:52:22 +0300

    Create step14.58.js

---
+++ step14.58.js
@@ -0,0 +1,12 @@
+/*Step 58
+Position the .eye elements 45% from the top of their parent, and give all corners a radius of 50%.*/
+
+
+
+.eye {
+    width: 15%;
+    height: 17%;
+    background-color: black;
+    top: 45%;
+    border-radius: 50%;
+  }
