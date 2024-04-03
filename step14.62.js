ae6c7e9512f277f5d6253353e21ecf30af437a8a
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-04-03 07:52:13 +0300

    Create step14.62.js

---
+++ step14.62.js
@@ -0,0 +1,13 @@
+/*Step 62
+Position the .eye-lid elements 25% from the top, and -23% from the left of their parents. Then, give all corners a radius of 50%.*/
+
+
+
+.eye-lid {
+    width: 150%;
+    height: 100%;
+    background-color: var(--penguin-face);
+    top: 25%;
+    left: -23%;
+    border-radius: 50%;
+  }
