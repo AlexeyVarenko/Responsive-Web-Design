5b0ace0abf28b7c9177e30c5c86afc990e980162
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-04-04 07:37:45 +0300

    Create step14.64.js

---
+++ step14.64.js
@@ -0,0 +1,11 @@
+/*Step 64
+Target the .blush elements, and give them a width of 15%, height of 10%, and background-color of pink.*/
+
+
+
+
+.blush{
+    width: 15%;
+    height: 10%;
+    background-color: pink;
+  }
