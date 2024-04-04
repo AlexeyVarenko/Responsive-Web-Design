6b71e981dc04a014d68143c35e393b68a48abed8
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-04-04 07:37:43 +0300

    Create step14.63.js

---
+++ step14.63.js
@@ -0,0 +1,6 @@
+/*Step 63
+Below the .eye.right element, add two div elements each with a class of blush. Also, give the first .blush element a class of left, and the second .blush element a class of right.*/
+
+
+<div class="blush left"></div>
+<div class="blush right"></div>
