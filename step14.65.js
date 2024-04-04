646f3a165844ed653ef7cbdf5d0c2e37ba43dedf
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-04-04 07:37:47 +0300

    Create step14.65.js

---
+++ step14.65.js
@@ -0,0 +1,11 @@
+/*Step 65
+Position the .blush elements 65% from the top of their parent, and give all corners a radius of 50%.*/
+
+
+.blush {
+    width: 15%;
+    height: 10%;
+    background-color: pink;
+    top: 65%;
+    border-radius: 50%;
+  }
