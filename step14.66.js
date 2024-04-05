127c614e493e6eda44679f24e03ab10b1e2a60fc
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-04-05 11:47:00 +0300

    Create step14.66.js

---
+++ step14.66.js
@@ -0,0 +1,11 @@
+/*Step 66
+Target the .blush element with a class of left, and position it 15% left of its parent. Then, target the .blush element with a class of right, and position it 15% right of its parent.*/
+
+
+
+.blush.left{
+    left: 15%;
+    }
+    .blush.right{
+      right: 15%;
+    }
