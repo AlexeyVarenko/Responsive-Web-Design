82f965ded03186b87d7f6f52e835da4f26df27c5
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-04-05 11:47:02 +0300

    Create step14.67.js

---
+++ step14.67.js
@@ -0,0 +1,8 @@
+/*Step 67
+Below the .blush.right element, add two div elements each with a class of beak. Also, give the first .beak element a class of top, and the second .beak element a class of bottom.*/
+
+
+
+
+<div class="beak top"></div>
+<div class="beak bottom"></div>
