10607b411787bbdb77c4d5c43d1c88319251e4ac
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-04-05 11:47:03 +0300

    Create step14.68.js

---
+++ step14.68.js
@@ -0,0 +1,10 @@
+/*Step 68
+Target the .beak elements, and give them a height of 10%, background-color of orange, and give all corners a radius of 50%.*/
+
+
+
+.beak{
+    height: 10%;
+    background-color: orange;
+    border-radius: 50%;
+  }
