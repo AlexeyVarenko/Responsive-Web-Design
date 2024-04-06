b647deb868c1cf713d86b7cc994ffa7709a5970d
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-04-06 08:56:50 +0300

    Create step14.70.js

---
+++ step14.70.js
@@ -0,0 +1,16 @@
+/*Step 70
+Target the .beak element with a class of bottom, and give it a width 4% smaller than .beak.top, 5% further from the top, and 2% further from the left of its parent than .beak.top.*/
+
+
+
+.beak.top {
+  width: 20%;
+  top: 60%;
+  left: 40%;
+}
+.beak.bottom{
+  width: 16%;
+  top: 65%;
+  left: 42%;
+  
+}
