3bc62ea2847b4d6bedf16ee9cfd2fd82c63eb735
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-04-08 07:50:07 +0300

    Create step14.74.js

---
+++ step14.74.js
@@ -0,0 +1,12 @@
+/*Step 74
+Target the .shirt element, and set its font-size to 25px, font-family to Helvetica with a fallback of sans-serif, and font-weight to bold.*/
+
+
+
+
+.shirt{
+    font-size: 25px;
+    font-family: Helvetica, sans-serif;
+    font-weight: bold;
+  
+  }
