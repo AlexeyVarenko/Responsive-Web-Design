2f17e436cf0d6dcfcf3c156f1da4e88b56426094
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-04-08 07:50:06 +0300

    Create step14.73.js

---
+++ step14.73.js
@@ -0,0 +1,9 @@
+/*Step 73
+Within .shirt, after the div element, add a p element with the following content: I CSS*/
+
+
+
+<div class="shirt">
+        <div>💜</div>
+        <p>I CSS</p>
+      </div>
