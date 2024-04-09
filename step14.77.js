272179b1f5efe6bf8be236ecf04cd0e4c789bac9
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-04-09 11:38:59 +0300

    Create step14.77.js

---
+++ step14.77.js
@@ -0,0 +1,16 @@
+/*Step 77
+Position the .shirt element 165px from the top, and 127.5px from the left of its parent. Then, increase its stacking order such that it appears above the .penguin-body element.*/
+
+
+.shirt {
+    font: bold 25px Helvetica, sans-serif;
+    top: 165px;
+    left: 127.5px;
+    z-index: 1;
+  }
+  
+  .shirt div {
+    font-weight:  initial;
+    top: 22.5px;
+    left: 12px;
+  }
