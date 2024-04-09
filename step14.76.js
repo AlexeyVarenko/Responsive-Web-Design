d89e2655dfc16be11e17ebd72ce4014be4ce3bb8
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-04-09 11:38:58 +0300

    Create step14.76.js

---
+++ step14.76.js
@@ -0,0 +1,14 @@
+/*Step 76
+Position the div with the heart emoji 22.5px from the top, and 12px from the left of its parent.*/
+
+
+
+.shirt {
+    font: bold 25px Helvetica, sans-serif;
+  }
+  
+  .shirt div {
+    font-weight: initial;
+    top: 22.5px;
+    left: 12px;
+  }
