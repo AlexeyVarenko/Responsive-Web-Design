c83a85a46bac897b99e9dbfcb4c2e8b28f10fd71
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-04-10 07:40:15 +0300

    Create step14.80.js

---
+++ step14.80.js
@@ -0,0 +1,9 @@
+/*Step 80
+Target the .foot elements, and give them a width of 15%, height of 30%, and background-color of orange.*/
+
+
+.foot{
+    width: 15%;
+    height: 30%;
+    background-color: orange;
+  }
