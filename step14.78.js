601c4c7c3eb63889b95ed743c432a86910918575
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-04-10 07:40:12 +0300

    Create step14.78.js

---
+++ step14.78.js
@@ -0,0 +1,18 @@
+/*Step 78
+For the shirt's final touch, set the color to #6a6969.*/
+
+
+
+.shirt {
+    font: bold 25px Helvetica, sans-serif;
+    top: 165px;
+    left: 127.5px;
+    z-index: 1;
+    color: #6a6969;
+  }
+  
+  .shirt div {
+    font-weight:  initial;
+    top: 22.5px;
+    left: 12px;
+  }
