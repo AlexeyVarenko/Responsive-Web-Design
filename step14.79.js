8f5b636195f87d2e3c12089df321033a4568499f
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-04-10 07:40:14 +0300

    Create step14.79.js

---
+++ step14.79.js
@@ -0,0 +1,10 @@
+/*Step 79
+Fun fact: Penguins cannot stand without at least two feet.
+
+Within the .penguin-body element, add two div elements each with a class of foot. Give the first .foot a class of left, and the second .foot a class of right.*/
+
+
+<div class="penguin-body">
+        <div class="foot left"></div>
+        <div class="foot right"></div>
+      </div>
