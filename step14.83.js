8ae41669a694bf2c2d0aa7e74c7a8d82710e9dfb
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-04-11 07:55:01 +0300

    Create step14.83.js

---
+++ step14.83.js
@@ -0,0 +1,19 @@
+/*Step 83
+Target the .foot element with a class of left, and position it 25% left of its parent. Then, target the .foot element with a class of right, and position it 25% right of its parent.*/
+
+
+
+
+.foot {
+    width:  15%;
+    height: 30%;
+    background-color: var(--penguin-picorna);
+    top: 85%;
+    border-radius: 50%;
+  }
+  .foot.left{
+    left: 25%;
+  }
+  .foot.right{
+    right: 25%;
+  }
