cff281cf0430ebddc6b27d60978ccfb52152da9c
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-04-11 07:54:54 +0300

    Create step14.81.js

---
+++ step14.81.js
@@ -0,0 +1,11 @@
+/*Step 81
+Position the .foot elements 85% from the top of their parent, and give all corners a radius of 50%.*/
+
+
+.foot {
+    width:  15%;
+    height: 30%;
+    background-color: orange;
+    top: 85%;
+    border-radius: 50%;
+  }
