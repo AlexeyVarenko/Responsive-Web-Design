bb6deaf79e9a5de353921a6f8431a31684bd47ef
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-04-13 10:39:03 +0300

    Create step14.84.js

---
+++ step14.84.js
@@ -0,0 +1,15 @@
+/*Step 84
+To make the penguin's feet look more penguiny, rotate the left foot by 80deg, and the right by -80deg.*/
+
+
+
+
+.foot.left {
+    left: 25%;
+    transform: rotate(80deg);
+  }
+  
+  .foot.right {
+    right: 25%;
+    transform: rotate(-80deg);
+  }
