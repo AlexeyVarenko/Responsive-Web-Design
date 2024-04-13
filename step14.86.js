50b86dd0f084272cd4f9eea5654b5a2c424f45bf
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-04-13 10:39:08 +0300

    Create step14.86.js

---
+++ step14.86.js
@@ -0,0 +1,12 @@
+/*Step 86
+Fun fact: Penguins cannot fly without wings.
+
+Within .penguin-body, before the .foot elements, add two div elements each with a class of arm. Give the first .arm a class of left, and the second .arm a class of right.*/
+
+
+<div class="penguin-body">
+        <div class="arm left"></div>
+        <div class="arm right"></div>
+        <div class="foot left"></div>
+        <div class="foot right"></div>
+      </div>
