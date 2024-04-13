a5d8c7635906b1de43d69ae2a53b3182b7c97651
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-04-13 10:39:05 +0300

    Create step14.85.js

---
+++ step14.85.js
@@ -0,0 +1,13 @@
+/*Step 85
+Change the stacking order of the .foot elements such that they appear beneath the .penguin-body element.*/
+
+
+
+.foot {
+    width:  15%;
+    height: 30%;
+    background-color: var(--penguin-picorna);
+    top: 85%;
+    border-radius: 50%;
+    z-index: -1
+  }
