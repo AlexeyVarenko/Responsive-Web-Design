6acb803d4111b30e8e811e7adc702a26eccd4da6
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-04-15 09:34:26 +0300

    Create step14.89.js

---
+++ step14.89.js
@@ -0,0 +1,13 @@
+/*Step 89
+Target the .arm element with a class of left, and position it 35% from the top, and 5% from the left of its parent. Then, target the .arm element with a class of right, and position it 0% from the top, and -5% from the right of its parent.*/
+
+
+
+.arm.left{
+    top: 35%;
+    left: 5%;
+     }
+   .arm.right{
+     top: 0%;
+     right: -5%;
+     }
