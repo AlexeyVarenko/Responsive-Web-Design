0ed5e46b9b444e69ff84d3a678ef5fc2a45b1a45
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-04-15 09:34:30 +0300

    Create step14.87.js

---
+++ step14.87.js
@@ -0,0 +1,11 @@
+/*Step 87
+Target the .arm elements, and give them a width of 30%, a height of 60%, and a background of linear gradient at 90deg from clockwise, starting at gray, and ending at rgb(209, 210, 199).*/
+
+
+
+.arm{
+    width:30%;
+    height: 60%;
+    background: linear-gradient(90deg, gray, rgb(209, 210, 199));
+    
+  }
