e6c645493a9019a851e7afe37f924a9aca946821
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-04-16 09:28:53 +0300

    Create step14.90.js

---
+++ step14.90.js
@@ -0,0 +1,15 @@
+/*Step 90
+Within the .arm.left selector, alter the origin of the transform function to be the top left corner of its parent.*/
+
+
+
+.arm.left {
+    top: 35%;
+    left: 5%;
+    transform-origin: 0% 0%;
+  }
+  
+  .arm.right {
+    top: 0%;
+    right: -5%;
+  }
