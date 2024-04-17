3c3ef065b60cfa234098013f05456115560e55f8
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-04-17 08:48:28 +0300

    Create step14.96.js

---
+++ step14.96.js
@@ -0,0 +1,20 @@
+/*Step 96
+Give wave four waypoints starting at 10%, and incrementing by 10%.*/
+
+
+
+@keyframes wave {
+    10%{
+   
+    }
+    20%{
+      
+    }
+    30%{
+      
+    }
+    40%{
+      
+    }
+   
+   }
