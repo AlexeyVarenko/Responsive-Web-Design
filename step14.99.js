e22a6e70e8eebfdf19091ea27fddffed7c025c9a
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-04-18 09:17:15 +0300

    Create step14.99.js

---
+++ step14.99.js
@@ -0,0 +1,19 @@
+/*Step 99
+For the third and fourth waypoints, repeat the transform pattern once more.*/
+
+
+
+@keyframes wave {
+    10% {
+      transform: rotate(110deg) scaleX(-1);
+    }
+    20% {
+      transform: rotate(130deg) scaleX(-1);
+    }
+    30% {
+      transform: rotate(110deg) scaleX(-1);
+    }
+    40% {
+      transform: rotate(130deg) scaleX(-1);
+    }
+  }
