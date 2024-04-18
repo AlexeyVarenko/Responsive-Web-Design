fc3a01d2c032520c24c4d3d5ae70de2fd654d6d9
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-04-18 09:17:13 +0300

    Create step14.98.js

---
+++ step14.98.js
@@ -0,0 +1,19 @@
+/*Step 98
+Within the second waypoint, rotate to 130deg, and retain the scaling of the left arm.*/
+
+
+
+@keyframes wave {
+    10% {
+      transform: rotate(110deg) scaleX(-1);
+    }
+    20% {
+      transform: rotate(130deg) scaleX(-1);
+    }
+    30% {
+  
+    }
+    40% {
+  
+    }
+  }
