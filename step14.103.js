f27ff30ff6e8e6115ea68e345e020983a3e1dd00
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-04-20 08:40:41 +0300

    Create step14.103.js

---
+++ step14.103.js
@@ -0,0 +1,16 @@
+/*Step 103
+Change the .penguin element's transition behavior during transformation to have a duration of 1s, a timing function of ease-in-out, and a delay of 0ms.*/
+
+
+
+.penguin {
+    width: 300px;
+    height: 300px;
+    margin: auto;
+    margin-top: 75px;
+    z-index: 4;
+    position: relative;
+    transition-duration: 1s;
+    transition-timing-function: ease-in-out;
+    transition-delay: 0ms;
+  }
