c27b4e066824316308479c48ae9fc3e6284ce7cd
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-04-25 08:22:21 +0300

    Create step15.11.js

---
+++ step15.11.js
@@ -0,0 +1,7 @@
+/*Step 11
+You are now ready to declare your next variable. Remove both console.log statements, and the character reassignment.
+
+Also remove your secondCharacter variable, but leave the character initialization unchanged.*/
+
+
+let character = 'Hello';
