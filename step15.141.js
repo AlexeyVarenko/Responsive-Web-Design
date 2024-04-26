bc766c4f9a1c5c0c3adab45ef2fddb33526d1da9
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-04-26 10:51:18 +0300

    Create step15.141.js

---
+++ step15.141.js
@@ -0,0 +1,5 @@
+/*Step 14
+You can also perform subtraction (-), multiplication (*), and division (/). Feel free to experiment with the operators and numbers in your console.log. When you are ready to move on, remove the console.log.*/
+
+
+let count = 8;
