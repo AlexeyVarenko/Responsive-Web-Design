cb8bd71937f67e5ef4bbe5c75ec8c157a8b6413c
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-04-29 10:59:37 +0300

    Create step15.18.js

---
+++ step15.18.js
@@ -0,0 +1,13 @@
+/*Step 18
+Arrays are special in that they are considered mutable. This means you can change the value at an index directly.
+
+For example, this code would assign the number 25 to the second element in the array:
+
+let array = [1, 2, 3];
+array[1] = 25;
+Update the third element of your rows array to be the number 10. Then print the rows array to your console.*/
+
+
+
+rows[2]=10;
+console.log(rows);
