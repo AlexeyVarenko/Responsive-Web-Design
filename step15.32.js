74aa67aa1b4fc22b4fdfbef2270b09ffa0b209b1
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-05-03 07:51:16 +0300

    Create step15.32.js

---
+++ step15.32.js
@@ -0,0 +1,8 @@
+/*Step 32
+Your loop should now run eight times. Inside the body of the loop, print the value of the i iterator and see what happens.*/
+
+
+
+for (let i = 0; i < count; i = i + 1) {
+    console.log(i)
+    }
