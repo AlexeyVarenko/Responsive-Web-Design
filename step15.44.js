072bed22795d778657307c5e3895f6ed6ffffb4d
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-05-09 08:32:32 +0300

    Create step15.44.js

---
+++ step15.44.js
@@ -0,0 +1,8 @@
+/*Step 44
+You are calling your padRow function, but not doing anything with that function call. All functions in JavaScript return a value, meaning they provide the defined result of calling them for you to use elsewhere.
+
+To see the result of calling your padRow function, declare a call variable and assign your existing padRow call to that variable.*/
+
+
+
+const call=padRow();
