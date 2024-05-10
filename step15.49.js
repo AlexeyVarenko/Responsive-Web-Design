f8bd0d14c254383271ba29e4ef3ecb48960e9ca5
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-05-10 08:13:14 +0300

    Create step15.49.js

---
+++ step15.49.js
@@ -0,0 +1,14 @@
+/*Step 49
+If you open your console again, you'll see that your padRow function is returning undefined, even though you defined a return value! This is because parameters need to be given a value when you call the function.
+
+When you pass a value to a function call, that value is referred to as an argument. Here is an example of calling a demo function and passing "Naomi" as the argument for the name parameter.
+
+function demo(name) {
+  return name;
+}
+demo("Naomi");
+Pass your own name as the argument for the name parameter in your padRow call. Remember that your name is a string, so you'll need to use quotes.*/
+
+
+
+const call = padRow("Aliaksei");
