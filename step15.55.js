d1f547ff33cf1a1e0b0674f9be170d528a3019bb
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-05-13 12:52:11 +0300

    Create step15.55.js

---
+++ step15.55.js
@@ -0,0 +1,12 @@
+/*Step 55
+Because your function was no longer using the parameter, changing the value passed in the call did not affect it.
+
+Go ahead and remove the test declaration from your padRow function. Also, remove the return statement, so your function is empty again.*/
+
+
+
+function padRow() {
+
+}
+const call = padRow();
+console.log(call);
