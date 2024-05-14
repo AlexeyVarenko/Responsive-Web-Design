0e4403f16febc6f8541b7a8d5de4e8ef57f26f93
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-05-14 09:06:00 +0300

    Create step15.58.js

---
+++ step15.58.js
@@ -0,0 +1,15 @@
+/*Step 58
+Remember in an earlier step, you learned about return values. A function can return a value for your application to consume separately.
+
+In a function, the return keyword is used to specify a return value. For example, this function would return the value given to the first parameter:
+
+function name(parameter) {
+  return parameter;
+}
+Use the return keyword to return the value of the character variable, repeated rowNumber times.*/
+
+
+
+function padRow(rowNumber, rowCount) {
+    return character.repeat(rowNumber)
+  }
