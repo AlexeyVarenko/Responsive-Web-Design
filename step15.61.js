13a897e0df59c45e5d5a0e6cfb6279529a7a2cd6
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-05-15 09:03:50 +0300

    Create step15.61.js

---
+++ step15.61.js
@@ -0,0 +1,14 @@
+/*Step 61
+You should now see the same bunch of characters in your console. Your padRow function is doing the exact same thing you were doing earlier, but now it's in a reusable section of its own.
+
+Use the addition operator to concatenate a blank space " " to the beginning and end of your repeated character string.
+
+Remember that you can use the + operator to concatenate strings like this:
+
+" " + "string"*/
+
+
+
+function padRow(rowNumber, rowCount) {
+    return  " " +character.repeat(rowNumber)+ " ";
+  }
