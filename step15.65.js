f0f79f58d1e051bb1ee6580e9cdd4f07574d56a7
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-05-17 09:04:50 +0300

    Create step15.65.js

---
+++ step15.65.js
@@ -0,0 +1,10 @@
+/*Step 65
+Because you are only increasing i by 1, you can use the increment operator ++. This operator increases the value of a variable by 1, updating the assignment for that variable. For example, test would become 8 here:
+
+let test = 7;
+test++;
+Replace your addition assignment with the increment operator for your loop iteration.*/
+
+
+
+for (let i = 0; i < count; i ++) {
