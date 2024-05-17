887d64ce706083597ff8eef2817eb58203deb2b2
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-05-17 09:04:48 +0300

    Create step15.64.js

---
+++ step15.64.js
@@ -0,0 +1,11 @@
+/*Step 64
+Your pyramid generator now functions as expected. But this is an excellent opportunity to further explore the code you have written.
+
+The addition operator is not the only way to add values to a variable. The addition assignment operator can be used as shorthand to mean "take the original value of the variable, add this value, and assign the result back to the variable." For example, these two statements would yield the same result:
+
+test = test + 1;
+test += 1;
+Update your iterator statement in the for loop to use addition assignment.*/
+
+
+for (let i = 0; i < count; i +=1) {
