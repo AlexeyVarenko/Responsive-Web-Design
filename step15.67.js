abe814ffd8decccd483054d32788d46f627cf7d3
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-05-17 09:04:57 +0300

    Create step15.67.js

---
+++ step15.67.js
@@ -0,0 +1,10 @@
+/*Step 67
+The pyramid looks a little funny now. Because you are starting the loop at 1 instead of 0, you do not need to add one to i when you pass it to padRow.
+
+Update the first argument of your padRow call to be i.*/
+
+
+
+for (let i = 1; i < count; i++) {
+    rows.push(padRow(i, count));
+  }
