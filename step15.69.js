060e1ad06c0f22bfb25946c6604d6dddb3d62bdc
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-05-18 09:24:22 +0300

    Create step15.69.js

---
+++ step15.69.js
@@ -0,0 +1,13 @@
+/*Step 69
+Comments can be helpful for explaining why your code takes a certain approach, or leaving to-do notes for your future self.
+
+In JavaScript, you can use // to leave a single-line comment in your code.
+
+Add a single-line comment above your function to remind yourself to change the code to a different kind of loop*/
+
+
+
+//Add a single-line comment above your function to remind yourself to change the code to a different kind of loop.
+for (let i = 1; i <= count; i++) {
+    rows.push(padRow(i, count));
+  }
