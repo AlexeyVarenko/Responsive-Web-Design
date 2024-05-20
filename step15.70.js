21e383c59bd487b15374cff7e7e8e5a16ec8409b
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-05-20 09:10:45 +0300

    Update step15.70.js

---
+++ step15.70.js
@@ -0,0 +1,6 @@
+/*Step 70
+JavaScript also has support for multi-line comments. A multi-line comment starts with /* and ends with .
+
+Unlike a single-line comment, a multi-line comment will encapsulate multiple lines.
+
+Use /* and */ to turn your current for loop, including the body, into a multi-line comment.
