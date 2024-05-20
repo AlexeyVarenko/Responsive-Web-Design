003dc4270f0047a2260d222f8112c6c769e7e007
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-05-20 09:10:47 +0300

    Create step15.71.js

---
+++ step15.71.js
@@ -0,0 +1,15 @@
+/*Step 71
+Your pyramid has disappeared again. That's okay - that is to be expected.
+
+Before you create your new loop, you need to learn about if statements. An if statement allows you to run a block of code only when a condition is met. They use the following syntax:
+
+if (condition) {
+  logic
+}
+Create an if statement with the boolean true as the condition. In the body, print the string "Condition is true".*/
+
+
+
+f (true) {
+    console.log("Condition is true") 
+   }
