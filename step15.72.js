34e2cf499c5345df5230cbac30b1013cc25e9cd5
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-05-20 09:10:49 +0300

    Create step15.72.js

---
+++ step15.72.js
@@ -0,0 +1,10 @@
+/*Step 72
+You'll see the string printed in the console, because true is in fact true.
+
+Change the condition of your if statement to the boolean false.*/
+
+
+
+if (false) {
+    console.log("Condition is true");
+  }
