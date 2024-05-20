9104e680d00c9e9e2b89bbd21e611739c8f5b1d4
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-05-20 09:10:51 +0300

    Create step15.73.js

---
+++ step15.73.js
@@ -0,0 +1,10 @@
+/*Step 73
+Now the string is no longer printing, because false is not true. But what about other values?
+
+Try changing the condition to the string "false".*/
+
+
+
+if ("false") {
+    console.log("Condition is true");
+  }
