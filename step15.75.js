7c4ae8d9d9421aaa363adb7f5da9706bc50040e5
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-05-21 08:54:49 +0300

    Create step15.75.js

---
+++ step15.75.js
@@ -0,0 +1,9 @@
+/*Step 75
+The text is gone again! Empty strings evaluate to false, making them a falsy value. You will learn more about truthy and falsy values in future projects.
+
+For now, remove your if statement entirely. Use let to declare a continueLoop variable and assign it the boolean false. Then use let to declare a done variable and assign it the value 0.*/
+
+
+
+let continueLoop=false;
+let done=0;
