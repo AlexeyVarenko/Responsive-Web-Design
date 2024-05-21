7561e9aa653bcea1d8e6abf6c89b8e47c9fad11e
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-05-21 08:54:51 +0300

    Create step15.76.js

---
+++ step15.76.js
@@ -0,0 +1,14 @@
+/*Step 76
+A while loop will run over and over again until the condition specified is no longer true. It has the following syntax:
+
+while (condition) {
+  logic;
+}
+Use that syntax to declare a while loop with continueLoop as the condition. The body should be empty.*/
+
+
+let continueLoop = false;
+let done = 0;
+while (continueLoop) {
+ 
+}
