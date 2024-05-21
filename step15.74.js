b4ce225649b99ea889f3e952a333e4217a9a880e
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-05-21 08:54:48 +0300

    Create step15.74.js

---
+++ step15.74.js
@@ -0,0 +1,13 @@
+/*Step 74
+The text has appeared again! This is because "false" is a string, which when evaluated to a boolean becomes true. This means "false" is a truthy value.
+
+A truthy value is a value that is considered true when evaluated as a boolean. Most of the values you encounter in JavaScript will be truthy.
+
+A falsy value is the opposite - a value considered false when evaluated as a boolean. JavaScript has a defined list of falsy values. Some of them include false, 0, "", null, undefined, and NaN.
+
+Try changing your if condition to an empty string "", which is a falsy value.*/
+
+
+if ("") {
+    console.log("Condition is true");
+  }
