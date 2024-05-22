7d2d1acd6f255cd8683cc9a8e50f7f8b76ab8e81
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-05-22 09:57:14 +0300

    Create step15.77.js

---
+++ step15.77.js
@@ -0,0 +1,10 @@
+/*Step 77
+Right now, if you change continueLoop to true, your while loop will run forever. This is called an infinite loop, and you should be careful to avoid these. An infinite loop can lock up your system, requiring a full restart to escape.
+
+To avoid this, start by using the increment operator to increase the value of the done variable inside your loop.*/
+
+
+
+while (continueLoop) {
+    done++
+   }
