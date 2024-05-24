e234c152a51704cc4271e53920ffc2d33662aa69
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-05-24 09:36:32 +0300

    Create step15.80.js

---
+++ step15.80.js
@@ -0,0 +1,10 @@
+/*Step 80
+When done has reached the value of count, we want the loop to stop executing.
+
+Inside your if body, assign the boolean false to your continueLoop variable.*/
+
+
+
+if (done === count) {
+    continueLoop=false
+  }
