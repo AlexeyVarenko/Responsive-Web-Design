4ba01ad3c5a7ef30d11143dbf451a3ee32974552
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-05-27 13:52:45 +0300

    Create step15.88.js

---
+++ step15.88.js
@@ -0,0 +1,8 @@
+/*Step 88
+Now you no longer need your done variable. Remove the increment operation from your loop, and the variable declaration for done.*/
+
+
+while (rows.length < count) {
+  
+    rows.push(padRow(rows.length + 1, count));
+  }
