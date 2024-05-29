9360f8f978e35e1e7facf26e6eaf953d52cf892e
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-05-29 13:20:11 +0300

    Create step15.93.js

---
+++ step15.93.js
@@ -0,0 +1,10 @@
+/*Step 93
+Again, push the result of calling padRow with your i and count variables to your rows array.
+
+Open up the console to see the upside-down pyramid.*/
+
+
+
+for (let i = count; i > 0; i = i - 1) {
+    rows.push(padRow(i, count))
+  }
