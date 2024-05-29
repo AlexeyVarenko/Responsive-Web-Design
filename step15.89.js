f8dbf9ca64b13288711d11a1cb2aa2e338816ce2
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-05-29 13:20:04 +0300

    Create step15.89.js

---
+++ step15.89.js
@@ -0,0 +1,9 @@
+/*Step 89
+That's a very clean and functional loop. Nice work! But there's still more to explore.
+
+Use a multi-line comment to comment out your while loop.*/
+
+
+/*while (rows.length < count) {
+  rows.push(padRow(rows.length + 1, count));
+}*/
