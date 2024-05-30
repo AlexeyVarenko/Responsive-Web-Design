9d0c7e76cdb984510ad6fbc1cca35c83c6191cfc
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-05-30 09:50:56 +0300

    Create step15.99.js

---
+++ step15.99.js
@@ -0,0 +1,9 @@
+/*Step 99
+Because you are only subtracting one from i, you can use the decrement operator --.
+
+Replace your subtraction assignment with the decrement operator.*/
+
+
+for (let i = count; i > 0; i--) {
+    rows.push(padRow(i, count));
+  }
