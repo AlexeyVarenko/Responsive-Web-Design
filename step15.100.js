503063860e26042b77770299d2a1a4d63b96197d
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-05-30 09:51:38 +0300

    Create step15.100.js

---
+++ step15.100.js
@@ -0,0 +1,7 @@
+/*Step 100
+Use a multi-line comment to comment out this loop as well, to prepare for the next approach.*/
+
+
+/*for (let i = count; i > 0; i--) {
+  rows.push(padRow(i, count));
+}*/
