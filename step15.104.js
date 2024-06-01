ff58124a69a9065467731bd21938968ceb606ae6
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-06-01 09:03:58 +0300

    Create step15.104.js

---
+++ step15.104.js
@@ -0,0 +1,6 @@
+/*Step 104
+Now that you've tried these methods, you can do another inverted pyramid approach. But first you need to clean up your experimentation.
+
+Remove your numbers array, and the method calls and log calls.*/
+
+
