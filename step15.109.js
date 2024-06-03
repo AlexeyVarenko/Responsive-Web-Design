c19493ca5eb2d0fc660402b8076b8c7d5b5dee78
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-06-03 11:47:52 +0300

    Create step15.109.js

---
+++ step15.109.js
@@ -0,0 +1,10 @@
+/*Step 109
+Now move your .unshift() call into your if block.*/
+
+
+for (let i = 1; i <= count; i++) {
+    if (inverted) {
+      rows.unshift(padRow(i, count));
+    }
+    
+  }
