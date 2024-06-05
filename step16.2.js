4317143112cb52c7e2ab1436f091ab934ae16517
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-06-05 12:00:37 +0300

    Create step16.2.js

---
+++ step16.2.js
@@ -0,0 +1,12 @@
+/*Step 2
+Now you can start writing your JavaScript. Begin by creating a script element. This element is used to load JavaScript into your HTML file.
+
+<script>
+  // JavaScript code goes here
+</script>*/
+
+
+
+<script>
+  // JavaScript code goes here
+</script>
