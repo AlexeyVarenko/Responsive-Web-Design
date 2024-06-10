3759fa17b7c865b440554f252fe9595c25ad21af
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-06-10 07:48:35 +0300

    Create step16.10.js

---
+++ step16.10.js
@@ -0,0 +1,17 @@
+/*Step 10
+Declare two more variables named monsterHealth and inventory.
+
+For your inventory variable, assign it the value of an array containing the string "stick".
+
+Remember that you worked with arrays in the previous project like this:
+
+let exampleArray = ["first", "second", "third"];*/
+
+
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeaponIndex = 0;
+let fighting;
+let monsterHealth;
+let inventory=["stick"];
