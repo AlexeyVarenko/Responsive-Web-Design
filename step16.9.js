f90a19a394a94b332f8c4ac2908e64bcb61404ea
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-06-10 07:48:45 +0300

    Create step16.9.js

---
+++ step16.9.js
@@ -0,0 +1,10 @@
+/*Step 9
+Declare a variable called fighting but do not initialize it with a value.*/
+
+
+
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeaponIndex = 0;
+let fighting;
