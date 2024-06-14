b8c8ab07e02728217ff65a42c0f1afc2c7a85e3f
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-06-14 09:04:09 +0200

    Create step16.22.js

---
+++ step16.22.js
@@ -0,0 +1,9 @@
+/*Step 22
+Give the #text element a background-color of #0a0a23, a color of #ffffff, and 10px of padding on all sides.*/
+
+
+#text{
+    background-color: #0a0a23;
+    color: #ffffff;
+    padding: 10px;
+  }
