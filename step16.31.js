7f256b173f103dff9fdd98d584f811d5ff68dbd2
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-06-18 13:49:04 +0300

    Create step16.31.js

---
+++ step16.31.js
@@ -0,0 +1,14 @@
+/*Step 31
+In the previous project, you learned how to create a function like this:
+
+Example Code
+function functionName() {
+
+}
+Create an empty function named goStore.*/
+
+
+
+function goStore() {
+
+}
