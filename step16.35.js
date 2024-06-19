89c409cbbc03454f528931e654456d57f62a77dc
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-06-19 09:21:05 +0300

    Create step16.35.js

---
+++ step16.35.js
@@ -0,0 +1,15 @@
+/*Step 35
+In the previous project, you learned how to work with single line and multi-line comments like this:
+
+Example Code
+// I am a single-line comment
+
+/*
+  I am a multi-line comment
+*/
+Add a single-line comment that says initialize buttons.
+
+
+
+
+//initialize buttons
