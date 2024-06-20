0448e9aee7e3c6e80b9fb9675014308b27e84fdb
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-06-20 15:10:30 +0300

    Create step16.37.js

---
+++ step16.37.js
@@ -0,0 +1,10 @@
+/*Step 37
+Using the same syntax, set the onclick properties of button2 and button3 to goCave and fightDragon respectively.
+
+Once you have done that, open your console and try clicking the buttons on your project.*/
+
+
+// initialize buttons
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
