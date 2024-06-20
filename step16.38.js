fadce43f90cbd70d8bfbc7e8d57bf1362be7dd1e
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-06-20 15:10:34 +0300

    Create step16.38.js

---
+++ step16.38.js
@@ -0,0 +1,17 @@
+/*Step 38
+The innerText property controls the text that appears in an HTML element. For example:
+
+Example Code
+<p id="info">Demo content</p> 
+Example Code
+const info = document.querySelector("#info"); 
+info.innerText = "Hello World"; 
+The following example would change the text of the p element from Demo content to Hello World.
+
+When a player clicks your Go to store button, you want to change the buttons and text. Remove the code inside the goStore function and add a line that updates the text of button1 to say "Buy 10 health (10 gold)".*/
+
+
+
+function goStore() {
+    button1.innerText="Buy 10 health (10 gold)";
+  }
