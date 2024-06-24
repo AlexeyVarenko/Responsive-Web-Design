fe9b0f4aa7426406bdcb7b428144d72c2bc4b845
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-06-24 21:16:38 +0300

    Create step16.46.js

---
+++ step16.46.js
@@ -0,0 +1,18 @@
+/*Step 46
+You have repetition in the goTown and goStore functions. Repetition in your code is a sign that you need another function.
+
+In the previous project, you learned how to work with function parameters like this:
+
+Example Code
+function myFunction(param) {
+  console.log(param);
+}
+Function parameters act as placeholders for values that you pass to the function when you call it.
+
+Create an empty update function that takes a parameter called location.*/
+
+
+
+function update(location) {
+  
+}
