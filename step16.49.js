e02b556dd5e9fa8038d6cbc016ac0e7aef1eea18
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-06-26 08:31:23 +0300

    Create step16.49.js

---
+++ step16.49.js
@@ -0,0 +1,20 @@
+/*Step 49
+Objects are similar to arrays, except that instead of using indexes to access and modify their data, you access the data in objects through properties.
+
+Properties consist of a key and a value. The key is the name of the property, and the value is the data stored in the property.
+
+Here is an example of an object with a single property:
+
+Example Code
+const obj = {
+  name: "Quincy Larson"
+};
+Inside your cat object, add a new property. The key should be name and the value should be the string "Whiskers".
+
+Open up the console to see the updates to your object.*/
+
+
+
+const cat = {
+    name:"Whiskers"
+   }
