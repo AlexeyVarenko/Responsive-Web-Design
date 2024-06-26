f4390cd651539a01019304b5708def6b3ca966b4
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-06-26 08:31:20 +0300

    Create step16.47.js

---
+++ step16.47.js
@@ -0,0 +1,8 @@
+/*Step 47
+In your role playing game, you will be able to visit different locations like the store, the cave, and the town square. You will need to create a data structure that will hold the different locations.
+
+Use const to create a variable called locations and assign it an empty array.*/
+
+
+
+const locations=[]
