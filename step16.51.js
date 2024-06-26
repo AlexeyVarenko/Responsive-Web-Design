a8f972d2e7b5c4f5f93cfc07dbbdfb496c1203e1
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-06-26 08:31:30 +0300

    Create step16.51.js

---
+++ step16.51.js
@@ -0,0 +1,23 @@
+/*Step 51
+There are two ways to access the properties of an object: dot notation (.) and bracket notation ([]), similar to an array.
+
+Dot notation is what you use when you know the name of the property you're trying to access ahead of time.
+
+Example Code
+object.property;
+Here is a sample of using dot notation (.) to read the name property of the developer object:
+
+Example Code
+const developer = {
+  name: "Jessica",
+}
+
+// Output: Jessica
+console.log(developer.name); 
+Update your console statement to access the name property of the cat object using dot notation.
+
+Open up the console to see the name of "Whiskers" logged to the console.*/
+
+
+
+console.log(cat.name)
