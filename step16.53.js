8ba742616a6c554610d558f18b7d4415295eefb4
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-06-27 08:09:14 +0300

    Create step16.53.js

---
+++ step16.53.js
@@ -0,0 +1,7 @@
+/*Step 53
+Later on in the curriculum, you will dive deeper into objects. But for now, it is time to apply what you have learned to your role playing game.
+
+Start by deleting your cat object and console statement.*/
+
+
+
