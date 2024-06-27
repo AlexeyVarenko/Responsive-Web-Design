3609937b234d1d44d78c11bd0749ee74ac614d37
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-06-27 08:09:16 +0300

    Create step16.54.js

---
+++ step16.54.js
@@ -0,0 +1,18 @@
+/*Step 54
+Your locations array will hold different locations like the store, the cave, and the town square. Each location will be represented as an object.
+
+Inside your locations array, add an object. Inside that object add a key called name with a value of "town square".
+
+Remember to follow this syntax:
+
+Example Code
+{
+  key: value
+}*/
+
+
+const locations = [
+    {
+      name: "town square"
+    }
+  ];
