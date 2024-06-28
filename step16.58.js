56c47d900b195d8b9f9d01054759532c381d0df5
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-06-28 11:14:33 +0300

    Create step16.58.js

---
+++ step16.58.js
@@ -0,0 +1,12 @@
+/*Step 58
+Add one final property to the object named text. Give this property the same string value as the one assigned to text.innerText in the goTown function.*/
+
+
+const locations = [
+    {
+      name: "town square",
+      "button text": ["Go to store", "Go to cave", "Fight dragon"],
+      "button functions": [goStore, goCave, fightDragon],
+      "text": "You are in the town square. You see a sign that says \"Store\"."
+    }
+  ];
