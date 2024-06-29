456573c6e5c14270ad9a8730a320adb083cb6deb
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-06-29 09:35:45 +0300

    Create step16.59.js

---
+++ step16.59.js
@@ -0,0 +1,19 @@
+/*Step 59
+Add a second object to your locations array (remember to separate them with a comma). Following the pattern you used in the first object, create the same properties but use the values from the goStore function. Set the name property to store.*/
+
+
+
+const locations = [
+    {
+      name: "town square",
+      "button text": ["Go to store", "Go to cave", "Fight dragon"],
+      "button functions": [goStore, goCave, fightDragon],
+      text: "You are in the town square. You see a sign that says \"Store\"."
+    },
+    {
+      name: "store",
+      "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
+      "button functions": [buyHealth, buyWeapon, goTown],
+      text: "You enter the store."
+    }
+  ];
