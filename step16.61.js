c0d8c5e0d8677cb0abaf36706fb23192eb44aecf
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-06-29 09:35:48 +0300

    Create step16.61.js

---
+++ step16.61.js
@@ -0,0 +1,12 @@
+/*Step 61
+Instead of assigning the innerText and onclick properties to specific strings and functions, the update function will use data from the location that is passed into it. First, that data needs to be passed.
+
+Inside the goTown function, call the update function. Here is an example of calling a function named myFunction:
+
+Example Code
+myFunction();*/
+
+
+function goTown() {
+    update()
+   }
