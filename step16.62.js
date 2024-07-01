4e1f6a76b4b21910927ac098660acb1bc30c2475
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-07-01 09:48:18 +0300

    Create step16.62.js

---
+++ step16.62.js
@@ -0,0 +1,14 @@
+/*Step 62
+Now it is time to use your update function. Pass in your locations array into the update function call.
+
+You pass arguments by including them within the parentheses of the function call. For example, calling myFunction with an arg argument would look like:
+
+Example Code
+myFunction(arg)
+Pass your locations array into the update call.*/
+
+
+
+function goTown() {
+    update(locations);
+  }
