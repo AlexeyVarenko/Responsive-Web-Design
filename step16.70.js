317bb3f357e69259a1e5428f5dd38eae45500d67
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-07-03 07:47:15 +0300

    Create step16.70.js

---
+++ step16.70.js
@@ -0,0 +1,10 @@
+/*Step 70
+Create two more empty functions named fightSlime and fightBeast. These functions will be used in your upcoming cave object.*/
+
+
+function fightSlime() {
+
+}
+function fightBeast() {
+
+}
