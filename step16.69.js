bc6de13df6e78578cb8688dec9843a96e38f9b45
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-07-03 07:47:13 +0300

    Create step16.69.js

---
+++ step16.69.js
@@ -0,0 +1,10 @@
+/*Step 69
+Now update your goStore function to call the update function. Pass the second element of the locations array as your argument.
+
+To make sure your refactoring is correct, try clicking your first button again. You should see the same changes to your webpage that you saw earlier.*/
+
+
+
+function goStore() {
+    update(locations[1])
+  }
