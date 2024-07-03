e5d309be969cb80d0ca130ba97981e835713156b
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-07-03 07:47:11 +0300

    Create step16.68.js

---
+++ step16.68.js
@@ -0,0 +1,18 @@
+/*Step 68
+Finally, update the text.innerText assignment to equal the text from the location object. However, instead of using bracket notation, use dot notation.
+
+Here is an example of accessing the name property of an object called person:
+
+Example Code
+person.name*/
+
+
+function update(location) {
+    button1.innerText = location["button text"][0];
+    button2.innerText = location["button text"][1];
+    button3.innerText = location["button text"][2];
+    button1.onclick = location["button functions"][0];
+    button2.onclick = location["button functions"][1];
+    button3.onclick = location["button functions"][2];
+    text.innerText = location.text;
+  }
