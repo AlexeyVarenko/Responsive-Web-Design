e60305daad2ab9f44e147892bec7d9bc08ba369c
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-07-04 07:46:43 +0300

    Create step16.73.js

---
+++ step16.73.js
@@ -0,0 +1,10 @@
+/*Step 73
+Now that your "store" and "cave" locations are complete, you can code the actions the player takes at those locations. Inside the buyHealth function, set gold equal to gold minus 10.
+
+For example, here is how you would set num equal to 5 less than num: num = num - 5;.*/
+
+
+
+function buyHealth() {
+    gold=gold-10
+  }
