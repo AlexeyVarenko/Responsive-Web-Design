748a06e524be40d1570240cd968deb899d8cab5e
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-07-04 07:46:42 +0300

    Create step16.72.js

---
+++ step16.72.js
@@ -0,0 +1,10 @@
+/*Step 72
+Now that you have a "cave" location object, update your goCave function to call update and pass that new "cave" location. Remember that this is the third element in your locations array.
+
+Don't forget to remove your console.log call!*/
+
+
+
+function goCave() {
+    update(locations[2]);
+   }
