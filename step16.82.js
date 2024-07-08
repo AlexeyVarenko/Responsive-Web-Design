17c434214dce3e84a0fb5c8b87f13530c678ce69
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-07-08 07:49:59 +0300

    Create step16.82.js

---
+++ step16.82.js
@@ -0,0 +1,10 @@
+/*Step 82
+Inside your buyWeapon function, add an if statement to check if gold is greater than or equal to 30.*/
+
+
+
+function buyWeapon() {
+    if(gold>=30){
+      
+    }
+   }
