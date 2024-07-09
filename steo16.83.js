a1c3ba241e553e6a499dd83ebd268dfee711f6e3
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-07-09 10:30:49 +0300

    Create steo16.83.js

---
+++ steo16.83.js
@@ -0,0 +1,10 @@
+/*Step 83
+Similar to your buyHealth function, set gold equal to 30 less than its current value. Make sure this is inside your if statement.*/
+
+
+
+function buyWeapon() {
+    if (gold >= 30) {
+       gold -= 30;
+    }
+  }
