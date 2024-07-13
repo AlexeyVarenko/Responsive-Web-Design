67cef09399a62ded460e4da177804d4e96e0c3de
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-07-13 11:09:03 +0300

    Create step16.92.js

---
+++ step16.92.js
@@ -0,0 +1,18 @@
+/*Step 92
+Up until now, any time text.innerText was updated, the old text was erased. This time, use the += operator to add text to the end of text.innerText.
+
+Add the string " In your inventory you have: " - include the spaces at the beginning and the end.*/
+
+
+
+function buyWeapon() {
+    if (gold >= 30) {
+      gold -= 30;
+      currentWeaponIndex++;
+      goldText.innerText = gold;
+      let newWeapon = weapons[currentWeaponIndex].name;
+      text.innerText = "You now have a " + newWeapon + ".";
+      inventory.push(newWeapon);
+      text.innerText += " In your inventory you have: "
+    }
+  }
