db6c02ee0a5d4e82d553914adf6a80fbed243554
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-07-13 11:09:07 +0300

    Create step16.94.js

---
+++ step16.94.js
@@ -0,0 +1,18 @@
+/*Step 94
+Add an else statement to your buyWeapon function. In that statement, set text.innerText to equal "You do not have enough gold to buy a weapon.".*/
+
+
+
+function buyWeapon() {
+    if (gold >= 30) {
+      gold -= 30;
+      currentWeaponIndex++;
+      goldText.innerText = gold;
+      let newWeapon = weapons[currentWeaponIndex].name;
+      text.innerText = "You now have a " + newWeapon + ".";
+      inventory.push(newWeapon);
+      text.innerText += " In your inventory you have: " + inventory;
+    } else {
+      text.innerText = "You do not have enough gold to buy a weapon."
+    }
+  }
