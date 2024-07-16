45123a57756d5ff2f9d5690f8fcc1ee268e01486
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-07-16 10:01:49 +0300

    Create step16.100.js

---
+++ step16.100.js
@@ -0,0 +1,8 @@
+/*Step 100
+Now that you are finished testing that portion of the buyWeapon function, you can set your gold variable back to 50.
+
+Note: The HTML has already been updated to reflect the original value of gold.*/
+
+
+
+let gold = 50;
