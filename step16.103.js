fa9cdd4fac98ab010e1054ffe99730c4f612925e
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-07-17 09:20:51 +0300

    Create step16.103.js

---
+++ step16.103.js
@@ -0,0 +1,10 @@
+/*Step 103
+Players should not be able to sell their only weapon. Inside the sellWeapon function, add an if statement with a condition that checks if the length of the inventory array is greater than 1.*/
+
+
+
+function sellWeapon() {
+    if(inventory.length>1){
+      
+    }
+  }
