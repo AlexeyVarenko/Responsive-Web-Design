32cb16aff27f2eee8306479cbcab5206c4bc8260
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-07-18 10:39:08 +0300

    Create step16.105.js

---
+++ step16.105.js
@@ -0,0 +1,21 @@
+/*Step 105
+The next step is to create a variable called currentWeapon.
+
+Example Code
+let num = 1;
+if (num === 1) {
+  let num = 2; // this num is scoped to the if statement
+  console.log(num); // expected output: 2
+}
+console.log(num); // expected output: 1 (the global variable)
+Use the let keyword to create a variable named currentWeapon. Don't assign it a value yet.*/
+
+
+
+function sellWeapon() {
+    if (inventory.length > 1) {
+      gold += 15;
+      goldText.innerText = gold;
+    let currentWeapon;
+    }
+  }
