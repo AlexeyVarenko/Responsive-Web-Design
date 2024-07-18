e86bc34bfa9be2db48b8f0587a6417fee7c9a344
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-07-18 10:39:06 +0300

    Create step16.104.js

---
+++ step16.104.js
@@ -0,0 +1,11 @@
+/*Step 104
+Inside the if statement, set gold equal to 15 more than its current value. Also update goldText.innerText to the new value.*/
+
+
+
+function sellWeapon() {
+    if (inventory.length > 1) {
+      gold+=15;
+      goldText.innerText=gold;
+    }
+  }
