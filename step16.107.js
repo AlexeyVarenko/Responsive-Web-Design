a0058cba6591c00b51ed218992096afd9edfcbbd
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-07-18 10:39:11 +0300

    Create step16.107.js

---
+++ step16.107.js
@@ -0,0 +1,13 @@
+/*Step 107
+After your currentWeapon, use the concatenation operator to set text.innerText to the string "You sold a ", then currentWeapon, then the string ".".*/
+
+
+
+function sellWeapon() {
+    if (inventory.length > 1) {
+      gold += 15;
+      goldText.innerText = gold;
+      let currentWeapon = inventory.shift();
+    text.innerText="You sold a " + currentWeapon + ".";
+    }
+  }
