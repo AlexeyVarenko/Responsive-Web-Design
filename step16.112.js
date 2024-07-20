296a99595d36a681eecd112464b66d3a1e20f819
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-07-20 10:02:54 +0300

    Create step16.112.js

---
+++ step16.112.js
@@ -0,0 +1,10 @@
+/*Step 112
+In your fightSlime function, set fighting equal to 0 - the index of slime in the monsters array. Remember that you already declared fighting earlier in your code, so you do not need let or const here.
+
+On the next line, call the goFight function.*/
+
+
+function fightSlime() {
+    fighting=0;
+    goFight();
+  }
