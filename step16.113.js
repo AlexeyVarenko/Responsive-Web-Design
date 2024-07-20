c3723bbb0cb548fb9b96ec6430204395c1a07e4a
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-07-20 10:02:56 +0300

    Create step16.113.js

---
+++ step16.113.js
@@ -0,0 +1,12 @@
+/*Step 113
+Following the same pattern as the fightSlime function, use that code in the fightBeast and fightDragon functions. Remember that beast is at index 1 and dragon is at index 2. Also, remove the console.log call from your fightDragon function.*/
+
+
+
+function fightBeast() {
+    fighting=1;
+  }
+  
+  function fightDragon() {
+    fighting=2;
+  }
