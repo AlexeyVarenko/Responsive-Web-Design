91021a0a5219a320451cf4a9e98552023b75d238
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-07-23 12:29:54 +0300

    Create step16.117.js

---
+++ step16.117.js
@@ -0,0 +1,8 @@
+/*Step 117
+Below your update call, set the monsterHealth to be the health of the current monster. You can get this value by accessing the health property of monsters[fighting] with dot notation.*/
+
+
+function goFight() {
+    update(locations[3]);
+   monsterHealth = monsters[fighting].health;
+  }
