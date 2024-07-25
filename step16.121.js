f38c4ba57f63ceae656d8ac1a41dfcc97c79efc3
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-07-25 08:07:51 +0300

    Create step16.121.js

---
+++ step16.121.js
@@ -0,0 +1,9 @@
+/*Step 121
+On a new line, use the addition assignment operator(+=), to add the string " You attack it with your <weapon>." to the text value, replacing <weapon> with the player's current weapon. Additionally, remember that this line of text starts with a space so it will properly display.*/
+
+
+
+function attack() {
+  text.innerText = "The " + monsters[fighting].name + " attacks.";
+   text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name +"."
+}
