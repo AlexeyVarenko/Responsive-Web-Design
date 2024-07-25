ebc0a66d84a998e8957b9d12655e908c7960b74f
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-07-25 08:07:55 +0300

    Create step16.119.js

---
+++ step16.119.js
@@ -0,0 +1,14 @@
+/*Step 119
+Now, you will need to update the text for the current monster's name and health.
+
+Start by assigning monsters[fighting].name to the innerText property of monsterName. Then, assign monsterHealth to the innerText property of monsterHealthText.*/
+
+
+
+function goFight() {
+    update(locations[3]);
+    monsterHealth = monsters[fighting].health;
+    monsterStats.style.display = "block";
+    monsterName.innerText=monsters[fighting].name;
+    monsterHealthText.innerText=monsterHealth;
+  }
