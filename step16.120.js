48c5b203b07093dfaeb4fb5b1bf571d386a7755e
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-07-25 08:07:52 +0300

    Create step16.120.js

---
+++ step16.120.js
@@ -0,0 +1,8 @@
+/*Step 120
+Now you can build the attack function. First, update the text message to say "The <monster name> attacks.", replacing <monster name> with the name of the monster. Remember you can use the concatenation operator for this.*/
+
+
+
+function attack() {
+    text.innerText="The " +monsters[fighting].name+ " attacks."
+  }
