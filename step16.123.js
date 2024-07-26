8e0d1ca8373c9827ddfcddf4cb13e2679e5bc558
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-07-26 09:30:03 +0300

    Create step16.123.js

---
+++ step16.123.js
@@ -0,0 +1,13 @@
+/*Step 123
+Set monsterHealth to monsterHealth minus the power of the player's current weapon.
+
+Remember that you can access the power of the player's current weapon using weapons[currentWeaponIndex].power.*/
+
+
+
+function attack() {
+    text.innerText = "The " + monsters[fighting].name + " attacks.";
+    text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
+    health -= monsters[fighting].level;
+    monsterHealth -= weapons[currentWeaponIndex].power;
+  }
