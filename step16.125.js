975e6a6965af4410eb4fcb8bd2f651d34141eb6f
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-07-27 09:45:39 +0300

    Create step16.125.js

---
+++ step16.125.js
@@ -0,0 +1,15 @@
+/*Step 125
+Update healthText.innerText and monsterHealthText.innerText to equal health and monsterHealth. */
+
+
+
+
+
+function attack() {
+    text.innerText = "The " + monsters[fighting].name + " attacks.";
+    text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
+    health -= monsters[fighting].level;
+    monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+    healthText.innerText = health;
+    monsterHealthText.innerText= monsterHealth;
+  }
