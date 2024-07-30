3bb49d6a285f2b2341f7eca9d4f3401a82a15040
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-07-30 11:26:06 +0300

    Create step16.132.js

---
+++ step16.132.js
@@ -0,0 +1,11 @@
+/*Step 132
+Now update goldText and xpText to display the updated values.*/
+
+
+
+function defeatMonster() {
+    gold += Math.floor(monsters[fighting].level * 6.7);
+    xp += monsters[fighting].level;
+    goldText.innerText = gold;
+    xpText.innerText = xp;
+  }
