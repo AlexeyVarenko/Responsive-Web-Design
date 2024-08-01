0b8e3d9ccb7e7cc3b920f8854e31aeaf1432e6ec
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-08-01 08:13:47 +0300

    Create step16.137.js

---
+++ step16.137.js
@@ -0,0 +1,8 @@
+/*Step 137
+In the lose function, call the update function and pass in the sixth object of your locations array. Note that you haven't created this object just yet.*/
+
+
+
+function lose() {
+    update(locations[5]);
+  }
