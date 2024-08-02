7229f08626375b3b8ee0db59e4daf9d67cb4c3d3
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-08-02 07:57:07 +0300

    Create step16.142.js

---
+++ step16.142.js
@@ -0,0 +1,8 @@
+/*Step 142
+After the lose function, create a function called winGame. Inside the winGame function, call the update function and pass in locations[6].*/
+
+
+
+function winGame() {
+    update(locations[6]);
+  }
