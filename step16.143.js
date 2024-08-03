78bad4478bc252800a452f00373865a5a25614be
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-08-03 08:58:44 +0300

    Create step16.143.js

---
+++ step16.143.js
@@ -0,0 +1,17 @@
+/*Step 143
+Add another object in the locations array. Everything should be the same as the lose object, except the name should be "win" and the text should be "You defeat the dragon! YOU WIN THE GAME! &#x1F389;".*/
+
+
+
+{
+    name: "lose",
+    "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+    "button functions": [restart, restart, restart],
+    text: "You die. &#x2620;"
+  },
+  {
+    name: "win",
+    "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+    "button functions": [restart, restart, restart],
+    text: "You defeat the dragon! YOU WIN THE GAME! &#x1F389;"
+  }
