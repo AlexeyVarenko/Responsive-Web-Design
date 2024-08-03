b63d0dc832322e56aa67b137b022ca2e882bbf85
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-08-03 08:58:47 +0300

    Create step16.145.js

---
+++ step16.145.js
@@ -0,0 +1,8 @@
+/*Step 145
+Below your attack function, create an empty function named getMonsterAttackValue. It should take level as a parameter.*/
+
+
+
+function getMonsterAttackValue(level) {
+  
+}
