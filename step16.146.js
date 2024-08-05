48f280a74d3ed5ab045572d8611acd5135d06c57
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-08-05 07:40:13 +0300

    Create step16.146.js

---
+++ step16.146.js
@@ -0,0 +1,10 @@
+/*Step 146
+The attack of the monster will be based on the monster's level and the player's xp. In the getMonsterAttackValue function, use const to create a variable called hit. Assign it the equation (level * 5) - (Math.floor(Math.random() * xp));.
+
+This will set the monster's attack to five times their level minus a random number between 0 and the player's xp.*/
+
+
+
+function getMonsterAttackValue(level) {
+    const hit=(level * 5) - (Math.floor(Math.random() * xp));
+  }
