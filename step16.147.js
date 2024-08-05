1eaef8fc3e7b9f3ab6cde775df5ed30f611f2910
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-08-05 07:40:15 +0300

    Create step16.147.js

---
+++ step16.147.js
@@ -0,0 +1,9 @@
+/*Step 147
+Log the value of hit to the console to use in debugging. Remember that you can do this with console.log().*/
+
+
+
+function getMonsterAttackValue(level) {
+    const hit = (level * 5) - (Math.floor(Math.random() * xp));
+    console.log(hit)
+  }
