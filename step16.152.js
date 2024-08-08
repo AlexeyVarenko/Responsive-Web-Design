f820a3287462096e7a6f77be905c98ad901b4f10
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-08-08 14:05:08 +0300

    Create step16.152.js

---
+++ step16.152.js
@@ -0,0 +1,10 @@
+/*Step 152
+Now create the isMonsterHit function. This will return a boolean value (true or false) to be used in your if statement. Return the result of the comparison Math.random() > .2.*/
+
+
+
+function isMonsterHit() {
+  
+    return Math.random() > .2
+  
+}
