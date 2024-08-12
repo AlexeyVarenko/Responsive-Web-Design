87677419adbe3dfdd19febeebafcae6cde04f2c8
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-08-12 08:38:04 +0300

    Create step16.158.js

---
+++ step16.158.js
@@ -0,0 +1,10 @@
+/*Step 158
+Now you can add a small easter egg (hidden feature) to your game.
+
+Create a new function called easterEgg which calls the update function with locations[7] as the argument.*/
+
+
+
+function easterEgg() {
+    update(locations[7]);
+  }
