dd372b033b67a89cca12ddc75d5a840f77cdd155
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-08-12 08:37:53 +0300

    Create step16.163.js

---
+++ step16.163.js
@@ -0,0 +1,18 @@
+/*Step 163
+After your numbers array, create a while loop that runs as long as numbers.length is less than 10.
+
+In the previous project, you learned how to work with while loops like this:
+
+Example Code
+while (condition) {
+  // code to run
+}*/
+
+
+
+function pick(guess) {
+    const numbers = [];
+    while (numbers.length<10) {
+    
+  }
+  }
