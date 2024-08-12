2518407c6ed18c307263ba1a1a44e233fef27758
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-08-12 08:37:59 +0300

    Create step16.160.js

---
+++ step16.160.js
@@ -0,0 +1,14 @@
+/*Step 160
+Create two new functions named pickTwo and pickEight.
+
+Inside each of those, call the pick() function and pass either 2 or 8 as the argument depending on the function name.*/
+
+
+
+function pickTwo() {
+    pick(2);
+  };
+  
+  function pickEight() {
+    pick(8);
+  }
