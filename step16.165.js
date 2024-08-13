a2f16c6c2acd7261df66e24929ff1fb92c4be975
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-08-13 09:07:22 +0300

    Create step16.165.js

---
+++ step16.165.js
@@ -0,0 +1,11 @@
+/*Step 165
+After the while loop, set text.innerText to equal "You picked <someGuess>. Here are the random numbers:". Replace <someGuess> with the guess function parameter.*/
+
+
+function pick(guess) {
+    const numbers = [];
+    while (numbers.length < 10) {
+      numbers.push(Math.floor(Math.random() * 11));
+    }
+    text.innerText= "You picked " + guess + ". Here are the random numbers:"
+  }
