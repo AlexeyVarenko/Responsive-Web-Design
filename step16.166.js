63ee8ac1e59d085329e88ce6ecddfaff27130544
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-08-13 09:07:24 +0300

    Create step16.166.js

---
+++ step16.166.js
@@ -0,0 +1,12 @@
+/*Step 166
+At the end of the string, before the final quote, insert the new line escape character \n. This will cause the next part you add to text.innerText to appear on a new line.*/
+
+
+
+function pick(guess) {
+    const numbers = [];
+    while (numbers.length < 10) {
+      numbers.push(Math.floor(Math.random() * 11));
+    }
+    text.innerText = "You picked " + guess +  ". Here are the random numbers:\n";
+  }
