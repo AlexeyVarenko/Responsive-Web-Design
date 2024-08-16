1ee5f8eed09f185ef723a43fba7e391a6b279c10
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-08-16 09:08:49 +0300

    Create step17.2.js

---
+++ step17.2.js
@@ -0,0 +1,11 @@
+/*Step 2
+In your form, users will be able to input a number which represents their daily calorie budget.
+
+Create a label element, give it a for attribute set to budget and the text Budget, then create an input element with the id set to budget.*/
+
+
+
+<form id="calorie-counter">
+          <label for="budget">Budget</label>
+          <input id="budget"></input>
+        </form>
