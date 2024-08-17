8850f9c8035af87502ec5ab4d7895222a315927b
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-08-17 09:43:00 +0300

    Create step17.3.js

---
+++ step17.3.js
@@ -0,0 +1,11 @@
+/*Step 3
+Your input element needs some additional attributes. Give it a type set to number to only allow numeric inputs, a min attribute set to 0 to only allow positive numbers, and a placeholder set to Daily calorie budget.
+
+Finally, mark the input element as required.*/
+
+
+
+<form id="calorie-counter">
+          <label for="budget">Budget</label>
+          <input id="budget" type="number" min="0" placeholder="Daily calorie budget" required/>
+        </form>
