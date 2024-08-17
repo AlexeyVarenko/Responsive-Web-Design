58cf7fbfc502f789ab3846c82cbb0f985c0f0b97
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-08-17 09:43:03 +0300

    Create step17.5.js

---
+++ step17.5.js
@@ -0,0 +1,11 @@
+/*Step 5
+Next, create a fieldset element with the id set to lunch.
+
+Within that element, create a legend element with the text Lunch, and an empty div with the class set to input-container.*/
+
+
+
+<fieldset id="lunch">
+            <legend>Lunch</legend>
+            <div class="input-container"></div>
+          </fieldset>
