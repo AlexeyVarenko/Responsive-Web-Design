260ca363b9f5bfb1b87f98e03d7568072f3268a5
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-08-20 08:08:32 +0300

    Create step17.6.js

---
+++ step17.6.js
@@ -0,0 +1,9 @@
+/*Step 6
+Continuing the pattern, create a fieldset for dinner with the same nested elements.*/
+
+
+
+<fieldset id="dinner">
+<legend>Dinner</legend>
+<div class="input-container"></div>
+</fieldset>
