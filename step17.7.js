0fb30058f61944f297590d3392427c54bd9f098e
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-08-20 08:08:34 +0300

    Create step17.7.js

---
+++ step17.7.js
@@ -0,0 +1,13 @@
+/*Step 7
+You need two more of these fieldset code blocks – one for snacks and one for exercise.*/
+
+
+
+<fieldset id="snacks">
+            <legend>Snacks</legend>
+            <div class="input-container"></div>
+          </fieldset>
+          <fieldset id="exercise">
+            <legend>Exercise</legend>
+            <div class="input-container"></div>
+          </fieldset>
