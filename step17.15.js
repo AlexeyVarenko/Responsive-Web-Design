9741793c5a0f5c8bdd162c571dbcea87b0051196
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-08-23 07:50:17 +0300

    Create step17.15.js

---
+++ step17.15.js
@@ -0,0 +1,8 @@
+/*Step 15
+Get your #budget element and assign it to budgetNumberInput, and your #entry-dropdown element and assign it to entryDropdown.*/
+
+
+
+const calorieCounter = document.getElementById('calorie-counter');
+const budgetNumberInput = document.getElementById('budget');
+const entryDropdown = document.getElementById('entry-dropdown');
