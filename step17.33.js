8d809de5ff972b6adfe2e30d2f8c7c4cf2f92d59
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-08-30 10:25:51 +0300

    Create step17.33.js

---
+++ step17.33.js
@@ -0,0 +1,8 @@
+/*Step 33
+There is a shorthand character class to match any digit: \d. Replace your [0-9] character classes with this shorthand.*/
+
+
+
+function isInvalidInput(str) {
+    const regex = /\d+e\d+/i;
+  }
