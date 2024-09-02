fdbe60ad7ba911f2addd4269503880eeec8ec91f
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-09-02 08:30:29 +0300

    Create step17.38.js

---
+++ step17.38.js
@@ -0,0 +1,7 @@
+/*Step 38
+Your next step is to allow users to add entries to the calorie counter. Declare an empty function addEntry. This function should not take any parameters.*/
+
+
+function addEntry() {
+  
+}
