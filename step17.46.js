d3a187f5ff3eebbcb409d19e452b7b30ac040345
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-09-05 08:05:24 +0300

    Create step17.46.js

---
+++ step17.46.js
@@ -0,0 +1,6 @@
+/*Step 46
+Inside your template literal, create a label element and give it the text Entry # Name. Using your template literal syntax, replace # with the value of entryNumber.*/
+
+
+
+<label>Entry ${entryNumber} Name </label>  
