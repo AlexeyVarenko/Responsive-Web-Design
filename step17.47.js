6059025313f3b3cd0349fd461089c935c1cebf76
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-09-06 10:05:58 +0300

    Create step17.47.js

---
+++ step17.47.js
@@ -0,0 +1,7 @@
+/*Step 47
+Give your label element a for attribute with the value X-#-name, where X is the value of the entryDropdown element and # is the value of entryNumber. Remember that HTML attributes should be wrapped in double quotes.*/
+
+
+
+
+  <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>`;
