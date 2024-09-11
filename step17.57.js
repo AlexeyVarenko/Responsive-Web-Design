4aa05ea27ef9d83c43ac3a9f567c48f03ccd174a
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-09-11 07:45:20 +0300

    Create step17.57.js

---
+++ step17.57.js
@@ -0,0 +1,7 @@
+/*Step 57
+In your new function, declare a calories variable and assign it the value 0. Use let to declare it, since you will be reassigning it later.*/
+
+
+function getCaloriesFromInputs(list) {
+    let calories=0;
+  }
