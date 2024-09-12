3b77610b91957e78b63cb1ddc0bd0e5fe6323713
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-09-12 07:50:17 +0300

    Create step17.60.js

---
+++ step17.60.js
@@ -0,0 +1,8 @@
+/*Step 60
+Remember that you wrote a function earlier to clean the user's input? You'll need to use that function here.
+
+Update your currVal declaration to be the result of calling cleanInputString with item.value.*/
+
+
+
+const currVal = cleanInputString(item.value);
