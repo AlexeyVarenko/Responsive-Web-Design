74d87dc990527481144d005db913745aad696aa3
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-09-12 07:50:15 +0300

    Create step17.59.js

---
+++ step17.59.js
@@ -0,0 +1,9 @@
+/*Step 59
+The NodeList values you will pass to list will consist of input elements. So you will want to look at the value attribute of each element.
+
+Assign item.value to a const variable called currVal.*/a
+
+
+for (const item of list) {
+    const currVal=item.value;
+  }
