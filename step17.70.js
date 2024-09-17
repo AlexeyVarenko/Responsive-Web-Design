3c2d67726f74345aaea2de4b5eb0ae4aa315e587
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-09-17 08:48:32 +0300

    Create step17.70.js

---
+++ step17.70.js
@@ -0,0 +1,16 @@
+/*Step 70
+Using that same syntax, query your number inputs in the #lunch element and assign them to lunchNumberInputs.*/
+
+
+
+function calculateCalories(e) {
+    e.preventDefault();
+    isError = false;
+    
+    const breakfastNumberInputs = document.querySelectorAll('#breakfast input[type=number]');
+    const lunchNumberInputs = document.querySelectorAll('#lunch input[type=number]');
+  
+  }
+
+
+
