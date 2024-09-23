2762af20f4b1849ece9ee697f95ec7d3a818e67a
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-09-23 07:42:10 +0300

    Create steo17.83.js

---
+++ steo17.83.js
@@ -0,0 +1,40 @@
+/*Step 83
+When the user has a calorie surplus, the remainingCalories value will be negative. You don't want to display a negative number in the result string.
+
+Math.abs() is a built-in JavaScript method that will return the absolute value of a number.
+
+Example Code
+const num = -5;
+Math.abs(num); // 5
+In your span text, wrap your remainingCalories reference in Math.abs() to ensure that the value is positive.*/
+
+
+
+function calculateCalories(e) {
+    e.preventDefault();
+    isError = false;
+  
+    const breakfastNumberInputs = document.querySelectorAll('#breakfast input[type=number]');
+    const lunchNumberInputs = document.querySelectorAll('#lunch input[type=number]');
+    const dinnerNumberInputs = document.querySelectorAll('#dinner input[type=number]');
+    const snacksNumberInputs = document.querySelectorAll('#snacks input[type=number]');
+    const exerciseNumberInputs = document.querySelectorAll('#exercise input[type=number]');
+  
+    const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
+    const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
+    const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
+    const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
+    const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
+    const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
+  
+    if (isError) {
+      return;
+    }
+  
+    const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
+    const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
+    const surplusOrDeficit = remainingCalories < 0 ? 'Surplus' : 'Deficit';
+    output.innerHTML = `
+    <span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
+    `;
+  }
