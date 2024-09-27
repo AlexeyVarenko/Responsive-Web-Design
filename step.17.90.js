2384066fc5651b3b80ceaab587557a21f5e63fc2
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-09-27 07:49:51 +0300

    Create step.17.90.js

---
+++ step.17.90.js
@@ -0,0 +1,8 @@
+/*Step 90
+You need to get all of the input containers. Declare an inputContainers variable, and assign it to the value of querying the document for all elements with the class input-container.*/
+
+
+
+function clearForm() {
+    const inputContainers = document.querySelectorAll(".input-container");
+  }
