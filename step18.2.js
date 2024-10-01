001c2b2bf05a2c5dd5b3a2f7ef49f63b19f29e49
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-10-01 08:17:05 +0300

    Create step18.2.js

---
+++ step18.2.js
@@ -0,0 +1,10 @@
+/*Step 2
+Access the #next, #previous and #shuffle elements from the HTML file.
+
+Assign them to variables named nextButton, previousButton, and shuffleButton, respectively.*/
+
+
+
+const nextButton = document.getElementById("next");
+const previousButton = document.getElementById("previous");
+const shuffleButton = document.getElementById("shuffle");
