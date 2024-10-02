40a646036178942e133f08afde0f5aef8c01f272
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-10-02 08:06:08 +0300

    Create step18.5.js

---
+++ step18.5.js
@@ -0,0 +1,19 @@
+/*Step 5
+Add a second object with the following keys and values:
+
+Example Code
+id: 1,
+title: "Can't Stay Down",
+artist: "Quincy Larson",
+duration: "4:15",
+src: "https://cdn.freecodecamp.org/curriculum/js-music-player/can't-stay-down.mp3", */
+
+
+
+{
+    id: 1,
+  title: "Can't Stay Down",
+  artist: "Quincy Larson",
+  duration: "4:15",
+  src: "https://cdn.freecodecamp.org/curriculum/js-music-player/can't-stay-down.mp3",
+  }
