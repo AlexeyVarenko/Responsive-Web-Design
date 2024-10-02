a3e0e4d73a2148d18154a1b368fbad2dd3458f61
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-10-02 08:06:06 +0300

    Create step18.4.js

---
+++ step18.4.js
@@ -0,0 +1,19 @@
+/*Step 4
+Inside the allSongs array, create an object with the following properties and values:
+
+Example Code
+id: 0,
+title: "Scratching The Surface",
+artist: "Quincy Larson",
+duration: "4:25",
+src: "https://cdn.freecodecamp.org/curriculum/js-music-player/scratching-the-surface.mp3",*/
+
+
+
+{
+    id: 0,
+  title: "Scratching The Surface",
+  artist: "Quincy Larson",
+  duration: "4:25",
+  src: "https://cdn.freecodecamp.org/curriculum/js-music-player/scratching-the-surface.mp3",
+  }
