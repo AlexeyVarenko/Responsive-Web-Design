b41523e7f7aaf523d15dea8c78c23591b81961d2
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-10-04 07:54:59 +0300

    Create step18.7.js

---
+++ step18.7.js
@@ -0,0 +1,10 @@
+/*Step 7
+We've added the rest of the songs to the allSongs array for you.
+
+Next, you'll learn about the Web Audio API and how to use it to play songs. All modern browsers support the Web Audio API, which lets you generate and process audio in web applications.
+
+Use const to create a variable named audio and set it equal to new Audio(). This will create a new HTML5 audio element.*/
+
+
+
+const audio = new Audio();
