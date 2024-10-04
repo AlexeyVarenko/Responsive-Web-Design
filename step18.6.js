f78c66f2b7faafa373b9e65f8a24d50c59a48cc3
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-10-04 07:54:57 +0300

    Create step18.6.js

---
+++ step18.6.js
@@ -0,0 +1,18 @@
+/*Step 6
+Add a third object with the following properties and values:
+
+Example Code
+id: 2,
+title: "Still Learning",
+artist: "Quincy Larson",
+duration: "3:51",
+src: "https://cdn.freecodecamp.org/curriculum/js-music-player/still-learning.mp3",*/
+
+
+{
+    id: 2,
+  title: "Still Learning",
+  artist: "Quincy Larson",
+  duration: "3:51",
+  src: "https://cdn.freecodecamp.org/curriculum/js-music-player/still-learning.mp3",
+  }
