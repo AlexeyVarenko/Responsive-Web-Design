5c3bd46831250b4618a5d659be0a229b4aaf1070
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-10-04 07:54:55 +0300

    Create step18.11.js

---
+++ step18.11.js
@@ -0,0 +1,23 @@
+/*Step 11
+In the previous projects, you used regular functions. But in the rest of the projects, you will be working with arrow functions. The next few steps will focus on teaching you the basics of arrow functions.
+
+An arrow function is an anonymous function expression and a shorter way to write functions. Anonymous means that the function does not have a name. Arrow functions are always anonymous.
+
+Here is the basic syntax:
+
+Example Code
+() => {}
+To create a named arrow function, you can assign the function to a variable:
+
+Example Code
+const exampleFunction = () => {
+  // code goes here
+}
+Create a new named arrow function called printGreeting. Inside the body of that function use the console.log() method to print the string "Hello there!".*/
+
+
+
+
+const printGreeting = () => {
+    console.log('Hello there!')
+  }
