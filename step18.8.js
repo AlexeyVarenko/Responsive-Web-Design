5e7607bcb28a8d9c917e4e0d56207188d8438330
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-10-04 07:55:01 +0300

    Create step18.8.js

---
+++ step18.8.js
@@ -0,0 +1,8 @@
+/*Step 8
+Your music player should keep track of the songs, the current song playing, and the time of the current song. To do this, you will need to create an object to store this information.
+
+Start by using the let keyword to declare a new variable called userData and assign it an empty object.*/
+
+
+
+let userData = {};
