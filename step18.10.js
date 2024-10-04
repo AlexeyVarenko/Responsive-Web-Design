be66259ac060d931baa8eac648ef9b6dcbb5dae2
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-10-04 07:54:54 +0300

    Create step18.10.js

---
+++ step18.10.js
@@ -0,0 +1,10 @@
+/*Step 10
+To handle the current song's information and track its playback time, create a currentSong and songCurrentTime properties. Set the values to null and 0 respectively.*/
+
+
+
+let userData = {
+    songs: [...allSongs],
+    currentSong: null,
+    songCurrentTime: 0,
+  };
