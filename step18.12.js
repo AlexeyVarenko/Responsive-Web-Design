72c3e747787e94a2674e61482d676f5b32bb070f
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-10-05 09:28:44 +0300

    Create step18.12.js

---
+++ step18.12.js
@@ -0,0 +1,12 @@
+/*Step 12
+To call a named arrow function expression, you can reference the function by its name.
+
+Example Code
+exampleArrowFunction();
+Below your printGreeting function definition, call the function. Open up the console to see the output.*/
+
+
+const printGreeting = () => {
+    console.log('Hello there!'); 
+  }
+  printGreeting()
