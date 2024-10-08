51d7973318c447362fd2ad8ea7d48f870aba06d6
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-10-08 07:46:33 +0300

    Create step18.17.js

---
+++ step18.17.js
@@ -0,0 +1,10 @@
+/*Step 17
+To display the songs in the UI (User Interface), you'll need to create a function.
+
+Use the arrow function syntax to create a renderSongs function that takes in array as its parameter.*/
+
+
+
+const renderSongs = (array) =>{
+    
+}
