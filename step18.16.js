44e8726e3bd92e5ef411e9ed06c1f0fdeb7450dd
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-10-08 07:46:32 +0300

    Create step18.16.js

---
+++ step18.16.js
@@ -0,0 +1,7 @@
+/*Step 16
+Now it is time to apply what you have learned about arrow functions to your music player project.
+
+Start by removing the printGreeting, printMessage, and addTwoNumbers functions. Also, remove all of the console statements and function calls.*/
+
+
+
