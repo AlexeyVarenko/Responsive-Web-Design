277d5d626d34d8782df5be599a04551e73a28e31
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-10-08 07:46:36 +0300

    Create step18.18.js

---
+++ step18.18.js
@@ -0,0 +1,10 @@
+/*Step 18
+When the songs are displayed on the page, it should show the title, artist, duration of each song and a delete button.
+
+In the next few steps, you will learn how to add new HTML for each song using the map() method. This method will be explained in more detail in the next step.
+
+Start by using const to declare a variable named songsHTML and assign it array.map().*/
+
+
+
+const songsHTML=array.map();
