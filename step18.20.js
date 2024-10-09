ff7e959d02befb591c60672cb28f31561cc1af8c
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-10-09 07:41:40 +0300

    Create step18.20.js

---
+++ step18.20.js
@@ -0,0 +1,7 @@
+/*Step 20
+Inside the map(), add a return statement with backticks where you will interpolate all the elements responsible to displaying the song details.
+
+Inside the backticks, create an li element with an id attribute of song-${song.id} and a class attribute of playlist-song.*/
+
+
+return `<li id="song-${song.id}" class= "playlist-song"></li>`
