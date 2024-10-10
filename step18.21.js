36ad8917298720032812725d78aef0c5af0e9b16
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-10-10 07:48:45 +0300

    Create step18.21.js

---
+++ step18.21.js
@@ -0,0 +1,7 @@
+/*Step 21
+Create a button element with class playlist-song-info. Inside the button, add a span element with the class playlist-song-title, then interpolate song.title as the text.*/
+
+
+<button class="playlist-song-info">
+<span class="playlist-song-title">${song.title}</span>
+</button>
