da3f20bb9fbf5da56dd043bdef3d14b04b44cf68
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-10-12 09:25:02 +0300

    Create step18.27.js

---
+++ step18.27.js
@@ -0,0 +1,8 @@
+/*Step 27
+Now that you have the list of songs displayed on the screen, it would be nice to sort them in alphabetical order by title.
+
+Start by creating an arrow function called sortSongs.*/
+
+let sortSongs = ()=> {
+
+}
