549736ed6c25f6973b8c82670d07f5e781a30bf4
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-10-16 08:48:04 +0300

    Create step18.34.js

---
+++ step18.34.js
@@ -0,0 +1,10 @@
+/*Step 34
+Right now the song order has not changed. That is because the updates you made using the sort method will not happen until the sortSongs function is called.
+
+Change your renderSongs function to call the sortSongs function.
+
+Now you should see the songs in alphabetical order.*/
+
+
+
+renderSongs(sortSongs());
