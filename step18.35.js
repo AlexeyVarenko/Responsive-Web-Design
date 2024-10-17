59cc1d752b9914d5b08fb83a2a47031e4698e69a
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-10-17 09:06:34 +0300

    Create step18.35.js

---
+++ step18.35.js
@@ -0,0 +1,8 @@
+/*Step 35
+It's time to begin implementing the functionality for playing the displayed songs.
+
+Define a playSong function using const. The function should take an id parameter which will represent the unique identifier of the song you want to play.*/
+
+
+
+const playSong=(id)=>{}
