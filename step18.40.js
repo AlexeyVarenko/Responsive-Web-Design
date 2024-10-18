a7faac309c6e839a3f86d07c72da508a5572cd0c
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-10-18 09:11:44 +0300

    Create step18.40.js

---
+++ step18.40.js
@@ -0,0 +1,9 @@
+/*Step 40
+You need to update the current song being played as well as the appearance of the playButton element.
+
+Assign song to the currentSong property on the userData object.
+
+Note: You should not use the optional chaining operator ?. in this step because userData.currentSong will not be null or undefined at this point.*/
+
+
+userData.currentSong=song;
