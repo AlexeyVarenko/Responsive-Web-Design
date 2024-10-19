a9918cf691e47be28ba09cd2e102ccf8ab105218
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-10-19 10:02:49 +0300

    Create step18.42.js

---
+++ step18.42.js
@@ -0,0 +1,8 @@
+/*Step 42
+In previous steps you built out the functionality for playing a song. Now you need to add the functionality to the play button so that it will play the current song when it is clicked on.
+
+Use the addEventListener() method and pass in a "click" event for the first argument and an empty callback function with arrow syntax for the second argument, e.g., () => {}.*/
+
+
+
+playButton.addEventListener('click', () => {})
