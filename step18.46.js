6a447db8e6ea2f24438230957a8408d6cc5cc77c
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-10-21 09:10:42 +0300

    Create step18.46.js

---
+++ step18.46.js
@@ -0,0 +1,9 @@
+/*Step 46
+Now you need to work on pausing the currently playing song.
+
+Define a pauseSong function using the const keyword and arrow function syntax. The function should take no parameters.*/
+
+
+
+const pauseSong = () => {
+}
