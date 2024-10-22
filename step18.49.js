1cece4efcd9acb903abc4a6258e6e28422ef9dbc
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-10-22 09:20:30 +0300

    Create step18.49.js

---
+++ step18.49.js
@@ -0,0 +1,10 @@
+/*Step 49
+Now it is time to test out the pause button.
+
+Add a "click" event listener to the pauseButton element, then pass in pauseSong as the second argument of the event listener. This is the function the event listener will run.
+
+Test out your app by first clicking on the play button followed by the pause button. You should see that everything is working as expected*/
+
+
+
+pauseButton.addEventListener("click", pauseSong)
