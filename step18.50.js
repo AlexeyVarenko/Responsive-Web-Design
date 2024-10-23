1fba6fcf1ae2bbbf1690bd8ad1290afb088f66eb
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-10-23 08:41:11 +0300

    Create step18.50.js

---
+++ step18.50.js
@@ -0,0 +1,8 @@
+/*Step 50
+Before you start working on playing the next and previous song, you need to get the index of each song in the songs property of userData.
+
+Start by creating an arrow function called getCurrentSongIndex.*/
+
+
+
+const getCurrentSongIndex = () => {}
