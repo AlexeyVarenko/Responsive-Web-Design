a6c39cba30c89f4c4ab97d1f8938f7d14244e631
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-10-23 08:41:15 +0300

    Create step18.52.js

---
+++ step18.52.js
@@ -0,0 +1,7 @@
+/*Step 52
+You need to work on playing the next song and the previous song. For this, you will need a playNextSong and playPreviousSong function.
+
+Use const and arrow syntax to create an empty playNextSong function.*/
+
+
+const playNextSong = () => {}
