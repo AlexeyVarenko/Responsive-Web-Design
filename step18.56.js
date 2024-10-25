d8f14661303bcc0c61d53f57e8980d43812540c4
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-10-25 07:50:23 +0300

    Create step18.56.js

---
+++ step18.56.js
@@ -0,0 +1,10 @@
+/*Step 56
+Now it is time to test out the playNextSong function.
+
+Add a "click" event listener to the nextButton element, then pass in playNextSong as the second argument of your event listener. This is the function the event listener will run.
+
+Test out your app by first clicking on the play button followed by the next button. You should see that everything is working as expected.*/
+
+
+
+nextButton.addEventListener("click", playNextSong)
