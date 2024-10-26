dfc0e851c66847a2f274909e9f6ffbf433ec4b55
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-10-26 09:08:35 +0300

    Create step18.60.js

---
+++ step18.60.js
@@ -0,0 +1,5 @@
+/*Step 60
+Add a "click" event listener to the previousButton element, then pass in playPreviousSong as the second argument.*/
+
+
+previousButton.addEventListener("click",  playPreviousSong );
