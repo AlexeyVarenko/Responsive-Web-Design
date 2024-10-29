732276b22ed167784c4530dfbdb78219296ef2fc
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-10-29 07:51:58 +0300

    Create step18.63.js

---
+++ step18.63.js
@@ -0,0 +1,19 @@
+/*Step 63
+Loop through the playlistSongElements with a forEach method.
+
+The forEach method is used to loop through an array and perform a function on each element of the array. For example, suppose you have an array of numbers and you want to log each number to the console.
+
+Example Code
+const numbers = [1, 2, 3, 4, 5];
+
+// Using forEach to iterate through the array
+numbers.forEach((number) => {
+  console.log(number); // 1, 2, 3, 4, 5
+});
+Use the forEach method on playlistSongElements. Pass in songEl as the parameter and use arrow syntax to add in an empty callback.*/
+
+
+
+playlistSongElements.forEach((songEl)=>{
+
+});
