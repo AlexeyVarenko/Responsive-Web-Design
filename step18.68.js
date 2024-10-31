606c8cd4f7816f818c029933dbe7c4042575f967
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-10-31 08:45:48 +0300

    Create step18.68.js

---
+++ step18.68.js
@@ -0,0 +1,10 @@
+/*Step 68
+Inside the function, obtain references to the HTML elements responsible for displaying the song title and artist.
+
+Access the #player-song-title and #player-song-artist elements with the getElementById() method. Assign them to variables playingSong and songArtist respectively.*/
+
+
+const setPlayerDisplay = () => {
+    const  playingSong=document.getElementById('player-song-title');
+    const  songArtist=document.getElementById('player-song-artist');
+   };
