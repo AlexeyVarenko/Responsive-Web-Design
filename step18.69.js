99b21207404d7a9ce3f5df59dd64974dd829770b
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-10-31 08:45:50 +0300

    Create step18.69.js

---
+++ step18.69.js
@@ -0,0 +1,6 @@
+/*Step 69
+Access the userData?.currentSong?.title and userData?.currentSong?.artist properties and assign them to a currentTitle and currentArtist variables respectively.*/
+
+
+const currentTitle= userData?.currentSong?.title;
+  const currentArtist = userData?.currentSong?.artist;
