a7fb69bc2b188654e9ca91cb999011a0e4dc0fe5
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-10-31 08:45:46 +0300

    Create step18.67.js

---
+++ step18.67.js
@@ -0,0 +1,5 @@
+/*Step 67
+Next, you need to display the current song title and artist in the player display. Use const and arrow syntax to create an empty setPlayerDisplay function.*/
+
+
+const setPlayerDisplay = () => {}
