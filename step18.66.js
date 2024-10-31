c095f492cea1dc678b1bcc6307b82270f689c5d1
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-10-31 08:45:44 +0300

    Create step18.66.js

---
+++ step18.66.js
@@ -0,0 +1,8 @@
+/*Step 66
+Inside the playSong function, call the highlightCurrentSong function.
+
+After that, play around with the control buttons to see how the highlightCurrentSong function works.*/
+
+
+
+highlightCurrentSong();
