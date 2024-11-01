1af6c52d55ae163b538ca7f3e3c9511958174489
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-11-01 07:59:42 +0300

    Create step18.71.js

---
+++ step18.71.js
@@ -0,0 +1,9 @@
+/*To ensure the player's display updates whenever a new song begins playing, call the setPlayerDisplay() function within the playSong() function.
+
+Now you should see the song title and the artist show up in the display.*/
+
+
+
+function playSong() {
+    setPlayerDisplay()
+  }
