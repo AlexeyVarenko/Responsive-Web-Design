c78433e6290ad36e7fa74c513bb066db1143cce7
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-11-02 08:16:10 +0300

    Create step18.72.js

---
+++ step18.72.js
@@ -0,0 +1,8 @@
+/*Step 72
+To make the application more accessible, it is important that the play button describes the current song or the first song in the playlist.
+
+Start by creating an empty arrow function called setPlayButtonAccessibleText.*/
+
+
+
+const setPlayButtonAccessibleText = () => {}
