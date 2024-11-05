2e45d74959359ce84ceb7d2011d6aeb35e5a61fb
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-11-05 07:56:55 +0300

    Create step18.76.js

---
+++ step18.76.js
@@ -0,0 +1,7 @@
+/*Step 76
+Using const and arrow syntax to create an empty function called shuffle.
+
+This function is responsible for shuffling the songs in the playlist and performing necessary state management updates after the shuffling.*/
+
+
+const shuffle = () => {}
