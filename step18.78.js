581449366cbad6bd802c579382153a6a281204eb
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-11-06 07:56:09 +0300

    Create step18.78.js

---
+++ step18.78.js
@@ -0,0 +1,11 @@
+/*Step 78
+When the shuffle button is pressed, you want to set the currentSong to nothing and the songCurrentTime to 0.
+
+Set userData.currentSong to null and userData.songCurrentTime to 0.
+
+Note: You should not use optional chaining for this step because you are explicitly setting the currentSong and songCurrentTime properties to be null and 0 respectively.*/
+
+
+
+userData.currentSong=null;
+  userData.songCurrentTime=0;
