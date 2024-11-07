75bb0fcb53f244d0df8c1983681ece5a589ec4a1
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-11-07 08:05:47 +0300

    Create step18.81.js

---
+++ step18.81.js
@@ -0,0 +1,8 @@
+/*Step 81
+It's time to implement a delete functionality for the playlist. This would manage the removal of a song from the playlist, handle other related actions when a song is deleted, and create a Reset Playlist button.
+
+Use const and arrow syntax to create an empty deleteSong function and pass in id as a parameter.*/
+
+
+
+const deleteSong = (id) => {}
