41d38575eeed8734dc1ee1fd1734c58b81207453
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-11-07 08:05:46 +0300

    Create step18.80.js

---
+++ step18.80.js
@@ -0,0 +1,6 @@
+/*Step 80
+Add a "click" event listener to the shuffleButton element. For the function to run, pass in the shuffle function.
+
+Note: You don't need a callback inside this particular event listener. You also don't need to call the shuffle function, just pass in its identifier.*/a
+
+shuffleButton.addEventListener("click", shuffle);
