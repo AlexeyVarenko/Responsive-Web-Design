3b0f30a26a0f076a4918836c9524981f85be6947
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-11-11 07:55:01 +0300

    Create step18.87.js

---
+++ step18.87.js
@@ -0,0 +1,10 @@
+/*Step 87
+Next, you need to check if the playlist is empty. If it is, you should reset the userData object to its original state.
+
+Use an if statement to check if the userData?.songs has a length of 0.*/
+
+
+
+if (userData?.songs.length===0){
+
+}
