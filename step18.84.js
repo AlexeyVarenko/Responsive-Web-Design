3fdf63fa484f7e8c520def5d5801c325f1e09818
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-11-11 07:52:49 +0300

    Create step18.84.js

---
+++ step18.84.js
@@ -0,0 +1,10 @@
+/*Step 84
+Before deleting a song, you need to check if the song is currently playing. If it is, you need to pause the song and play the next song in the playlist.
+
+Use an if statement to check if the userData?.currentSong?.id is equal to the id of the song you want to delete.*/
+
+
+
+if (userData?.currentSong?.id===id){
+    
+}
