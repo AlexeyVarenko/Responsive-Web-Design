09b7126456684fd20cf8f2f6c80de90ca27a19c2
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-11-15 08:39:32 +0100

    Create step18.92.js

---
+++ step18.92.js
@@ -0,0 +1,10 @@
+/*Step 92
+Now, it's time to add the reset functionality to the resetButton. This will bring back the songs in the playlist when clicked.
+
+Add a click event listener to the resetButton variable. Pass in a callback using arrow syntax and leave it empty for now.*/
+
+
+
+resetButton.addEventListener("click", ()=>{
+    
+})
