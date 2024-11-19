fd863f98f375a1e7ffe92f79f940c32fdf262a6f
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-11-19 09:44:45 +0300

    Create step18.97.js

---
+++ step18.97.js
@@ -0,0 +1,8 @@
+/*Step 97
+Use an if statement to check if nextSongExists exists, then call the playNextSong() function in the if block. This will automatically play the next song when the current song ends.*/
+
+
+
+if (nextSongExists){
+    playNextSong()
+  }
