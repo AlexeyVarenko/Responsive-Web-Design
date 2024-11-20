ae4605a22fce6bbabdb9538d02f554edcec01596
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-11-20 10:12:05 +0300

    Create step19.2.js

---
+++ step19.2.js
@@ -0,0 +1,5 @@
+/*Step 2
+Use the .getElementById() method to get the #date-options element and use const to assign it to a variable named dateOptionsSelectElement.*/
+
+
+const dateOptionsSelectElement = document.getElementById("date-options")
