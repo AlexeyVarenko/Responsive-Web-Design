97bf91f9b9ccaec1f1c2330a97a2d39e36638723
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-11-21 09:05:49 +0300

    Create step19.3.js

---
+++ step19.3.js
@@ -0,0 +1,16 @@
+/*Step 3
+In JavaScript, there are many built-in constructors that create objects. A constructor is like a regular function, but starts with a capital letter, and is initialized with the new operator.
+
+For example, you can use the Date() constructor with the new operator to create a new Date object that returns a string with the current date and time:
+
+Example Code
+const currentDate = new Date();
+console.log(currentDate);
+
+// Output:
+// Mon Aug 23 2021 15:31:00 GMT-0400 (Eastern Daylight Time)
+Create a new const variable called date and assign it a Date object with new Date().*/
+
+
+
+const date = new Date();
