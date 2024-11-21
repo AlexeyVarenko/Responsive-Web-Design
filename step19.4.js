7be86f68771f318249f2128a00e040558724688f
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-11-21 09:05:50 +0300

    Create step19.4.js

---
+++ step19.4.js
@@ -0,0 +1,14 @@
+/*Step 4
+The Date object has a number of methods that allow you to get the date and time in different formats.
+
+One of those is the .getDate() method, which returns a number between 1 and 31 that represents the day of the month for that date. For example:
+
+Example Code
+const date = new Date();
+const dayOfTheMonth = date.getDate();
+console.log(dayOfTheMonth); // 20
+Using const, create a variable named day and assign it the day of the month from date with the .getDate() method.*/
+
+
+
+const day = date.getDate();
