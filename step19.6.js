6c52a8ecd8a21eda84cec4b891402e273c65875a
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-11-23 08:07:48 +0300

    Create step19.6.js

---
+++ step19.6.js
@@ -0,0 +1,8 @@
+/*Step 6
+The .getFullYear() method returns a number which represents the year for the provided date.
+
+Using const, create a variable named year and assign it the year from date with the .getFullYear() method.*/
+
+
+
+const year = date.getFullYear();
