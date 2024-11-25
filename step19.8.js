1789f4cdafcaa1acf52e1901ba51f3acd481e052
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-11-25 08:39:26 +0300

    Create step19.8.js

---
+++ step19.8.js
@@ -0,0 +1,8 @@
+/*Step 8
+The .getMinutes() method returns a number between 0 and 59 which represents the minutes for the provided date.
+
+Create a const variable named minutes and assign it the minutes from date with the .getMinutes() method.*/
+
+
+
+const minutes =date.getMinutes();
