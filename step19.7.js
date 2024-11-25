9fc2b92cf587574cf4d94f6034689e63b898f22b
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-11-25 08:39:24 +0300

    Create step19.7.js

---
+++ step19.7.js
@@ -0,0 +1,8 @@
+/*Step 7
+The .getHours() method returns a number between 0 and 23. This represents the hour for the provided date, where 0 is midnight and 23 is 11 p.m.
+
+Create a const variable named hours and assign it the hour from date with the .getHours() method.*/
+
+
+
+const hours= date.getHours();
