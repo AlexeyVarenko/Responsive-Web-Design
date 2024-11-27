d73f44b7928ffc2518f09bc1a2365a622f1b46ae
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-11-27 09:19:17 +0300

    Create step19.12.1.js

---
+++ step19.12.1.js
@@ -0,0 +1,9 @@
+/*Step 13
+To see the results of the formattedDate variable, add a console.log() statement and pass in the formattedDate variable as an argument.
+
+Open up the console and you should see the date printed out.*/
+
+
+
+const formattedDate = `${day}-${month}-${year}`;
+console.log(formattedDate);
