a2a8fa0d8ebd44e659616bcca1a0c004db566ebd
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-11-27 09:19:23 +0300

    Create step19.15.js

---
+++ step19.15.js
@@ -0,0 +1,14 @@
+/*Step 15
+In JavaScript, the change event is used to detect when the value of an HTML element has changed:
+
+Example Code
+element.addEventListener("change", () => {
+    
+});
+Attach the addEventListener method to the dateOptionsSelectElement. The first argument of the event listener should be the string "change" and the second argument should be an empty arrow function.*/
+
+
+
+dateOptionsSelectElement.addEventListener("change", () => {
+    
+});
