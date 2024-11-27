80a20dbb2461fd92138627b49ef7a7f3c6060577
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-11-27 09:19:20 +0300

    Create step19.14.js

---
+++ step19.14.js
@@ -0,0 +1,8 @@
+/*Step 14
+Use the textContent property on currentDateParagraph to set its text content to the value of the formattedDate variable.
+
+Also, make sure to remove your console.log(formattedDate); line.*/
+
+
+const formattedDate = `${day}-${month}-${year}`;
+currentDateParagraph.textContent=formattedDate;
