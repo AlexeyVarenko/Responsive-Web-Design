aee5e202ef904075da897743569af8f71e232e25
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-11-29 08:07:09 +0300

    Create step19.20.js

---
+++ step19.20.js
@@ -0,0 +1,19 @@
+/*Step 20
+To reverse an array of elements, you can use the reverse method. This method reverses the order of the elements in the array in place. The first element becomes the last, and the last element becomes the first.
+
+Here is an example of using the reverse method:
+
+Example Code
+// returns [5, 4, 3, 2, 1]
+[1, 2, 3, 4, 5].reverse(); 
+Chain the reverse method to your split method. Open up the console again to see the result.
+
+Remember that you learned how to chain methods in the previous project like this:
+
+Example Code
+method1().method2().method3();*/
+
+
+
+const exampleSentence = "selur pmaCedoCeerf".split("").reverse();
+console.log(exampleSentence);
