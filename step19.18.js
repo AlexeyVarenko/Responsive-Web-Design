14be5c8dfba90d97ade0411e0c97f9afc935b56c
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-11-29 08:07:05 +0300

    Create step19.18.js

---
+++ step19.18.js
@@ -0,0 +1,18 @@
+/*Step 18
+To format the date into yyyy-mm-dd, you will need to use the split, reverse, and join methods. But first, you will need to go through a few practice examples so you can better understand how to use them in the context of this project.
+
+The split() method is used to divide a string into substrings based on a specified separator. It then returns these substrings as elements of an array.
+
+Here is an example of taking the words "Hello World" and returning an array of one element:
+
+Example Code
+const greeting = "Hello World";
+greeting.split(); // ["Hello World"]
+Create a new const variable called exampleSentence and assign it the result of "selur pmaCedoCeerf".split().
+
+Then add a console statement to log the value of exampleSentence. Open up the console to see the result.*/
+
+
+
+const exampleSentence= "selur pmaCedoCeerf".split();
+console.log(exampleSentence);
