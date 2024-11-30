179418b6f7a9e863e3911bd9fb56e25a462c7f7f
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-11-30 09:27:06 +0300

    Create step19.22.js

---
+++ step19.22.js
@@ -0,0 +1,4 @@
+/*Step 22
+Now that you have a better understanding on how to work with the split, reverse, and join methods, you can delete your exampleSentence variable and console statement.*/
+
+
