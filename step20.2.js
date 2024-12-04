7cd2c53ced9ce1e339d45e64c747ff4295548b22
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-12-04 07:59:13 +0300

    Create step20.2.js

---
+++ step20.2.js
@@ -0,0 +1,7 @@
+/*Step 2
+Next, access the id called "sport" from the HTML document and store it in a const variable called typeOfSport. Below that variable, assign the id of "year" to a const variable called worldCupYear.*/
+
+
+
+const typeOfSport=document.getElementById("sport");
+const worldCupYear=document.getElementById("year");
