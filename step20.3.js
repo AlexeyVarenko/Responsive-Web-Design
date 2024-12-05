5a08497f77f38058e8eb2b981559ffabc0c7ce19
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-12-05 08:05:14 +0300

    Create step20.3.js

---
+++ step20.3.js
@@ -0,0 +1,6 @@
+/*Step 3
+Next, access the id called "head-coach" from the HTML document and store it in a const variable called headCoach. Below that variable, assign the id of "player-cards" to a const variable called playerCards.*/
+
+
+const headCoach = document.getElementById("head-coach");
+const playerCards = document.getElementById("player-cards");
