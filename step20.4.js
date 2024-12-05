cc8fcbf09d2bc8a28bd6676fedf9662617c1a7c1
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-12-05 08:05:15 +0300

    Create step20.4.js

---
+++ step20.4.js
@@ -0,0 +1,5 @@
+/*Step 4
+Create one more const variable called playersDropdownList and assign it the id of "players" using the getElementById method.*/
+
+
+const playersDropdownList = document.getElementById("players");
