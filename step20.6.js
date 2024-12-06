5e79b0da12b210b53c1bfd2a950ede042110a61c
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-12-06 08:07:30 +0300

    Create step20.6.js

---
+++ step20.6.js
@@ -0,0 +1,7 @@
+/*Step 6
+Inside the myFavoriteFootballTeam object, add a new property with a key named team and a string value of "Argentina".*/
+
+
+const myFavoriteFootballTeam = {
+    team:"Argentina"
+  };
