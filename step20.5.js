ca0976a52e81f295b8c49b180e06cd3ce5e53a4a
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-12-06 08:07:32 +0300

    Create step20.5.js

---
+++ step20.5.js
@@ -0,0 +1,8 @@
+/*Step 5
+Now it is time to build out the data structure that will hold all of the information for your football team.
+
+Below the variables you just created, create a new const variable called myFavoriteFootballTeam and assign it an empty object.*/
+
+
+
+const myFavoriteFootballTeam = {};
