636c98ad8e00260fe8f1cb68156e5c304faa36ec
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-12-07 09:07:34 +0300

    Create step20.8.js

---
+++ step20.8.js
@@ -0,0 +1,10 @@
+/*Step 8
+Inside the headCoach object, add a property with a key of coachName and a string value of "Carlos Bilardo". Below that property, add another key called matches with a number value of 7.*/
+
+
+
+headCoach: {
+    coachName: "Carlos Bilardo",
+    matches: 7,
+    
+  },
