0d95977652bc16dd8b7567a439a69e87db2a6e86
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-12-07 09:07:33 +0300

    Create step20.7.js

---
+++ step20.7.js
@@ -0,0 +1,16 @@
+/*Step 7
+Below the team property, add four property key-value pairs.
+
+A key named sport and a string value of "Football".
+A key named year and a number value of 1986.
+A key named isWorldCupWinner and a boolean value set to true.
+A new key called headCoach with a value of an empty object. */
+
+
+const myFavoriteFootballTeam = {
+    team: "Argentina",
+    sport: "Football",
+    year: 1986,
+    isWorldCupWinner: true,
+    headCoach: {},
+  };
