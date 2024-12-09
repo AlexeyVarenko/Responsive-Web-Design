e6a3ecc5b61e43223ebd586a742f426b7837226a
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-12-09 07:51:25 +0300

    Create step20.10.js

---
+++ step20.10.js
@@ -0,0 +1,18 @@
+/*Step 10
+Inside that players array, create a new object with the following properties:
+
+Example Code
+name: "Sergio Almirón"
+position: "forward"
+number: 1
+isCaptain: false
+nickname: null
+NOTE: The numbers for the team are organized alphabetically by last name. This differs from conventional numbering where the numbers correspond with what is on the player's jerseys.*/
+
+
+
+{name: "Sergio Almirón",
+    position: "forward",
+    number: 1,
+    isCaptain: false,
+    nickname: null}
