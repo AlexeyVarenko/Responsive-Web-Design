351b8fb3f3299df15f83b0243c95e2fb8067e0d4
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-12-09 07:51:27 +0300

    Create step20.9.js

---
+++ step20.9.js
@@ -0,0 +1,12 @@
+/*Step 9
+Below the headCoach property, create a new property with a key named players with the value of an empty array.*/
+
+
+
+headCoach: {
+    coachName: "Carlos Bilardo",
+    matches: 7,
+  },
+  players: [
+
+  ],
