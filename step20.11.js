cd3f908bd8f810ef5385b86f84a32e748cf1bbb0
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-12-10 08:08:04 +0300

    Create step20.11.js

---
+++ step20.11.js
@@ -0,0 +1,17 @@
+/*Step 11
+Below that object, create a new object with the following properties:
+
+Example Code
+name: "Sergio Batista"
+position: "midfielder"
+number: 2
+isCaptain: false
+nickname: null      */
+
+
+
+{name: "Sergio Batista",
+    position: "midfielder",
+    number: 2,
+    isCaptain: false,
+    nickname: null }
