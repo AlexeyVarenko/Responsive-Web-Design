3d960b1a271a560590ea114d4ed48390f1101790
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-12-10 08:08:02 +0300

    Create step20.12.js

---
+++ step20.12.js
@@ -0,0 +1,10 @@
+/*Step 12
+The rest of the data for the myFavoriteFootballTeam.players array has been filled out for you.
+
+The next step is to ensure that you can't modify this object by adding or removing any properties. We are going to use a method called Object.freeze(obj) which will freeze this object and prevent any changes being made to it.
+
+Use the Object.freeze() method to freeze the myFavoriteFootballTeam object.*/
+
+
+
+Object.freeze(myFavoriteFootballTeam)
