4a2d329b4ade23b59dc5b54da8af7872d4ae75b0
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-12-11 07:40:32 +0300

    Create step20.13.js

---
+++ step20.13.js
@@ -0,0 +1,8 @@
+/*Step 13
+The next step is to access the key called sport from the myFavoriteFootballTeam object and assign it to a new const variable called sport.
+
+Remember you can use dot notation for this.*/
+
+
+
+const sport= myFavoriteFootballTeam.sport;
