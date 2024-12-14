9e7b294d24d613f849d53c4bf86dc967c5fb1fbb
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-12-14 20:11:26 +0300

    Create step20.17.js

---
+++ step20.17.js
@@ -0,0 +1,7 @@
+/*Step 17
+Now you need to access the coachName value from the myFavoriteFootballTeam.headCoach object using the destructuring syntax.*/
+
+
+
+
+const { coachName } = myFavoriteFootballTeam.headCoach;
