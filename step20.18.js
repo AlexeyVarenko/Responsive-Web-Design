1b4af5f142e7fbbe6edc4312c09767df0c799a12
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-12-14 20:11:28 +0300

    Create step20.18.js

---
+++ step20.18.js
@@ -0,0 +1,10 @@
+/*Step 18
+Now you need to start displaying the team's information on the screen.
+
+Below your destructuring assignments, assign the sport variable to typeOfSport.textContent.
+
+Once you complete that task, you should see the result in the preview window.*/
+
+
+
+typeOfSport.textContent=sport;
