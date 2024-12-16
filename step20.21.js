4d31d35ac3bc07aa8bd200574b75ad006e14660e
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-12-16 09:06:30 +0300

    Create step20.21.js

---
+++ step20.21.js
@@ -0,0 +1,8 @@
+/*Step 21
+Now you will start building out the function that will show player cards based on the selections made by the user in the Filter Teammates dropdown menu.
+
+Start by creating an empty arrow function called setPlayerCards. You do not need to add a parameter because that will be taken care of in the next step.*/
+
+
+
+const setPlayerCards=()=>{}
