acaff7e4cf5a0b226d3940feef5b4407f94776f4
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-12-16 09:06:28 +0300

    Create step20.20.js

---
+++ step20.20.js
@@ -0,0 +1,11 @@
+/*Step 20
+Assign the year variable to worldCupYear.textContent.
+
+Below that, assign the coachName variable to headCoach.textContent.
+
+You should now see all of that information displayed on the screen below Team stats.*/
+
+
+
+worldCupYear.textContent=year;
+headCoach.textContent=coachName;
