960393b410b7b0a3c0068d09b06a92f121b2c697
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-12-24 07:38:36 +0300

    Create step20.33.js

---
+++ step20.33.js
@@ -0,0 +1,8 @@
+/*Step 33
+The next step is to create a function that will detect when a user makes a selection from the playersDropdownList.
+
+Use the .addEventListener() method on playersDropdownList. Inside the event listener, pass in a "change" event type and an empty callback function.*/
+
+
+
+playersDropdownList.addEventListener("change", ()=>{})
