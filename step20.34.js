48d7978c5c45111d6ea4550c3537faa09e684ee7
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-12-25 08:25:18 +0300

    Create step20.34.js

---
+++ step20.34.js
@@ -0,0 +1,10 @@
+/*Step 34
+For the callback function, pass in e as a parameter.
+
+e represents an object which contains the information for that event.*/
+
+
+
+playersDropdownList.addEventListener("change", (e) => {
+
+});
