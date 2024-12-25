78bf545ffc48fa92c2222dccc7522866136d4279
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-12-25 08:25:20 +0300

    Create step20.35.js

---
+++ step20.35.js
@@ -0,0 +1,10 @@
+/*Step 35
+Inside the callback function, add a console.log with the value of e.target.value.
+
+Open the console, and make a selection from the teammates dropdown menu. You should see the value of that selection in the console.
+
+e.target.value represents the value property from the playersDropdownList element. In future steps, you will use this value to show player cards based on the position they play.*/
+
+
+
+console.log(e.target.value);
