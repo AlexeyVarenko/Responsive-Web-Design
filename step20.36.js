a0ef61a555b5d3ff6b3b52d41f27cd17eb215730
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-12-26 07:57:13 +0300

    Create step20.36.js

---
+++ step20.36.js
@@ -0,0 +1,10 @@
+/*Step 36
+Remove the console.log statement you created in the previous step.
+
+The next step would be to reset the content for the playerCards element.
+
+Inside the callback function, access the innerHTML property of the playerCards element and assign it a value of an empty string.*/
+
+
+
+playerCards.innerHTML=""
