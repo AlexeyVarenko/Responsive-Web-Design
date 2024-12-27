52653672913504d17d51c0377468f452adc12475
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-12-27 07:49:39 +0300

    Create step20.37.js

---
+++ step20.37.js
@@ -0,0 +1,10 @@
+/*Step 37
+The next step would be to add a switch statement which will check for the user's selection from the player dropdown menu and filter out cards based on the player's positions.
+
+Add a switch statement and use e.target.value for the expression.*/
+
+
+
+switch (e.target.value) {
+  
+}
