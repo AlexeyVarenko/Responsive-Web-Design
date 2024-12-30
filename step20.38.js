262def25f83d669326439ee2a248e5aa4e510709
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2024-12-30 08:10:05 +0300

    Update step20.38.js

---
+++ step20.38.js
@@ -0,0 +1,7 @@
+/*Step 38
+If the user selects Nicknames from the dropdown menu you will want to filter out player cards that have a nickname.
+
+Start by adding a case clause for "nickname" inside your switch statement.*/
+
+
+
