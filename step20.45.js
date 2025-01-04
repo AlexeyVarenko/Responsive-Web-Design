7e0f6cfd37443013a20d90fbc9e2b28d5269cc04
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-01-04 09:54:18 +0300

    Create step20.45.js

---
+++ step20.45.js
@@ -0,0 +1,14 @@
+/*Step 45
+The final step is to add a default clause if none of the other case clauses match the user selection.
+
+For the default clause, call the setPlayerCards function without any arguments passed in.
+
+Test out your dropdown menu, and you should see the player cards be filtered out by position or nickname.
+
+Congratulations on completing the football team cards project!*/
+
+
+
+default: 
+setPlayerCards();
+break;
