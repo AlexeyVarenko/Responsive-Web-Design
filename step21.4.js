6e8669b574e2aa67dca3747b9adb3d64d9660d0c
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-01-07 12:30:29 +0300

    Create step21.4.js

---
+++ step21.4.js
@@ -0,0 +1,6 @@
+/*Step 4
+The last set of elements you need to get from the HTML file are the date-input and description-input elements. Save them in the variables dateInput and descriptionInput respectively.*/
+
+
+const dateInput = document.getElementById("date-input");
+const descriptionInput = document.getElementById("description-input");
