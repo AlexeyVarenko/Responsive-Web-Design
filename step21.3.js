89ae57466aa0eba1d21366b3adf4e70b91400a37
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-01-07 12:30:27 +0300

    Create step21.3.js

---
+++ step21.3.js
@@ -0,0 +1,8 @@
+/*Step 3
+Next, access the discard-btn, tasks-container, and title-input elements using the getElementById() method. Save them in variables named discardBtn, tasksContainer, and titleInput, respectively.*/
+
+
+
+const discardBtn = document.getElementById("discard-btn");
+const tasksContainer = document.getElementById("tasks-container");
+const titleInput = document.getElementById("title-input");
