159712c17606f4e19e156064849d7a6db937feb6
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-01-07 12:30:25 +0300

    Create step21.2.js

---
+++ step21.2.js
@@ -0,0 +1,8 @@
+/*Step 2
+You need to access more elements with the getElementById() method. This time you need the close-task-form-btn, add-or-update-task-btn, and cancel-btn elements. Save them in the variables closeTaskFormBtn, addOrUpdateTaskBtn, and cancelBtn.*/
+
+
+
+const closeTaskFormBtn = document.getElementById("close-task-form-btn");
+const addOrUpdateTaskBtn = document.getElementById("add-or-update-task-btn");
+const cancelBtn = document.getElementById("cancel-btn");
