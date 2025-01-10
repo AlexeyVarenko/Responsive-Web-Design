f7aae7adf243c1eb4b7c500e036c8a26f9c64906
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-01-10 07:54:02 +0300

    Create step21.10.js

---
+++ step21.10.js
@@ -0,0 +1,9 @@
+/*Step 10
+Now that you've worked on opening and closing the modal, it's time to get the values from the input fields, save them into the taskData array, and display them on the page.
+
+To start, add a submit event listener to your taskForm element and pass in e as the parameter of your arrow function. Inside the curly braces, use the preventDefault() method to stop the browser from refreshing the page after submitting the form.*/
+
+
+taskForm.addEventListener("submit", e => {
+    e.preventDefault();
+  });
