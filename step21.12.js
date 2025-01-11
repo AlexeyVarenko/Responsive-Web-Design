09ba04451e1213a26f54a0eddd8f58c18da61a23
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-01-11 09:11:55 +0300

    Create step21.12.js

---
+++ step21.12.js
@@ -0,0 +1,13 @@
+/*Step 12
+When a user creates a task, it should be saved in an object.
+
+Create a const variable called taskObj and assign it the value of an empty object.
+
+Then below that, add a console statement that logs the value of taskObj to the console.
+
+Open up the console to see the empty object.*/
+
+
+
+const taskObj={};
+console.log(taskObj)
