7ea40afaca12fbb774aceb5ce6d5fc1a60fcd1a7
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-01-11 09:11:56 +0300

    Create step21.13.js

---
+++ step21.13.js
@@ -0,0 +1,10 @@
+/*Step 13
+Inside your taskObj, add an id property name. For the value use the value of the titleInput.
+
+To see the new result, click on the "Add New Task" button. Then add a title and click on the "Add Task" button. Open up the console to see the result.*/
+
+
+const taskObj = {
+    id: titleInput.value
+  };
+  console.log(taskObj);
