f50e213c4a372f3a121f1d0905adb22be86ff16a
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-01-14 08:40:57 +0300

    Create step21.14.js

---
+++ step21.14.js
@@ -0,0 +1,12 @@
+/*Step 14
+The user should be able to input a title with upper and lowercase letters. But for the id, the value should be all lowercase.
+
+Update your titleInput.value to be all lowercase. You can use the toLowerCase() method to do this.
+
+Example Code
+str.toLowerCase();
+To see the new result, click on the "Add New Task" button. Then add a title of WALK DOG and click on the "Add Task" button. Open up the console to see the result of "walk dog".*/
+
+
+
+id: titleInput.value.toLowerCase();
