208ff51f7108df9ee6869e70ea19c1722f127a5b
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-01-16 10:03:46 +0300

    Create step21.19.js

---
+++ step21.19.js
@@ -0,0 +1,11 @@
+/*Step 19
+Now it is time to add the remaining properties to the taskObj object.
+
+Retrieve the values from the titleInput, dateInput, and descriptionInput fields, and then save them in the properties title, date, and description of the taskObj object.
+
+Add a new task and open up the console to see the taskObj object with the new properties.*/
+
+
+title:titleInput.value,
+    date:dateInput.value,
+    description:descriptionInput.value,
