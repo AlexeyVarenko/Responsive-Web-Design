4fc695bacb2ac8634a1ef609e340565c64152ba0
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-01-17 09:30:11 +0300

    Create step21.22.js

---
+++ step21.22.js
@@ -0,0 +1,7 @@
+/*Step 22
+Now that you have saved the task in the taskData array, you should display the task on the page by looping through it.
+
+Use forEach() on taskData, then destructure id, title, date, description as the parameters. Don't return anything yet.*/
+
+
+taskData.forEach(({id, title, date, description}=>{}))
