9367cb7beac1732ba110c2d50456412ecfa12b56
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-01-18 10:42:23 +0300

    Create step21.24.js

---
+++ step21.24.js
@@ -0,0 +1,6 @@
+/*Step 24
+Create a div element with the class of task. Utilize template strings to set the id attribute of the div to the id you destructured from the task data.*/
+
+
+
+<div class="task" id="${id}"></div> 
