b408e7bcb16bc07cf60e5308273fc6aae9db29ea
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-01-20 10:04:49 +0300

    Create step21.25.js

---
+++ step21.25.js
@@ -0,0 +1,5 @@
+/*Step 25
+Create a p element and use template strings to set its content to the title you destructured. Right before the content of the p element, create a strong element with the text Title:.*/a
+
+
+<p><strong>Title:</strong>${title}</p> 
