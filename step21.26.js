f11753bc5c8d0dff3cd83df1316e35e55540ad93
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-01-20 10:04:51 +0300

    Create step21.26.js

---
+++ step21.26.js
@@ -0,0 +1,6 @@
+/*Step 26
+Similarly to the previous step, create another p element, and interpolate the date you destructured as the text content. Inside this paragraph, create a strong element with the text Date:.*/
+
+
+
+<p><strong>Date:</strong> ${date}</p> 
