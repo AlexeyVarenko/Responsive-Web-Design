a3d7a959f6729b315408aa0aae8650f0deca8652
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-01-22 08:07:53 +0300

    Create step21.27.js

---
+++ step21.27.js
@@ -0,0 +1,5 @@
+/*Step 27
+Create one more p element and interpolate the description you destructured as the text. Also, create a strong element inside the paragraph with the text Description:.*/
+
+
+<p><strong>Description:</strong> ${description}</p> 
