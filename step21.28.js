ace29a8730bf5b170ef731a944952f6de6c1b614
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-01-22 08:07:55 +0300

    Create step21.28.js

---
+++ step21.28.js
@@ -0,0 +1,9 @@
+/*Step 28
+To allow for task management, you need to include both a delete and an edit button for each task.
+
+Create two button elements with the type attribute set to button and the class attribute set to btn. Set the text of the first button to Edit and the text of the second button to Delete.*/
+
+
+
+<button type="button" class="btn">Edit</button>
+  <button type="button" class="btn">Delete</button>
