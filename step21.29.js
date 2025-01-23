1872da9b38712c934a8358817e79861cbf01a6d1
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-01-23 07:52:34 +0300

    Create step21.29.js

---
+++ step21.29.js
@@ -0,0 +1,6 @@
+/*Step 29
+After adding the task to the page, you should close the form modal to view the task. To do this, utilize classList to toggle the hidden class on the taskForm element.*/
+
+
+
+taskForm.classList.toggle("hidden")
