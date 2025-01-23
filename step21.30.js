a6ffe28a5b372947b50f0baaa6764732fee849a0
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-01-23 07:52:36 +0300

    Create step21.30.js

---
+++ step21.30.js
@@ -0,0 +1,10 @@
+/*Step 30
+If you attempt to add another task now, you'll notice that the input fields retain the values you entered for the previous task. To resolve this, you need to clear the input fields after adding a task.
+
+Instead of clearing the input fields one by one, it's a good practice to create a function that handles clearing those fields. You can then call this function whenever you need to clear the input fields again.
+
+Use arrow syntax to create a reset function and set it to a pair of curly braces.*/
+
+
+
+const reset=()=>{}
