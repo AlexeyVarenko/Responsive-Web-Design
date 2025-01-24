9e97eb4013e08c57ffbc11deb108702e5fe967e3
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-01-24 08:01:33 +0300

    Create step21.32.js

---
+++ step21.32.js
@@ -0,0 +1,6 @@
+/*Step 32
+Remove the existing code toggling the class of hidden on taskForm and call the reset function instead. This would clear the input fields and also hide the form modal for the user to see the added task.*/
+
+
+
+reset()
