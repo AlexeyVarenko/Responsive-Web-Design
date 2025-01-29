1c9eac81d9ffbc9c778d7855d2f9471e5077bad6
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-01-29 07:58:08 +0300

    Create step21.40.js

---
+++ step21.40.js
@@ -0,0 +1,8 @@
+/*Step 40
+There's a problem. If you add a task, and then add another, the previous task gets duplicated. This means you need to clear out the existing contents of tasksContainer before adding a new task.
+
+Set the innerHTML of tasksContainer back to an empty string.*/
+
+
+
+tasksContainer.innerHTML=""
