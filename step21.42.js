35fbe10be0c52113cdf85849dd4ab995dbd6579e
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-01-30 07:50:00 +0300

    Create step21.42.js

---
+++ step21.42.js
@@ -0,0 +1,6 @@
+/*Step 42
+Create a deleteTask function using arrow syntax. Pass buttonEl as the parameter and define an empty set of curly braces for the function body.*/
+
+
+
+const deleteTask = (buttonEl) => {}
