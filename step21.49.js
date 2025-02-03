48f4e7bdb15efc19f7155488df281351a52715bd
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-02-03 08:14:26 +0300

    Create step21.49.js

---
+++ step21.49.js
@@ -0,0 +1,17 @@
+/*Step 49
+Set the innerText of the addOrUpdateTaskBtn button to Update Task.*/
+
+
+
+const editTask = (buttonEl) => {
+    const dataArrIndex = taskData.findIndex(
+    (item) => item.id === buttonEl.parentElement.id
+  );
+
+  currentTask = taskData[dataArrIndex];
+
+  titleInput.value = currentTask.title;
+  dateInput.value = currentTask.date;
+  descriptionInput.value = currentTask.description;
+  addOrUpdateTaskBtn.innerText="Update Task";
+}
