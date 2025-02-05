740f0ece44123d8c801abdb3122526f1ea40d083
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-02-05 08:43:59 +0300

    Create step21.50.js

---
+++ step21.50.js
@@ -0,0 +1,19 @@
+/*Step 50
+Finally, display the form modal with the values of the input fields by using classList to toggle the hidden class on taskForm.*/by
+
+
+const editTask = (buttonEl) => {
+    const dataArrIndex = taskData.findIndex(
+    (item) => item.id === buttonEl.parentElement.id
+  );
+
+  currentTask = taskData[dataArrIndex];
+
+  titleInput.value = currentTask.title;
+  dateInput.value = currentTask.date;
+  descriptionInput.value = currentTask.description;
+
+  addOrUpdateTaskBtn.innerText = "Update Task";
+  taskForm.classList.toggle("hidden");
+  
+}
