82672aa9bbd85cfc93033b2402b3b86d5a776594
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-02-10 10:12:14 +0300

    Create step21.59.js

---
+++ step21.59.js
@@ -0,0 +1,7 @@
+/*Step 59
+Using localStorage.clear() won't just delete a single item from local storage but will remove all items.
+
+Remove localStorage.removeItem() and use localStorage.clear() instead. You don't need to pass in anything. You should also see null in the console.*/
+
+
+localStorage.clear();
