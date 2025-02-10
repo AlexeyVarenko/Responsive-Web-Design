f38eba54aace9802a14c8718bbf5731cc14513b8
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-02-10 10:12:12 +0300

    Create step21.58.js

---
+++ step21.58.js
@@ -0,0 +1,8 @@
+/*Step 58
+You can use localStorage.removeItem() to remove a specific item and localStorage.clear() to clear all items in the local storage.
+
+Remove the data item from local storage and open the console to observe the result. You should see null.*/
+
+
+
+localStorage.removeItem("data")
