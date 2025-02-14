b1e9f3495a62d0f0519a2ab5c4138075896ebb9b
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-02-14 08:01:57 +0300

    Create step21.65.js

---
+++ step21.65.js
@@ -0,0 +1,8 @@
+/*Step 65
+If you try to add a new task, edit that task, and then click on the Add New Task button, you will notice a bug.
+
+The form button will display the incorrect text of "Update Task" instead of "Add Task". To fix this, you will need to assign the string "Add Task" to addOrUpdateTaskBtn.innerText inside your reset function.*/
+
+
+
+addOrUpdateTaskBtn.innerText="Add Task"
