30e95d7b0dd8d31de7e6b9eb517b3c4e94d036c0
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-02-17 07:47:31 +0300

    Create step22.1.js

---
+++ step22.1.js
@@ -0,0 +1,14 @@
+/*Step 1
+In this project, you'll build a decimal and binary converter and learn about both number systems. You'll also learn about recursion by using it to perform the conversions.
+
+All of the HTML and CSS for this project has been provided for you.
+
+When you're ready to get started, use the .getElementById() method to get the input element with the id "number-input", and store it in a variable called numberInput. Use the same method to get the button element with the id "convert-btn" and store it in a variable called convertBtn, and the output element with the id "result" and store it in a variable called result.
+
+NOTE: This project will only convert positive numbers into binary.*/
+
+
+
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("result");
