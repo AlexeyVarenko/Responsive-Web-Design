87d3b4440dcceea78dcd9fdfaed16f1568ea2d17
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-02-18 07:54:05 +0300

    Create step22.2.js

---
+++ step22.2.js
@@ -0,0 +1,9 @@
+/*Step 2
+Now you'll do some setup to check the value in the number input element whenever the user clicks the Convert button.
+
+First, create an empty function called checkUserInput.*/
+
+
+const checkUserInput= ()=>{
+  
+}
