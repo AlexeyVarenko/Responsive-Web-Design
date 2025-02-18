708cbb35b909396dbfd9264eb6b19d47f5bfc290
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-02-18 07:54:03 +0300

    Create step22.3.js

---
+++ step22.3.js
@@ -0,0 +1,7 @@
+/*Step 3
+A good way to test that everything is working is to log the value attribute of numberInput to the console. As a reminder, you can access the value attribute of an element by using dot or bracket notation.
+
+Within the checkUserInput function, use console.log() to log the value of numberInput to the console.*/
+
+
+console.log(numberInput.value)
