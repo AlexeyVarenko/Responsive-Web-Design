606373383fe0783cfb2458a1481cc186e5cebddc
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-02-20 18:49:49 +0300

    Create step22.9.js

---
+++ step22.9.js
@@ -0,0 +1,11 @@
+/*Step 9
+Now that your Convert button and number input are listening for clicks and Enter key presses, it's time to complete the checkUserInput() function.
+
+It would be helpful to alert users if they don't enter a value into the number input, or the number they enter is invalid. While the input type="number" element makes validation easier by only allowing numbers and some special characters, remember that all values you get from HTML elements are actually strings. Also, if the number input is empty, the value property will be an empty string.
+
+Inside your checkUserInput function, use an if statement to check if the value of numberInput is equal to an empty string. Leave the body of the if statement empty for now.*/
+
+
+if (numberInput.value=== "") {
+   
+}
