aa1cee40197cd6335a5248ab3aed8f15f4704718
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-02-20 18:49:47 +0300

    Create step22.8.js

---
+++ step22.8.js
@@ -0,0 +1,6 @@
+/*Step 8
+Next, within the body of the if statement, call the checkUserInput() function. After this, if you enter numbers into the number input and press the Enter / Return key, you should see numbers logged to the console.*/
+
+
+
+checkUserInput()
