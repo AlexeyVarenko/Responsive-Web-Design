be189ee27c77462f9fc9d322689d4187e149f54c
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-02-24 07:57:34 +0300

    Create step22.13.js

---
+++ step22.13.js
@@ -0,0 +1,9 @@
+/*Step 13
+Now you can alert the user if they don't enter a number, or the number is invalid before you attempt to convert it into binary.
+
+In the body of the if statement, use the alert() method to display the text "Please provide a decimal number greater than or equal to 0".
+
+Note that alert() is a method on the window object in the browser, so you can use either window.alert() or alert().*/
+
+
+alert("Please provide a decimal number greater than or equal to 0")
