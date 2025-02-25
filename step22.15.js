3bdb99d3a74568a9c4bfc8424b40a2e6ccb113f8
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-02-25 08:04:01 +0300

    Create step22.15.js

---
+++ step22.15.js
@@ -0,0 +1,10 @@
+/*Step 15
+Now you'll start building the function to actually do the decimal to binary conversion.
+
+Create a function called decimalToBinary with input as a parameter. Leave the body of the function empty for now.*/
+
+
+
+const decimalToBinary =(input)=>{
+
+}
