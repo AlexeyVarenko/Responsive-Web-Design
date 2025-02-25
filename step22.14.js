5a9102c37db5aea25ba7d229cc74832729e1c994
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-02-25 08:03:59 +0300

    Create step22.14.js

---
+++ step22.14.js
@@ -0,0 +1,7 @@
+/*Step 14
+After alerting the user if the number input is empty or the value is not a number, you can use the return keyword to break out of this function early. This will prevent future code in this function from running.
+
+Add the return keyword after alert().*/
+
+alert("Please provide a decimal number greater than or equal to 0");
+    return
