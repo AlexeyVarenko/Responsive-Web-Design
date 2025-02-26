c927fa4ac54aa9718c1afb72c90c9492a6b8f0a1
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-02-26 07:46:30 +0300

    Create step22.16.js

---
+++ step22.16.js
@@ -0,0 +1,5 @@
+/*Step 16
+Within your checkUserInput function, remove the console.log() statement. Then, call the decimalToBinary function and pass in the value of numberInput as an argument. Also, make sure to use the parseInt() function to convert the input into a number.*/
+
+
+decimalToBinary(parseInt(numberInput.value))
