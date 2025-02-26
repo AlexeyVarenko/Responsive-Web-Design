b93c231dbb9651c62c3dfbc4462be0647a964868
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-02-26 07:46:32 +0300

    Create step22.17.js

---
+++ step22.17.js
@@ -0,0 +1,8 @@
+/*Step 17
+Finally, you should clear the number input by setting its value to an empty string. Then later when you convert several numbers in a row, you won't have to delete the previous number before entering the next one.
+
+Set the value property of numberInput to an empty string.*/
+
+
+
+numberInput.value="";
