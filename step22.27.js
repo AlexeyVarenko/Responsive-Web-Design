211e97e585e6cecc10e61d67d70bc56afc6ad2b6
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-03-05 09:37:54 +0300

    Create step22.27.js

---
+++ step22.27.js
@@ -0,0 +1,8 @@
+/*Step 27
+Now that you have an operation that will lower the value of input each time the loop runs, you don't have to worry about the loop running forever.
+
+Update the last line of your while loop and assign quotient to input.*/
+
+
+
+input = quotient;
