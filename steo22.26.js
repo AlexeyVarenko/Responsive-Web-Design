d0fe7215d92f7cac21087d188e378ceff57a9493
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-03-05 09:37:52 +0300

    Create steo22.26.js

---
+++ steo22.26.js
@@ -0,0 +1,7 @@
+/*Step 26
+Like you saw in the last step, division can lead to a floating point number, or a number with a decimal point. The best way to handle this is to round down to the nearest whole number.
+
+Use the Math.floor() function to round down the quotient of input divided by 2 before it's assigned to quotient.*/
+
+
+const quotient = Math.floor(input / 2);
