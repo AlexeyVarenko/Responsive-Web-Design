8677e3a5ae474e64f488672ea8f19e21f1ff3137
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-03-06 10:05:33 +0300

    Create step22.29.js

---
+++ step22.29.js
@@ -0,0 +1,6 @@
+/*Step 29
+Inside your while loop, use the .push() method to append input to the inputs array. This will help you get a better idea of how the conversion works later when you log the contents of your arrays to the console.*/
+
+
+
+inputs.push(input);
