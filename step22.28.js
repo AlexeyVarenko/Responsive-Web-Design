5342d5f6aa20c786493cc90fc21b684fa1f7fab1
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-03-06 10:05:35 +0300

    Create step22.28.js

---
+++ step22.28.js
@@ -0,0 +1,12 @@
+/*Step 28
+Next, you need to calculate the remainder of input divided by 2. You can do this by using the remainder operator (%), which returns the remainder of the division of two numbers. For example:
+
+Example Code
+const remainder = 5 % 2; // 1
+In other words, the dividend, 5, can be divided by the divisor, 2, multiple times. Then you're left with a remainder of 1.
+
+Inside your while loop, create a variable named remainder and use the remainder operator to assign it the remainder of input divided by 2.*/
+
+
+
+const remainder=input%2;
