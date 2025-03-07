58901230fd49923e07c47eef5818130043f02509
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-03-07 10:48:45 +0300

    Create step22.30.js

---
+++ step22.30.js
@@ -0,0 +1,7 @@
+/*Step 30
+Use .push() to append the quotient variable to the quotients array. Also, append the remainder variable to the remainders array.*/
+
+
+inputs.push(input);
+    quotients.push(quotient);
+    remainders.push(remainder);
