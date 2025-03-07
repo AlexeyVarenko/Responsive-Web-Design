d319862dacc1a8881d5cd1b5560294010c872ccc
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-03-07 10:48:47 +0300

    Create step22.31.js

---
+++ step22.31.js
@@ -0,0 +1,8 @@
+/*Step 31
+Now's a good time to check your work.
+
+Log the text "Inputs: ", followed by a comma, followed by the inputs array to the console.*/
+
+
+
+console.log("Inputs: ", inputs);
