586fad13813b9767b8a94f902735e34b2bb4950b
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-03-08 14:22:37 +0300

    Create step22.32.js

---
+++ step22.32.js
@@ -0,0 +1,8 @@
+/*Step 32
+Next, log the text "Quotients: ", followed by a comma, followed by the quotients array to the console. Also, log the text "Remainders: ", followed by a comma, followed by the remainders array.*/
+
+
+
+console.log("Inputs: ", inputs);
+  console.log("Quotients: ", quotients);
+  console.log("Remainders: ", remainders);
