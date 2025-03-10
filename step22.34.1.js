a32adfa61f03aa4a3234b44f41ca673821820ddc
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-03-10 08:13:42 +0300

    Create step22.34.1.js

---
+++ step22.34.1.js
@@ -0,0 +1,10 @@
+/*Step 34
+Your decimalToBinary function works well, but there is an issue – because of the condition in your while loop, it only works for numbers greater than 0. If you try to convert 0 to binary, nothing will get added to the page.
+
+To fix this, add an if statement to check if input is equal to 0. Leave the body of the if statement empty for now.*/
+
+
+
+if(input===0){
+  
+}
