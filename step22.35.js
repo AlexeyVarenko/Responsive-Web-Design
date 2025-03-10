e977b9adf2378f7fbf3e4408d5a588165ea5f81b
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-03-10 08:13:48 +0300

    Create step22.35.js

---
+++ step22.35.js
@@ -0,0 +1,9 @@
+/*Step 35
+Within the body of the if statement, set the innerText property of result equal to the string "0". Then, use an early return statement to break out of the function early.*/
+
+
+
+if (input === 0) {
+    result.innerText="0";
+    return
+  }
