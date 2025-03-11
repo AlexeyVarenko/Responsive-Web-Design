f2629045a56108c9485585ff82fe0ddfe4d3cc86
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-03-11 09:25:56 +0300

    Create step22.37.js

---
+++ step22.37.js
@@ -0,0 +1,6 @@
+/*Step 37
+Since you'll want to display the result of the conversion, assign the binary variable to the innerText property of result at the end of the function.*/
+
+
+
+result.innerText= binary;
