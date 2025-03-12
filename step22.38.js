5ae645dfadee1041a12446e0ec45004f5ccf351c
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-03-12 08:26:46 +0300

    Create step22.38.js

---
+++ step22.38.js
@@ -0,0 +1,10 @@
+/*Step 38
+Create a while loop that runs as long as input is greater than 0. Inside the loop, assign 0 to input for now.
+
+Note: Be careful not to trigger the decimalToBinary function before you set input equal to 0 inside the loop. Otherwise, you could cause an infinite loop.*/
+
+
+
+while(input>0) {
+    input=0;
+  } 
