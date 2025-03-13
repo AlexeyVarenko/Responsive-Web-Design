5a9b9abdc281697bcc01ca96e86b76f35736091c
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-03-13 08:00:50 +0300

    Create step22.41.js

---
+++ step22.41.js
@@ -0,0 +1,7 @@
+/*Step 41
+Then, use the addition operator to add the current value of binary to the end of the equation input % 2. This is what will build the binary string from right to left.*/
+
+
+
+
+binary =input % 2+binary;
