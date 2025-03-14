4511495bf6eafbe96733d4311095ff40017b55a0
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-03-14 08:01:34 +0300

    Create step22.43.js

---
+++ step22.43.js
@@ -0,0 +1,10 @@
+/*Step 43
+Finally, you need to handle cases where input is 0. Rather than update the DOM and return early like you did before, you can update the binary string and let the rest of the code in the function run.
+
+Create an if statement that checks if input is equal to 0. If it is, set binary equal to the string "0".*/
+
+
+
+if (input==0){
+    binary="0";
+  }
