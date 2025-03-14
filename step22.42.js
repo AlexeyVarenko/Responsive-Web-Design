cf94c8de1463733f2b2744efb694a7696c559f39
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-03-14 08:01:33 +0300

    Create step22.42.js

---
+++ step22.42.js
@@ -0,0 +1,6 @@
+/*Step 42
+To clean things up a bit, wrap input % 2 in parentheses. This can sometimes change the order of operations, but in this case, it just makes your code easier to read.*/
+
+
+
+binary = (input % 2) + binary;
