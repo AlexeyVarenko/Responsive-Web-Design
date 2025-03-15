8884e2521ab0931438ad629947d5708d67b1fc25
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-03-15 08:54:43 +0300

    Create step22.45.js

---
+++ step22.45.js
@@ -0,0 +1,13 @@
+/*Step 45
+Next, create a function named b that returns the following: "is " + c().
+
+Also, create a function named c that returns the following: "awesome!".*/
+
+
+
+const b = () => {
+    return "is " + c();
+  };
+  const c = () => {
+    return "awesome!";
+  };
