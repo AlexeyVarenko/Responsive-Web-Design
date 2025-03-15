c0cb5e2af577013fec5df606e6abffa8956c8179
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-03-15 08:54:41 +0300

    Create step22.44.js

---
+++ step22.44.js
@@ -0,0 +1,10 @@
+/*Step 44
+Awesome. Now you have a more efficient way to convert decimal numbers into binary. After learning a bit about the call stack and recursion, you'll refactor the decimalToBinary function to use recursion instead of a while loop.
+
+Create a function named a that returns the following: "freeCodeCamp " + b().*/
+
+
+
+const a=()=>{
+    return ("freeCodeCamp " + b())
+  }
