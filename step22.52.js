1e3b5ca8716e079d6fd8734de70c439c94d6fcd3
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-03-20 08:04:04 +0300

    Create step22.52.js

---
+++ step22.52.js
@@ -0,0 +1,10 @@
+/*Step 52
+Then the function b() executes and evaluates to "is " + "awesome!".
+
+Update your mock call to b() so it looks like this: "b(): returns 'is ' + 'awesome!'".*/
+
+
+const callStack = [
+    'a(): returns "freeCodeCamp " + b()',
+    "b(): returns 'is ' + 'awesome!'"
+  ];
