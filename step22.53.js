3e3c2f2b9a27bb1a79778dd977fcb350aa523e0d
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-03-20 08:04:06 +0300

    Create step22.53.js

---
+++ step22.53.js
@@ -0,0 +1,9 @@
+/*Step 53
+Now that b() has executed, pop it off the call stack. Then, update your mock call to a() to the following: "a(): returns 'freeCodeCamp ' + 'is awesome!'".*/
+
+
+
+const callStack = [
+    'a(): returns "freeCodeCamp" + "is awesome!"',
+    
+  ];
