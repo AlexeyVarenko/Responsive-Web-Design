a4e77eac56933beb6b709a52c6b5cb663ab9160e
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-03-22 12:16:22 +0300

    Create step22.56.js

---
+++ step22.56.js
@@ -0,0 +1,9 @@
+/*Step 56
+Now you'll create a function that will count down from a given number to zero using recursion.
+
+Create a new function named countdown with number as a parameter. Leave the function body empty for now.*/
+
+
+
+const countdown = (number) => {
+}
