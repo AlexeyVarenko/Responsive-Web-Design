3b6700052123bc3c5e53447f701cccbe6d1cce4b
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-03-22 12:16:20 +0300

    Create step22.57.js

---
+++ step22.57.js
@@ -0,0 +1,8 @@
+/*Step 57
+The first thing you need to do is log the current value of number to the console to act as the countdown.
+
+Use console.log() to log number to the console.*/
+
+
+
+console.log(number);
