0931d7401a8b41a32dbf5b29c69ea64ffd28cd52
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-03-24 09:59:51 +0300

    Create step22.59.js

---
+++ step22.59.js
@@ -0,0 +1,12 @@
+/*Step 59
+Recursive functions also have a recursive case, which is where the function calls itself.
+
+First, convert your if statement into an if...else statement. Leave the body of your else statement empty for now.*/
+
+
+
+if (number === 0) {
+    return;
+  } else{
+
+  };
