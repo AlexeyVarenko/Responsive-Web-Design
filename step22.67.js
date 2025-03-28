eecacca9e0807a5dc07ca8ccf5308216a86d16f2
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-03-28 07:48:33 +0300

    Create step22.67.js

---
+++ step22.67.js
@@ -0,0 +1,12 @@
+/*Step 67
+As a reminder, it's often best to start with the base case when writing a recursive function so you know what you're working towards, and to prevent an infinite loop.
+
+Similar to your last implementation, you'll divide input by 2 repeatedly until input is 0.
+
+Use an if statement to check if input is equal to 0. If it is, return an empty string.*/
+
+
+
+if (input===0){
+    return ("")
+  }
