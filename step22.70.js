d91c9cca9d0bd6036831a92ff15aee228988697a
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-04-02 08:11:38 +0300

    Create step22.70.js

---
+++ step22.70.js
@@ -0,0 +1,6 @@
+/*Step 70
+Finally, in your checkUserInput() function, set the textContent property of result equal to the string returned by your decimalToBinary() function.*/
+
+
+
+result.textContent=decimalToBinary(parseInt(numberInput.value));
