d60363ec5fea1683d41aa01aebf01898c795292d
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-04-02 08:11:40 +0300

    Create step22.71.js

---
+++ step22.71.js
@@ -0,0 +1,10 @@
+/*Step 71
+Your converter should be working now. Feel free to try out different numbers and think about what is happening each time decimalToBinary() is called.
+
+But if you test your converter with 0, you'll see that nothing happens. This is because you return an empty string in your base case when input is 0. We can fix this now.
+
+Update your base case so that it returns the string "0" when input is equal to 0.*/
+
+
+
+return "0";
