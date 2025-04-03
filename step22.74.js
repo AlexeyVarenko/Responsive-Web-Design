54bde3138cbea0f3875380a575e02649b67ebfc6
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-04-03 09:16:06 +0300

    Create step22.74.js

---
+++ step22.74.js
@@ -0,0 +1,9 @@
+/*Step 74
+Now your decimalToBinary() function is complete. Feel free to test it out.
+
+If you're still confused about how it works under the hood, don't worry. Next, you'll create a simple animation to help you understand what's happening each step of the way.
+
+Create a new function called showAnimation. Leave the body of the function empty for now.*/a
+
+
+const showAnimation = () => {};
