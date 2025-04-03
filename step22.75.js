9c3dee16ebcd6cecae0c51144a1cfc552718cdb1
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-04-03 09:16:07 +0300

    Create step22.75.js

---
+++ step22.75.js
@@ -0,0 +1,10 @@
+/*Step 75
+You'll show the animation when users try to convert the decimal number 5 to binary, so you'll need to add a check for that within your checkUserInput() function.
+
+Use an if statement to check if the value attribute of numberInput is equal to the number 5. Remember to use the parseInt() function to convert the string into a number before comparing it to 5. Leave the if statement empty for now.*/
+
+
+
+if (parseInt(numberInput.value)==5){
+
+}
