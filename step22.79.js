6036e608c83a55fb07c5df9aa94f60d8c0290594
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-04-05 12:41:41 +0300

    Create step22.79.js

---
+++ step22.79.js
@@ -0,0 +1,10 @@
+/*Step 79
+Now that your showAnimation() function is set up, let's do some testing.
+
+Add three console.log() statements in the showAnimation() function to log the text "free", "Code", and "Camp" to the console. You should see that text in the console when you enter 5 into the number input and click the Convert button.*/
+
+
+
+console.log("free");
+console.log("Code");
+console.log("Camp");
