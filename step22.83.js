337b04e1ff83557b36a410f794d686afdfaa4c7c
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-04-08 09:52:58 +0300

    Create step22.83.js

---
+++ step22.83.js
@@ -0,0 +1,7 @@
+/*Step 83
+Now you're ready to start on the animation itself. You'll use an array of objects to store data for each frame of the animation.
+
+First, create a new variable called animationData and assign it an empty array.*/
+
+
+const animationData=[];
