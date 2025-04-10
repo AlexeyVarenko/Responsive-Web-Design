57753015e23165946246cab5e793952f2a93a516
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-04-10 10:53:47 +0300

    Create step22.84.js

---
+++ step22.84.js
@@ -0,0 +1,13 @@
+/*Step 84
+Next, you'll create an object to represent the first frame of your animation. Your object should have two properties or keys: inputVal, and addElDelay.
+
+inputVal will represent the value of the input each time your recursive function runs. And addElDelay will be the delay between adding DOM elements to the page.
+
+Add an object to animationData with an inputVal property set to 5, and an addElDelay property set to 1000.*/
+
+
+
+{
+    inputVal: 5,
+    addElDelay: 1000,
+  }
