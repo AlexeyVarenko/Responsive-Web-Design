fbcecd5225572e93e1de5fe29a0272934b3ae1be
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-04-11 09:59:32 +0300

    Create 22.86.js

---
+++ 22.86.js
@@ -0,0 +1,16 @@
+/*Step 86
+Add another object to the animationData array. Your new object should have the properties inputVal, and addElDelay set to 1, and 2000, respectively. Remember to treat the animationData array as a stack and add the new object to the top of the stack.*/
+
+
+{
+    inputVal: 5,
+    addElDelay: 1000
+  },
+  {
+    inputVal: 2,
+    addElDelay: 1500
+  },
+  {
+    inputVal: 1,
+    addElDelay: 2000
+  },
