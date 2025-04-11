3702f70345a67e870bcba6a41e69c98f307b44de
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-04-11 09:59:34 +0300

    Create step22.87.js

---
+++ step22.87.js
@@ -0,0 +1,7 @@
+/*Step 87
+Now you'll start building the animation itself.
+
+First, use the document.getElementById() method to select the element with the id animation-container and assign it to a variable called animationContainer.*/
+
+
+const animationContainer = document.getElementById('animation-container');
