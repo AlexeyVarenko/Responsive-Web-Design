7718514a5ebb40eb4e3898d0d93292b12fbe81d3
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-04-13 11:35:40 +0300

    Create step22.90.js

---
+++ step22.90.js
@@ -0,0 +1,7 @@
+/*Step 90
+Next, use the .forEach() method to loop through the animationData array. For the .forEach() method's callback function, pass in obj as a parameter, but leave the body of the callback function empty for now.*/
+
+
+animationData.forEach(function(obj){
+  
+});
