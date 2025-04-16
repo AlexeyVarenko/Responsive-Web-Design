d1ef243f1272357b0f79af25b2c99c7cf711dfd3
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-04-16 13:07:30 +0300

    Create step22.96.js

---
+++ step22.96.js
@@ -0,0 +1,6 @@
+/*Step 96
+Finally, use string interpolation to set the text of the paragraph element to decimalToBinary(${currVal}), where currVal is the inputVal property of the current object. After this, test out your code by entering the number 5 into the number input and clicking the Convert button.*/
+
+
+
+decimalToBinary(${obj.inputVal})
