b8ad13179bf3a3aa12f877f1cf2e8042f8c7be82
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-04-16 13:07:32 +0300

    Create step22.97.js

---
+++ step22.97.js
@@ -0,0 +1,12 @@
+/*Step 97
+Now it's time to set up for the next phase of the animation where you'll update and remove the paragraphs you append to the DOM during the animation.
+
+Add the property msg to the animation object at the top of the stack, and set its value to an empty string.*/
+
+
+
+{
+    inputVal: 1,
+    addElDelay: 2000,
+    msg: ""
+  }
