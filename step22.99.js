b5a6b0db400a6a6dc5e7fcd16c9b4bb26780afa3
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-04-17 11:24:33 +0300

    Create step22.99.js

---
+++ step22.99.js
@@ -0,0 +1,12 @@
+/*Step 99
+Next, add the property showMsgDelay with the value 5000 and removeElDelay with the value 10000.*/
+
+
+{
+    inputVal: 1,
+    addElDelay: 2000,
+    msg: "decimalToBinary(1) returns '1' (base case) and gives that value to the stack below. Then it pops off the stack.",
+    showMsgDelay: 5000,
+    removeElDelay:10000 
+  }
+
