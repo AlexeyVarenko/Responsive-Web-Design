f70db48ba6ea3134f33eeedb2e059ef11569afc2
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-04-17 11:24:30 +0300

    Create step22.98.js

---
+++ step22.98.js
@@ -0,0 +1,8 @@
+/*Step 98
+Set the value of the msg property to the following string:
+
+Example Code
+"decimalToBinary(1) returns '1' (base case) and gives that value to the stack below. Then it pops off the stack."*/
+
+
+msg: "decimalToBinary(1) returns '1' (base case) and gives that value to the stack below. Then it pops off the stack."
