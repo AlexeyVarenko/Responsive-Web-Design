37b25ae3b35afb8a10e1d6114633525f687ec7f2
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-04-18 10:57:01 +0300

    Create step22.101.js

---
+++ step22.101.js
@@ -0,0 +1,16 @@
+/*Step 101
+For the last animation object, add the property msg set to the following string:
+
+Example Code
+"decimalToBinary(5) returns '10' + 1 (5 % 2). Then it pops off the stack."
+Also, add the property showMsgDelay set to 15000 and the property removeElDelay set to 20000.*/
+
+
+
+{
+    inputVal: 5,
+    addElDelay: 1000,
+    msg: "decimalToBinary(5) returns '10' + 1 (5 % 2). Then it pops off the stack.",
+    showMsgDelay: 15000,
+    removeElDelay: 20000,
+  },
