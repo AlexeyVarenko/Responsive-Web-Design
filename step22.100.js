ebccd0b6c60a0ce44cc3b66a72b293e8e5e17f67
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-04-18 10:56:59 +0300

    Create step22.100.js

---
+++ step22.100.js
@@ -0,0 +1,17 @@
+/*Step 100
+For the object in the middle of the stack, add the property msg set to the following string:
+
+Example Code
+"decimalToBinary(2) returns '1' + 0 (2 % 2) and gives that value to the stack below. Then it pops off the stack."
+Also, add the property showMsgDelay set to 10000 and the property removeElDelay set to 15000.*/
+
+
+
+
+{
+    inputVal: 2,
+    addElDelay: 1500,
+    msg: "decimalToBinary(2) returns '1' + 0 (2 % 2) and gives that value to the stack below. Then it pops off the stack.",
+    showMsgDelay: 10000,
+    removeElDelay: 15000,
+  },
