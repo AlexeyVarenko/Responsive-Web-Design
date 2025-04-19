344a95b8a64365fbb5565ecbec5628e6a6880014
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-04-19 08:37:46 +0300

    Create step22.102.js

---
+++ step22.102.js
@@ -0,0 +1,8 @@
+/*Step 102
+For the next phase of the animation you'll update the paragraphs with the msg text. Since you have the delays for each step of the animation already, you can add your code to the same .forEach() loop.
+
+Add another setTimeout() function. Pass in an empty callback function as the first argument, and pass in the showMsgDelay property of the current object as the second argument.*/
+
+
+
+setTimeout(function(){}, obj.showMsgDelay)
