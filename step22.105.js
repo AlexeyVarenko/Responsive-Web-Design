86f047f0e1fbb23d4a8a7163acc04ab74a09398b
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-04-21 10:08:40 +0300

    Create step22.105.js

---
+++ step22.105.js
@@ -0,0 +1,5 @@
+/*Step 105
+Use the .getElementById() method to target the element with the id attribute with the value of the inputVal property of the current object. Then, use the .remove() method on that element to remove it from the DOM after the delay.*/
+
+
+document.getElementById(obj.inputVal).remove();
