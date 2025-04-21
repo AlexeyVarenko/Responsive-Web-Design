ecd7de60f4d1252f816f766d0c1a9855b1cab2c8
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-04-21 10:08:39 +0300

    Create step22.104.js

---
+++ step22.104.js
@@ -0,0 +1,10 @@
+/*Step 104
+Next, you'll remove the paragraph elements from the #show-animation element after the delays you specified earlier.
+
+Add a setTimeout() function to your .forEach() loop. Pass in an empty callback function as the first argument, and pass in the removeElDelay property of the current object as the second argument.*/
+
+
+
+setTimeout( function (){
+
+}, obj.removeElDelay)
