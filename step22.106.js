7ecd2c65393c38ef3e72d0ae37796c7d5d5128df
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-04-22 07:43:13 +0300

    Create step22.106.js

---
+++ step22.106.js
@@ -0,0 +1,11 @@
+/*Step 106
+Now your animation is complete. When you enter 5 in the number input and click the Convert button, the animation will add paragraphs to the DOM, update the text of each paragraph, and then remove the paragraphs from the DOM.
+
+The last thing you need to do is add the result of converting the number 5 into binary to the page once the animation is complete.
+
+After the .forEach() method, add another setTimeout() function. Pass in an empty callback function as the first argument, and a delay of 20000 milliseconds as the second argument.*/a
+
+
+setTimeout(function(){
+
+}, 20000);
