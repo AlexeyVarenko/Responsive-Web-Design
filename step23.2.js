e8c892ea9ce6632258780bdfdda12bef52ce1243
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-04-23 07:03:15 +0300

    Create step23.2.js

---
+++ step23.2.js
@@ -0,0 +1,5 @@
+/*Step 2
+Attach an event listener to your checkMessageButton, listening for the click event. Give it an empty callback function.*/
+
+
+checkMessageButton.addEventListener("click", ()=>{})
