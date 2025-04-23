53b1d8db1bbda26477120e21f1a48e956ecdaf7f
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-04-23 07:03:13 +0300

    Create step23.1.js

---
+++ step23.1.js
@@ -0,0 +1,8 @@
+/*Step 1
+To begin the project, use the .getElementById() method to retrieve the #message-input, #result, and #check-message-btn elements from the HTML document, and assign them to the variables messageInput, result, and checkMessageButton, respectively.*/
+
+
+
+const messageInput = document.getElementById("message-input")
+const result = document.getElementById("result")
+const checkMessageButton = document.getElementById("check-message-btn")
