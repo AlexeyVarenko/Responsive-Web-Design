aaef75094b914c92732b55001fbb7865873f03d9
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-04-24 10:21:00 +0300

    Create step23.4.js

---
+++ step23.4.js
@@ -0,0 +1,6 @@
+/*Step 4
+Create an isSpam function using the const keyword and arrow syntax. The function should take a single parameter msg and implicitly return false for now.*/
+
+
+
+const isSpam=(msg) =>false;
