291cbdc779b15146bd67b10ca8b434f89ada7476
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-04-25 08:17:28 +0300

    Create step23.6.js

---
+++ step23.6.js
@@ -0,0 +1,11 @@
+/*Step 6
+Your first regular expression will be used to catch help requests. Declare a helpRegex variable, and assign it a regular expression that matches the string please help.
+
+As a refresher, here is a regular expression to match the string hello world:
+
+Example Code
+const regex = /hello world/;*/
+
+
+
+const helpRegex = /please help/;
