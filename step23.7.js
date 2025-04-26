8ef079b34f6b211b5110b9ec6dca22fd902add20
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-04-26 08:57:37 +0300

    Create step23.7.js

---
+++ step23.7.js
@@ -0,0 +1,8 @@
+/*Step 7
+Regular expressions can take flags to modify their behavior. For instance, the i flag can be used to make the expression ignore case, causing it to match hello, HELLO, and Hello for the expression /hello/.
+
+Flags are added after the trailing slash. Add the i flag to your helpRegex.*/
+
+
+
+const helpRegex = /please help/i;
