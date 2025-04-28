f8451d63e519d6ab621e4f2347e06f0cfa0c45c8
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-04-28 07:46:29 +0300

    Create step23.10.js

---
+++ step23.10.js
@@ -0,0 +1,7 @@
+/*Step 10
+The alternate sequence | can be used to match either the text on the left or the text on the right of the |. For example, the regular expression /yes|no/ will match either yes or no.
+
+Update your helpRegex to match either please help or assist me.*/
+
+
+const helpRegex = /please help|assist me/i;
