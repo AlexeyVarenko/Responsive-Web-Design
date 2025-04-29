d80d79b1a2e110fa8c661977d4da7dea73cff90c
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-04-29 11:35:08 +0300

    Create step23.11.js

---
+++ step23.11.js
@@ -0,0 +1,8 @@
+/*Step 11
+Before you start creating additional regular expressions, you need to update your application to check more than one regular expression.
+
+Start by declaring a denyList variable. Assign it an array containing your helpRegex.*/
+
+
+
+const denyList=[helpRegex]
