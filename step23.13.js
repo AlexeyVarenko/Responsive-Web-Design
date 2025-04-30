e03fabee0f85e2afb21210d06e1f420e2bfedcf8
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-04-30 13:30:49 +0300

    Create step23.13.js

---
+++ step23.13.js
@@ -0,0 +1,8 @@
+/*Step 13
+The next regular expression you will work on is one that matches mentions of dollar amounts.
+
+Start by declaring a dollarRegex variable, and assign it a case-insensitive regular expression that matches the text dollars.*/
+
+
+
+const dollarRegex=/dollars/i
