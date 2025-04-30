efe111ddbc0511f10c20dbe051886a4f533f66a3
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-04-30 13:30:51 +0300

    Create step23.14.js

---
+++ step23.14.js
@@ -0,0 +1,7 @@
+/*Step 14
+Add your dollarRegex to the denyList array so that you can test the regular expression.
+
+Then try entering a message in your app.*/
+
+
+const denyList = [helpRegex, dollarRegex];
