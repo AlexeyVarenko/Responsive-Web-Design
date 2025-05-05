5e7e17571227c84dd3ec626b01a85f35568bc00c
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-05-05 10:46:20 +0300

    Create step23.22.js

---
+++ step23.22.js
@@ -0,0 +1,5 @@
+/*Step 22
+Your next regular expression will look for strings like free money. Start by declaring a freeRegex variable and assigning it a regular expression that will match the string free money. Remember to make it case-insensitive.*/
+
+
+const freeRegex=/free money/i
