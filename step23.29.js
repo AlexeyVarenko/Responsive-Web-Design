245910a20d60ef131e287b5c80cd6b3011d564a5
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-05-09 09:55:07 +0300

    Create step23.29.js

---
+++ step23.29.js
@@ -0,0 +1,10 @@
+/*Step 29
+Your next regular expression will match strings like stock alert. Declare a stockRegex variable and assign it a regular expression that will match the string stock alert. Remember to make it case insensitive.
+
+Add it to your denyList array as well.*/
+
+
+
+const stockRegex = /stock alert/i
+
+const denyList = [helpRegex, dollarRegex, freeRegex, stockRegex];
