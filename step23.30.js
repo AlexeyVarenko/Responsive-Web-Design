f8119e3fa2e63f1c22823e768ee8c6c24e07c857
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-05-09 09:55:09 +0300

    Create step23.30.js

---
+++ step23.30.js
@@ -0,0 +1,6 @@
+/*Step 30
+Like your freeRegex, update your stockRegex to replace the e and o characters with character classes to match the letter and the corresponding number.*/
+
+
+
+const stockRegex = /st[o0]ck al[e3]rt/i;
