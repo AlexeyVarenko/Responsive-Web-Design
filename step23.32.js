097176cf088659b32d112bf4f37f1fb9bbc23dc2
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-05-10 09:33:02 +0300

    Create step23.32.js

---
+++ step23.32.js
@@ -0,0 +1,6 @@
+/*Step 32
+Character classes can take more than two characters. Replace your a character with a character class that matches a, @, and 4.*/
+
+
+
+const stockRegex = /[s5][t7][o0]ck [a@4]l[e3]r[t7]/i;
