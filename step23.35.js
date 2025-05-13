e0e794653b0721bf6c53ad6914cddcf2544e74c9
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-05-13 08:40:00 +0300

    Create step23.35.js

---
+++ step23.35.js
@@ -0,0 +1,8 @@
+/*Step 35
+Your final regular expression will look for strings like dear friend. Declare a dearRegex and assign it a regular expression that will match the string dear friend. Remember to make it case insensitive, and add it to your denyList array.*/
+
+
+
+const dearRegex=/dear friend/i;
+
+const denyList = [helpRegex, dollarRegex, freeRegex, stockRegex, dearRegex];
