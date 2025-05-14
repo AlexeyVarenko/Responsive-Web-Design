86196f4055a5256da1faa3fc6c2699824f4b7b11
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-05-14 08:39:26 +0300

    Create step24.1.js

---
+++ step24.1.js
@@ -0,0 +1,8 @@
+/*Step 1
+In this project, you will be building a number sorter. The HTML and CSS have been provided for you. Feel free to explore them.
+
+When you are ready, declare a sortButton variable and assign it the value of .getElementById() with the argument "sort".*/
+
+
+
+const sortButton=document.getElementById("sort")
