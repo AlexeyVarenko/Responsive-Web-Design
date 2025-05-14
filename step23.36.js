f40b28de6923a96a51858c31c8da7bb3d5e5386f
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-05-14 08:39:24 +0300

    Create step23.36.js

---
+++ step23.36.js
@@ -0,0 +1,11 @@
+
+/* Step 36
+To put everything you have learned together, update your dearRegex to map the vowels to the corresponding numbers (note that i should match 1, and also match the pipe symbol |), and to match whole words.
+
+With that, your spam filter project is complete.*/
+
+
+
+
+
+const dearRegex = /(?:^|\s)d[e3][a@4]r fr[i1|][e3]nd(?:$|\s)/i;
