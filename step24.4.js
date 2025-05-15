4453177e6a769abba36addfe90b0b6153f8e547f
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-05-15 13:29:58 +0300

    Create step24.4.js

---
+++ step24.4.js
@@ -0,0 +1,7 @@
+/*Step 4
+To test your code as you write it, mount an event listener to your sortButton element. It should listen for the "click" event, and take sortInputArray as the callback.*/
+
+
+
+
+sortButton.addEventListener("click", sortInputArray)
