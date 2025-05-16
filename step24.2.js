c72e36cfd253b9072c9d385b82bce325f552172d
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-05-16 08:39:59 +0300

    Create step24.2.js

---
+++ step24.2.js
@@ -0,0 +1,5 @@
+/*Step 2
+To prepare your project's logic, use const and arrow syntax to declare a sortInputArray function. It should take a single event parameter.*/
+
+
+const sortInputArray=(event)=>{}
