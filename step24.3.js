d0f89307e49d36c700a48e589b23319b306cfb37
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-05-16 08:39:57 +0300

    Create step24.3.js

---
+++ step24.3.js
@@ -0,0 +1,10 @@
+/*Step 3
+You will be using this as an event listener for the sortButton. Because buttons associated with a form element submit by default, you need to prevent that behavior. Call event.preventDefault() in your function to do this.*/
+
+
+
+
+
+const sortInputArray = (event) => {
+    event.preventDefault()
+    }
