476fe81504e7ca6759328cc6c5a09e35af8c19f7
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-05-21 08:35:03 +0300

    Create step24.12.js

---
+++ step24.12.js
@@ -0,0 +1,12 @@
+/*Step 12
+To perform an action on each element in the array, use the method that is meant for iterating over arrays.
+
+Use the forEach() method, and pass it an empty callback which takes num and i as the parameters.*/
+
+
+
+const updateUI = (array = []) => {
+  array.forEach((num, i)=>{
+    
+  })
+}
