6363447bb60592fec9ef7acb980d69bdaa774594
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-05-22 14:40:01 +0300

    Create step24.15.js

---
+++ step24.15.js
@@ -0,0 +1,7 @@
+/*Step 15
+In your sortInputArray() function, call your updateUI() function and pass inputValues as the argument.
+
+You should now be able to click the Sort button and see the inputted array in the Output section.*/
+
+
+updateUI(inputValues);
