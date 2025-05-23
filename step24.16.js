ed8d91112296e3cf8d94aa5eec4437d9c07e3baa
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-05-23 09:27:42 +0300

    Create step24.16.js

---
+++ step24.16.js
@@ -0,0 +1,8 @@
+/*Step 16
+Now you need to actually sort the array. The first sorting algorithm you will implement is the bubble sort, which starts at the beginning of the array and 'bubbles up' unsorted values towards the end, iterating through the array until it is completely sorted.
+
+Begin by declaring a bubbleSort variable and assigning it an arrow function that takes an array parameter.*/
+
+
+
+const bubbleSort = (array) => {}
