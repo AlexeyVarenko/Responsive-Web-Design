e535c8c5adf4815388b257c44d8c66fa70e2e567
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-05-23 09:27:44 +0300

    Create step24.17.js

---
+++ step24.17.js
@@ -0,0 +1,11 @@
+/*Step 17
+You'll need to iterate through the array. For simplicity, use a for loop to do so.*/
+
+
+
+
+const bubbleSort = (array) => {
+  for (let i=0; i<array.length;i++){
+    
+  }
+}
