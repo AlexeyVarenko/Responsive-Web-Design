98e4058473ab7edf097e1eb6ddcb41d0fb57d1c4
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-05-24 13:24:28 +0300

    Create step24.18.js

---
+++ step24.18.js
@@ -0,0 +1,12 @@
+/*Step 18
+Because you need to compare elements, you'll need to use a nested for loop. This loop should iterate through every element in the array except the last one. Use j as your inner loop's iterator variable.*/
+
+
+
+const bubbleSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    for (let j=0; j<array.length -1;j++){
+      
+    }
+  }
+}
