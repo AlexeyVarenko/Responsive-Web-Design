73908c5dfe1ded12796a313a659f5d5a8a93e226
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-05-26 08:15:47 +0300

    Create step24.21.js

---
+++ step24.21.js
@@ -0,0 +1,10 @@
+/*Step 21
+To achieve the "bubble up" result, you need to check if the current element is larger than the next element. You can do this by accessing the array at j and j+1.
+
+Create an if condition that checks if the current element is larger than the next element.*/
+
+
+
+if (array[j]>array[j+1]){
+    
+  }
