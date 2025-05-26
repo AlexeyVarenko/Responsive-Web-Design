a6e3444c55b1989de3585031941b614ac6ec1a3b
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-05-26 08:15:45 +0300

    Create step24.20.js

---
+++ step24.20.js
@@ -0,0 +1,10 @@
+/*Step 20
+In your sortInputArray() function, declare a sortedValues variable. Assign it the value of calling bubbleSort with your inputValues array.
+
+Then, update your updateUI call to pass sortedValues as the argument.*/
+
+
+
+const sortedValues=bubbleSort(inputValues)
+
+  updateUI(sortedValues);
