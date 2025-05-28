d56ebc816c7c360bb2338ef40bde1c690285ff0a
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-05-28 08:45:27 +0300

    Create step24.25.js

---
+++ step24.25.js
@@ -0,0 +1,10 @@
+/*Step 25
+Time to implement another sorting algorithm. This time, you'll be implementing a selection sort. Selection sort works by finding the smallest value in the array, then swapping it with the first value in the array. Then, it finds the next smallest value in the array, and swaps it with the second value in the array. It continues iterating through the array until it is completely sorted.
+
+Start by declaring a selectionSort variable and assigning it an arrow function that takes an array parameter.*/
+
+
+
+const selectionSort =(array)=>{
+
+}
