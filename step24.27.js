eacec05c4ebf45c1a81ccdfb0d3b89971bbad12f
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-05-29 08:09:07 +0300

    Create step24.27.js

---
+++ step24.27.js
@@ -0,0 +1,10 @@
+/*Step 27
+Like a bubble sort, a selection sort needs to iterate through the array. Declare a for loop to do so.*/
+
+
+
+const selectionSort = (array) => {
+for (let i = 0; i < array.length; i++) {
+  
+}
+}
