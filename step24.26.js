bff43560269b19b1be55927d077a621e365961be
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-05-29 08:09:05 +0300

    Create step24.26.js

---
+++ step24.26.js
@@ -0,0 +1,8 @@
+/*Step 26
+Update your sortedValues variable to be the result of calling selectionSort instead of bubbleSort.*/
+
+
+
+  const sortedValues = selectionSort(inputValues);
+
+  updateUI(sortedValues);
