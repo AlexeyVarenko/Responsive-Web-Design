7f9ba3b419a083eb7975c07f6c6b81efea7c32e4
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-06-02 09:29:39 +0300

    Create step24.33.js

---
+++ step24.33.js
@@ -0,0 +1,9 @@
+/*Step 33
+The last sorting algorithm you will implement is the insertion sort. This algorithm works by building up a sorted array at the beginning of the list. It begins the sorted array with the first element. Then it inspects the next element and swaps it backward into the sorted array until it is in a sorted position, and so on.
+
+Start by declaring an insertionSort variable and assigning it an arrow function which takes an array parameter.*/
+
+
+
+
+const insertionSort = (array) => {}
