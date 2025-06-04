5a275a874af1cb586476e3cb377007c730733b84
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-06-04 08:03:05 +0300

    Create step24.36.js

---
+++ step24.36.js
@@ -0,0 +1,10 @@
+/*Step 36
+Declare a currValue variable and assign it the value at array[i]. Then, declare a j variable and assign it i - 1. Your j variable should be re-assignable.*/a
+
+
+const insertionSort = (array) => {
+  for (let i = 1; i < array.length; i++) {
+    const currValue=array[i];
+    let j=i-1;
+  }
+}
