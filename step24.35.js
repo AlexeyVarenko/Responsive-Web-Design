f5d65d1d2b39017d2569edf267f6c7b31270641b
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-06-04 08:03:01 +0300

    Create step24.35.js

---
+++ step24.35.js
@@ -0,0 +1,10 @@
+/*Step 35
+An insertion sort algorithm starts the sort at the beginning of the list, meaning the first element is already sorted. With this in mind, create a for loop that starts at the second element in the array - it should still iterate through the rest of the array.*/
+
+
+
+const insertionSort = (array) => {
+  for (let i=1; i<array.length; i++){
+    
+  }
+}
