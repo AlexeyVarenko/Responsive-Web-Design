e2138c228fc2e59fa169067f2856598466ff1afc
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-06-06 08:10:51 +0300

    Create step24.40.js

---
+++ step24.40.js
@@ -0,0 +1,18 @@
+/*Step 40
+After your for loop has finished, you need to return the array. You should then be able to see the Output change when you click the Sort button again.*/
+
+
+
+const insertionSort = (array) => {
+  for (let i = 1; i < array.length; i++) {
+    const currValue = array[i];
+    let j = i - 1;
+
+    while (j >= 0 && array[j] > currValue) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = currValue;
+  }
+  return array;
+}
