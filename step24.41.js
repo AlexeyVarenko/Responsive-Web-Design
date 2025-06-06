48f6724f8527dc0b259a99c8d8931d28afad4911
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-06-06 08:10:52 +0300

    Create step24.41.js

---
+++ step24.41.js
@@ -0,0 +1,8 @@
+/*Step 41
+To sort the elements of an array, you can use the built-in method called .sort(). Therefore, you can update the sortedValues variable by assigning it the result of calling .sort() on the inputValues array.*/
+
+
+
+  const sortedValues = inputValues.sort();
+
+  updateUI(sortedValues);
