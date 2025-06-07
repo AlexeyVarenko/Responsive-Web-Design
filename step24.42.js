815cc48568b5f8b5d0ae3c7761eb736690bab383
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-06-07 11:38:17 +0300

    Create step24.42.js

---
+++ step24.42.js
@@ -0,0 +1,8 @@
+/*Step 42
+The Sort button may appear to work correctly when clicked, but this is only because all the values in the array are single digits, and the sorting may not work as expected with more complex values.
+
+Change the value and text of the option element that is selected from 1 to 10, and click the Sort button again.*/
+
+
+
+<option value="10" selected>10</option>
