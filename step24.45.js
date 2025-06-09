853c0884e4893c8a8366553d2c7ffabef2711b43
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-06-09 10:11:05 +0300

    Create step24.45.js

---
+++ step24.45.js
@@ -0,0 +1,7 @@
+/*Step 45
+If you press the Sort button again, you should see that 10 is now in the correct position of the Output.
+
+To finish this project, change your option back to a value and text of 1.*/
+
+
+<option value="1" selected>1</option>
