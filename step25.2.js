50dd14f9b14ae91c0364fb56d96c2a5861c3c0b1
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-06-10 08:39:39 +0300

    Create step25.2.js

---
+++ step25.2.js
@@ -0,0 +1,10 @@
+/*Step 2
+To begin, the calculate function needs to find the number that was entered in the #numbers input field. To do this, use a .querySelector to locate the input field and then use the .value property to get the number entered.
+
+Store this in a value variable.*/
+
+
+
+const calculate = () => {
+  const value=document.querySelector("#numbers").value;
+}
