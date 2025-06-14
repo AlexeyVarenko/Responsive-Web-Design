725fa2d9df3dadf33cae36e34b500a5ad921b87d
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-06-14 15:34:15 +0300

    Create step25.10.js

---
+++ step25.10.js
@@ -0,0 +1,10 @@
+/*Step 10
+The mean is the average value of all numbers in a list. The first step in calculating the mean is to take the sum of all numbers in the list. Arrays have another method, called .reduce(), which is perfect for this situation. The .reduce() method takes an array and applies a callback function to condense the array into a single value.
+
+Declare a sum variable and assign array.reduce() to it.*/
+
+
+
+const getMean = (array) => {
+  const sum=array.reduce();
+}
