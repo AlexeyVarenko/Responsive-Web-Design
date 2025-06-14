ba4c7220bbc9addf163ed4fe494d8af219b3bbeb
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-06-14 15:34:16 +0300

    Create step25.9.js

---
+++ step25.9.js
@@ -0,0 +1,10 @@
+/*Step 9
+That is as far as you can get with the calculate function for now. It is time to write your mean logic.
+
+Create an empty function called getMean. It should take a single parameter array.*/
+
+
+
+const getMean=(array)=>{
+
+}
