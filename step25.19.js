1a56c863a63aea9c863f6e0a57586c2d5c7970a9
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-06-20 08:12:15 +0300

    Create step25.19.js

---
+++ step25.19.js
@@ -0,0 +1,9 @@
+/*Step 19
+Time to start working on the median calculation. The median is the midpoint of a set of numbers.
+
+Begin with an empty function called getMedian, which should take an array parameter.*/
+
+
+
+
+const getMedian = (array) =>{}
