fd3962983158c1a7472ae6c395f9a281693aadca
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-06-20 08:12:17 +0300

    Create step25.20.js

---
+++ step25.20.js
@@ -0,0 +1,9 @@
+/*Step 20
+The first step in calculating the median is to ensure the list of numbers is sorted from least to greatest. Once again, there is an array method ideal for this – the .sort() method.
+
+Declare a sorted variable and assign array.sort() to it.*/
+
+
+const getMedian = (array) => {
+  const sorted=array.sort();
+}
