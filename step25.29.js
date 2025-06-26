21bf48a5de580b4326f46f4cd16bb73ca9de95dc
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-06-26 10:22:01 +0300

    Create step25.29.js

---
+++ step25.29.js
@@ -0,0 +1,6 @@
+/*Step 29
+Your next calculation is the mode, which is the number that appears most often in the list. To get started, declare a getMode function that takes the same array parameter you have been using.*/
+
+
+
+const getMode = (array) => {}
