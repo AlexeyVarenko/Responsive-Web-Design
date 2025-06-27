2c389ba813a82bb2518757fefb686dc3a471f1f6
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-06-27 09:45:14 +0300

    Create step25.31.js

---
+++ step25.31.js
@@ -0,0 +1,8 @@
+/*Step 31
+To better understand how the getMode function is going to work, you will need to print out its contents. This will allow you to see what is happening as you build out the function. But first you will need to return the array so it can be tested properly.
+
+Inside your getMode function return your array parameter.*/
+
+
+
+return array;
