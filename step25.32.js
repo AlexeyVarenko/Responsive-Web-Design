1896b1cecdab135c352b674712796bf6abf045c8
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-06-27 09:45:16 +0300

    Create step25.32.js

---
+++ step25.32.js
@@ -0,0 +1,13 @@
+/*Step 32
+Inside the calculate function, you have already called the getMean and getMedian functions.
+
+Below those function calls, add a console.log(getMode(numbers)).
+
+To see the result, enter the numbers 4, 4, 2, 5 and click on the "Calculate" button. Open up the console to see the following array:
+
+Example Code
+[ 4, 4, 2, 5 ]*/
+
+
+
+console.log(getMode(numbers))
