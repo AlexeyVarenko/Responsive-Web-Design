732f0d50d36e69056ecf6ffb8b0508101a4265ca
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-06-28 17:25:58 +0300

    Create step25.33.js

---
+++ step25.33.js
@@ -0,0 +1,12 @@
+/*Step 33
+Inside your getMode function, on the empty line above your return statement, call forEach on array. Your .forEach() method should have an empty callback function that takes an el parameter.
+
+In the next few steps, you will use this loop to count the frequency of occurrences of each number in the array.*/
+
+
+
+const getMode = (array) => {
+  const counts = {};
+  array.forEach(el=>{})
+  return array;
+}
