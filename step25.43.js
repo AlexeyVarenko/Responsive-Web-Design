fc78e0bbafd46006b41612e58fc879d0aae6c6ae
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-07-04 12:13:30 +0300

    Create step25.43.js

---
+++ step25.43.js
@@ -0,0 +1,23 @@
+/*Step 43
+Time to return your mode variable.
+
+mode is an array, so return it as a string with the .join() method. Separate the elements with a comma followed by a space.*/
+
+
+
+const getMode = (array) => {
+  const counts = {};
+  array.forEach((el) => {
+    counts[el] = (counts[el] || 0) + 1;
+  })
+  if (new Set(Object.values(counts)).size === 1) {
+    return null;
+  }
+  const highest = Object.keys(counts).sort(
+    (a, b) => counts[b] - counts[a]
+  )[0];
+  const mode = Object.keys(counts).filter(
+    (el) => counts[el] === counts[highest]
+  );
+  return mode.join(", ")
+}
