eb5aac9652e5ff6c186ba18fc2bc760b36a60956
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-07-07 09:34:23 +0300

    Create step25.47.js

---
+++ step25.47.js
@@ -0,0 +1,11 @@
+/*Step 47
+The variance of a series represents how much the data deviates from the mean, and can be used to determine how spread out the data are. The variance is calculated in a few steps.
+
+Start by declaring a getVariance function that takes an array parameter. Within that function, declare a mean variable and assign it the value of the getMean function, passing array as the argument.*/
+
+
+
+
+const getVariance = (array) => {
+  const mean =getMean(array);
+};
