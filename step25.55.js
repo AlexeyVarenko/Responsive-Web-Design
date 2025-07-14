86f0c2d285b56f93d767adee2bc2dfc300e60ef7
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-07-14 08:29:40 +0300

    Create step25.55.js

---
+++ step25.55.js
@@ -0,0 +1,10 @@
+/*Step 55
+Your final calculation is the standard deviation, which is the square root of the variance.
+
+Begin by declaring a getStandardDeviation function, with the array parameter. In the function body, declare a variance variable and assign it the variance of the array.*/
+
+
+
+const getStandardDeviation = (array) => {
+  const variance=getVariance(array);
+}
