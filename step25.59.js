e07abba860fd962d75d9c5f23fa6387806c2ec44
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-07-16 08:17:58 +0300

    Create step25.59.js

---
+++ step25.59.js
@@ -0,0 +1,21 @@
+/*Step 59
+Lastly update the calculate function to include the standard deviation logic, like you did with your other functions.
+
+Congratulations! Your project is complete.*/
+
+
+
+const mean = getMean(numbers);
+  const median = getMedian(numbers);
+  const mode = getMode(numbers);
+  const range = getRange(numbers);
+  const variance = getVariance(numbers);
+  const standardDeviation = getStandardDeviation(numbers);
+
+
+  document.querySelector("#mean").textContent = mean;
+  document.querySelector("#median").textContent = median;
+  document.querySelector("#mode").textContent = mode;
+  document.querySelector("#range").textContent = range;
+  document.querySelector("#variance").textContent = variance;
+  document.querySelector("#standardDeviation").textContent = standardDeviation;
