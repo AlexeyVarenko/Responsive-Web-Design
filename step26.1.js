1b6118dfb7d9bb7a1b05fc06fd8e388ff4c4c3f6
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-07-16 08:18:01 +0300

    Create step26.1.js

---
+++ step26.1.js
@@ -0,0 +1,11 @@
+/*Step 1
+Your project starts with a basic HTML container and some corresponding CSS. Your first task will be to programmatically generate the cells for your spreadsheet.
+
+The global window object represents the browser window (or tab). It has an onload property which allows you to define behavior when the window has loaded the entire page, including stylesheets and scripts.
+
+Start by setting the onload property of window to an arrow function with no parameters. In the function, declare a container variable and assign it the value of getting the element by the id of "container".*/
+
+
+window.onload=()=>{
+  const container=document.getElementById("container")
+}
