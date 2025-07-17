94a8d1a5ecd72d7d39d53e95fea83933ea4c3b40
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-07-17 08:27:29 +0300

    Create step26.2.js

---
+++ step26.2.js
@@ -0,0 +1,19 @@
+/*Step 2
+Functions are ideal for reusable logic. When a function itself needs to reuse logic, you can declare a nested function to handle that logic. Here is an example of a nested function:
+
+Example Code
+const outer = () => {
+  const inner = () => {
+
+  };
+};
+Declare a nested createLabel function using arrow syntax. It should take a name parameter.*/
+
+
+
+window.onload = () => {
+  const container = document.getElementById("container");
+ const createLabel=(name)=>{
+
+ }
+}
