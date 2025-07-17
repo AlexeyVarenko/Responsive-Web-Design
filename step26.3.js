776d39bd87b5152bd7869e6852ebd8bd113be76c
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-07-17 08:27:31 +0300

    Create step26.3.js

---
+++ step26.3.js
@@ -0,0 +1,11 @@
+/*In your createLabel function, declare a label variable and assign it a new div element.*/
+
+
+
+window.onload = () => {
+  const container = document.getElementById("container");
+  const createLabel = (name) => {
+    const label = document.createElement("div");
+
+  }
+}
