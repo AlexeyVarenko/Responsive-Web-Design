3a4b474adbc1ef7d7e38504b0675ca3ddfbb2c36
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-07-18 08:17:21 +0300

    Create step26.5.js

---
+++ step26.5.js
@@ -0,0 +1,14 @@
+/*Step 5
+Finally, use the .appendChild() method to add your label element to the container element.*/
+
+
+
+window.onload = () => {
+  const container = document.getElementById("container");
+  const createLabel = (name) => {
+    const label = document.createElement("div");
+    label.className = "label";
+    label.textContent = name;
+    container.appendChild(label);
+  }
+}
