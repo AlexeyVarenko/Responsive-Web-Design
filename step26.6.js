8b6a23194b9418fc6035705b89220e5a6e17080d
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-07-19 16:44:29 +0300

    Create step26.6.js

---
+++ step26.6.js
@@ -0,0 +1,8 @@
+/*Step 6
+You will need a function to generate a range of numbers.
+
+Declare an empty range function which takes a start and end parameter. Use the Array() constructor and implicitly return an empty array.*/
+
+
+
+const range=(start,end)=>Array()
