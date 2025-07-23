f6b383a582d593cf488c244e16e917a606c67151
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-07-23 08:23:24 +0300

    Create step26.11.js

---
+++ step26.11.js
@@ -0,0 +1,8 @@
+/*Step 10
+Now that you have a range function, you can use it to create a range of letters as well.
+
+Declare a charRange function using const and arrow syntax. It should take a start and end parameter. The function should implicitly return the result of calling range() with start and end as the arguments.*/
+
+
+
+const charRange=(start, end)=>range(start, end)
