15542405df40223c2a6c15cb1f789835e53cf171
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-07-23 08:23:28 +0300

    Create step26.13.js

---
+++ step26.13.js
@@ -0,0 +1,6 @@
+/*Step 13
+Now that your helper functions are complete, back in your onload event handler you should declare a letters variable. Assign it the result of calling charRange() with the letters "A" and "J" as arguments.*/
+
+
+
+const letters=charRange("A", "J")
