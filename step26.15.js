a6254172395e7ef3c3349fda9d2356115497eff8
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-07-25 08:34:31 +0300

    Create step26.15.js

---
+++ step26.15.js
@@ -0,0 +1,10 @@
+/*Step 15
+Remember that range() returns an array, so you can chain array methods directly to the function call.
+
+Call range() with 1 and 99 as the arguments, and chain the .forEach() method. Pass the .forEach() method an empty callback which takes number as the parameter.*/
+
+
+
+range(1, 99).forEach(number=>{
+  
+})
