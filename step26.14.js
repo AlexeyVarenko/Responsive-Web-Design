860c4b714a0b74a0dbcc4ef04ea5c00489f26c38
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-07-25 08:34:29 +0300

    Create step26.14.js

---
+++ step26.14.js
@@ -0,0 +1,8 @@
+/*Step 14
+Now call the .forEach() method of your letters array, and pass your createLabel function reference as the callback.
+
+You should see some letters appear across the top of your spreadsheet.*/
+
+
+
+letters.forEach(createLabel);
