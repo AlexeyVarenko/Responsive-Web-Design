90a158dd98b92877cba0765296a50731f87ec9c9
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-07-29 08:55:44 +0300

    Create step26.21.js

---
+++ step26.21.js
@@ -0,0 +1,6 @@
+/*Step 21
+Declare an isEven function, which takes a num parameter and returns true if the number is even, and false otherwise. Use the modulo operator % to determine if a number is even or odd.*/
+
+
+
+const isEven=num=>num%2===0;
