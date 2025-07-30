73ebf41a7c582e21800cf6372b80421fb3a29c87
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-07-30 08:30:37 +0300

    Create step26.22.js

---
+++ step26.22.js
@@ -0,0 +1,8 @@
+/*Step 22
+Declare an average function which takes an array of numbers as the nums parameter. It should return the average of all the numbers in the array.
+
+The average can be calculated by dividing the sum of all the numbers in the array by the length of the array. Remember that you have a sum function you can use.*/
+
+
+
+const average = nums =>sum(nums)/nums.length;
