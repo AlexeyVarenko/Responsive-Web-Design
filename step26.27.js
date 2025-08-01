8859ef66e9e75a1a587361ec0423400cacfaa303
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-08-01 10:33:52 +0300

    Create step26.27.js

---
+++ step26.27.js
@@ -0,0 +1,8 @@
+/*Step 27
+Now you can start using your spreadsheet functions. Begin by declaring an update arrow function. It should take an event parameter.*/
+
+
+
+const update=(event)=>{
+  
+}
