7a569500e479ed7dbe5b864155d6a5bb31cc9d9b
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-08-02 13:13:16 +0300

    Create step26.28.js

---
+++ step26.28.js
@@ -0,0 +1,7 @@
+/*Step 28
+In your window.onload function, you need to tell your input elements to call the update function when the value changes. You can do this by directly setting the onchange property.
+
+Set the onchange property to be a reference to your update function.*/
+
+
+input.onchange=update;
