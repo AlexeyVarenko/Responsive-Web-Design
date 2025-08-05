7bbce488ddc2012ac39d8c266953112027d4f566
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-08-05 08:50:21 +0300

    Create step26.33.js

---
+++ step26.33.js
@@ -0,0 +1,10 @@
+/*Step 33
+In order to run your spreadsheet functions, you need to be able to parse and evaluate the input string. This is a great time to use another function.
+
+Declare an evalFormula arrow function which takes the parameters x and cells.*/
+
+
+
+const evalFormula=(x, cells)=>{
+  
+}
