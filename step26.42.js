f6decff348dfa3562e1ac443613a4b973b3fda59
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-08-11 08:21:21 +0300

    Create step26.42.js

---
+++ step26.42.js
@@ -0,0 +1,13 @@
+/*Step 42
+Declare a function elemValue which takes a num parameter. The function should be empty.*/
+
+
+
+const evalFormula = (x, cells) => {
+  const idToText = id => cells.find(cell => cell.id === id).value;
+  const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
+  const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
+  const elemValue=num=>{
+
+  }
+}
