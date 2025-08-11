94b4e5adaaacb847fa4fd71033d13803e6ffe4ea
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-08-11 08:21:23 +0300

    Create step26.43.js

---
+++ step26.43.js
@@ -0,0 +1,18 @@
+/*Step 43
+In your elemValue function, declare a function called inner which takes a character parameter.
+
+Then, return your inner function.*/
+
+
+
+const evalFormula = (x, cells) => {
+  const idToText = id => cells.find(cell => cell.id === id).value;
+  const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
+  const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
+  const elemValue = num => {
+    const inner=character=>{
+
+    }
+    return inner();
+  }
+}
