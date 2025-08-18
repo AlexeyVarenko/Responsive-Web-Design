c08b12eaa451a2117b24b0d1b07c0c560ba9e4d9
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-08-18 08:45:34 +0300

    Create step26.53.js

---
+++ step26.53.js
@@ -0,0 +1,13 @@
+/*Step 53
+Declare a rangeExpanded variable and assign it the result of calling the .replace() method of your x parameter. Pass the rangeRegex variable as the argument.*/
+
+
+
+const evalFormula = (x, cells) => {
+  const idToText = id => cells.find(cell => cell.id === id).value;
+  const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
+  const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
+  const elemValue = num => character => idToText(character + num);
+  const addCharacters = character1 => character2 => num => charRange(character1, character2).map(elemValue(num));
+  const rangeExpanded =x.replace(rangeRegex);
+}
