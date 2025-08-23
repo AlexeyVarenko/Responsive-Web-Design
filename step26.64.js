bd4315e99eb38ccc25e2545b45180901ddcb15db
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-08-23 10:49:00 +0300

    Create step26.64.js

---
+++ step26.64.js
@@ -0,0 +1,10 @@
+/*Step 64
+In mathematics, an infix is a mathematical operator that appears between its two operands. For example, 1 + 2 is an infix expression.
+
+To parse these expressions, you will need to map the symbols to relevant functions. Declare an infixToFunction variable, and assign it an empty object.*/
+
+
+
+const infixToFunction={
+  
+}
