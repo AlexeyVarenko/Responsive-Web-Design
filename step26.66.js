fa9c18cceb472857ac9e4373bdeaea092482d741
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-08-25 10:31:28 +0300

    Create step26.66.js

---
+++ step26.66.js
@@ -0,0 +1,9 @@
+/*Step 66
+Now create a - property that is a function that takes an x and y parameter and implicitly returns the result of subtracting y from x.*/
+
+
+
+const infixToFunction = {
+  "+": (x, y) => x + y,
+  "-": (x, y) => x - y,
+}
