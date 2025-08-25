ebae5955b833b13fda683272953d72a167470799
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-08-25 10:31:29 +0300

    Create step26.67.js

---
+++ step26.67.js
@@ -0,0 +1,11 @@
+/*Step 67
+Following the same pattern, add a property for multiplication * and division / with the appropriate functions.*/
+
+
+
+const infixToFunction = {
+  "+": (x, y) => x + y,
+  "-": (x, y) => x - y,
+  "*": (x, y) => x * y,
+  "/": (x, y) => x / y,
+}
