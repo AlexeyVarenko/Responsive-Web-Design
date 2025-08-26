38c64c3b868cffbdd14ffee7db9b28e99237e7e3
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-08-26 12:42:48 +0300

    Create step26.69.js

---
+++ step26.69.js
@@ -0,0 +1,7 @@
+/*Step 69
+Your callback needs four parameters. match, arg1, operator, and arg2.
+
+You will not be using the match parameter, so remember to prefix it.*/
+
+
+const infixEval = (str, regex) => str.replace(regex, (_match, arg1, operator, arg2) => {});
