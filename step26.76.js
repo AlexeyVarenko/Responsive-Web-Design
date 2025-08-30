eaaa84bb353ab0f90e9c39c934f9545d0211bd52
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-08-30 11:39:29 +0300

    Create step26.76.js

---
+++ step26.76.js
@@ -0,0 +1,9 @@
+/*Step 76
+Remove both the console.log() with your highPrecedence call, and the return statement from your highPrecedence function.*/
+
+
+
+const highPrecedence = str => {
+  const regex = /([\d.]+)([*\/])([\d.]+)/;
+  
+}
