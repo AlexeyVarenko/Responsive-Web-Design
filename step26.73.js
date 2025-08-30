b5cd11c4dc552a673d5a71ecf32202c04041b4fe
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-08-30 11:39:22 +0300

    Create step26.73.js

---
+++ step26.73.js
@@ -0,0 +1,8 @@
+/*Step 73
+Now that you can evaluate mathematical expressions, you need to account for order of operations. Declare a highPrecedence function that takes a str parameter.*/
+
+
+
+const highPrecedence=str=>{
+
+}
