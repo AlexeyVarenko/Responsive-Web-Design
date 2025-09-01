183294654e94f8183bb8d0449e31ca197ccdd45b
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-09-01 08:36:25 +0300

    Create step26.79.js

---
+++ step26.79.js
@@ -0,0 +1,8 @@
+/*Step 79
+Now you can start applying your function parsing logic to a string. Declare a function called applyFunction, which takes a str parameter.*/
+
+
+
+const applyFunction=str=>{
+  
+}
