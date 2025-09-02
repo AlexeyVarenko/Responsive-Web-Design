7834beae0e5c081ab1619a11e97a6e5593813af6
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-09-02 10:18:26 +0300

    Create step26.80.js

---
+++ step26.80.js
@@ -0,0 +1,8 @@
+/*Step 80
+First you need to handle the higher precedence operators. Declare a noHigh variable, and assign it the result of calling highPrecedence() with str as an argument.*/
+
+
+
+const applyFunction = str => {
+  const noHigh=highPrecedence(str);
+}
