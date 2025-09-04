8bb048a0a93e8c52b078d95e4d556407cf76891e
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-09-04 10:24:26 +0300

    Create step26.85.js

---
+++ step26.85.js
@@ -0,0 +1,13 @@
+/*Step 85
+Declare an apply function that takes a fn and args parameter.*/
+
+
+
+const applyFunction = str => {
+  const noHigh = highPrecedence(str);
+  const infix = /([\d.]+)([+-])([\d.]+)/;
+  const str2 = infixEval(noHigh, infix);
+  const functionCall = /([a-z0-9]*)\(([0-9., ]*)\)(?!.*\()/i;
+  const toNumberList = args => args.split(",").map(parseFloat);
+  const apply=(fn,args)=>{}
+}
