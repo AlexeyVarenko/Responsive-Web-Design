b79c4981119ceac5136df62bf59c20cab28e0f3e
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-09-05 08:29:21 +0300

    Create step26.86.js

---
+++ step26.86.js
@@ -0,0 +1,6 @@
+/*Step 86
+The fn parameter will be passed the name of a function, such as "SUM". Update apply to implicitly return the function from your spreadsheetFunctions object using the fn variable as the key for the object access.
+
+Remember that fn might not contain a lowercase string, so you'll need to convert it to a lowercase string.*/
+
+
