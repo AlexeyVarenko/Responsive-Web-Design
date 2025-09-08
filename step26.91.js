7e75146fa61fd31b98344dcd9e9eb748a67418a9
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-09-08 10:27:51 +0300

    Create step26.91.js

---
+++ step26.91.js
@@ -0,0 +1,15 @@
+/*Step 91
+Now you can start applying your function parser to your evalFormula logic. Declare a functionExpanded variable, and assign it the result of calling applyFunction with your cellExpanded string.*/
+
+
+const const evalFormula = (x, cells) => {
+  const idToText = id => cells.find(cell => cell.id === id).value;
+  const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
+  const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
+  const elemValue = num => character => idToText(character + num);
+  const addCharacters = character1 => character2 => num => charRange(character1, character2).map(elemValue(num));
+  const rangeExpanded = x.replace(rangeRegex, (_match, char1, num1, char2, num2) => rangeFromString(num1, num2).map(addCharacters(char1)(char2)));
+  const cellRegex = /[A-J][1-9][0-9]?/gi;
+  const cellExpanded = rangeExpanded.replace(cellRegex, match => idToText(match.toUpperCase()));
+ const functionExpanded=applyFunction(cellExpanded);
+}
