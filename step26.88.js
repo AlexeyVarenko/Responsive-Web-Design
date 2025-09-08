c278e988728c0207b8424a78ef43bbbbdba58a01
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-09-08 10:27:38 +0300

    Create step26.88.js

---
+++ step26.88.js
@@ -0,0 +1,14 @@
+/*Step 88
+Now your applyFunction needs to return a result. Return the result of calling the .replace() method on str2. Pass your functionCall regex and an empty callback.*/
+
+
+
+const applyFunction = str => {
+  const noHigh = highPrecedence(str);
+  const infix = /([\d.]+)([+-])([\d.]+)/;
+  const str2 = infixEval(noHigh, infix);
+  const functionCall = /([a-z0-9]*)\(([0-9., ]*)\)(?!.*\()/i;
+  const toNumberList = args => args.split(",").map(parseFloat);
+  const apply = (fn, args) => spreadsheetFunctions[fn.toLowerCase()](toNumberList(args));
+  return str2.replace(functionCall, ()=>{});
+}
