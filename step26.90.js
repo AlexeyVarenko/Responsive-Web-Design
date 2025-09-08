f2fc5dcfc2cb05068c42e6c6a54a3571fd0c78c9
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-09-08 10:27:48 +0300

    Create step26.90.js

---
+++ step26.90.js
@@ -0,0 +1,17 @@
+/*Step 90
+Use the ternary operator to turn your .hasOwnProperty() call into the condition. If the object has the property, return the result of calling apply with fn and args as arguments. Otherwise, return match.*/
+
+
+
+const applyFunction = str => {
+  const noHigh = highPrecedence(str);
+  const infix = /([\d.]+)([+-])([\d.]+)/;
+  const str2 = infixEval(noHigh, infix);
+  const functionCall = /([a-z0-9]*)\(([0-9., ]*)\)(?!.*\()/i;
+  const toNumberList = args => args.split(",").map(parseFloat);
+  const apply = (fn, args) => spreadsheetFunctions[fn.toLowerCase()](toNumberList(args));
+  return str2.replace(functionCall, (match, fn, args) => spreadsheetFunctions.hasOwnProperty(fn.toLowerCase())
+  ? apply(fn, args)
+  :match
+   );
+}
