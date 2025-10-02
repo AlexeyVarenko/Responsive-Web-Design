b1e6a8b8d2e194afe0b612a5127b52338c3e413f
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-10-02 08:15:18 +0300

    Create step26.100.js

---
+++ step26.100.js
@@ -0,0 +1,15 @@
+/*Step 100
+Add a someeven property to your spreadsheetFunctions - use the .some() method to check if any element in the array is even.*/
+
+
+const spreadsheetFunctions = {
+  sum,
+  average,
+  median,
+  even: nums => nums.filter(isEven),
+  someeven: nums => nums.some(isEven),
+  firsttwo: nums => nums.slice(0, 2),
+  lasttwo: nums => nums.slice(-2),
+  has2: nums => nums.includes(2),
+  increment: nums => nums.map(num => num + 1),
+}
