8a6d591d28cfed95fbf70ceca1bd06c99eebb1af
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-10-03 08:53:14 +0300

    Create step26.103.js

---
+++ step26.103.js
@@ -0,0 +1,19 @@
+/*Step 103
+Add a range property which generates a range from the first number in nums to the second number in nums. Remember that you have a range function you can reuse here.*/
+
+
+
+const spreadsheetFunctions = {
+  sum,
+  average,
+  median,
+  even: nums => nums.filter(isEven),
+  someeven: nums => nums.some(isEven),
+  everyeven: nums => nums.every(isEven),
+  firsttwo: nums => nums.slice(0, 2),
+  lasttwo: nums => nums.slice(-2),
+  has2: nums => nums.includes(2),
+  increment: nums => nums.map(num => num + 1),
+  random: ([x, y]) => Math.floor(Math.random() * y + x),
+  range: nums=> range(...nums),
+}
