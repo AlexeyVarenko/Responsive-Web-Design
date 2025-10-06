70e235850e817a49fb9fe128fe29e09c97b01bdf
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-10-06 11:32:22 +0300

    Create step27.2.js

---
+++ step27.2.js
@@ -0,0 +1,7 @@
+/*Step 2
+Now you need to get your two buttons. Continuing the pattern, get the #cart-btn and #clear-cart-btn elements. Store them in variables named cartBtn and clearCartBtn, respectively.*/
+
+
+
+const cartBtn=document.getElementById("cart-btn");
+const clearCartBtn=document.getElementById("clear-cart-btn");
