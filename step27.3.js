37dde6d8bd7c7685d02ab444d1e890ac96ae0406
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-10-07 11:26:09 +0300

    Create step27.3.js

---
+++ step27.3.js
@@ -0,0 +1,9 @@
+/*Step 3
+Next is to get your totals. Get the #total-items, #subtotal, #taxes, and #total elements. Store them in variables named totalNumberOfItems, cartSubTotal, cartTaxes, and cartTotal, respectively.*/
+
+
+
+const totalNumberOfItems = document.getElementById("total-items");
+const cartSubTotal = document.getElementById("subtotal");
+const cartTaxes = document.getElementById("taxes");
+const cartTotal = document.getElementById("total");
