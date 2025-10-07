d1bfc4652cab034316b71464c54a3cf12cddfe21
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-10-07 11:26:11 +0300

    Create step27.4.js

---
+++ step27.4.js
@@ -0,0 +1,8 @@
+/*Step 4
+The last element to get is the #show-hide-cart element. Store it in a variable named showHideCartSpan.
+
+Then, use let to declare a variable named isCartShowing and set it to false.*/
+
+
+const showHideCartSpan= document.getElementById("show-hide-cart");
+let isCartShowing=false;
