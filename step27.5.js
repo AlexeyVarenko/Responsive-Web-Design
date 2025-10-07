a89986a3a2ad6f51907ad80ca9460ccaeedec0fc
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-10-07 11:26:13 +0300

    Create step27.5.js

---
+++ step27.5.js
@@ -0,0 +1,6 @@
+/*Step 5
+A shopping cart does not serve much purpose without products. Declare a products variable and set it to an empty array. Using an array will allow you to store multiple products.*/
+
+
+
+const products=[];
