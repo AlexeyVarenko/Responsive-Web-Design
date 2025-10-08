2adc45c9389934fb18e6fe08614056ab7c84d54f
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-10-08 08:46:53 +0300

    Create step27.6.js

---
+++ step27.6.js
@@ -0,0 +1,14 @@
+/*Step 6
+You now need to start adding products. Before you do that, you need to consider the structure of your product data. A product will need a unique identifier to distinguish it from other products, a price so people know how much it costs, and a name so people know what they are buying. You should also add a category to each product.
+
+Add an object to your products array. Give this object an id property set to the number 1, a name property set to the string "Vanilla Cupcakes (6 Pack)", a price property set to the number 12.99, and a category property set to the string "Cupcake".*/
+
+
+const products = [
+  {
+    id:1,
+    name:"Vanilla Cupcakes (6 Pack)",
+    price: 12.99,
+    category: "Cupcake",
+  }
+];
