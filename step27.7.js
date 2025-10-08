58351d19d5c54a1472f8a981c8349b6fa9c5a0d7
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-10-08 08:46:54 +0300

    Create step27.7.js

---
+++ step27.7.js
@@ -0,0 +1,94 @@
+/*Step 7
+Following that same data structure, add the products from this table (in order) to your products array. Increment the id for each product, counting up.
+
+name	price	category
+French Macaron	3.99	Macaron
+Pumpkin Cupcake	3.99	Cupcake
+Chocolate Cupcake	5.99	Cupcake
+Chocolate Pretzels (4 Pack)	10.99	Pretzel
+Strawberry Ice Cream	2.99	Ice Cream
+Chocolate Macarons (4 Pack)	9.99	Macaron
+Strawberry Pretzel	4.99	Pretzel
+Butter Pecan Ice Cream	2.99	Ice Cream
+Rocky Road Ice Cream	2.99	Ice Cream
+Vanilla Macarons (5 Pack)	11.99	Macaron
+Lemon Cupcakes (4 Pack)	12.99	Cupcake
+*/
+
+
+
+const products = [
+  {
+    id: 1,
+    name: "Vanilla Cupcakes (6 Pack)",
+    price: 12.99,
+    category: "Cupcake",
+  },
+  {
+    id: 2,
+    name: "French Macaron",
+    price: 3.99,
+    category: "Macaron",
+  },
+   {
+    id: 3,
+    name: "Pumpkin Cupcake",
+    price: 3.99,
+    category: "Cupcake",
+  },
+   {
+    id: 4,
+    name: "Chocolate Cupcake",
+    price: 5.99,
+    category: "Cupcake",
+  },
+   {
+    id: 5,
+    name: "Chocolate Pretzels (4 Pack)",
+    price: 10.99,
+    category: "Pretzel",
+  },
+   {
+    id: 6,
+    name: "Strawberry Ice Cream",
+    price: 2.99,
+    category: "Ice Cream",
+  },
+   {
+    id: 7,
+    name: "Chocolate Macarons (4 Pack)",
+    price: 9.99,
+    category: "Macaron",
+  },
+   {
+    id: 8,
+    name: "Strawberry Pretzel",
+    price: 4.99,
+    category: "Pretzel",
+  },
+   {
+    id: 9,
+    name: "Butter Pecan Ice Cream",
+    price: 2.99,
+    category: "Ice Cream",
+   },
+  {
+    id: 10,
+    name: "Rocky Road Ice Cream",
+    price: 2.99,
+    category: "Ice Cream",
+   },
+   {
+    id: 11,
+    name: "Vanilla Macarons (5 Pack)",
+    price: 11.99,
+    category: "Macaron",
+   },
+   {
+    id: 12,
+    name: "Lemon Cupcakes (4 Pack)",
+    price: 12.99,
+    category: "Cupcake",
+   },
+
+];
