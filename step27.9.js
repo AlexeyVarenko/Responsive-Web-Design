f401e166e82c458da94747883a4107b77b28f963
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-10-09 09:19:32 +0300

    Create step27.9.js

---
+++ step27.9.js
@@ -0,0 +1,9 @@
+/*Step 9
+Remember that you can use destructuring to extract multiple values from an array or object in a single statement.
+
+For the first parameter of your callback function, destructure the name, id, price, and category properties from the object passed in.*/
+
+
+products.forEach(
+  ([name, id, price, category]) => {}
+);
