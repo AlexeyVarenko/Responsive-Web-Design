06bf42746b50871adad2ea035a386ce28a19a467
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-10-10 08:36:13 +0300

    Create step27.10.js

---
+++ step27.10.js
@@ -0,0 +1,10 @@
+/*Step 10
+You need to display the available products in your HTML. Start by using the addition assignment operator to add an empty template literal string to the innerHTML property of the dessertCards variable.*/
+
+
+
+products.forEach(
+  ({ name, id, price, category }) => {
+    dessertCards.innerHTML +=``;
+  }
+);
