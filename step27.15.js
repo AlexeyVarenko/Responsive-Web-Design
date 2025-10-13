00a73d862483c24073aae70970c1c6fc42e90992
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-10-13 09:48:06 +0300

    Create step27.15.js

---
+++ step27.15.js
@@ -0,0 +1,15 @@
+/*Step 15
+Classes have a special constructor method, which is called when a new instance of the class is created. The constructor method is a great place to initialize properties of the class. Here is an example of a class with a constructor method:
+
+Example Code
+class Computer {
+  constructor() {
+  }
+}
+Add an empty constructor method to the ShoppingCart class.*/
+
+
+class ShoppingCart {
+  constructor() {
+  }
+};
