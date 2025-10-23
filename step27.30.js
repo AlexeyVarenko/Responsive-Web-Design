1842dde17b9e7d938fb181f2da30735904cd8d19
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-10-23 10:52:48 +0300

    Create step27.30.js

---
+++ step27.30.js
@@ -0,0 +1,27 @@
+/*Step 30
+Inside your div, add two p elements. Set the text of the second p element to be the value of the price variable.*/
+
+
+
+addItem(id, products) {
+    const product = products.find((item) => item.id === id);
+    const { name, price } = product;
+    this.items.push(product);
+
+    const totalCountPerProduct = {};
+    this.items.forEach((dessert) => {
+      totalCountPerProduct[dessert.id] = (totalCountPerProduct[dessert.id] || 0) + 1;
+    })
+
+    const currentProductCount = totalCountPerProduct[product.id];
+    const currentProductCountSpan = document.getElementById(`product-count-for-id${id}`);
+
+    currentProductCount > 1 
+      ? currentProductCountSpan.textContent = `${currentProductCount}x`
+      : productsContainer.innerHTML += `
+      <div id="dessert${id}" class="product">
+        <p></p>;
+        <p>${price}</p>
+      </div>
+      `;
+  }
