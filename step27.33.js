a730c3f40561bcf779b3ce5b62237b674add6b54
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-10-24 09:06:57 +0300

    Create step27.33.js

---
+++ step27.33.js
@@ -0,0 +1,6 @@
+/*Step 33
+You need to get all of the Add to cart buttons that you added to the DOM earlier. Declare an addToCartBtns variable, and assign it the value of calling the getElementsByClassName() method on the document object, passing in the string "add-to-cart-btn".*/
+
+
+
+const addToCartBtns = document.getElementsByClassName("add-to-cart-btn")
