e2d96895757bca2a0b783224f11679ba26a2377d
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-10-24 09:06:55 +0300

    Create step27.32.js

---
+++ step27.32.js
@@ -0,0 +1,10 @@
+/*Step 32
+There is still more functionality that your ShoppingCart class needs, but first you need to be able to test the code you have currently written. You'll need to instantiate a new ShoppingCart object and assign it to a variable. Here is an example of instantiating the Computer class from earlier examples:
+
+Example Code
+const myComputer = new Computer();
+Declare a cart variable, and assign it a new ShoppingCart object. Note the use of the new keyword when instantiating the object.*/
+
+
+
+const cart = new ShoppingCart();
