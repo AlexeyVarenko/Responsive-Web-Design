21bc962fab1c5929fb813490d61eeb3b850be2d0
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-10-27 09:24:35 +0300

    Create step27.35.js

---
+++ step27.35.js
@@ -0,0 +1,12 @@
+/*Step 35
+Add your callback function to the forEach method. It should take a btn parameter. Then, in the callback, add an event listener to the btn. The event listener should listen for a click event, and should take another callback with an event parameter. The second callback should be empty.*/
+
+
+
+[...addToCartBtns].forEach(
+(btn)=>{
+  btn.addEventListener("click", (event)=>{
+
+  })
+}
+);
