a8c7b01f20c46763a7b05a16133b273d38ba3ad4
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-10-28 14:11:23 +0300

    Create step27.37.js

---
+++ step27.37.js
@@ -0,0 +1,8 @@
+/*Step 37
+Your cart currently isn't visible on the webpage. To make it visible, start by adding an event listener to the cartBtn variable, listening for the click event. The callback does not need any parameters.*/
+
+
+
+cartBtn.addEventListener("click", ()=>{
+  
+})
