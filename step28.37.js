cf1c18a1d0089f4c30248699eea88486e1ff90eb
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-10-29 08:41:19 +0300

    Create step28.37.js

---
+++ step28.37.js
@@ -0,0 +1,12 @@
+/*Step 38
+Start by inverting the value of isCartShowing. Remember that you can use the logical not operator ! to invert the value of a boolean. Assign the inverted value to isCartShowing.*/
+
+
+
+cartBtn.addEventListener("click", () => {
+isCartShowing =!isCartShowing
+});
+
+
+
+
