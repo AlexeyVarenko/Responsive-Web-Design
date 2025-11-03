1aa08453079c4d2527c8dcf9f0ab9a950b717025
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-11-03 08:23:55 +0300

    Create step27.44.js

---
+++ step27.44.js
@@ -0,0 +1,8 @@
+/*Step 44
+Part of the total cost will include the tax, so you need to calculate that as well. Create a calculateTaxes method in the ShoppingCart class. This method should take an amount parameter.*/
+
+
+
+calculateTaxes(amount){
+  
+}
