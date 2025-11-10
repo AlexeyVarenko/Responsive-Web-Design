5e468fb2b6e235ea9f0d607dbbc2cb4fd02161ca
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-11-10 10:26:34 +0300

    Create step27.53.js

---
+++ step27.53.js
@@ -0,0 +1,14 @@
+/*Step 53
+Now call your .calculateTotal() method inside your forEach loop.*/
+
+
+
+[...addToCartBtns].forEach(
+  (btn) => {
+    btn.addEventListener("click", (event) => {
+      cart.addItem(Number(event.target.id), products);
+      totalNumberOfItems.textContent = cart.getCounts();
+      cart.calculateTotal();
+    })
+  }
+);
