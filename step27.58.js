5353a80c0768efc7b0a68604d0ba02f8ab6a6775
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-11-13 10:15:34 +0300

    Create step27.58.js

---
+++ step27.58.js
@@ -0,0 +1,21 @@
+/*Step 58
+You also need to start clearing the HTML. Set the innerHTML property of the productsContainer back to an empty string.*/
+
+
+
+clearCart() {
+    if (!this.items.length) {
+      alert("Your shopping cart is already empty");
+      return;
+    }
+
+    const isCartCleared = confirm(
+      "Are you sure you want to clear all items from your shopping cart?"
+    );
+
+    if (isCartCleared) {
+      this.items = [];
+      this.total = 0;
+      productsContainer.innerHTML=""
+    }
+  }
