13660a5725596be6c78cc02631968f1a67ee8386
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-11-14 11:20:15 +0300

    Create step28.1.js

---
+++ step28.1.js
@@ -0,0 +1,10 @@
+/*Step 1
+In this project, you are going to learn intermediate Object Oriented Programming principles by building a platformer game. All of the HTML and CSS have been provided for you.
+
+Start by using document.getElementById() to get the #start-btn and #canvas elements.
+
+Store them in const variables named startBtn and canvas respectively.*/
+
+
+const startBtn = document.getElementById("start-btn");
+const canvas = document.getElementById("canvas");
