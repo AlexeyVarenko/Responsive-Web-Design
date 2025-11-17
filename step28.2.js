588644074dd58cc87d0971e99929e0ab0b665341
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-11-17 10:07:40 +0300

    Create step28.2.js

---
+++ step28.2.js
@@ -0,0 +1,9 @@
+/*Step 2
+Next, you will need to use document.querySelector to get the .start-screen and .checkpoint-screen elements.
+
+Store them in const variables called startScreen and checkpointScreen respectively.*/
+
+
+
+const startScreen=document.querySelector(".start-screen");
+const checkpointScreen=document.querySelector(".checkpoint-screen")
