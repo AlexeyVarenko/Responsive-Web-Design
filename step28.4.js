5698e87a49daa6cfb5f1426cce2621d58bb4e54c
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-11-18 11:47:35 +0300

    Create step28.4.js

---
+++ step28.4.js
@@ -0,0 +1,14 @@
+/*Step 4
+Before you can begin building out the functionality for the game, you will need to set up the ability to add 2D graphics.
+
+The Canvas API can be used to create graphics in games using JavaScript and the HTML canvas element.
+
+You will need to use the getContext method which will provide the context for where the graphics will be rendered.
+
+Example Code
+canvas.getContext("2d");
+Assign that getContext method to a const variable called ctx.*/
+
+
+
+const  ctx=canvas.getContext("2d");
