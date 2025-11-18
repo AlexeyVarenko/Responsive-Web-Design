8abbd52bf53cb1ee85422fffd525a648ff2f2dd8
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-11-18 11:47:37 +0300

    Create step28.5.js

---
+++ step28.5.js
@@ -0,0 +1,10 @@
+/*Step 5
+The canvas element has a width property which is a positive number that represents the width of the canvas.
+
+Example Code
+canvas.width
+Below your const declarations, append the width property to the canvas variable.*/
+
+
+
+canvas.width;
