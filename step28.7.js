d2e5d2b5919b68565c69e4a47db56e769eb644f2
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-11-19 11:49:18 +0300

    Create step28.7.js

---
+++ step28.7.js
@@ -0,0 +1,8 @@
+/*Step 7
+The innerHeight property is a number that represents the interior height of the browser window.
+
+Below your canvas.width, append the height property to the canvas variable and assign it innerHeight.*/
+
+
+
+canvas.height=innerHeight;
