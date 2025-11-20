0c79e50ca5c27c48ff92fb1932541743d8759cd4
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-11-20 13:18:28 +0300

    Create step28.8.js

---
+++ step28.8.js
@@ -0,0 +1,8 @@
+/*Step 8
+In your platformer game, the main player will need to jump between the different platforms. When the player jumps, you will need to apply gravity to bring them back down.
+
+Create a new const variable called gravity and assign it the number 0.5.*/
+
+
+
+const gravity=0.5;
