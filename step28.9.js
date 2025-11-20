ce2dbf007e2f991ebf9b853b4ad665630bfccd05
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-11-20 13:18:30 +0300

    Create step28.9.js

---
+++ step28.9.js
@@ -0,0 +1,7 @@
+/*Step 9
+In the game, the player will have the opportunity to cross different checkpoints. You will need to keep track of the status for the checkpoint collision detection.
+
+Use let to create a new variable called isCheckpointCollisionDetectionActive and assign it the value of true.*/
+
+
+let isCheckpointCollisionDetectionActive=true;
