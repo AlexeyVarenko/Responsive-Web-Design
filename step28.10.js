0b5ac6951be30709822916e5dd12a9f08c2a63b1
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-11-21 11:39:23 +0300

    Create step28.10.js

---
+++ step28.10.js
@@ -0,0 +1,10 @@
+/*Step 10
+As you are designing the game, you will need to make sure that the size of the elements in the game are responsive and adapt to different screen sizes.
+
+Start by creating an arrow function called proportionalSize that takes in a size parameter.*/
+
+
+
+const proportionalSize=(size)=>{
+  
+}
