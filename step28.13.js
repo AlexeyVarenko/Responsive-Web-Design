aa67fac0f8792d8298bcb0afdca54354c84f0edb
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-11-24 09:42:39 +0300

    Create step28.13.js

---
+++ step28.13.js
@@ -0,0 +1,12 @@
+/*Step 13
+Inside your Player class, you will need to define the player's position, velocity, width, and height values. All of these values will be defined inside the constructor method.
+
+Create an empty constructor inside your Player class.*/
+
+
+
+class Player {
+ constructor (){
+   
+ }
+}
