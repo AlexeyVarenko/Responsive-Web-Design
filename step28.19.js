2d5983cba1edf4c3ece31915e0792df0bf28b377
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-11-27 13:06:05 +0300

    Create step28.19.js

---
+++ step28.19.js
@@ -0,0 +1,10 @@
+/*Step 19
+Now, you need to set the color for your player.
+
+Inside the draw() method, assign the string "#99c9ff" to ctx.fillStyle.*/
+
+
+
+  draw() {
+    ctx.fillStyle='#99c9ff'
+  }
