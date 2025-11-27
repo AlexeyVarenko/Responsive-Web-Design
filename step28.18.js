2f72496b153c9c012c74994410dcb4e71ca3cb18
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-11-27 13:06:03 +0300

    Create step28.18.js

---
+++ step28.18.js
@@ -0,0 +1,8 @@
+/*Step 18
+The next step is to create a draw() method, which will be responsible for creating the player's width, height, position, and fill color.
+
+Below your constructor, create an empty draw() method.*/
+
+
+
+draw(){}
