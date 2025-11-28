8a2e887bfedc66c33b13c2c7982d64c3cc7c9046
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-11-28 15:12:34 +0300

    Create step28.21.js

---
+++ step28.21.js
@@ -0,0 +1,10 @@
+/*Step 21
+The next step is to create an update() method which will be responsible for updating the player's position and velocity as it moves throughout the game.
+
+Below your draw() method, create an empty update() method.*/
+
+
+
+update(){
+  
+}
