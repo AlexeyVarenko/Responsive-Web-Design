605af9ce3e1e5b5e8cdf6d0f6de3f53992779161
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-12-01 10:40:20 +0300

    Create step28.22.js

---
+++ step28.22.js
@@ -0,0 +1,10 @@
+/*Step 22
+Inside the update() method, call the draw() method to ensure that the player is continually drawn on the screen as the game updates.
+
+Don't forget to include the this keyword.*/
+
+
+
+  update() {
+    this.draw();
+  }
