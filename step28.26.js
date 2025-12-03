6ef4a76b3f883d4675e0580eb9f16abc669c055c
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-12-03 13:55:57 +0300

    Create step28.26.js

---
+++ step28.26.js
@@ -0,0 +1,10 @@
+/*Step 26
+In the if statement, add another if statement to check if the player's y position is less than 0.*/
+
+
+
+if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+    if (this.position.y<0){
+      
+    }
+    }
