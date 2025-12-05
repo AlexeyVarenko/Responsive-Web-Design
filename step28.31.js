199aead2ef993145963855104921bf9f3e5c7e17
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-12-05 08:27:47 +0300

    Create step28.31.js

---
+++ step28.31.js
@@ -0,0 +1,10 @@
+/*Step 31
+The final condition you need to add inside the Player class is to ensure that the player stays within the boundaries of the canvas screen and doesn't move too far off to the left.
+
+Create an if statement, to check if the player's x position is less than the width.*/
+
+
+
+if (this.position.x<this.width){
+  
+}
