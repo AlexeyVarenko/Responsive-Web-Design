24dd060c63f626054a63a40d6ed7583553a94c8c
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-12-09 09:20:09 +0300

    Create step28.34.js

---
+++ step28.34.js
@@ -0,0 +1,9 @@
+/*Step 34
+Inside your if statement, assign canvas.width - this.width * 2 to this.position.x.
+
+This will ensure that the player's x position will never exceed the right edge of the canvas.*/
+
+
+
+
+this.position.x=canvas.width - this.width * 2
