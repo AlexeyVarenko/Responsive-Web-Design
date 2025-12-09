2929f0e36efe53bec430e416e22e79038feaed37
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-12-09 09:20:11 +0300

    Create step28.35.js

---
+++ step28.35.js
@@ -0,0 +1,5 @@
+/*Step 35
+The next step is to use the new keyword to create a new instance of the Player object and assign it to a new const variable called player.*/
+
+
+const player=new Player();
