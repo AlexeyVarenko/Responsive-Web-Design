070e511aca7960fe3514ef4bf68c2dd48d8d16c4
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-12-11 08:37:24 +0300

    Create step28.37.1.js

---
+++ step28.37.1.js
@@ -0,0 +1,14 @@
+/*Step 37
+Inside your startGame function, you will need to display the canvas element and hide the startScreen container.
+
+Use canvas.style.display to change the display value to "block".
+
+Below that, use startScreen.style.display to change the display value to "none".*/
+
+
+
+
+const startGame = () => {
+  canvas.style.display="block";
+  startScreen.style.display="none";
+}
