ad4b89d56e26fb8cbd04a8610c49001712fe43de
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-12-12 10:42:31 +0300

    Create step28.39.js

---
+++ step28.39.js
@@ -0,0 +1,10 @@
+/*Step 39
+Now it's time to add the functionality for the start game button.
+
+Add an addEventListener to the startBtn and pass in a click event and a reference to the startGame function.
+
+Click on the start game button, and you should see a light blue square on the screen which represents the main player.*/
+
+
+
+startBtn.addEventListener('click', startGame)
