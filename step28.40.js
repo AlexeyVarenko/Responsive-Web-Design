f8e3924869bf3936bb622ab08141e299bf2104ed
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-12-15 08:28:43 +0300

    Create step28.40.js

---
+++ step28.40.js
@@ -0,0 +1,8 @@
+/*Step 40
+Now that you can see the player on the screen, it is time to start adding the functionality for moving the player across the screen.
+
+Create a new empty arrow function called animate.*/
+
+
+
+const animate = () => {};
