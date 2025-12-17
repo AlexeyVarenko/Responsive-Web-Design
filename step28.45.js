91bef989aaebfea96327e349101ea81dff868de4
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-12-17 10:43:26 +0300

    Create step28.45.js

---
+++ step28.45.js
@@ -0,0 +1,15 @@
+/*Step 45
+Inside the keys object, add a new key called rightKey and assign it an object with the key-value pair of pressed: false.
+
+Below the rightKey object, create a leftKey object and assign it an object with the key-value pair of pressed: false.*/
+
+
+
+const keys = {
+  rightKey:{
+    pressed: false
+  },
+  leftKey:{
+    pressed: false
+  },
+}
