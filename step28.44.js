9e7c54a56b314895af834b78ac18ddbf99a93a6c
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-12-17 10:43:24 +0300

    Create step28.44.js

---
+++ step28.44.js
@@ -0,0 +1,8 @@
+/*Step 44
+To manage the player's movement in the game, you will need to monitor when the left and right arrow keys are pressed.
+
+Create a new const variable called keys and assign it an empty object.*/
+
+
+
+const keys={};
