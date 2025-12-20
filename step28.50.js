408b22301a434cde7f5bb43bd050a8b33486ad84
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-12-20 08:18:00 +0300

    Create step28.50.js

---
+++ step28.50.js
@@ -0,0 +1,8 @@
+/*Step 50
+The next step is to add the functionality that will be responsible for moving the player across the screen.
+
+Create a new arrow function called movePlayer that has three parameters called key, xVelocity, isPressed.*/
+
+
+
+const movePlayer = (key, xVelocity, isPressed) => {}
