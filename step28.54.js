c7a28f88d0996f7d2380db883a232935ec398b78
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-12-23 11:00:54 +0300

    Create step28.54.js

---
+++ step28.54.js
@@ -0,0 +1,13 @@
+/*Step 54
+The first case you will want to add is when the left arrow key is pressed.
+
+Inside the switch statement, add a new case called "ArrowLeft".*/
+
+
+
+switch (key) {
+    case "ArrowLeft":
+    break;
+    case "case2"
+    break;
+  }
