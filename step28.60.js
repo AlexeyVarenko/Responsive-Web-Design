0d415cd40f8f23235e829c7f0703dcc6d07cb596
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-12-29 15:02:05 +0300

    Create step28.60.js

---
+++ step28.60.js
@@ -0,0 +1,8 @@
+/*Step 60
+Inside the arrow function, call the movePlayer function and pass in key, 8, and true as arguments.*/
+
+
+
+window.addEventListener("keydown", ({ key }) => {
+  movePlayer(key, 8, true)
+});
