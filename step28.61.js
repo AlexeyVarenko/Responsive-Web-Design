d921c19ba1ca1afbca7ed0d05ef9dead6bbcc4df
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-12-29 15:02:07 +0300

    Create step28.61.js

---
+++ step28.61.js
@@ -0,0 +1,8 @@
+/*Step 61
+Add another addEventListener to the global window object and pass in the keyup event and use destructuring to pass in the key property from the event.*/
+
+
+
+window.addEventListener("keyup", ({ key }) => {
+  
+});
