5e2201d9548ce46904bffe366e76364681efa218
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2025-12-30 11:51:01 +0300

    Create step28.63.js

---
+++ step28.63.js
@@ -0,0 +1,11 @@
+/*Step 63
+Before you can start moving your player across the screen, you will need to use the animate function.
+
+Inside the startGame function, delete player.draw() and call the animate function.
+
+Click the Start Game button and use the left and right arrow keys to move the player across the screen. You can also use the spacebar or the up arrow key to jump up.*/
+
+
+
+
+animate();
