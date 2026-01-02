1eb97b0f7660e01783b8bd2c9bb56b30ae091906
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-01-02 11:21:38 +0300

    Create step28.69.js

---
+++ step28.69.js
@@ -0,0 +1,19 @@
+/*Step 69
+Next, add a draw method to the Platform class.*/
+
+
+
+
+class Platform {
+    constructor(x, y) {
+      this.position = {
+        x,
+        y,
+      };
+      this.width = 200;
+      this.height = proportionalSize(40);
+    }
+    draw(){
+      
+    }
+  }
