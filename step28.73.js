68ce25c5e99552ef2d8c426ef357815ebaa7ee92
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-01-05 16:51:01 +0300

    Create step28.73.js

---
+++ step28.73.js
@@ -0,0 +1,9 @@
+/*Step 73
+Below that, add another object with an x property with a value of 700 and a y property with a value of proportionalSize(400).*/
+
+
+
+const platformPositions = [
+  { x: 500, y: proportionalSize(450) },
+  {x:700, y: proportionalSize(400)}
+];
