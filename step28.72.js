d89ffc7f46a7ea7b08bf917e2ea4888265a52ffe
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-01-05 16:50:58 +0300

    Create step28.72.js

---
+++ step28.72.js
@@ -0,0 +1,14 @@
+/*Step 72
+Inside the platformPositions, you will need to add the list of positions for the platforms.
+
+Add a new object that has an x property with a value of 500 and a y property with a value of proportionalSize(450).*/
+
+
+
+const platformPositions = [
+  {
+    x:500,
+    y:proportionalSize(450)
+    }
+
+];
