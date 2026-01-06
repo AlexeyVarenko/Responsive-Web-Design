6e20bd1fd5cf042a5505e48f5682587f5c7ba980
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-01-06 11:37:35 +0300

    Create step28.75.js

---
+++ step28.75.js
@@ -0,0 +1,8 @@
+/*Step 75
+The next step is to create a list of new platform instances using the Platform class. You will later reference this list to draw the platforms on the canvas.
+
+Start by creating a new const variable called platforms and assign it platformPositions.map().*/
+
+
+
+const platforms=platformPositions.map()
