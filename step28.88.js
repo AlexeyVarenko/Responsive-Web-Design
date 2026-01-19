8a1c446dd1ac35778aa6695b13ffa0cd31e5e43f
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-01-19 15:59:29 +0300

    Create step28.88.js

---
+++ step28.88.js
@@ -0,0 +1,9 @@
+/*Step 88
+Inside that array, add a boolean expression that checks if the player's x position is greater than or equal to the platform's x position minus half of the player's width. Use the division operator (/) to calculate half of the player's width.*/
+
+
+
+const platformDetectionRules = [
+    
+      player.position.x >= platform.position.x - player.width / 2
+    ];
