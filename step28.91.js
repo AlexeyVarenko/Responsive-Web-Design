5d909b15eab6edeeeb93b9a63cb34c0f3ea71367
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-01-19 15:59:34 +0300

    Create step28.91.js

---
+++ step28.91.js
@@ -0,0 +1,7 @@
+/*Step 91
+Add an if statement that checks if every platform detection rule is true. Make sure to use the every method for this.*/
+
+
+ if (platformDetectionRules.every((rule)=>rule)){
+
+    }
