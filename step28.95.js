9341065ba2b07ded7566be2ab0ff9b97d89c2553
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-01-21 13:28:43 +0300

    Create step28.95.js

---
+++ step28.95.js
@@ -0,0 +1,11 @@
+/*Step 95
+Inside the constructor, create an object with x and y parameters and assign it to the position.
+
+Remember to use the this keyword to access the properties.*/
+
+
+class CheckPoint {
+  constructor(x, y, z) {
+    this.position={x,y}
+  };
+};
