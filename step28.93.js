ab117c313d353e741f6f8547a9d0e0e936db0182
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-01-21 13:28:38 +0300

    Create step28.93.js

---
+++ step28.93.js
@@ -0,0 +1,7 @@
+/*Step 93
+The last portion of the project is to add the logic for the checkpoints. When a player collides with a checkpoint, the checkpoint screen should appear.
+
+Start by creating a new class called CheckPoint.*/
+
+
+lass CheckPoint{}
