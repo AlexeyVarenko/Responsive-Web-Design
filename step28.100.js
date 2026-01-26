2a7b2d828bcb71551a791b0a375f3990af413d73
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-01-26 16:11:37 +0300

    Create step28.100.js

---
+++ step28.100.js
@@ -0,0 +1,17 @@
+/*Step 100
+Use const to create a new array called checkpointPositions.
+
+Inside that array, add an object for each of the following positions:
+
+Example Code
+ x: 1170, y: proportionalSize(80), z: 1
+ x: 2900, y: proportionalSize(330), z: 2 
+ x: 4800, y: proportionalSize(80), z: 3 */
+
+
+
+ const checkpointPositions=[
+  {x: 1170, y: proportionalSize(80), z: 1},
+  {x: 2900, y: proportionalSize(330), z: 2},
+  { x: 4800, y: proportionalSize(80), z: 3},
+]
