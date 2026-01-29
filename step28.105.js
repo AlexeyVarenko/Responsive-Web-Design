d7fc4bf0625780145a88bc817fa14ce8b3ad02f5
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-01-29 13:29:17 +0300

    Create step28.105.js

---
+++ step28.105.js
@@ -0,0 +1,10 @@
+/*Step 105
+The next step is to create a function that will show the checkpoint message when the player reaches a checkpoint.
+
+Create a new arrow function called showCheckpointScreen that takes in a msg parameter.*/
+
+
+
+const showCheckpointScreen=(msg)=>{
+
+}
