03c1512d5fa6dbe627833d177860c0fb2a815c5f
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-01-29 13:29:20 +0300

    Create step28.106.js

---
+++ step28.106.js
@@ -0,0 +1,8 @@
+/*Step 106
+Inside the showCheckpointScreen function, set the checkpointScreen style.display property to "block".*/
+
+
+
+const showCheckpointScreen = (msg) => {
+checkpointScreen.style.display="block"
+};
