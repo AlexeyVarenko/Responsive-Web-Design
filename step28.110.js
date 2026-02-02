249f90eaf2b7d88bcc9bc4eb7932d587063097d4
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-02-02 08:43:59 +0300

    Create step28.110.js

---
+++ step28.110.js
@@ -0,0 +1,13 @@
+/*Step 110
+Create a new const variable called checkpointDetectionRules and assign it an empty array.
+
+Inside that array, add a boolean expression that checks if the player's position.x is greater than or equal to the checkpoint's position.x.*/
+
+
+
+const checkpointDetectionRules=[
+  player.position.x>=checkpoint.position.x
+];
+  checkpoints.forEach((checkpoint, index, checkpoints) => {
+
+  });
