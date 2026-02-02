e711e1fcea23e6cbe55f4f27692abeec8dc1de56
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-02-02 08:43:57 +0300

    Create step28.109.js

---
+++ step28.109.js
@@ -0,0 +1,9 @@
+/*Step 109
+The last few steps involve updating the animate function to display the checkpoint screen when the player reaches a checkpoint.
+
+Start by adding a forEach to the checkpoints array. For the callback function, use checkpoint, index and checkpoints for the parameters.*/
+
+
+checkpoints.forEach((checkpoint, index, checkpoints)=>{
+
+})
