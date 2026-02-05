e620b426730d07c756fc6ede273b47ec12f27930
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-02-05 08:44:19 +0300

    Create step28.115.js

---
+++ step28.115.js
@@ -0,0 +1,10 @@
+/*Step 115
+The next step is to write a condition that checks if the player has reached the last checkpoint.
+
+Start by adding an if statement that checks if the index is equal to the length of the checkpoints array minus one.*/
+
+
+
+if(index===checkpoints.length-1){
+
+      }
