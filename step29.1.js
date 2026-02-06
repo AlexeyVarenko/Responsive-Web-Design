4ecaf165bc3cd5606cdfed6abce51059dfe195e7
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-02-06 13:42:50 +0300

    Create step29.1.js

---
+++ step29.1.js
@@ -0,0 +1,10 @@
+/*Step 1
+All the HTML and CSS for this project has been provided for you. You can take a look at the two files to familiarize yourself with them.
+
+Start by getting the #author-container and #load-more-btn elements with the .getElementById() method. Assign them to the variables authorContainer and loadMoreBtn, respectively.
+
+The variables will not change, so use const to declare them.*/
+
+
+const authorContainer=document.getElementById("author-container");
+const loadMoreBtn=document.getElementById("load-more-btn");
