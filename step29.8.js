27ba16ff5668f096875dd8f991db7a640e37c475
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-02-12 12:57:39 +0300

    Create step29.8.js

---
+++ step29.8.js
@@ -0,0 +1,8 @@
+/*Step 8
+Now you'll create a function to populate the UI with the author data. You will call this function inside the second .then() method.
+
+Create an empty arrow function named displayAuthors that takes authors as a parameter.*/
+
+
+
+const displayAuthors=authors=>{};
