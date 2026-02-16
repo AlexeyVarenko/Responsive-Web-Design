e49ee4b266a8d94c26747f33376df95da65faba7
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-02-16 09:31:00 +0300

    Create step29.11.js

---
+++ step29.11.js
@@ -0,0 +1,8 @@
+/*Step 11
+Now it's time to start building the HTML for the page with your destructured data. You can do this with a combination of the compound assignment operator (+=) and the innerHTML property.
+
+Inside your callback function, use the compound assignment operator to append an empty template literal to the innerHTML of authorContainer.*/
+
+
+
+authorContainer.innerHTML+=``;
