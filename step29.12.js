aa2e82b55d572a454f55a862d0248f07d91bb69f
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-02-16 09:31:03 +0300

    Create step29.12.js

---
+++ step29.12.js
@@ -0,0 +1,8 @@
+/*Step 12
+Inside the template literal, create a div element with the id set to the index from the .forEach() array method. Remember to use string interpolation to do this.
+
+Also, add a class of "user-card" to the div.*/
+
+
+
+  authorContainer.innerHTML += `<div id="${index}" class="user-card"></div>`;
