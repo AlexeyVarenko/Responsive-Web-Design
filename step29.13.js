d37a8282b05103e49c562d992086de2b1bd4c129
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-02-17 08:42:25 +0300

    Create step29.13.js

---
+++ step29.13.js
@@ -0,0 +1,10 @@
+/*Step 13
+Now you need to show some information about the author. First, show the author's name.
+
+Create an h2 tag with the class "author-name". Then, interpolate author inside the h2 tag. This is the author's name.*/
+
+
+
+
+
+ <h2 class="author-name">${author}</h2>
