0e346bb39ebd1539dc2397ceca051e72d436f68c
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-02-17 08:42:27 +0300

    Create step29.14.js

---
+++ step29.14.js
@@ -0,0 +1,8 @@
+/*Step 14
+To see the authors' names on the page, you need to call the displayAuthors function inside the second .then() method. But before that, you need to assign the author data to the empty authorDataArr array.
+
+First, remove your console.log() statement. Then, assign data to the authorDataArr variable.*/
+
+
+
+authorDataArr=data;
