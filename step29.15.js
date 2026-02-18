74e565cb540c5146084667f42599e7f28a97cb09
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-02-18 11:18:57 +0300

    Create step29.15.js

---
+++ step29.15.js
@@ -0,0 +1,9 @@
+/*Step 15
+Now authorDataArr is the same as the data you logged to the console a while ago. Log authorDataArr to the console to confirm this.
+
+Inside your console.log() statement, add the text "Author Data Array:" as the first argument and authorDataArr as the second argument. Use comma to separate the text from authorDataArr.*/
+
+
+
+authorDataArr = data;
+    console.log("Author Data Array:", authorDataArr)
