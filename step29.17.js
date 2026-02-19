733fdcd1ec38087e72199b107a105663a27f10af
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-02-19 09:13:58 +0300

    Create step29.17.js

---
+++ step29.17.js
@@ -0,0 +1,6 @@
+/*Step 17
+Now create an image tag and give it the class "user-img". Use string interpolation to set the src attribute to the image you destructured earlier. Set the alt attribute to author followed by the text "avatar". Make sure there is a space between the author variable and the word "avatar", for example, "Quincy Larson avatar".*/
+
+
+
+<img class="user-img" src="${image}" alt="${author} avatar"></img>
