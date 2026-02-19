ddf768d9bec5a58e66f354dfef16bcc30f558074
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-02-19 09:14:00 +0300

    Create step29.18.js

---
+++ step29.18.js
@@ -0,0 +1,7 @@
+/*Step 18
+The next thing you'll show are biographical details about the author. You can do this with bio that you destructured earlier.
+
+Add a paragraph element with the class "bio", then interpolate bio inside the paragraph element.*/
+
+
+ <p class="bio">${bio}</p>
