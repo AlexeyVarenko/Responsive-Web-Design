ec2cdfd0dc1233716dd01cfc5c47728c69d01782
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-02-21 17:49:37 +0300

    Create step29.19.js

---
+++ step29.19.js
@@ -0,0 +1,8 @@
+/*Step 19
+Next, add a link to the author's page on freeCodeCamp News.
+
+Add an anchor element with the class "author-link", interpolate url as the value for the href attribute, and set target to "_blank". For the text of the anchor element, interpolate author followed by the text "'s author page". For example, "Quincy Larson's author page".*/
+
+
+
+ <a class="author-link" href="${url}" target="_blank">${author}'s author page</a>
