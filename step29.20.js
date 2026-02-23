52dedd59b5fe6a1eae17d8f4f7afe31b5e672924
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-02-23 10:24:46 +0300

    Create step29.20.js

---
+++ step29.20.js
@@ -0,0 +1,8 @@
+/*Step 20
+Now you have everything you want to include in the UI. The next step is to make the Load More Authors button fetch more authors whenever it's clicked. You can do this by adding a click event to the button and carefully incrementing the startingIndex and endingIndex variables.
+
+Create a fetchMoreAuthors function with the arrow function syntax. Don't put anything in it yet. Make sure you use curly braces because you'll have more than one expression inside the function.*/
+
+
+
+const fetchMoreAuthors=()=>{};
