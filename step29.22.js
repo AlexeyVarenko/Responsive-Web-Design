dea13cc22c5093590e57478bc3a281785c604e2b
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-02-24 14:36:49 +0300

    Create step29.22.js

---
+++ step29.22.js
@@ -0,0 +1,8 @@
+/*Step 22
+Now call the displayAuthors function with a portion of the author data just like you did before.
+
+If you click the Load More Authors button after calling the function, it won't work. That's because you still have to add the click event listener to the button. You'll do that next.*/
+
+
+
+displayAuthors(authorDataArr.slice(startingIndex, endingIndex))
