11ff3c6fe8432df355f2191f453e6063df736895
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-02-25 08:40:04 +0300

    Create step29.23.js

---
+++ step29.23.js
@@ -0,0 +1,10 @@
+/*Step 23
+Remember that in step 1 you selected the Load More Authors button and assigned it to loadMoreBtn.
+
+Use addEventListener to add a "click" event listener to loadMoreBtn. Also, pass in a reference to the fetchMoreAuthors function to run whenever the button is clicked.
+
+After that, when you click the button you should see 8 more authors.*/
+
+
+
+loadMoreBtn.addEventListener("click", fetchMoreAuthors)
