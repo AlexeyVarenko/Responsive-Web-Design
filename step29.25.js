0165d5ee135952a734228ce25b1694d9786f3e63
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-02-26 11:48:57 +0300

    Create step29.25.js

---
+++ step29.25.js
@@ -0,0 +1,6 @@
+/*Step 25
+If this condition is met, disable the button by setting its disabled property to true. Also, set the textContent of the button to "No more data to load".*/
+
+
+loadMoreBtn.disabled=true;
+loadMoreBtn.textContent="No more data to load";   
