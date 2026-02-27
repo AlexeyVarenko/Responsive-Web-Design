77cf5c04008805f18a39d894eae2c4575a575aaf
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-02-27 15:14:25 +0300

    Create step29.26.js

---
+++ step29.26.js
@@ -0,0 +1,8 @@
+/*Step 26
+Next, there's not a lot of separation between each author's name and image, and the rest of the details on the card. A divider will give the author cards a clear visual hierarchy.
+
+Add a div element above the author's bio and give it the class "purple-divider".*/
+
+
+
+<div class="purple-divider"></div>
