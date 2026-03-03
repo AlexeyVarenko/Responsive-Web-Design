a1aa71cd4501e110316e2fb5ac4b64b767070665
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-03-03 12:45:44 +0300

    Create step29.29.js

---
+++ step29.29.js
@@ -0,0 +1,10 @@
+/*Step 29
+One more thing. If you keep clicking the Load More Authors button until there's no more data to load and the text changes to "No more data to load", the cursor value is still pointer. Why not change the cursor value to not-allowed instead?
+
+Access the style property of the Load More Authors button and set cursor to "not-allowed".
+
+With that, your author page is complete!*/
+
+
+
+loadMoreBtn.style.cursor="not-allowed"
