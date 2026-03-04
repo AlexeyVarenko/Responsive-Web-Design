ed490cb3bbf1a8cf816b0dba55a6fe20c84afd5f
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-03-04 12:39:48 +0300

    Create step30.1.js

---
+++ step30.1.js
@@ -0,0 +1,11 @@
+/*Step 1
+In this project, you will build a freeCodeCamp forum leaderboard that displays the latest topics, users, and replies from the freeCodeCamp forum. The HTML and CSS have been provided for you. Feel free to explore them.
+
+When you are ready, use const to declare a forumLatest variable and assign it the string "https://cdn.freecodecamp.org/curriculum/forum-latest/latest.json".
+
+Below that, create another const variable called forumTopicUrl and assign it the string "https://forum.freecodecamp.org/t/".*/
+
+
+
+const forumLatest="https://cdn.freecodecamp.org/curriculum/forum-latest/latest.json";
+const forumTopicUrl= "https://forum.freecodecamp.org/t/";
