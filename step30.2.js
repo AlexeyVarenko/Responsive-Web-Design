0a911dd8884f01face617b45efdb0a68b9697f16
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-03-04 12:39:50 +0300

    Create step30.2.js

---
+++ step30.2.js
@@ -0,0 +1,11 @@
+/*Step 2
+Next, create a const variable called forumCategoryUrl and assign it the string "https://forum.freecodecamp.org/c/".
+
+Below that, create another const variable called avatarUrl and assign it the string "https://sea1.discourse-cdn.com/freecodecamp".*/
+
+
+
+const forumLatest = "https://cdn.freecodecamp.org/curriculum/forum-latest/latest.json";
+const forumTopicUrl = "https://forum.freecodecamp.org/t/";
+const forumCategoryUrl="https://forum.freecodecamp.org/c/";
+const avatarUrl="https://sea1.discourse-cdn.com/freecodecamp";
