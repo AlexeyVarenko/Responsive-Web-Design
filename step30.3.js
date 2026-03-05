21f91c8ef4f609b37c9d8b9d79288a522eb7ef42
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-03-05 08:31:59 +0300

    Create step30.3.js

---
+++ step30.3.js
@@ -0,0 +1,5 @@
+/*Step 3
+Next, access the #posts-container element by using the getElementById() method. Assign it to a new constant called postsContainer.*/
+
+
+const postsContainer=document.getElementById("posts-container")
