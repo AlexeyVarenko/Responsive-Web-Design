c7919465efd51e4dff28c06f1670609c8b3437e8
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-03-07 13:22:12 +0300

    Create step30.5.js

---
+++ step30.5.js
@@ -0,0 +1,25 @@
+/*Step 5
+In the last project, you used the .catch() method to handle errors. Here you'll use a try...catch statement instead.
+
+The try block is designed to handle potential errors, and the code inside the catch block will be executed in case an error occurs.
+
+Example Code
+try {
+  const name = "freeCodeCamp";
+  name = "fCC";
+} catch (err) {
+  console.log(err); // TypeError: Assignment to constant variable.
+}
+Inside your fetchData function, add a try...catch statement. The catch block should have an error parameter named err.*/
+
+
+
+const fetchData = async () => { 
+  try {
+  
+} catch (err) {
+  
+  
+}
+  
+}
