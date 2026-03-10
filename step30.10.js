b554824aaaf00c8149d1860854c86c2b1c87f0d1
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-03-10 09:49:53 +0300

    Create step30.10.js

---
+++ step30.10.js
@@ -0,0 +1,8 @@
+/*Step 10
+Now it is time to display the data on the page.
+
+Start by creating an arrow function called showLatestPosts, which takes a single data parameter.*/
+
+
+
+const showLatestPosts =(data) => { };
