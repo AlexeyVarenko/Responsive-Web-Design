c50b6fec1d38a5aa102d7fbcee508aa5451717a1
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-03-13 09:39:39 +0300

    Create step30.13.js

---
+++ step30.13.js
@@ -0,0 +1,11 @@
+/*Step 13
+The topic_list object contains a topics array which contains the latest topics posted to the forum.
+
+Destructure the topics array from the topic_list object.*/
+
+
+const showLatestPosts = (data) => {
+  const { topic_list, users } = data;
+  const{topics}=topic_list;
+  
+};
