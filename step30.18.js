3dafc880ff580126f6ca69f9a67f4b833551f54d
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-03-16 09:26:31 +0300

    Create step30.18.js

---
+++ step30.18.js
@@ -0,0 +1,13 @@
+/*Step 18
+Inside your tr element, add five empty td elements.*/
+
+
+return `
+      <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      </tr>
+    `;
