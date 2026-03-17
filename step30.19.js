87bdccd922d6abe28a17ea78a0e45f076c54f8b8
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-03-17 09:00:42 +0300

    Create step30.19.js

---
+++ step30.19.js
@@ -0,0 +1,12 @@
+/*Step 19
+To display the topic title, add a p element inside the first td element.
+
+Between the paragraph tags, add an embedded expression that contains the title variable. Then add a class called "post-title" inside the opening paragraph tag.*/
+
+
+
+ return `
+    <tr>
+      <td>
+      <p class="post-title">${title}</p>
+      </td>
