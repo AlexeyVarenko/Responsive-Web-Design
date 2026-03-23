23fd6d14ff10875f6d1b06b4721b22262b1498fc
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-03-23 11:01:32 +0300

    Create step30.26.js

---
+++ step30.26.js
@@ -0,0 +1,6 @@
+/*Step 26
+Inside the fourth td element, update the current value to instead call the viewCount function with the views variable as an argument.*/
+
+
+
+<td>${viewCount(views)}</td>
