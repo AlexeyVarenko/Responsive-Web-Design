d8a5b8311c40d5f60c0091ad29644564207a68f2
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-03-24 08:19:40 +0300

    Create step30.28.js

---
+++ step30.28.js
@@ -0,0 +1,13 @@
+/*Step 28
+Inside your allCategories object, add a new key for the number 299 with a value of an empty object.
+
+Inside that object, add a property with a key of category and a string value of "Career Advice". Below that property, add another key called className with a string value of "career".*/
+
+
+
+const allCategories = {
+  "299":{
+    category: "Career Advice",
+    className:"career"
+  }
+};
