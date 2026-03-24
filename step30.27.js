5532796bf2b5d66f171c8a2e4d922f56b8104d59
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-03-24 08:19:38 +0300

    Create step30.27.js

---
+++ step30.27.js
@@ -0,0 +1,8 @@
+/*Step 27
+Each of the forum topics includes a category like Python or JavaScript. In the next few steps, you will build out a category object which holds all of the forum categories and classNames for the styling.
+
+Start by creating a new constant called allCategories and assign it the value of an empty object.*/
+
+
+
+const allCategories={};
