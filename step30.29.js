9fd7cf272c6b71ad01380114c51333ec65cc6a32
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-03-25 08:46:57 +0300

    Create step30.29.js

---
+++ step30.29.js
@@ -0,0 +1,14 @@
+/*Step 29
+Add a new key for the number 409 with a value of an empty object.
+
+Inside that object, add a property with a key of category and a string value of "Project Feedback".
+
+Below that property, add another key called className with a string value of "feedback".*/
+
+
+
+const allCategories = {
+  299: { category: "Career Advice", className: "career" },
+  409: { category: "Project Feedback", className: "feedback"}
+
+};
