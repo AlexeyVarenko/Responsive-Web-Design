aa902ea35e97d01e1126c9dd3439f1dec2ebcefc
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-03-25 08:47:00 +0300

    Create step30.30.js

---
+++ step30.30.js
@@ -0,0 +1,14 @@
+/*Step 30
+Add a new key for the number 417 with a value of an empty object.
+
+Inside that object, add a property with a key of category and a string value of "freeCodeCamp Support".
+
+Below that property, add another key called className with a string value of "support".*/
+
+
+const allCategories = {
+  299: { category: "Career Advice", className: "career" },
+  409: { category: "Project Feedback", className: "feedback" },
+  417: { category: "freeCodeCamp Support", className: "support"}
+
+};
