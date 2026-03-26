5688057d0667e88bed6a67262daa19819b482428
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-03-26 12:09:36 +0300

    Create step30.32.js

---
+++ step30.32.js
@@ -0,0 +1,7 @@
+/*Step 32
+Inside your forumCategory function, create a new let variable named selectedCategory and assign it an empty object. This will be used to store the category name and class name for each category.*/
+
+
+const forumCategory = (id) => {
+  let selectedCategory={}
+};
