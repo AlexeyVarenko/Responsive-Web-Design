e34a5dc6d99f539a948c31b8df4ceb8d62ee62cb
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-03-26 12:09:34 +0300

    Create step30.31.js

---
+++ step30.31.js
@@ -0,0 +1,10 @@
+/*Step 31
+The rest of the allCategories object has been completed for you.
+
+In the next few steps, you will create a function to retrieve the category name from the allCategories object.
+
+Start by creating an arrow function named forumCategory, with id as the parameter name.*/
+
+
+
+const forumCategory=id=>{};
