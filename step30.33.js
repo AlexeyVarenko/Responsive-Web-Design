0375cdcffad7d8efd03360666397b556ecb53e9a
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-03-27 08:24:37 +0300

    Create step30.33.js

---
+++ step30.33.js
@@ -0,0 +1,9 @@
+/*Step 33
+Create an if statement to check if the allCategories object has a property of id. Remember, you can use the hasOwnProperty() method for this.*/
+
+
+
+const forumCategory = (id) => {
+  let selectedCategory = {};
+  if(allCategories.hasOwnProperty(id));
+};
