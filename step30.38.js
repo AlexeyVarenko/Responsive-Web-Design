a18aed9790b7990c83dbfef767d896d7e6903ca8
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-03-31 08:43:43 +0300

    Create step30.38.js

---
+++ step30.38.js
@@ -0,0 +1,8 @@
+/*Step 38
+Every category will have a URL that points to the category on the freeCodeCamp forum.
+
+Create a constant called url and assign it a template literal. Inside that template literal, place the value of ${forumCategoryUrl}${selectedCategory.className}/${id}.*/
+
+
+
+const url=`${forumCategoryUrl}${selectedCategory.className}/${id}`
