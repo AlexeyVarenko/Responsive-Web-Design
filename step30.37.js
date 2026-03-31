d73ff6633aad81284a20bd784b1e24630aefd4ec
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-03-31 08:43:39 +0300

    Create step30.37.js

---
+++ step30.37.js
@@ -0,0 +1,12 @@
+/*Step 37
+Inside your else clause, assign the string "general" to selectedCategory.className.
+
+Below that, assign the string "General" to selectedCategory.category.
+
+Lastly, assign the number 1 to selectedCategory.id.*/
+
+
+
+selectedCategory.className="general" ;
+selectedCategory.category="General";
+selectedCategory.id=1;
