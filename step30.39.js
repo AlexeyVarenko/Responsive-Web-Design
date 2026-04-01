baa835d2e8c1574c165d861283a00a8cdce0e656
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-04-01 11:12:26 +0300

    Create step30.39.js

---
+++ step30.39.js
@@ -0,0 +1,4 @@
+/*Step 39
+Create a constant called linkText and assign it the value of selectedCategory.category. This will display the name of the category in the anchor element.*/
+
+const linkText=selectedCategory.category;
