7343f9a741a18287dad014862db6edc3fb30d126
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-04-01 11:12:28 +0300

    Create step30.40.js

---
+++ step30.40.js
@@ -0,0 +1,8 @@
+/*Step 40
+Create a constant called linkClass and assign it a template literal. Inside that template literal, add the value of category ${selectedCategory.className}.
+
+These class names will be used to apply styles for the anchor element.*/
+
+
+
+const linkClass= `category ${selectedCategory.className}`
