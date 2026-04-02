a86f9a0bf063ea74a3b51142ffc22af40db62d87
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-04-02 08:45:29 +0300

    Create step30.41.js

---
+++ step30.41.js
@@ -0,0 +1,6 @@
+/*Step 41
+Next, return an anchor element inside template literals. For the href attribute, assign the value of the url constant.*/
+
+
+
+return `<a href="${url}" class="${linkClass}" target="_blank">${linkText}</a>`;
