f9300d5bbe82ee02f0d81d058bc4e3cd645062f3
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-04-02 08:45:31 +0300

    Create step30.42.js

---
+++ step30.42.js
@@ -0,0 +1,9 @@
+/*Step 42
+After the href attribute, set the class attribute to the constant named linkClass.
+
+After the class attribute, set the target attribute to "_blank".
+
+Lastly, place the linkText constant in between the anchor tags to display the text in the link.*/
+
+
+ return `<a href="${url}" class="${linkClass}" target="_blank">${linkText}</a>`;
