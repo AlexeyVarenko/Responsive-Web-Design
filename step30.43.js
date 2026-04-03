8e41a4e3673c363c9eae529dd4b86ff78678cb85
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-04-03 08:26:10 +0300

    Create step30.43.js

---
+++ step30.43.js
@@ -0,0 +1,8 @@
+/*Step 43
+Inside the first td element, add an embedded expression ${}. Inside that expression, call the forumCategory function with the argument of category_id.
+
+Now, you should see a category displayed underneath each post topic.*/
+
+
+
+ ${forumCategory(category_id)};
