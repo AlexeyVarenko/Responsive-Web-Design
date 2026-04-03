22ae80c5106345bcc33ef10a0a044abdf99fdd5c
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-04-03 08:26:42 +0300

    Create step30.44.js

---
+++ step30.44.js
@@ -0,0 +1,10 @@
+/*Step 44
+Each forum post will include a list of user avatar images which represent all of the users participating in the conversation for that topic.
+
+Start by creating an arrow function called avatars, with two parameters called posters and users.*/
+
+
+
+const avatars=(posters, users)=>{
+
+}
