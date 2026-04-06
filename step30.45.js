750c744e611ffe4ab670655c15d9cf02d2cc1cd2
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-04-06 09:46:40 +0300

    Create step30.45.js

---
+++ step30.45.js
@@ -0,0 +1,11 @@
+/*Step 45
+The next step is to loop through the posters array to get all of their avatars.
+
+Start by adding a return keyword followed by posters.map(). For the callback function, add a parameter called poster.*/
+
+
+
+
+const avatars = (posters, users) => {
+  return posters.map(poster=>{});
+};
