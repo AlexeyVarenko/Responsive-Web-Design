95e398078a6e1958cfe8c6adaedaac0fde52745d
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-04-09 08:42:53 +0300

    Create step30.52.js

---
+++ step30.52.js
@@ -0,0 +1,8 @@
+/*Step 52
+For the remaining steps, you will add the functionality to display the user avatars.
+
+Inside the second td element, add a div element with a class name of "avatar-container".*/
+
+
+
+<div class="avatar-container"></div>
