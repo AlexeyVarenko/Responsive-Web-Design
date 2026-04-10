453283b06163f932ff278d39f04df030138f8dee
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-04-10 08:57:23 +0300

    Create step30.53.js

---
+++ step30.53.js
@@ -0,0 +1,8 @@
+/*Step 53
+Inside the div element, call the avatars function and pass in the arguments of posters and users.
+
+Now you should see the avatars displayed on the page.*/
+
+
+
+${avatars(posters,users)};
