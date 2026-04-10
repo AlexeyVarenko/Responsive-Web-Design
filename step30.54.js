8e62ba23bb8cb9268d9b33464bff1c88acbec2ea
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-04-10 08:57:25 +0300

    Create step30.54.js

---
+++ step30.54.js
@@ -0,0 +1,9 @@
+/*Step 54
+Your project is almost complete. It is just missing one last piece.
+
+Users should be able to click on any post title and be directed to the actual post on the freeCodeCamp forum.
+
+Start by changing the existing paragraph element inside the first td element to be an anchor element.*/
+
+
+ <a class="post-title">${title}</a>
