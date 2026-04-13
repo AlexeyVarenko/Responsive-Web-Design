d4b75f76a7407bc1fecf123958bf80240248a4c0
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-04-13 09:11:23 +0300

    Create step30.55.js

---
+++ step30.55.js
@@ -0,0 +1,8 @@
+/*Step 55
+For the opening a tag, set the target attribute to "_blank". Then, set the href attribute to ${forumTopicUrl}${slug}/${id}.
+
+And with those changes, your freeCodeCamp forum leaderboard project is now complete!*/
+
+
+
+<a class="post-title" target="_blank" href="${forumTopicUrl}${slug}/${id}">${title}</a>
