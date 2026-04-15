988a4c5ea9b395eba55487cfe18a3e458a9b321d
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-04-15 08:20:58 +0300

    Create step31.4.js

---
+++ step31.4.js
@@ -0,0 +1,16 @@
+/*Step 4
+Forms consist of inputs where users can input their data. You can group related inputs together using the fieldset element.
+
+Here is an example of using a fieldset element:
+
+Example Code
+<form action="/example-url">
+  <fieldset>
+  <!-- inputs go inside here-->
+  </fieldset>
+</form>
+Inside your form element, add a fieldset element.*/
+
+
+
+<fieldset> </fieldset>  
