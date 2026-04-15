8e8f91acd32445facada1ef35584563005003c10
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-04-15 08:20:56 +0300

    Create step31.3.js

---
+++ step31.3.js
@@ -0,0 +1,18 @@
+/*Step 3
+In previous lessons, you learned how to work with the form element like this:
+
+Example Code
+<form method="value-goes-here" action="url-goes-here">
+  <!-- inputs go inside here -->
+</form>
+The action attribute is used to specify where the form data should be sent when the form is submitted.
+
+The method attribute is used to specify the HTTP method to use when sending the form data. The most common methods are GET and POST.
+
+NOTE: You will learn about how HTTP methods work in later modules.
+
+Inside your main element, add a form element with an action attribute set to "https://hotel-feedback.freecodecamp.org" and a method attribute set to "POST".*/
+
+
+
+      <form action="https://hotel-feedback.freecodecamp.org" method ="POST" </form></form>
