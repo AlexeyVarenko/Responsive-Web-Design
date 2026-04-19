97b3de1441d64cb00e128ddbc5e8162b8bbd1726
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-04-19 08:29:41 +0300

    Create step31.6.js

---
+++ step31.6.js
@@ -0,0 +1,16 @@
+/*Step 6
+In previous lessons, you learned how to associate a label element with an input like this:
+
+Example Code
+<label for="name">Name:</label>
+<input type="text" id="name" name="name">
+The for attribute on the label element should match the id attribute on the input element. This is known as an explicit association.
+
+Below your legend element, add a label element with the text of Name (required):. Set its for attribute to the value of "full-name".
+
+Then below your label element, add an input element with no attributes. In the next steps, you will add the necessary attributes.*/
+
+
+
+<label for="full-name">Name (required):</label>
+          <input ></input>
