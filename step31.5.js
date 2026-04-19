aaa65ca90aa65e049c94c981eb56f7afc1775250
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-04-19 08:29:39 +0300

    Create step31.5.js

---
+++ step31.5.js
@@ -0,0 +1,17 @@
+/*Step 5
+When working with fieldset elements, it is common to use a caption to describe the group of inputs. You can use the legend element for this.
+
+Here is an example of using a legend element:
+
+Example Code
+<form action="/example-url">
+  <fieldset>
+    <legend>Personal Information</legend>
+    <!-- inputs go inside here-->
+  </fieldset>
+</form>
+Inside your fieldset element, add a legend element with the text Personal Information.*/
+
+
+
+<legend>Personal Information</legend>
