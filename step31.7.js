e52f80c3747072da8ff5b155c1dd984c61c23c8e
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-04-20 08:06:16 +0300

    Create step31.7.js

---
+++ step31.7.js
@@ -0,0 +1,13 @@
+/*Step 7
+When a user provides their full name, the input will accept plaintext.
+
+In previous lessons, you learned how to work with the type attribute like this:
+
+Example Code
+<input type="text">
+For your existing input element, add a type attribute set to "text".
+
+Also give the input element an id attribute with the value of "full-name".*/
+
+
+<input type="text" id="full-name"></input>
