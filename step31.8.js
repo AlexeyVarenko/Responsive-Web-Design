9f60ddd63a8660ba354f593d3c9babaf38d40d76
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-04-20 08:06:19 +0300

    Create step31.8.js

---
+++ step31.8.js
@@ -0,0 +1,11 @@
+/*Step 8
+The name attribute is used to identify form data after it has been submitted to the server.
+
+Here is an example of how to use the name attribute:
+
+Example Code
+<input type="email" name="email">
+Add a name attribute to the input element with the value of "name".*/
+
+
+ <input type="text" id="full-name" name="name"></input>
