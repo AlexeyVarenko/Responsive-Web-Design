2d620ba1bb50ac081cd780aa14fcad6be2536ace
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-04-21 15:37:39 +0300

    Create step31.10.js

---
+++ step31.10.js
@@ -0,0 +1,8 @@
+/*Step 10
+Your hotel feedback form should also collect an email address from the user.
+
+Start by adding a new label element with the text Email address (required): to the form. Your label element should have a for attribute set to the value of "email".*/
+
+
+
+<label for="email">Email address (required):</label>
