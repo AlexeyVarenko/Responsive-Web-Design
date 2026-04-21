07751183b572e50b11262fec05f021cc87ad5fd5
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-04-21 15:37:41 +0300

    Create step31.9.js

---
+++ step31.9.js
@@ -0,0 +1,11 @@
+/*Step 9
+In previous lessons, you learned how to work with the placeholder and required attributes like this:
+
+<input type="text" placeholder="e.g., John Doe" required>
+For your existing input element, add a placeholder attribute with the value of "e.g., John Doe".
+
+Also, add the required attribute to the input element.*/
+
+
+
+ <input type="text" id="full-name" name="name" placeholder="e.g., John Doe" required></input>
