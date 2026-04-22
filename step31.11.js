6fe704cfe8310603c45500d82e59df8ea7db3a5d
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-04-22 08:49:43 +0300

    Create step31.11.js

---
+++ step31.11.js
@@ -0,0 +1,11 @@
+/*Step 11
+Next, add an input with the type of "email" below your email label. This input should have an id attribute set to the value of "email". Also, set the name attribute to the value of "email".
+
+This input is also required, so make sure to add the required attribute.
+
+Finally, add a placeholder attribute set to "example@email.com".*/
+
+
+
+
+<input name="email" id="email" type="email" required placeholder="example@email.com"></input>  
