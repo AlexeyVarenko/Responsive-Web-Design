3b7e4ccd262d3069ac2430e0e3ce6ac0a83f1485
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-04-23 08:43:08 +0300

    Create step31.13.js

---
+++ step31.13.js
@@ -0,0 +1,10 @@
+/*Step 13
+Your hotel feedback form should have an option for users to add their age.
+
+Start by adding a label element with the text Age (optional): to the form.
+
+The for attribute should be set to "age".*/
+
+
+
+<label for="email">Email address (required):</label>
