d9422b7b76931979b0a7e171781acfa524f98e3b
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-04-23 08:43:10 +0300

    Create step31.14.js

---
+++ step31.14.js
@@ -0,0 +1,16 @@
+/*Step 14
+The number input is used to create a numeric input field.
+
+Here is an example of a number input field:
+
+Example Code
+<input type="number" id="age" name="age" min="18" max="100">
+The min and max attributes are used to set the minimum and maximum values that can be entered in the input field.
+
+Below your label element, add an input with the type attribute set to "number" and an id of "age".
+
+The name attribute should be set to "age", the min attribute should be set to "3" and the max attribute should be set to "100".*/
+
+
+
+<input type="number" id="age" name="age" min="3" max="100"></input>
