6353b93cda3b1821cd34d2f03f38f1c3a9a602c6
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-04-24 09:50:02 +0300

    Create step31.15.js

---
+++ step31.15.js
@@ -0,0 +1,9 @@
+/*Step 15
+The next section in the form will be responsible for asking users if they have stayed at the hotel before.
+
+Start by adding a fieldset element.
+
+Inside the fieldset element, add a legend element with the text of Was this your first time at our hotel?.*/
+
+
+<legend>Was this your first time at our hotel?</legend>
