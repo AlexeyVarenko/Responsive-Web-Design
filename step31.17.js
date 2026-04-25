d89ae5d2116eaf812edd5410f0e8a163382f8eab
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-04-25 09:02:54 +0300

    Create step31.17.js

---
+++ step31.17.js
@@ -0,0 +1,11 @@
+/*Step 17
+Below your label element, add a radio button with the id set to "no-option", the name attribute set to "hotel-stay", and a value attribute set to "no".
+
+Below your new radio button, add another label element with the for attribute set to "no-option". The text for the label should be No.
+
+When you are finished, you can now try out the radio buttons by selecting one option at a time.*/
+
+
+
+ <input id="no-option" type="radio" name="hotel-stay" value="no" />
+          <label for="no-option">No</label>
