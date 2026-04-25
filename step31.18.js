2274647f898291da37d95e50fb365228f5097a4a
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-04-25 09:02:56 +0300

    Create step31.18.js

---
+++ step31.18.js
@@ -0,0 +1,12 @@
+/*Step 18
+The next section of the form will ask users why they chose that particular hotel. Users will have the opportunity to select multiple options.
+
+Start by adding another fieldset element.
+
+Inside the fieldset element, add a legend element with the text Why did you choose to stay at our hotel? (Check all that apply).*/
+
+
+
+ <fieldset>
+           <legend>Why did you choose to stay at our hotel? (Check all that apply)</legend>
+            </fieldset>
