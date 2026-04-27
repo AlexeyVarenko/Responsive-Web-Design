e9b760195593c044fe138eec4eec7ca0888f8fcc
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-04-27 08:19:11 +0300

    Create step31.20.js

---
+++ step31.20.js
@@ -0,0 +1,9 @@
+/*Step 20
+Add another checkbox input with the idand value attributes set to "recommendation", and a name attribute set to "choice".
+
+Below the checkbox input, add another label with the text Personal Recommendation. The for attribute should be set to "recommendation".*/
+
+
+
+<input type="checkbox" id="recommendation" name="choice" value="recommendation" />
+          <label for="recommendation">Personal Recommendation</label>
