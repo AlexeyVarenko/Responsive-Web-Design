7ac36cd9774606d697ae2423c5c955980fd8bf38
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-04-28 08:29:45 +0300

    Create step31.22.js

---
+++ step31.22.js
@@ -0,0 +1,11 @@
+/*Step 22
+To make a checkbox input checked by default, you can add the checked attribute.
+
+Here is an example of using the checked attribute:
+
+<input checked type="checkbox" id="checked" name="checked">
+Add the checked attribute to the checkbox input with the id of "reputation" to make it checked by default.*/
+
+
+
+ <input type="checkbox" id="reputation" name="choice" value="reputation" checked type="checkbox" />
