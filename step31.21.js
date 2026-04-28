c1e34cbe4d8bc9ea4d9c254f0525fe99c5a31601
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-04-28 08:29:43 +0300

    Create step31.21.js

---
+++ step31.21.js
@@ -0,0 +1,19 @@
+/*Step 21
+Next, add another checkbox input with the id and value attributes set to "location", and the name attribute set to "choice".
+
+For the label element, the text of Location and the for attribute should be set to "location".
+
+Below that label element, add another checkbox input with the id and value attributes set to "reputation", and the name attribute set to "choice".
+
+For the label element, the text of Reputation and the for attribute should be set to "reputation".*/
+
+
+
+Step 21
+Next, add another checkbox input with the id and value attributes set to "location", and the name attribute set to "choice".
+
+For the label element, the text of Location and the for attribute should be set to "location".
+
+Below that label element, add another checkbox input with the id and value attributes set to "reputation", and the name attribute set to "choice".
+
+For the label element, the text of Reputation and the for attribute should be set to "reputation".
