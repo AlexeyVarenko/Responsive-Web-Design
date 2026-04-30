d0cf740ccccb5f3c7c603f1bca8470735cfb5108
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-04-30 13:49:30 +0300

    Create step31.23.js

---
+++ step31.23.js
@@ -0,0 +1,17 @@
+/*Step 23
+For the final input and label inside this fieldset, you will add a checkbox input with the id and value attributes set to "price", and a name attribute set to "choice".
+
+Then, a label element with the text Price and the for attribute set to "price".
+
+Now you can test out your form by selecting the various checkboxes.*/
+
+
+
+<input
+            checked
+            type="checkbox"
+            id="price"
+            name="choice"
+            value="price"
+          />
+          <label for="price">Price</label>
