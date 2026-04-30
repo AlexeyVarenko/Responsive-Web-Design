12b341e16363e984232fa972d843544132ef5b35
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-04-30 13:49:32 +0300

    Create step31.24.js

---
+++ step31.24.js
@@ -0,0 +1,14 @@
+/*Step 24
+The next section of the form will provide users with the ability to leave a rating for the hotel.
+
+Start by adding a new fieldset element with a legend element nested inside. The legend should have the text Ratings.
+
+Below the legend element, add a label element with the text How was the service?. The for attribute should be set to "service".*/
+
+
+
+<fieldset>
+          <legend>
+            Ratings
+          </legend>
+           <label for="service">How was the service?</label>
