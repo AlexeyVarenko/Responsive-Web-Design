b4647cbf55985f91c06c924ae9b59f2aa7ab926c
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-05-01 13:07:53 +0300

    Create step31.25.js

---
+++ step31.25.js
@@ -0,0 +1,18 @@
+/*Step 25
+When you want users to make selections from a dropdown menu, you can use the select and option elements.
+
+Here is an example of using the select and option elements to create a dropdown for different cities:
+
+Example Code
+<label for="city">Choose a City: </label>
+<select id="city" name="city">
+  <option value="new-york">New York</option>
+  <option value="los-angeles">Los Angeles</option>
+  <option value="chicago">Chicago</option>
+  <option value="miami">Miami</option>
+</select>
+Start by adding a select element with the name and id attributes set to "service".*/
+
+
+
+<select name="service" id="service"></select>
