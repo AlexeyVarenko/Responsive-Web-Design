1adfa4e38c110593b33ef3ccbde449167bc447ac
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-05-04 12:58:52 +0300

    Create step31.27.js

---
+++ step31.27.js
@@ -0,0 +1,12 @@
+/*Step 27
+To make an option selected by default, you can add the selected attribute to the option element you want to be selected.
+
+Here is an example using the selected attribute:
+
+Example Code
+<option selected value="amazing">Amazing</option>
+Inside your select element, add the selected attribute to the option element with the value of "excellent".*/
+
+
+
+<option selected value="excellent">Amazing</option>
