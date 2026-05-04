e60c4aab43ad3500ff6b870adbc82c0124920ee7
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-05-04 12:58:50 +0300

    Create step31.26.js

---
+++ step31.26.js
@@ -0,0 +1,26 @@
+/*Step 26
+Inside your select element, add the following five option elements with these corresponding values for the option text and value attribute:
+
+Value Attributes:
+
+poor
+satisfactory
+good
+very-good
+excellent
+Option Element Text:
+
+Poor
+Satisfactory
+Good
+Very Good
+Excellent*/
+
+
+
+ <option value="poor">Poor</option>
+            <option value="satisfactory">Satisfactory</option>
+            <option value="good">Good</option>
+            <option value="very-good">Very Good</option>
+            <option value="excellent">Excellent</option>            
+          </select>
