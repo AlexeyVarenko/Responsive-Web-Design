f23e6172e5881073b5221e52a96098261d26026f
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-05-05 10:40:33 +0300

    Create step31.28.js

---
+++ step31.28.js
@@ -0,0 +1,10 @@
+/*Step 28
+Your hotel feedback form should also give users the ability to rate the food.
+
+Start by adding a label element with the text of How was the food?. That label element should have a for attribute set to "food".
+
+Below your label element, add a select element with an id and name set to "food".*/
+
+ <label for="food">How was the food?</label>
+          <select id= "food" name="food"></select>
+
