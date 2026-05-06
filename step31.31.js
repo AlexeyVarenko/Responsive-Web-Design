1049b7400461ce0aae1bfbc99b8a7faf1341b34f
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-05-06 08:20:31 +0300

    Create step31.31.js

---
+++ step31.31.js
@@ -0,0 +1,17 @@
+/*Step 31
+If you want users to have more space to write their comments, you can use a textarea element.
+
+The textarea element is a multi-line text input control that allows users to enter text that is longer than a single line. It can be used to create a comment box, a message input, or other text input that requires multiple lines.
+
+Here's an example of a textarea element:
+
+Example Code
+<textarea id="comments" name="comments" rows="4" cols="50"></textarea>
+The rows attribute is used to specify the visible height of the textarea, and the cols attribute is used to specify the visible width of the textarea.
+
+Below your label element, add a textarea element. In the next step, you will add the necessary attributes.*/
+
+
+
+
+<textarea></textarea>
