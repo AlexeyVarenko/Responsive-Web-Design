59743048f39134932efab1c9a290cc88800fc8dc
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-05-06 08:20:26 +0300

    Create step31.30.js

---
+++ step31.30.js
@@ -0,0 +1,6 @@
+/*Step 30
+The last section of the form will allow users to provide any additional feedback they like about the hotel.
+
+Start by adding a label element with the text of Other Comments? and a for attribute set to "comments". */
+
+ <label for="comments">Other Comments?</label>
