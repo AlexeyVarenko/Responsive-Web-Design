3af66186d705442e8a198632adfe604bb789f415
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-05-07 09:27:52 +0300

    Create step31.32.js

---
+++ step31.32.js
@@ -0,0 +1,8 @@
+/*Step 32
+For your textarea element, add an id and name attribute with the value of "comments".
+
+For the cols attribute set the value to 30 and for the rows attribute set the value to 10.*/
+
+
+
+ <textarea id="comments" name="comments" rows="10" cols="30"></textarea>
