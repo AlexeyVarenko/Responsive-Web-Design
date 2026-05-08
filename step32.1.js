64a9ab536750df7ae3c4415e843e73ab5f9fb9bb
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-05-08 08:36:58 +0300

    Create step32.1.js

---
+++ step32.1.js
@@ -0,0 +1,10 @@
+/*Step 1
+In this workshop, you will practice working with HTML tables by building a final exam table for a group of students.
+
+In previous lessons, you learned how to work with the table element to represent tabular data.
+
+Inside your body element, nest a table element.*/
+
+
+
+ <table></table> 
