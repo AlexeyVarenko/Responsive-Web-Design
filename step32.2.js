94c9e261d8ee37ea14380a4c6e9ed906be580471
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-05-08 08:37:02 +0300

    Create step32.2.js

---
+++ step32.2.js
@@ -0,0 +1,13 @@
+/*Step 2
+To add a caption to a table, you can use the table caption element.
+
+Here is an example using the caption element:
+
+Example Code
+<table>
+  <caption>Football Scores</caption>
+</table>
+Inside your table element, nest a caption element with the text Calculus Final Exam Grades.*/
+
+
+ <caption>Calculus Final Exam Grades</caption>
