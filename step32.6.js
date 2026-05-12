54d40d2c20f985431643dc5095d03483e7bec10d
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-05-12 08:15:27 +0300

    Create step32.6.js

---
+++ step32.6.js
@@ -0,0 +1,28 @@
+/*Step 6
+To add student data to the table, you will need to use the table row and table data elements.
+
+The table data element, td, is used to create a cell in the table.
+
+Here is an example of using the td element for a sports players table:
+
+Example Code
+<tr>
+  <td>1</td>
+  <td>John Doe</td>
+  <td>USA</td>
+</tr>
+Inside your table body element, add a table row element, tr, with three table data elements, td.
+
+The first table data element should contain the last name of Davis.
+
+The second table data element should contain the first name of Alex.
+
+The third table data element should contain the grade of 54.*/
+
+
+
+<tr>
+        <td>Davis</td>
+        <td>Alex</td>
+        <td>54</td>
+        </tr> 
