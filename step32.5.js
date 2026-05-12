888bf02c0ea843032dd3f4a62e6ec6f8720ad870
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-05-12 08:15:26 +0300

    Create step32.5.js

---
+++ step32.5.js
@@ -0,0 +1,8 @@
+/*Step 5
+Now that you have completed the head section, it is time to add the table body, tbody. The table body will represent all of the student names and their grades.
+
+Add a table body element to your table.*/
+
+
+
+ <tbody></tbody>
