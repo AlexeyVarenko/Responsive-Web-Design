df1a7746bfd796ce3de958b62dceb64044bc1e68
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-05-12 08:15:22 +0300

    Create step32.3.js

---
+++ step32.3.js
@@ -0,0 +1,22 @@
+/*Step 3
+For the first section of the table, you will want to group the header content which represents the column labels for the student's first name, last name, and final exam grade.
+
+The table head element, thead, is used to group the header content in a table.
+
+Here is an example using the thead element:
+
+Example Code
+<table>
+  <thead>
+    <!-- header content goes here -->
+  </thead>
+</table>
+Below your caption element, add a table head element.*/
+
+
+
+<table>
+      <thead>
+        
+      </thead>
+      </table> 
