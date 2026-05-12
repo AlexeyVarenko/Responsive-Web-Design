ef21226b34e61e86b88d216bf245bfe68af913d7
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-05-12 08:15:24 +0300

    Create step32.4.js

---
+++ step32.4.js
@@ -0,0 +1,29 @@
+/*Step 4
+The table head element consists of a table row element, tr, which contains the table header cell elements, th.
+
+Here is an example using the tr and th elements for a sports table:
+
+Example Code
+<table>
+  <caption>Football Scores</caption>
+  <thead>
+    <tr>
+      <th>Team</th>
+      <th>Wins</th>
+      <th>Losses</th>
+    </tr>
+  </thead>
+</table>
+Inside your thead element, add a tr element.
+
+Inside your tr element, add three th elements.
+
+The first th element should contain the text Last Name. The second th element should contain the text First Name. The third th element should contain the text Grade.*/
+
+
+
+ <tr>
+        <th>Last Name</th>
+        <th>First Name</th>
+        <th>Grade</th>
+        </tr> 
