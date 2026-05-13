b075b044254fc5ca02e907865a35d0e90b3439cf
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-05-13 09:57:55 +0300

    Create step32.8.js

---
+++ step32.8.js
@@ -0,0 +1,26 @@
+/*Step 8
+Now it is time to add two more students to the table.
+
+Following the same pattern as the previous steps, add a fourth student table row. Use the following data for the table data elements:
+
+Last Name: Thompson
+First Name: Jane
+Grade: 77
+For the fifth student table row, use the following data for the table data elements:
+
+Last Name: Williams
+First Name: Natalie
+Grade: 83*/
+
+
+
+ <tr>
+          <td>Thompson</td>
+          <td>Jane</td>
+          <td>77</td>
+        </tr>
+        <tr>
+          <td>Williams</td>
+          <td>Natalie</td>
+          <td>83</td>
+        </tr>
