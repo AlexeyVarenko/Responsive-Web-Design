e314b74ca1221072c9fb40696fd4436eb3934906
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-05-14 08:34:25 +0300

    Create step32.9.js

---
+++ step32.9.js
@@ -0,0 +1,22 @@
+/*Step 9
+The last section to add to the table would be the table foot element, tfoot. The table foot element will be used to display the average grade for all of the students in the table.
+
+Add a tfoot element to the table.
+
+Inside the tfoot element, add a tr element.
+
+Inside the tr element, add two td elements.
+
+The first td element should contain the text Average Grade. The second td element should contain the text 78.8. */
+
+
+
+
+<tfoot>
+        <tr>
+          <td>Average Grade</td>
+          <td>78.8</td>
+        </tr>
+      
+
+      </tfoot>
