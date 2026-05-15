e90c0eab47bcefcf719e34e6174d7f889b9a6bcb
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-05-15 08:16:56 +0300

    Create step33.1.js

---
+++ step33.1.js
@@ -0,0 +1,19 @@
+/*Step 1
+Camperbot has created a coding journey blog page, but it looks like the page has some accessibility issues and bad practices. Your job in this workshop, is to fix these issues for Camperbot.
+
+The first set of errors has to deal with the use of headings. In a prior lesson, you learned that it is best practice to only use one h1 per page. But it looks like Camperbot is a using a few h1s.
+
+Leave the <h1>Welcome to Camperbot's Blog</h1> alone and change the other h1 elements to h2 elements.*/
+
+
+
+<div>
+    <h2 id="post1">My Journey Learning to Code</h2>
+    <p>I started learning to code a few months ago and it's been a wild ride!</p>
+    
+    <h4>Early Challenges</h4>
+    <p>At first, syntax was really confusing.</p>
+    
+    <h4>Breakthroughs</h4>
+    <p>Eventually things started to click.</p>
+  </div>
