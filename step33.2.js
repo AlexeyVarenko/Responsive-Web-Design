5490ebdc0b1bcdeee54a6e3ab40b5eb2eeedd1f1
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-05-15 08:16:58 +0300

    Create step33.2.js

---
+++ step33.2.js
@@ -0,0 +1,17 @@
+/*Step 2
+It looks like there are still some issues with the use of headings on the page. If you look at the first div element, there are two h4 elements used as subheadings. A better practice would be to use h3 elements for these subheadings since they are a level below the h2 element.
+
+Change the two h4 elements to h3 elements.*/
+
+
+
+<div>
+    <h2 id="post1">My Journey Learning to Code</h2>
+    <p>I started learning to code a few months ago and it's been a wild ride!</p>
+    
+    <h3>Early Challenges</h3>
+    <p>At first, syntax was really confusing.</p>
+    
+    <h3>Breakthroughs</h3>
+    <p>Eventually things started to click.</p>
+  </div>
