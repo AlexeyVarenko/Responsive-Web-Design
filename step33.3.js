c9d8bd1766bc1833cc6cb24247beb7b656e6535d
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-05-18 13:46:50 +0300

    Create step33.3.js

---
+++ step33.3.js
@@ -0,0 +1,14 @@
+/*Step 3
+In the second post section, there is an h5 element being used. It would be more appropriate to use an h3 element here.
+
+Change the h5 element to an h3 element.*/
+
+
+
+<div>
+    <h2 id="post2">Accessibility Matters</h2>
+    <p>Today I learned that not everyone uses the web the same way I do.</p>
+
+    <h3>Screen Readers</h3>
+    <p>These tools help visually impaired users browse websites.</p>
+  </div>
