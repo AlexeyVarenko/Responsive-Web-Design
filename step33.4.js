620b31462fa6da60d2337276c320626eacc77f8a
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-05-18 13:46:52 +0300

    Create step33.4.js

---
+++ step33.4.js
@@ -0,0 +1,13 @@
+/*Step 4
+Below the main title of the page, there is a navigation section that contains links to each blog post.
+
+This section should be wrapped in a nav element to indicate its purpose as a navigation landmark.*/
+
+
+ <nav><h2>Navigation</h2>
+  <ul>
+    <li><a href="#post1">My Journey</a></li>
+    <li><a href="#post2">Accessibility</a></li>
+    <li><a href="#post3">Next Steps</a></li>
+  </ul>
+  </nav>
