bb21e1a5bb0991e228c7c247972239cbd1a31d81
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-05-20 08:32:51 +0300

    Create step33.7.js

---
+++ step33.7.js
@@ -0,0 +1,11 @@
+/*Step 7
+For the last part of the workshop, there are a few changes needed for the contact section at the bottom.
+
+The first change would be to wrap the contact section inside of a footer element. This will help screen readers identify this section as the footer of the page.*/
+
+
+
+<footer>
+  <h3>Contact Me</h3>
+  <p>Email me at camperbot@blog.io</p>
+</footer>
