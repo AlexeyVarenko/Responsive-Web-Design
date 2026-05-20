ba00daea44292cae106b47abec769b9432989b2e
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-05-20 08:32:53 +0300

    Create step33.8.js

---
+++ step33.8.js
@@ -0,0 +1,10 @@
+/*Step 8
+Right now the footer section has an h3 heading. But it should be an h2 heading to maintain a proper heading hierarchy.
+
+Change the h3 to an h2.*/
+
+
+<footer>
+    <h2>Contact Me</h2>
+    <p>Email me at camperbot@blog.io</p>
+  </footer>
