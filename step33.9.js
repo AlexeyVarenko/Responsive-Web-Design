3b80b97f918a137a4f0725e811b2c210bf70b3aa
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-05-21 08:46:03 +0300

    Create step33.9.js

---
+++ step33.9.js
@@ -0,0 +1,12 @@
+/*Step 9
+The last change will be to the email text inside of the footer. In earlier lessons and workshops, you learned how to work with the mailto link like this:
+
+Example Code
+<p>Email me at <a href="mailto:janedoe@email.com">janedoe@email.com</a></p>
+Start by wrapping the camperbot@blog.io email address inside of an anchor element. Then, add the href attribute to the anchor element and set it equal to mailto:camperbot@blog.io.
+
+With that last change, you have successfully resolved all of the issues in the blog page!*/
+
+
+
+<p>Email me at <a href="mailto:camperbot@blog.io">camperbot@blog.io</a></p>
