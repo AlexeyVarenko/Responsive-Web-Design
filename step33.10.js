2260eef45989043abefc055d3f695d745e8ca74e
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-05-22 08:11:23 +0300

    Create step33.10.js

---
+++ step33.10.js
@@ -0,0 +1,16 @@
+/*Step 1
+In this workshop, you will learn how to make accessible table elements by building out a schedule for a tech conference.
+
+Start by adding an h1 element with the text Tech Conference 2025 Schedule.*/
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Tech Conference 2025 Schedule</title>
+</head>
+<body>
+    <h1>Tech Conference 2025 Schedule</h1>
+</body>
+</html>
