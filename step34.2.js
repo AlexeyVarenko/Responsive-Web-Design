f00af69033f6495474022a5e3b0f48453dc50f15
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-05-22 08:11:25 +0300

    Create step34.2.js

---
+++ step34.2.js
@@ -0,0 +1,20 @@
+/*Step 2
+Now it is time to start building out the table.
+
+Start by adding a table element below your h1 element. Inside of the table element, add a caption element with the text Schedule by Track and Time.*/
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Tech Conference 2025 Schedule</title>
+</head>
+<body>
+  <h1>Tech Conference 2025 Schedule</h1>
+
+<table>
+  <caption>Schedule by Track and Time</caption>
+  </table>    
+</body>
+</html>
