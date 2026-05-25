1c95d51f138212cfe2b5f9a7a7e4337dbbde345c
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-05-25 08:30:16 +0300

    Create step34.3.js

---
+++ step34.3.js
@@ -0,0 +1,10 @@
+/*Step 3
+The next step is to add the table head.
+
+Start by adding a thead element below the caption element. Inside the thead, add a tr element.*/
+
+
+
+<thead>
+      <tr></tr>
+      </thead>
