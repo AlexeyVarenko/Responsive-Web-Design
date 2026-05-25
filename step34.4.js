65e9ec54cc2cc74a9f6a4d302f662360d3874fb0
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-05-25 08:30:18 +0300

    Create step34.4.js

---
+++ step34.4.js
@@ -0,0 +1,13 @@
+/*Step 4
+Inside of your tr element, add four th elements. The first th element should have the text of Time, the second should have the text of Track A, the third should have the text of Track B, and the fourth should have the text of Track C.*/
+
+
+
+ <thead>
+      <tr>
+        <th>Time</th>
+        <th>Track A</th>
+        <th>Track B</th>
+        <th>Track C</th>
+      </tr>
+    </thead>
