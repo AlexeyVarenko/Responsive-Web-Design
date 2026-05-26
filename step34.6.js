1fa6bfdf970db2c3b488a4fa32fa3eb0672550f7
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-05-26 11:35:46 +0300

    Create step34.6.js

---
+++ step34.6.js
@@ -0,0 +1,9 @@
+/*Step 6
+For the next few steps, you will build out the body of the table.
+
+Start by adding a tbody element below the thead element. Then inside the tbody, add a tr element.*/
+
+
+<tbody>
+      <tr></tr>
+      </tbody>
