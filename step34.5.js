c54ea6e81cf3975122bb85374c41dc874528f106
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-05-26 11:35:43 +0300

    Create step34.5.js

---
+++ step34.5.js
@@ -0,0 +1,14 @@
+/*Step 5
+The scope attribute is used to specify whether a header cell is a header for a row, column, or group of rows or columns. Here is an example:
+
+Example Code
+<th scope="col">Example Header</th>
+This helps screen readers understand the relationship between header and data cells.
+
+For all th elements, add a scope attribute with a value of col.*/
+
+
+ <th scope="col">Time</th>
+        <th scope="col">Track A</th>
+        <th scope="col">Track B</th>
+        <th scope="col">Track C</th>
