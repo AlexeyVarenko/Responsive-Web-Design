25d7732a1a0a7d3c21b9231f8d23ae918fa0d4b5
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-05-27 14:55:39 +0300

    Create step34.7.js

---
+++ step34.7.js
@@ -0,0 +1,9 @@
+/*Step 7
+Inside your tr element, add a th element with the text of 9:00 AM. Then below that th element, add three td elements with the text of Keynote: Tech Future, Intro to Web Dev, and UX for All.*/
+
+
+
+ <th scope="col">9:00 AM</th>
+         <td>Keynote: Tech Future</td>
+         <td>Intro to Web Dev</td>
+         <td>UX for All</td>
