43e21cd03178fedb7d77d16c6edb8bdefe84dc6c
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-05-27 14:55:44 +0300

    Create step34.8.js

---
+++ step34.8.js
@@ -0,0 +1,13 @@
+/*Step 8
+Another value for the scope attribute is row, which indicates that a header cell is a header for its entire row.
+
+Inside of your th element, add a scope attribute with a value of row.*/
+
+
+
+<tr>
+        <th scope="row">9:00 AM</th>
+        <td scope="row">Keynote: Tech Future</td>
+        <td scope="row">Intro to Web Dev</td>
+        <td scope="row">UX for All</td>
+      </tr>
