15923c46a239484db990118e4d7aa423c1fe2937
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-05-28 08:38:40 +0300

    Create step34.10.js

---
+++ step34.10.js
@@ -0,0 +1,9 @@
+/*Step 10
+Next, add a third row to the table. Start by adding another tr element. Inside that tr element, add a th element with a scope attribute set to "row" and the text content of 11:00 AM. Then below that th element, add a td element with the text content of Break.*/
+
+
+
+<tr>
+        <th scope="row">11:00 AM</th>
+         <td>Break</td>
+      </tr>
