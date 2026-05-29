68f484b475121d10bedb9a9c9104a8378e50f937
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-05-29 08:09:09 +0300

    Create step34.12.js

---
+++ step34.12.js
@@ -0,0 +1,18 @@
+/*Step 12
+Now it is time to add another row to the table.
+
+Start by adding another tr element. Inside that tr element, add a th element with a scope attribute set to "row" and the text content of 11:30 AM.
+
+Then, add three td elements with the following text content:
+
+AR/VR in Education
+JavaScript Fundamentals
+Design Systems at Scale*/
+
+
+<tr>
+        <th scope="row">11:30 AM</th>
+        <td>AR/VR in Education</td>
+        <td>JavaScript Fundamentals</td>
+        <td>Design Systems at Scale</td>
+      </tr>
