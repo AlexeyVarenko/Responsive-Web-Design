1cc859ed1887ec7fc796d0dede034ee8c8b7e1a8
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-05-29 08:09:07 +0300

    Create step34.11.js

---
+++ step34.11.js
@@ -0,0 +1,13 @@
+/*Step 11
+Right now, the td element with the text content of Break only spans one column. But it would be nice if it spanned all three columns.
+
+As you recall from earlier workshops and lessons, you can use the colspan attribute to make a table cell span multiple columns.
+
+Example Code
+<tr>
+  <td colspan="3">Total Points</td>
+</tr>
+Add a colspan attribute to the td element and set its value to 3.*/
+
+
+ <td colspan="3">Break</td>
