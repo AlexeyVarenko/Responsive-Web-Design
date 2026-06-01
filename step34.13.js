3ae5aae4453d0d65443a517359034d205fed4d69
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-06-01 09:15:20 +0300

    Create step34.13.js

---
+++ step34.13.js
@@ -0,0 +1,9 @@
+/*Step 13
+Next, add a fifth row to the table. Start by adding another tr element. Inside that tr element, add a th element with a scope attribute set to "row" and the text content of 12:30 PM. Then below that th element, add a td element with the text content of Lunch Break. Your td element should also have a colspan attribute set to 3 so that it spans all three tracks.*/
+
+
+
+<tr>
+        <th scope="row">12:30 PM</th>
+        <td colspan="3">Lunch Break</td>
+      </tr>
