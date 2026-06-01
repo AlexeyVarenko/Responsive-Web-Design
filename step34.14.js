c6b65ed71d27a088b315ba0d8e442001463903be
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-06-01 09:15:22 +0300

    Create step34.14.js

---
+++ step34.14.js
@@ -0,0 +1,18 @@
+/*Step 14
+The last step is to add one more row to the table.
+
+Start by adding another tr element. Inside that tr element, add a th element with a scope attribute set to "row" and the text content of 2:00 PM.
+
+Then, add three td elements with the following text content:
+
+Voice UI Workshop
+Git & GitHub Essentials
+Color & Contrast in UI
+With those last set of changes, your table is now complete!*/
+
+
+
+<td>Voice UI Workshop</td>
+        <td>Git & GitHub Essentials</td>
+        <td>Color & Contrast in UI</td> 
+        </tr>
