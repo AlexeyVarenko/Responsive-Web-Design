44712dc547e5195f0e27c765fb6dff672b90e375
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-06-02 09:16:19 +0300

    Create step35.2.js

---
+++ step35.2.js
@@ -0,0 +1,19 @@
+/*Step 2
+Next, add a button element with the type attribute set to the value button.
+
+The button element's text should be Play.*/
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Accessible Controls</title>
+  </head>
+  <body>
+
+    <h1>Audio Controls</h1>
+    <button type="button">Play</button>
+  </body>
+</html>
