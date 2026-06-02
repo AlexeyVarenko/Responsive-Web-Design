d631f40b9fe68314ff7f17d5a526dc78fa587c95
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-06-02 09:16:17 +0300

    Create step35.1.js

---
+++ step35.1.js
@@ -0,0 +1,16 @@
+/*Step 1
+In this workshop, you will practice working with ARIA by building an accessible audio controller.
+
+To start, add an h1 element with the text Audio Controls.*/
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Accessible Controls</title>
+  </head>
+  <body>
+    <h1>Audio Controls</h1>
+  </body>
+</html>
