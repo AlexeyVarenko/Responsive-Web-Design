e1767a79fe3687351c90bdc69ba93692f79eeeb1
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-06-03 12:26:09 +0300

    Create step35.4.js

---
+++ step35.4.js
@@ -0,0 +1,10 @@
+/*Step 4
+Now it is time to add another span element.
+
+This span element should have an id attribute set to volume-description. Also, the text for this span element should be Adjust the sound level.
+
+Similar to the previous span element, the id of this span will later be referenced by ARIA attributes so assistive technologies can identify this text as the description for the volume control.*/
+
+
+
+<span id="volume-description">Adjust the sound level</span>
