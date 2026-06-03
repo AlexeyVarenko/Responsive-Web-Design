715fed2be5467d7e5c90a87bfdb9a0c4836ff5cf
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-06-03 12:26:07 +0300

    Create step35.3.js

---
+++ step35.3.js
@@ -0,0 +1,12 @@
+/*Step 3
+Next, add a div element to contain all the elements for the volume control.
+
+Inside the div element, nest a span element with an id attribute set to volume-label. Also, the text for the span element should be Volume.
+
+The id will later be referenced by ARIA attributes so assistive technologies can identify this text as the label for the volume control.*/
+
+
+
+<div>
+      <span id="volume-label">Volume</span>
+    </div>
