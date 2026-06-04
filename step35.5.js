c506a8df48410c9c130273d9662bc88cc8bb183a
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-06-04 08:22:43 +0300

    Create step35.5.js

---
+++ step35.5.js
@@ -0,0 +1,5 @@
+/*Step 5
+Next, you need to add an input element with the type attribute set to range to create the volume slider. Set the min attribute to 0, the max attribute to 100, and the value attribute to 50 to define the default volume level.*/
+
+
+<input type="range" min="0" max="100" value="50"></input>
