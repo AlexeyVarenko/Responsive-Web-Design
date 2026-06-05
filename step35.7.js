b657718c5ea87fe33311055b5669b95be471d3c7
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-06-05 08:38:49 +0300

    Create step35.7.js

---
+++ step35.7.js
@@ -0,0 +1,10 @@
+/*Step 7
+Your accessible audio controller is almost complete but there is one last thing to add.
+
+Add a button element with the type attribute set to button and with text Mute below the div element.
+
+And with that change, you have completed this workshop!*/
+
+
+
+<button type="button">Mute</button>
