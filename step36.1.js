676ece3e626aa833fd147e6376430b5d12e484eb
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-06-08 10:52:45 +0300

    Create step36.1.js

---
+++ step36.1.js
@@ -0,0 +1,7 @@
+/*Step 1
+In this workshop, you'll learn how to work with conditional statements and comparison operators.
+
+Begin by declaring a variable named truthyOrFalsy and assign it the boolean value true.*/
+
+
+const truthyOrFalsy=true;
