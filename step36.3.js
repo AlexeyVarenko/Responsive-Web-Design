bc542935738fe9ce9478506a561720543523f78b
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-06-09 14:21:57 +0300

    Create step36.3.js

---
+++ step36.3.js
@@ -0,0 +1,8 @@
+/*Step 3
+Change the assignment of truthyOrFalsy by setting its value to "freeCodeCamp". You will still see true logged to the console because a non-empty string is a truthy value.*/
+
+
+
+const truthyOrFalsy = "freeCodeCamp";
+
+console.log(Boolean(truthyOrFalsy));
