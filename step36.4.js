1d6b9f53c859e6237dfa82414932b626490cd0d8
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-06-09 14:21:59 +0300

    Create step36.4.js

---
+++ step36.4.js
@@ -0,0 +1,17 @@
+/*Step 4
+Falsy values are values that evaluate to false in a boolean context. Examples of falsy values are:
+
+"" (empty string)
+0
+false
+null
+undefined
+NaN
+You'll meet all of these going through this coursework.
+
+For now, change the assignment of truthyOrFalsy by setting its value to an empty string. After that, you'll see false logged to the console.*/
+
+
+const truthyOrFalsy = "";
+
+console.log(Boolean(truthyOrFalsy));
