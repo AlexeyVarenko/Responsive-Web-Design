cb60eedbc98291c87d348d4960164c8b0545fe13
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-06-11 15:58:22 +0300

    Create step36.8.js

---
+++ step36.8.js
@@ -0,0 +1,11 @@
+/*Step 8
+Declare another variable named isTimmyAGamer and assign it the boolean false.*/
+
+
+
+const hasDeveloperJob = true;
+
+if (hasDeveloperJob) {
+  console.log("Timmy is employed as a developer.");
+}
+const isTimmyAGamer = false;
