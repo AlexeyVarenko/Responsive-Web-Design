f73fc1de0513042feb8960df7db2284e0c1f885c
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-06-12 09:40:20 +0300

    Create step36.9.js

---
+++ step36.9.js
@@ -0,0 +1,17 @@
+/*Step 9
+Now create another if statement that uses isTimmyAGamer as its condition. Inside the body of your if statement, log "Timmy loves to play World of Warcraft." to the console.
+
+Note that you won't see anything logged to the console as the condition of your new if statement is false.*/
+
+
+const hasDeveloperJob = true;
+
+if (hasDeveloperJob) {
+  console.log("Timmy is employed as a developer.");
+}
+
+const isTimmyAGamer = false;
+
+if (isTimmyAGamer) {
+  console.log("Timmy loves to play World of Warcraft.");
+}
