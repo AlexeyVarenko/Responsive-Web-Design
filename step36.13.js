c6b50c0366120fe8484849ef82117f078cee22c3
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-06-16 10:41:38 +0300

    Create step36.13.js

---
+++ step36.13.js
@@ -0,0 +1,21 @@
+/*Step 13
+Finally, change the declaration of timmyAge and set it to a number less than 16. After that you'll see "Timmy is not old enough to drive." logged to the console.*/
+
+
+
+const hasDeveloperJob = true;
+
+if (hasDeveloperJob) {
+  console.log("Timmy is employed as a developer.");
+}
+
+const isTimmyAGamer = false;
+
+if (isTimmyAGamer) {
+  console.log("Timmy loves to play World of Warcraft.");
+}
+
+const timmyAge = 15;
+
+if (timmyAge >= 16) {
+  console.log("Timmy is old enough to drive.");
