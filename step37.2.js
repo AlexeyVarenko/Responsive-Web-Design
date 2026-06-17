72a62ec72084571b345e6bf59e5cf511c0a8a006
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-06-17 08:18:26 +0300

    Create step37.2.js

---
+++ step37.2.js
@@ -0,0 +1,12 @@
+/*Step 2
+The next part of the workshop will review how the Math.random() method works.
+
+Start by adding another console.log() that logs the string "The Math.random() method returns a pseudo random number greater than or equal to 0 and less than 1." to the console.*/
+
+
+
+const botName = "MathBot";
+const greeting = `Hi there! My name is ${botName} and I am here to teach you about the Math object!`;
+
+console.log(greeting);
+console.log("The Math.random() method returns a pseudo random number greater than or equal to 0 and less than 1.")
