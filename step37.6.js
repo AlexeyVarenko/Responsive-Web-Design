eb79c7d885f59755d0dc521e6afe5c5837a1f101
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-06-19 08:01:47 +0300

    Create step37.6.js

---
+++ step37.6.js
@@ -0,0 +1,10 @@
+/*Step 6
+Up until this point, you have only been dealing with floating point numbers and the number 0.
+
+For the next part of the workshop, you will review how to work with the Math.floor() method.
+
+Start by adding a console.log() that logs the message "The Math.floor() method rounds the value down to the nearest whole integer."*/
+
+
+
+console.log("The Math.floor() method rounds the value down to the nearest whole integer.")
