dae897bcab676927e0407646baf579362a0ec217
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-06-23 09:39:10 +0300

    Create step37.10.js

---
+++ step37.10.js
@@ -0,0 +1,8 @@
+/*Step 10
+Now, it is time to review how the Math.ceil() method works.
+
+Start by logging the string "The Math.ceil() method rounds the value up to the nearest whole integer." to the console.*/
+
+
+
+console.log("The Math.ceil() method rounds the value up to the nearest whole integer.");
