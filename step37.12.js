7b4ce392afb64a84118b9dd442d30e2b903c3823
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-06-24 07:47:25 +0300

    Create step37.12.js

---
+++ step37.12.js
@@ -0,0 +1,8 @@
+/*Step 12
+The next portion of the workshop will review how to round numbers to the nearest whole integer.
+
+Start by adding a console.log() that logs the message "The Math.round() method rounds the value to the nearest whole integer." to the console.*/
+
+
+
+console.log("The Math.round() method rounds the value to the nearest whole integer.");
