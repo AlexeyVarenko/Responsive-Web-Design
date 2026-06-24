4f7e1d0cbcce9ca6d914abd9fb293983fea3c16b
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-06-24 07:47:23 +0300

    Create step37.11.js

---
+++ step37.11.js
@@ -0,0 +1,16 @@
+/*Step 11
+In previous lessons, you learned how to work with the Math.ceil() method like this:
+
+Example Code
+const price = 10.01;
+Math.ceil(price); // 11
+The Math.ceil() method rounds the value up to the nearest whole integer.
+
+Create a variable called numRoundedUp and assign it the result of rounding the floating point number 3.2 up to the nearest whole integer.
+
+Then, log the numRoundedUp variable to the console to see the result.*/
+
+
+
+const numRoundedUp=Math.ceil(3.2)
+console.log(numRoundedUp)
