ba408542d803f31b82b9000549b268809df74537
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-06-25 08:23:57 +0300

    Create step37.13.js

---
+++ step37.13.js
@@ -0,0 +1,20 @@
+/*Step 13
+In previous lessons, you learned that the Math.round() method rounds the value to the nearest whole integer.
+
+Here are some examples:
+
+Example Code
+Math.round(6.7); // 7
+Math.round(3.2); // 3
+This differs from the Math.floor() and Math.ceil() methods, which round down and up to the nearest whole integer, respectively.
+
+Create a new variable called numRounded and assign the result of rounding the number 2.7. Then, log the value of numRounded to the console.
+
+Below that, create another new variable called numRounded2 and assign the result of rounding the number 11.2. Then, log the value of numRounded2 to the console.*/
+
+
+
+const numRounded=Math.round(2.7);
+console.log(numRounded)
+const numRounded2=Math.round(11.2);
+console.log(numRounded2)
