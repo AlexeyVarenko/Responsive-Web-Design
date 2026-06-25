4325e067609783ab0a6f20fc64e6c69f957bf2d5
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-06-25 08:23:59 +0300

    Create step37.14.js

---
+++ step37.14.js
@@ -0,0 +1,8 @@
+/*Step 14
+For the last portion of the workshop, you will review how to get the minimum and maximum values of a set of numbers.
+
+Start by adding a console.log() that logs the message "The Math.max() and Math.min() methods are used to get the maximum and minimum number from a range." to the console.*/
+
+
+
+console.log("The Math.max() and Math.min() methods are used to get the maximum and minimum number from a range.")
