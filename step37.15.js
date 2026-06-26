9dfdac1d146d1c913e3acf8cebbb6575fbf5af67
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-06-26 08:30:35 +0300

    Create step37.15.js

---
+++ step37.15.js
@@ -0,0 +1,17 @@
+/*Step 15
+In previous lessons, you learned how to get the maximum and minimum values from a range of numbers like this:
+
+Math.max(1, 2, 3, 4, 5); // 5
+Math.min(1, 2, 3, 4, 5); // 1
+Create a variable called maxNum and assign it the result of finding the maximum number between the numbers 3, 125, 55, 24. Then, log the value of maxNum to the console.
+
+Below that, create a variable called minNum and assign it the result of finding the minimum number between the numbers 6, 90, 14, 90, 2. Then, log the value of minNum to the console.*/
+
+
+
+const maxNum=Math.max(3, 125, 55, 24)
+
+const minNum=Math.min(6, 90, 14, 90, 2)
+
+console.log(maxNum);
+console.log(minNum);
