832dbf14d3032c4b0bae98b7cef8fcf0743c4a66
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-06-26 08:30:37 +0300

    Create step37.16.js

---
+++ step37.16.js
@@ -0,0 +1,8 @@
+/*Step 16
+For the last step of the workshop, you will log the message "It was fun learning about the different Math methods with you!" to the console.
+
+And with that last change, you have completed the MathBot workshop!*/
+
+
+
+console.log("It was fun learning about the different Math methods with you!");
