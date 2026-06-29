7a42d5652dda1e076e9c079cff8a96ba88cabe4f
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-06-29 08:14:14 +0300

    Create step38.1.js

---
+++ step38.1.js
@@ -0,0 +1,7 @@
+/*Step 1
+In this workshop, you will continue to learn about arrays by building a grocery shopping list.
+
+Start by adding a console.log that logs the string "Grocery shopping list" to the console.*/
+
+
+console.log("Grocery shopping list")
