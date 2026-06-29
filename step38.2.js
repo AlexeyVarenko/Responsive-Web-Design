c34ab0b27e1a12e3e0e7561b6749939eee7a9042
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-06-29 08:14:16 +0300

    Create step38.2.js

---
+++ step38.2.js
@@ -0,0 +1,14 @@
+/*Step 2
+For this shopping list, you will use an array to represent the items you need to buy.
+
+In previous lessons, you learned how to create arrays like this:
+
+Example Code
+const fruits = ['apple', 'banana', 'orange'];
+In this step, create a variable called shoppingList and assign it an empty array.*/
+
+
+
+console.log("Grocery shopping list");
+
+const shoppingList=[];
