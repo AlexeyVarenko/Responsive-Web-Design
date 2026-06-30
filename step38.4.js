4031fa8dac0226e9a04c555a9b5760e961633b40
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-06-30 14:08:04 +0300

    Create step38.4.js

---
+++ step38.4.js
@@ -0,0 +1,15 @@
+/*Step 4
+In previous lessons, you learned how to add items to the end of an array using the push method like this:
+
+fruits.push('pear');
+Using the push method, add the string "Apples" to the shoppingList array.*/
+
+
+
+console.log("Grocery shopping list");
+
+const shoppingList = [];
+
+console.log("It will be nice to have some fruit to eat.");
+
+shoppingList.push('Apples')
