77b838889fb547e5b918931e4e60ce5fde905874
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-06-30 14:08:02 +0300

    Create step38.3.js

---
+++ step38.3.js
@@ -0,0 +1,12 @@
+/*Step 3
+For this next portion of the workshop, you will practice adding food items to the grocery list.
+
+Start by using console.log to log the message "It will be nice to have some fruit to eat.".*/
+
+
+
+console.log("Grocery shopping list");
+
+const shoppingList = [];
+
+console.log ("It will be nice to have some fruit to eat.");
