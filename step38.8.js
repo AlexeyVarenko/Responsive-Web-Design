26aafde28d8a33591a9bb2c68e2d7fa2235dfb8d
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-07-02 08:58:52 +0300

    Create step38.8.js

---
+++ step38.8.js
@@ -0,0 +1,25 @@
+/*Step 8
+Now it is time to start adding items to the top of the grocery list.
+
+Start by adding a console.log that logs the message "It looks like we need to get some cooking oil." to the console.*/
+
+
+
+console.log("Grocery shopping list");
+
+const shoppingList = [];
+
+console.log("It will be nice to have some fruit to eat.");
+
+shoppingList.push("Apples");
+
+function getShoppingListMsg(arr) {
+  return `Current Shopping List: ${arr}`;
+}
+
+console.log(getShoppingListMsg(shoppingList));
+
+shoppingList.push("Grapes");
+console.log(getShoppingListMsg(shoppingList));
+
+console.log("It looks like we need to get some cooking oil.")
