aeba06251c49b6ac15a82e652d9925959f58167a
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-07-07 12:37:11 +0300

    Create step38.9.js

---
+++ step38.9.js
@@ -0,0 +1,11 @@
+/*Step 9
+In previous lessons, you learned how to add elements to the beginning of an array using the unshift() method.
+
+Here is a reminder of how to use the unshift() method:
+
+Example Code
+array.unshift(item1, item2, ..., itemX);
+Use the unshift() method to add the string "Vegetable Oil" to the beginning of the shoppingList array.*/
+
+
+shoppingList.unshift("Vegetable Oil");
