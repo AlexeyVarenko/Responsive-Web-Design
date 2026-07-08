705cfbc0cd41364ef83a5e7c90de7c51786f4502
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-07-08 14:03:49 +0300

    Create step38.14.js

---
+++ step38.14.js
@@ -0,0 +1,17 @@
+/*Step 14
+In previous lessons, you learned how to remove items from the end of an array using the pop method.
+
+Here is a reminder of how to use the pop method:
+
+Example Code
+let array = [1, 2, 3, 4, 5];
+array.pop();
+
+// [1, 2, 3, 4]
+console.log(array); 
+Use the pop method to remove the last item from the shoppingList array.*/
+
+
+
+
+shoppingList.pop();
