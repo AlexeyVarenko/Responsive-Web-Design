f6489211411cc20ba042f242ec9650402f987456
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-07-08 14:03:47 +0300

    Create step38.13.js

---
+++ step38.13.js
@@ -0,0 +1,8 @@
+/*Step 13
+For this next portion of the workshop, you will review how to remove items from the end of the array.
+
+Start by adding a console.log that logs the message "This looks like too much junk food.".*/
+
+
+
+console.log("This looks like too much junk food.")
