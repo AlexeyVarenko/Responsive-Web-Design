6a60767a1ac51c6fa40d6f352a258bccdfaf5308
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-07-09 12:39:00 +0300

    Create step38.15.js

---
+++ step38.15.js
@@ -0,0 +1,8 @@
+/*Step 15
+Now it is time to log the updated shoppingList array to the console.
+
+Add a console.log and call the getShoppingListMsg function with the shoppingList array as an argument inside console.log to see the updated list logged to the console.*/
+
+
+
+console.log(getShoppingListMsg(shoppingList))
