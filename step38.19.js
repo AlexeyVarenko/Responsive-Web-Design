2b893cced97c6a3c640d728d8833154e108bf6b6
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-07-13 08:00:07 +0300

    Create step38.19.js

---
+++ step38.19.js
@@ -0,0 +1,17 @@
+/*Step 19
+The last change to make to the grocery list is to update the first item in the list.
+
+In previous lessons, you learned how to update an item using bracket notation and the index of the item you want to update.
+
+Here is a reminder of how to update an item in an array:
+
+Example Code
+const array = [1, 2, 3, 4, 5];
+
+array[0] = 10;
+// Result: [10, 2, 3, 4, 5]
+console.log(array); 
+Update the first item in the shoppingList array to be "Canola Oil".*/
+
+
+shoppingList[0]="Canola Oil";
