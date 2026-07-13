7c3ae7560d99bb6abcad28f1aff050124df554bf
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-07-13 08:00:05 +0300

    Create step38.18.js

---
+++ step38.18.js
@@ -0,0 +1,15 @@
+/*Step 18
+In previous lessons, you learned how to remove an item from the beginning of the array using the shift method.
+
+Here is a reminder of how to use the shift method:
+
+Example Code
+const array = [1, 2, 3, 4, 5];
+array.shift();
+
+// Result: [2, 3, 4, 5]
+console.log(array); 
+Use the shift method to remove the first item from the shoppingList array.*/
+
+
+shoppingList.shift();
