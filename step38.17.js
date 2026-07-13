49bd688fb51a8f86cdb8c862e10bf3fa8a6043ee
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-07-13 08:00:03 +0300

    Create step38.17.js

---
+++ step38.17.js
@@ -0,0 +1,7 @@
+/*Step 17
+In this last part of the workshop, you will review how to remove an item from the beginning of an array.
+
+Start by adding a console.log that logs the message "On second thought, maybe we should be more health conscious.".*/
+
+
+console.log("On second thought, maybe we should be more health conscious.");
