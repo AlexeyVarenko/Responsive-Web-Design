30b5fa747eec89db0e2974b70c9c1f3518ed27cf
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-07-13 08:00:09 +0300

    Create step38.20.js

---
+++ step38.20.js
@@ -0,0 +1,10 @@
+/*Step 20
+In this final step of the workshop, log the final grocery list to the console.
+
+To do this, call the getShoppingListMsg function with the shoppingList array as an argument inside console.log.
+
+And with this last step your grocery list is complete!*/
+
+
+
+console.log(getShoppingListMsg(shoppingList));
