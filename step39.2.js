49b4e4895e5c85df6df5d7d80201be4a01d8deaf
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-07-14 08:30:55 +0300

    Create step39.2.js

---
+++ step39.2.js
@@ -0,0 +1,18 @@
+/*Step 2
+For this wildlife tracker, you will update an object with new properties.
+
+Here is an example of creating an object with a property:
+
+Example Code
+const animal = {
+  name: "Lion" // property: value
+};
+Now update the tiger object so it includes a property called species.
+
+Set its value to "Tiger".*/
+
+
+
+const tiger = {
+  species: "Tiger"
+};
