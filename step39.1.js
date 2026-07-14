6cdaa338b198e08720c738db280c84b2f6dd3097
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-07-14 08:30:53 +0300

    Create step39.1.js

---
+++ step39.1.js
@@ -0,0 +1,13 @@
+/*Step 1
+In this workshop, you will build a simple Wildlife Tracker using JavaScript objects.
+
+Here is an example of an object:
+
+const animal = {}; // empty object
+This creates an empty object.
+
+Now create a variable named tiger and assign it an empty object.*/
+
+
+
+const tiger = {};
