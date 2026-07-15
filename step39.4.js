79ac461280a93cd18194983b195d9ab6d645bc0c
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-07-15 08:29:17 +0300

    Create step39.4.js

---
+++ step39.4.js
@@ -0,0 +1,13 @@
+/*Step 4
+Now add another property to the tiger object called isEndangered.
+
+Set the isEndangered property to true.*/
+
+
+const tiger = {
+  species: "Tiger",
+  age: 5,
+  isEndangered: true,
+};
+
+
