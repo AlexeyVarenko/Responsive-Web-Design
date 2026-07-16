302afa8514ad0acece5ac0a96345b99e9c4641a8
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-07-16 08:12:38 +0300

    Create step39.5.js

---
+++ step39.5.js
@@ -0,0 +1,21 @@
+/*Step 5
+Now create a second object called elephant.
+
+Add the following properties:
+
+species with the value "Elephant"
+age with the value 10
+isEndangered with the value true*/
+
+
+const tiger = {
+  species: "Tiger",
+  age: 5,
+  isEndangered: true
+};
+
+const elephant = {
+  species: "Elephant",
+  age: 10,
+  isEndangered: true
+};
