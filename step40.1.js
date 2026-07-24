908948caedd226bf5f269d357e0341e6bf5e96cb
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-07-24 16:02:36 +0300

    Create step40.1.js

---
+++ step40.1.js
@@ -0,0 +1,7 @@
+/*Step 1
+In this workshop, you will create a recipe tracker using JavaScript objects.
+
+Start by creating an empty array named recipes. This is the array you'll push the recipe objects into later.*/
+
+
+const recipes=[];
