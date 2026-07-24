204dc02f1a2e022a983f680c6ca46da8b22d11a1
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-07-24 16:02:38 +0300

    Create step40.2.js

---
+++ step40.2.js
@@ -0,0 +1,12 @@
+/*Step 2
+Create an object named recipe1. Inside the recipe1 object, create a name property with the value "Spaghetti Carbonara".
+
+Also inside the recipe1 object, create an ingredients property with an array as the value. The array should have spaghetti, Parmesan cheese, pancetta, and black pepper inside it.*/
+
+
+
+const recipe1 = {
+  name: "Spaghetti Carbonara",
+  ingredients: ["spaghetti", "Parmesan cheese", "pancetta", "black pepper"],
+  
+};
