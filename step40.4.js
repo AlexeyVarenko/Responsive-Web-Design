cafaf013834ccb2448549167eaf3c75b9747008f
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-07-27 08:45:08 +0300

    Create step40.4.js

---
+++ step40.4.js
@@ -0,0 +1,30 @@
+/*Step 4
+Create a recipe2 object with the following properties and values:
+
+Key	Value
+name	Chicken Curry
+ingredients	["chicken breast", "coconut milk", "curry powder", "onion", "garlic"]
+cookingTime	42
+totalIngredients	null
+difficultyLevel	""
+*/
+
+
+
+const recipes = [];
+
+const recipe1 = {
+  name: "Spaghetti Carbonara",
+  ingredients: ["spaghetti", "Parmesan cheese", "pancetta", "black pepper"],
+  cookingTime: 22,
+  totalIngredients: null,
+  difficultyLevel: ""
+};
+
+const recipe2 = {
+  name: "Chicken Curry",
+  ingredients: ["chicken breast", "coconut milk", "curry powder", "onion", "garlic"],
+  cookingTime: 42,
+  totalIngredients: null,
+  difficultyLevel: ""
+};
