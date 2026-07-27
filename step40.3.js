82931c561d187b8fe5286cfe491265043d853230
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-07-27 08:45:06 +0300

    Create step40.3.js

---
+++ step40.3.js
@@ -0,0 +1,19 @@
+/*Step 3
+Add the following properties to the recipe1 object:
+
+Key	Value
+cookingTime	22
+totalIngredients	null
+difficultyLevel	""
+The properties with null and empty string values will be updated later after you calculate them.*/
+
+
+const recipes = [];
+
+const recipe1 = {
+  name: "Spaghetti Carbonara",
+  ingredients: ["spaghetti", "Parmesan cheese", "pancetta", "black pepper"],
+  cookingTime: 22,
+  totalIngredients: null,
+  difficultyLevel: ""
+};
