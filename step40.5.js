7f152c5118228a6e43e663f7df6647e7eedf0bef
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-07-28 09:15:05 +0300

    Create step40.5.js

---
+++ step40.5.js
@@ -0,0 +1,36 @@
+/*Step 5
+Create a recipe3 object with the following properties and values:
+
+Key	Value
+name	Vegetable Stir Fry
+ingredients	["broccoli", "carrot", "bell pepper"]
+cookingTime	15
+totalIngredients	null
+difficultyLevel	""*/
+
+
+const recipes = [];
+
+const recipe1 = {
+  name: "Spaghetti Carbonara",
+  ingredients: ["spaghetti", "Parmesan cheese", "pancetta", "black pepper"],
+  cookingTime: 22,
+  totalIngredients: null,
+  difficultyLevel: ""
+};
+
+const recipe2 = {
+  name: "Chicken Curry",
+  ingredients: ["chicken breast", "coconut milk", "curry powder", "onion", "garlic"],
+  cookingTime: 42,
+  totalIngredients: null,
+  difficultyLevel: ""
+};
+
+const recipe3 = {
+  name: "Vegetable Stir Fry",
+  ingredients: ["broccoli", "carrot", "bell pepper"],
+  cookingTime: 15,
+  totalIngredients: null,
+  difficultyLevel: ""
+};
