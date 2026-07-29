2e67dc4a79bea30574dddf06a3412857d324024e
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-07-29 08:39:55 +0300

    Create step40.8.js

---
+++ step40.8.js
@@ -0,0 +1,8 @@
+/*Step 8
+Create a getTotalIngredients function that takes a single argument, representing an array with ingredients, and returns the number of ingredients from the array passed to the function.*/
+
+
+
+function getTotalIngredients(ingredients) {
+  return ingredients.length;
+}
