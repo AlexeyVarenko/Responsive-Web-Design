08ea32f801397fe86ebcbbecd359f802bb35df55
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-08-03 08:59:24 +0300

    Create step40.11.js

---
+++ step40.11.js
@@ -0,0 +1,8 @@
+/*Step 11
+You can now fill in each item of the recipes array with values for the totalIngredients and difficultyLevel properties.
+
+For now, access the totalIngredients and difficultyLevel of recipe1 and set them to the appropriate results of function calls and arguments.*/
+
+
+recipe1.totalIngredients = getTotalIngredients(recipe1.ingredients);
+recipe1.difficultyLevel = getDifficultyLevel(recipe1.cookingTime);
