30ccf29fc3d8496217de74f2df0d858b8dbb5e3a
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-08-04 12:42:42 +0300

    Create step41.1.js

---
+++ step41.1.js
@@ -0,0 +1,10 @@
+/*Step 1
+In this workshop, you will practice using for...of loops by building a function that counts how often a string appears in an array of strings.
+
+Before you write that function, you will create a simpler one that logs each character of a string to the console.
+
+Start by defining an empty function called printCharacters with the parameter str.*/
+
+
+
+const printCharacters=(str)=>{}
