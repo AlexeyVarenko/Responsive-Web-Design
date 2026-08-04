d7e3c17b3e9a5ffcb3572b2b77ab52bfccf9a0a7
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-08-04 12:42:44 +0300

    Create step41.2.js

---
+++ step41.2.js
@@ -0,0 +1,12 @@
+/*Step 2
+Now it’s time to write your first for...of loop. In the preceding lessons, you learned that you can use a for...of loop to iterate over values from an iterable (e.g., strings or arrays).
+
+Here is an example for a for...of loop:
+
+Example Code
+for (const num of [1, 2, 3]) {
+  // code block to be executed
+}
+Add a for...of loop with an empty code block inside your function. It should iterate over every character in the str argument.*/
+
+
