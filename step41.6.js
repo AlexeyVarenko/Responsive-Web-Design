271a7c7b177941c745217028b635a3b8ce48c6ba
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-08-07 08:09:16 +0300

    Create step41.6.js

---
+++ step41.6.js
@@ -0,0 +1,18 @@
+/*Step 6
+Since the getMatchedWordCount function needs to return a numerical count, you will start by setting up a counter variable.
+
+Create a variable named count using the let keyword, initialize it with the value 0, and add a return statement that returns the count variable.*/
+
+
+
+function printCharacters(str) {
+  for (const char of str) {
+    console.log(char);
+  }
+}
+printCharacters("hello");
+
+function getMatchedWordCount(sentence, match) {
+  let count=0;
+  return count;
+}
