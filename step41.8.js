ec9e14a9522236ae2e1f37299aeb9a1f1d25ed98
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-08-07 08:09:20 +0300

    Create step41.8.js

---
+++ step41.8.js
@@ -0,0 +1,10 @@
+/*Step 8
+Great, your function returns a 0, but it doesn’t count anything yet.
+
+To fix this, inside getMatchedWordCount, create a for...of loop with an empty code block that iterates over each word in sentence.*/
+
+
+
+   for (const word of sentence) {
+    
+  }
