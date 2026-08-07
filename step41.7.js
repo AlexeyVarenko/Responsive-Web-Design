2047723b4d189777209059a96a54f9d794732eb0
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-08-07 08:09:18 +0300

    Create step41.7.js

---
+++ step41.7.js
@@ -0,0 +1,11 @@
+/*Step 7
+To view the output of your getMatchedWordCount function in its current state, add a console.log statement below it. Inside the console.log(), call the function with the argument ["I", "really", "really", "really", "like", "to", "code"] for the sentence parameter and "really" for the match parameter.*/
+
+
+
+console.log(
+  getMatchedWordCount(
+    ["I", "really", "really", "really", "like", "to", "code"],
+    "really"
+  )
+);
