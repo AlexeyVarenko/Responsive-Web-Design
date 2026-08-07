8dbf457a9c747ff7e5c12de2265f5016ff4fa9d3
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-08-07 08:09:14 +0300

    Create step41.5.js

---
+++ step41.5.js
@@ -0,0 +1,17 @@
+/*Step 5
+In the next few steps, you will build a function that counts how often a string appears in an array of strings.
+
+To start, define an empty function named getMatchedWordCount with the parameters sentence and match in that order.*/
+
+
+
+function printCharacters(str) {
+  for (const char of str) {
+    console.log(char);
+  }
+}
+printCharacters("hello");
+
+function getMatchedWordCount(sentence, match) {
+  
+}
