6fa5b7c936739f6e209078857298c19b19655ce8
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-08-10 08:17:38 +0300

    Create step41.9.js

---
+++ step41.9.js
@@ -0,0 +1,9 @@
+/*Step 9
+Now, inside the loop, log the following template literal to the console: Checking "${word}" against "${match}" | Running count: ${count}*/
+
+
+
+  for (const word of sentence) {
+    
+    console.log(`Checking "${word}" against "${match}" | Running count: ${count}`);
+  }
