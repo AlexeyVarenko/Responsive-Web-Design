733534777f78ce5fb6d238250cc5eca4445de738
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-08-11 07:47:59 +0300

    Create step41.11.js

---
+++ step41.11.js
@@ -0,0 +1,18 @@
+/*Step 11
+You’ve finished working on your getMatchedWordCount function!
+
+Now you are going to test it with different data to see how it behaves.
+
+Add a new console.log statement below the existing one that calls getMatchedWordCount with these parameters:
+
+sentence: ["Do", "not", "fear", "the", "dandy", "lion"]
+match: "dandy"
+Congratulations! You’ve completed this workshop.*/
+
+
+console.log(
+  getMatchedWordCount(
+    ["Do", "not", "fear", "the", "dandy", "lion"],
+    "dandy"
+  )
+);
