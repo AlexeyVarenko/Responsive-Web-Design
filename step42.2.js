921d103c132d25b289c729ff6b93bb86e56dc5c6
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-08-12 08:27:35 +0300

    Create step42.2.js

---
+++ step42.2.js
@@ -0,0 +1,11 @@
+/*Step 2
+Now it is time to test your getVowelCount function.
+
+Create a vowelCount variable and assign it the result of calling the getVowelCount function with the argument of "Apples are tasty fruits"
+
+After that, log the following to the console: "Vowel Count: [vowel count goes here]". Replace [vowel count goes here] with the actual variable name. You can choose to use template strings or string concatenation with the + operator here.*/
+
+
+
+const vowelCount = getVowelCount("Apples are tasty fruits");
+console.log(`Vowel Count: ${vowelCount}`);
