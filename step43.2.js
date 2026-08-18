8ee3f7bcc0486f284c82d1fe6a5dec69732425de
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-08-18 09:14:28 +0300

    Create step43.2.js

---
+++ step43.2.js
@@ -0,0 +1,22 @@
+/*Step 2
+Your roster begins with a single commander astronaut. Create an object named firstAstronaut with the following properties:
+
+Key	Value
+id	1
+name	"Andy"
+role	"Commander"
+isEVAEligible	true
+priority	3
+*/
+
+
+
+const squad = [];
+
+const firstAstronaut = {
+  id: 1,
+  name: "Andy",
+  role: "Commander",
+  isEVAEligible: true,
+  priority: 3
+};
