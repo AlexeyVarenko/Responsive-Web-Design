6bc693bf9df0bdc4803a06d23a7f1b3898957610
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-08-18 09:14:26 +0300

    Create step43.1.js

---
+++ step43.1.js
@@ -0,0 +1,8 @@
+/*Step 1
+In this workshop, you will leverage JavaScript to build a roster of astronauts for a space mission.
+
+To begin, create an empty array for your roster named squad.*/
+
+
+
+const squad=[];
