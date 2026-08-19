5908b99d56a0f7c04ecc714cf4320608a44c13e3
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-08-19 08:25:05 +0300

    Create step43.3.js

---
+++ step43.3.js
@@ -0,0 +1,18 @@
+/*Step 3
+Mission control requires a method for adding new members to the roster. Create an empty function named addCrewMember that accepts two parameters, crew and astronaut.*/
+
+
+
+const squad = [];
+
+const firstAstronaut = {
+  id: 1,
+  name: "Andy",
+  role: "Commander",
+  isEVAEligible: true,
+  priority: 3
+};
+
+const addCrewMember=(crew, astronaut)=>{
+
+}
