5c174aa9b84deb8cae86fd5756e654f463f0a2e1
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-08-20 08:15:35 +0300

    Create step43.7.js

---
+++ step43.7.js
@@ -0,0 +1,26 @@
+/*Step 6
+Now you can start building your roster. Use addCrewMember() to add the firstAstronaut to your squad.*/
+
+
+const squad = [];
+
+const firstAstronaut = {
+  id: 1,
+  name: "Andy",
+  role: "Commander",
+  isEVAEligible: true,
+  priority: 3
+};
+
+function addCrewMember(crew, astronaut) {
+  for (let i = 0; i < crew.length; i++) {
+    if (crew[i].id === astronaut.id) {
+      console.log("Duplicate ID: " + astronaut.id);
+      return;
+    }
+  }
+  crew.push(astronaut);
+  console.log(`Added ${astronaut.name} as ${astronaut.role}`);
+}
+
+addCrewMember(squad, firstAstronaut);
