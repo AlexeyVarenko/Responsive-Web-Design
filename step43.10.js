267628da854e9354011df53f2cd0143871cd0597
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-08-24 07:58:41 +0300

    Create step43.10.js

---
+++ step43.10.js
@@ -0,0 +1,76 @@
+/*Step 10
+As before, start by validating the input inside the swapCrewMembers function. Specifically, you must validate the indices of the astronauts to be swapped.
+
+If any of the following are true:
+
+fromIndex is negative
+
+fromIndex is greater than or equal to crew.length
+
+toIndex is negative
+
+toIndex is greater than or equal to crew.length
+
+then, you should log "Invalid crew indices" to the console and call return to exit the function.
+
+You can use the OR operator || to check multiple conditions at once:
+
+Example Code
+if (condition1 || condition2 || condition3 || condition4) {
+  console.log("Message");
+  return;
+}*/
+
+
+
+const squad = [];
+
+const firstAstronaut = {
+  id: 1,
+  name: "Andy",
+  role: "Commander",
+  isEVAEligible: true,
+  priority: 3
+};
+
+function addCrewMember(crew, astronaut) {
+  for (let i = 0; i < crew.length; i++) {
+    if (crew[i].id === astronaut.id) {
+      console.log("Duplicate ID: " + astronaut.id);
+      return;
+    }
+  }
+  crew.push(astronaut);
+}
+
+addCrewMember(squad, firstAstronaut);
+
+const remainingCrew = [
+  { id: 2, name: "Bart", role: "Pilot", isEVAEligible: false, priority: 8 },
+  { id: 3, name: "Caroline", role: "Engineer", isEVAEligible: true, priority: 4 },
+  { id: 4, name: "Diego", role: "Scientist", isEVAEligible: false, priority: 1 },
+  { id: 5, name: "Elise", role: "Medic", isEVAEligible: true, priority: 7 },
+  { id: 6, name: "Felix", role: "Navigator", isEVAEligible: true, priority: 6 },
+  { id: 7, name: "Gertrude", role: "Communications", isEVAEligible: false, priority: 4 },
+  { id: 8, name: "Hank", role: "Mechanic", isEVAEligible: true, priority: 2 },
+  { id: 9, name: "Irene", role: "Specialist", isEVAEligible: true, priority: 5 },
+  { id: 10, name: "Joan", role: "Technician", isEVAEligible: false, priority: 1 },
+];
+
+for (let i = 0; i < remainingCrew.length; i++) {
+  addCrewMember(squad, remainingCrew[i]);
+}
+
+function swapCrewMembers(crew, fromIndex, toIndex) {
+  if (
+    fromIndex < 0 || 
+    toIndex < 0 ||
+    fromIndex >= crew.length ||
+    toIndex >= crew.length
+  ) {
+    console.log("Invalid crew indices");
+    return;
+  }
+  
+  
+}
