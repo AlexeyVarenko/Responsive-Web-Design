64b8947722965579efe8f0e298db29db83a9e991
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2026-08-24 07:58:43 +0300

    Create step43.11.js

---
+++ step43.11.js
@@ -0,0 +1,11 @@
+/*Step 11
+You should create a shallow copy of the crew array using the slice() method. Name the new array updatedCrew.
+
+Recall that calling slice() without arguments returns a copy of the entire array:
+
+const copyArray = originalArray.slice();*/
+
+
+const updatedCrew = crew.slice();
+  
+}  
